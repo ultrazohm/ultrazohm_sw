39ceff2d0abbce30677259245346ba4f55aa8a19
EykeLiegmann <eyke.liegmann@tum.de>
2020-09-04 09:17:10 +0200

    add 3L PWM ip cores

---
+++ ip_cores/PWM_SS_3L/simulation/Init_IP_Core.m
@@ -0,0 +1,14 @@
+% IP Core PWM and Switching Signal Control
+% Author:   Barnabas Haucke-Korber
+% Date:     04.09.2018
+% Version:  0.1
+% release notes: First Tests
+
+%%
+clc
+clear all
+
+
+%% Parameter PWM
+f_pwm_clk=100e6;
+T_pwm_clk=1/f_pwm_clk;
---
+++ ip_cores/PWM_SS_3L/simulation/startup.m
@@ -0,0 +1,4 @@
+% Hierdruch wird dem HDL-Coder gesagt welches Synthesetool er für die HDL-Generierung nutzen soll.
+% adjust vivado version according to your installation
+hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath',...
+ 'C:\Xilinx\Vivado\2019.2\bin\vivado.bat'); 

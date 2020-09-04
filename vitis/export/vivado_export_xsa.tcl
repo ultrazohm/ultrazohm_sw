699bd522c64a49564d4f24f79c4c7ff4f53739b9
Tobias <tobias.schindler@th-nuernberg.de>
2020-09-04 12:08:26 +0200

    Updates export file

--- vitis/export/vivado_export_xsa.tcl
+++ vitis/export/vivado_export_xsa.tcl
@@ -3,4 +3,4 @@
 
 set work_directory [get_property DIRECTORY [current_project]] ; 
 cd $work_directory ; 
-write_hw_platform -fixed -force -include_bit -file {../vitis/vivado_exported_xsa/zusys_wrapper.xsa}
+write_hw_platform -fixed -force -include_bit -file {../../vitis/vivado_exported_xsa/zusys_wrapper.xsa}

1fae0985d280ded01d7aa602d7ed36a21dc2abd4
EykeLiegmann <eyke.liegmann@tum.de>
2020-09-18 13:40:29 +0200

    added axi_mux ip core

---
+++ ip_cores/mux_axi/hdl_prj/hdlsrc/mux_axi/clearhighlighting.m
@@ -0,0 +1,4 @@
+SLStudio.Utils.RemoveHighlighting(get_param('mux_axi', 'handle'));
+SLStudio.Utils.RemoveHighlighting(get_param('gm_mux_axi', 'handle'));
+annotate_port('gm_mux_axi/mux_axi/Bit Slice', 0, 1, '');
+annotate_port('gm_mux_axi/mux_axi/Multiport Switch', 0, 1, '');

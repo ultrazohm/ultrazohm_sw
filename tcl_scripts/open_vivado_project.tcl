509dd253c9d2dc5e9029580c04b39e7ca45df3c5
Tobias <tobias.schindler@th-nuernberg.de>
2020-10-08 14:18:18 +0200

    Adds first prototypes for tcl scripts to automate vivado build

--- tcl_scripts/open_vivado_project.tcl
+++ tcl_scripts/open_vivado_project.tcl
@@ -1,3 +1,11 @@
-vivado -mode batch -source ../vivado/project/ultrazohm.xpr
+open_project ../vivado/project/ultrazohm.xpr
 #vivado -source ../vivado/project/ultrazohm.xpr
-source check_syntax.tcl
+set msg [check_syntax -fileset sim_1 -return_string]
+set ret_val 0
+ 
+if {$msg != ""} {
+  set ret_val 1
+}
+ 
+puts $msg
+exit $ret_val

792b63fec7ae4881f70b109b0d337c79f1973940
Tobias <tobias.schindler@th-nuernberg.de>
2020-10-20 16:45:06 +0200

    rebuild

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Tue Oct 20 16:03:07 2020
+--Date        : Tue Oct 20 16:25:39 2020
 --Host        : TS-WS running 64-bit Ubuntu 20.04.1 LTS
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

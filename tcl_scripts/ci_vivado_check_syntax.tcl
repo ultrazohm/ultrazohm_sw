51aeb339726d00ad967047754f0c55b47cef0721
Tobias <tobias.schindler@th-nuernberg.de>
2020-11-01 15:05:29 +0100

    Adds ci script for check syntax and validate boar design

---
+++ tcl_scripts/ci_vivado_check_syntax.tcl
@@ -0,0 +1,11 @@
+open_project ../vivado/project/ultrazohm.xpr
+
+set return_msg [check_syntax -fileset sources_1 -return_string]
+set error_flag 0
+ 
+if {$return_msg != ""} {
+  set error_flag 1
+}
+ 
+puts $return_msg
+exit $error_flag

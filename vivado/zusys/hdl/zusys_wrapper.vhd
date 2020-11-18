bb817ec198165fff7d6a97ee90e4ede81789dbd4
EykeLiegmann <eyke.liegmann@tum.de>
2020-11-18 10:41:29 +0100

    - adjusted input clock frequency of PSS_REF_CLK from 33.33333 to 33.333333 (adding a 3 after the decimal to get even clocks in PL, 500MHz instead of 499.999) - generated xsa

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
---Date        : Wed Nov 18 08:36:47 2020
+--Date        : Wed Nov 18 09:41:13 2020
 --Host        : TUEIEAL-TM01 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

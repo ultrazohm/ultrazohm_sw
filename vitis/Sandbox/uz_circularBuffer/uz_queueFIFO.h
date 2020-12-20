63bbcd5390ed4f633b11e1dcf8a2589a68c8f015
Tobias <tobias.schindler@th-nuernberg.de>
2020-12-20 17:56:17 +0100

    Adds circular buffer prototype

---
+++ vitis/Sandbox/uz_circularBuffer/uz_queueFIFO.h
@@ -0,0 +1,15 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+
+// #define UZ_QUEUE_MAX_LENGTH 20
+
+// typedef struct uz_queue_ {
+//    char name[UZ_QUEUE_MAX_LENGTH];
+//    char vorname[UZ_QUEUE_MAX_LENGTH];
+//    int rnummer;
+//    struct uz_queue_ *previous;
+// }uz_queue;
+
+// void uz_queue_init(uz_queue *self);

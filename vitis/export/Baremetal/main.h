1b11ec9fb5ab64b6dfa60c47f8c860350f72d782
Tobias <tobias.schindler@th-nuernberg.de>
2020-12-21 14:13:01 +0100

    Restructures file folders

--- vitis/export/Baremetal/main.h
+++ vitis/export/Baremetal/main.h
@@ -45,7 +45,7 @@
 
 #include "uz/uz_HAL.h"
 
-#include "uz/uz_led_facade/uz_LED_facade.h"
+#include "uz/uz_LED/uz_LED_facade.h"
 #include "uz/uz_MioGpio_mediator/uz_MioGpio_mediator.h"
 #include "uz/uz_MioGpio_mediator/uz_MioGpioMapping.h"
 /* Definitions */
--- vitis/export/Baremetal/uz/uz_MioGpio_mediator/uz_MioGpio_mediator.h
+++ vitis/export/Baremetal/uz/uz_MioGpio_mediator/uz_MioGpio_mediator.h
@@ -5,7 +5,7 @@
 #include "../uz_HAL.h"
 #include "../uz_GPIO/uz_gpio.h"
 #include "../uz_LED/uz_LED.h"
-#include "../uz_led_facade/uz_LED_facade.h"
+#include "../uz_LED/uz_LED_facade.h"
 
 #include "xgpiops.h"
 #include "../uz_MioGpio_mediator/uz_MioGpioMapping.h"

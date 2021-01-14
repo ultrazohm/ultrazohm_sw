36e266f69e3f696cfdad0cc739bd7d3777d8fd7f
Tobias <tobias.schindler@th-nuernberg.de>
2021-01-14 17:35:46 +0100

    Deletes comments

--- vitis/export/Baremetal/Codegen/uz_codegen.c
+++ vitis/export/Baremetal/Codegen/uz_codegen.c
@@ -1,33 +1,10 @@
 #include "uz_codegen.h"
 
-
-//static RT_MODEL rtM_;
-//static RT_MODEL *const rtMPtr = &rtM_; /* Real-time model */
-//static 	DW rtDW;                        /* Observable states */
-//static 	ExtU rtU;                       /* External inputs */
-//static 	ExtY rtY;                       /* External outputs */
-
-//void uz_codegen_init(){
-//	rtMPtr->dwork = &rtDW;
-//	rtMPtr->inputs = &rtU;
-//	rtMPtr->outputs = &rtY;
-//	uz_codegen0_initialize(rtMPtr);
-//}
-
-//void uz_codegen_updateValues(uz_codegenStruct self){
-//	rtMPtr->inputs->time=data.time;
-//}
-
-//void uz_codegen_step(){
-//	  uz_codegen0_step(rtMPtr);
-//}
-
 void uz_codegen_init(uz_codegenStruct *self){
 	self->PtrToModelData=&self->modelData;
 	self->PtrToModelData->dwork=&self->rtDW;
 	self->PtrToModelData->inputs=&self->input;
 	self->PtrToModelData->outputs=&self->output;
-
 }
 
 void uz_codegen_step(uz_codegenStruct *self){

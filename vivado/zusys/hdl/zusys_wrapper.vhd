e325d943e2b0b99c296e0b253f8d4003f056c46a
EykeLiegmann <eyke.liegmann@tum.de>
2021-02-16 13:52:39 +0100

    vivado: added 64bit axi timer to use it as global uptime counter with 10ns resolution

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Thu Nov 26 11:10:39 2020
---Host        : TS-WS running 64-bit Ubuntu 20.04.1 LTS
+--Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
+--Date        : Tue Feb 16 12:44:44 2021
+--Host        : TUEIEAL-TM01 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

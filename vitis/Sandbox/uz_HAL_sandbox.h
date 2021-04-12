808ea456b266f6caf7331153211b39a0d36d7f6f
Tobias <tobias.schindler@th-nuernberg.de>
2021-04-12 18:28:43 +0200

    Updates sandbox HAL

--- vitis/Sandbox/uz_HAL_sandbox.h
+++ vitis/Sandbox/uz_HAL_sandbox.h
@@ -13,7 +13,7 @@
 #define uz_assert assert
 // Common to assert some pointer is not Null
 // Map to platform specific assert macro
-#define uz_assertNotNull(Expression) \
+#define uz_assert_not_NULL(Expression) \
 {                              \
     uz_assert(Expression!=NULL);        \
 }
@@ -22,47 +22,47 @@
 #define UZ_SUCCESS 0
 #define UZ_FAILURE 1
 
-static inline void uz_writeFloatToAxi(uintptr_t Addr, float Value){
+static inline void uz_axi_write_float(uintptr_t Addr, float Value){
   uz_printf("uz_writeFloatToAxi called");
 }
 
-static inline float uz_readFloatFromAxi(uintptr_t Addr)
+static inline float uz_axi_read_float(uintptr_t Addr)
 {
       uz_printf("uz_readFloatFromAxi called");
     return (0);
 }
 
-static inline void uz_writeUnsingedInt32ToAxi(uintptr_t Addr, uint32_t Value){
+static inline void uz_axi_write_uint32(uintptr_t Addr, uint32_t Value){
       uz_printf("uz_writeUnsingedInt32ToAxi called");
 }
 
-static inline uint32_t uz_readUnsingedInt32FromAxi(uintptr_t Addr)
+static inline uint32_t uz_axi_read_uint32(uintptr_t Addr)
 {
       uz_printf("uz_readUnsingedInt32FromAxi called");
     return (0);
 }
 
-static inline void uz_writeInt32ToAxi(uintptr_t Addr, int32_t Value){
+static inline void uz_axi_write_int32(uintptr_t Addr, int32_t Value){
       uz_printf("uz_writeInt32ToAxi called");
 }
 
-static inline int32_t uz_readInt32FromAxi(uintptr_t Addr)
+static inline int32_t uz_axi_read_int32(uintptr_t Addr)
 {
       uz_printf("uz_readInt32FromAxi called");
     return (0);
 }
 
-static inline void uz_writeBoolToAxi(uintptr_t Addr, _Bool enable){
+static inline void uz_axi_write_bool(uintptr_t Addr, _Bool enable){
 
       uz_printf("uz_writeBoolToAxi called");
 }
 
-static inline float uz_sfixedToFloat(int32_t data, int numberOfFractionalBits){
+static inline float uz_convert_sfixed_to_float(int32_t data, int numberOfFractionalBits){
 	uz_assert(numberOfFractionalBits>=0);
 	return (ldexpf((float)data, -numberOfFractionalBits));
 }
 
-static inline int32_t uz_FloatToSfixed(float data, int numberOfFractionalBits){
+static inline int32_t uz_convert_float_to_sfixed(float data, int numberOfFractionalBits){
 	uz_assert(numberOfFractionalBits>=0);
 	return ((int32_t)ldexpf(data, numberOfFractionalBits));
 }

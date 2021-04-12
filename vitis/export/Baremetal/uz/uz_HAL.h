e406208e8a05ff779fa34e567abcd16e00a86ed0
Tobias <tobias.schindler@th-nuernberg.de>
2021-04-12 18:03:16 +0200

    refactors names of hal

--- vitis/export/Baremetal/uz/uz_HAL.h
+++ vitis/export/Baremetal/uz/uz_HAL.h
@@ -22,52 +22,52 @@
 #define UZ_SUCCESS XST_SUCCESS
 #define UZ_FAILURE XST_FAILURE
 
-static inline void uz_writeFloatToAxi(uintptr_t Addr, float Value){
+static inline void uz_axi_write_float(uintptr_t Addr, float Value){
   volatile float *LocalAddr = (volatile float *)Addr;
   *LocalAddr = Value;
 }
 
-static inline float uz_readFloatFromAxi(uintptr_t Addr)
+static inline float uz_axi_read_float(uintptr_t Addr)
 {
     return *(volatile float *) Addr;
 }
 
-static inline void uz_writeUnsingedInt32ToAxi(uintptr_t Addr, uint32_t Value){
+static inline void uz_axi_write_uint32(uintptr_t Addr, uint32_t Value){
   volatile uint32_t *LocalAddr = (volatile uint32_t *)Addr;
   *LocalAddr = Value;
 }
 
-static inline uint32_t uz_readUnsingedInt32FromAxi(uintptr_t Addr)
+static inline uint32_t uz_axi_read_uint32(uintptr_t Addr)
 {
     return *(volatile uint32_t *) Addr;
 }
 
-static inline void uz_writeInt32ToAxi(uintptr_t Addr, int32_t Value){
+static inline void uz_axi_write_int32(uintptr_t Addr, int32_t Value){
   volatile int32_t *LocalAddr = (volatile int32_t *)Addr;
   *LocalAddr = Value;
 }
 
-static inline int32_t uz_readInt32FromAxi(uintptr_t Addr)
+static inline int32_t uz_axi_read_int32(uintptr_t Addr)
 {
     return *(volatile int32_t *) Addr;
 }
 
-static inline void uz_writeBoolToAxi(uintptr_t Addr, _Bool enable){
+static inline void uz_axi_write_bool(uintptr_t Addr, _Bool enable){
 
 	if(enable==true){
-		uz_writeUnsingedInt32ToAxi(Addr, (uint32_t)0x00000001);
+		uz_axi_write_uint32(Addr, (uint32_t)0x00000001);
 	}
 	if(enable==false){
-		uz_writeUnsingedInt32ToAxi(Addr, (uint32_t)0x00000000);
+		uz_axi_write_uint32(Addr, (uint32_t)0x00000000);
 	}
 }
 
-static inline float uz_sfixedToFloat(int32_t data, int numberOfFractionalBits){
+static inline float uz_convert_sfixed_to_float(int32_t data, int numberOfFractionalBits){
 	uz_assert(numberOfFractionalBits>=0);
 	return (ldexpf((float)data, -numberOfFractionalBits));
 }
 
-static inline int32_t uz_FloatToSfixed(float data, int numberOfFractionalBits){
+static inline int32_t uz_convert_float_to_sfixed(float data, int numberOfFractionalBits){
 	uz_assert(numberOfFractionalBits>=0);
 	return ((int32_t)ldexpf(data, numberOfFractionalBits));
 }

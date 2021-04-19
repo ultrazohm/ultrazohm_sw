7c379277b6054346b81b27058948fb0e5a76cb2f
Tobias <tobias.schindler@th-nuernberg.de>
2021-04-19 16:54:48 +0200

    Adds new ip core for simscape and deletes old software driver

--- vitis/export/Baremetal/IP_Cores/SimScapeExample/uz_simExpl_testbench.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <math.h>
-#include "uz_simExpl.h"
-#include "xparameters.h"
-#include "../../uz/uz_SystemTime/uz_SystemTime.h"
-
-void uz_simExpl_testbench_init();
-void uz_simExpl_stepTestbench();

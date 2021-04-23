6eba17ffcdbdd7fb56160f7648bab1e3a1f10fa0
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-04-23 12:03:06 +0200

    Added sine wave and sawtooth generator to the uz_wavegen.c file, as requested in #114

--- vitis/export/Baremetal/uz/uz_wavegen/uz_wavegen.c
+++ vitis/export/Baremetal/uz/uz_wavegen/uz_wavegen.c
@@ -1,1 +1,31 @@
 #include "uz_wavegen.h"
+
+
+
+//amplitude: 		Amplitude of the output sine wave
+//frequency:		Frequency of the output sine wave
+float uz_wavegen_sine(int amplitude, int frequency) {
+	//Variables
+	float angle = 0.0;
+
+	angle = 2.0 * M_PI * frequency * uz_SystemTime_GetIsrPeriodInUs() * 0.000001 * uz_SystemTime_GetInterruptCounter();
+
+	return (amplitude * sin(angle));
+}
+
+//amplitude: 		Amplitude of the output sawtooth wave
+//frequency:		Frequency of the output sawtooth wave
+float uz_wavegen_sawtooth(int amplitude, int frequency) {
+	//Variables
+	float sawtooth = 0.0;
+	static float counter = 0.0;
+
+	if (counter > (1 / (uz_SystemTime_GetIsrPeriodInUs() * 0.000001)) / frequency)
+		counter = 0;
+
+	sawtooth = amplitude * (counter * frequency * uz_SystemTime_GetIsrPeriodInUs() * 0.000001);
+
+	counter += 1;
+
+	return (sawtooth);
+}

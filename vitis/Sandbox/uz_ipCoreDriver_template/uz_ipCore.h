9f714c44f72f0849ee752be53c01905cf02b353a
Tobias <tobias.schindler@zohm-control.com>
2021-04-25 16:14:38 +0200

    Updates remote container and sandbox settings

--- vitis/Sandbox/uz_ipCoreDriver_template/uz_ipCore.h
+++ vitis/Sandbox/uz_ipCoreDriver_template/uz_ipCore.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 typedef struct uz_ipCore uz_ipCore;
-typedef uz_ipCore *const uz_ipCore_handle; // *const to indicate that the handle will never point to another instance of the ipCore
+typedef uz_ipCore *uz_ipCore_handle;
 
 uz_ipCore_handle uz_ipCore_init(uz_ipCore_handle self);
 void uz_ipCore_set_variable(uz_ipCore_handle self,int variable);

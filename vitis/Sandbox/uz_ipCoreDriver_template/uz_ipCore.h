f8f7ff95a2e39ffc06ec627c6012abe5ba19d21f
Tobias <tobias.schindler@th-nuernberg.de>
2021-04-26 17:50:23 +0200

    Gets rid of handle typedef and minor udpates for clearity

--- vitis/Sandbox/uz_ipCoreDriver_template/uz_ipCore.h
+++ vitis/Sandbox/uz_ipCoreDriver_template/uz_ipCore.h
@@ -4,8 +4,7 @@
 #include <stdlib.h>
 
 typedef struct uz_ipCore uz_ipCore;
-typedef uz_ipCore *const uz_ipCore_handle; // *const to indicate that the handle will never point to another instance of the ipCore
 
-uz_ipCore_handle uz_ipCore_init(uz_ipCore_handle self);
-void uz_ipCore_set_variable(uz_ipCore_handle self,int variable);
-int uz_ipCore_get_variable(uz_ipCore_handle self);
+uz_ipCore* uz_ipCore_init(uz_ipCore* self);
+void uz_ipCore_set_variable(uz_ipCore* self,int variable);
+int uz_ipCore_get_variable(uz_ipCore* self);
--- vitis/Sandbox/uz_ipCoreDriver_template/uz_ipCore_private.h
+++ vitis/Sandbox/uz_ipCoreDriver_template/uz_ipCore_private.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <stdint.h>
-
+#include <stdbool.h>
 struct uz_ipCore{
   const uint32_t base_address;
-  int variable; 
+  const uint32_t ip_clk_frequency_Hz;
+  bool is_ready;
+  int variable;
 };

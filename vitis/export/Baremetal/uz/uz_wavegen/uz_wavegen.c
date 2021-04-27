4fc8c8aecb2294516d024fd51ca6faadfe95f768
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-04-27 14:44:16 +0200

    Added functionality for pulse wave generation

--- vitis/export/Baremetal/uz/uz_wavegen/uz_wavegen.c
+++ vitis/export/Baremetal/uz/uz_wavegen/uz_wavegen.c
@@ -1,11 +1,21 @@
 #include "uz_wavegen.h"
 
+//typedef struct {
+//	float amplitude;
+//	float frequency;
+//	float DutyCycle;
+//
+//} wavegen_pulse_settings;
+
+
+//wavegen_pulse_settings test = { .amplitude = 0.0f, .frequency = 0.0f, .DutyCycle = 0.0f };
+
 float uz_wavegen_sine(int amplitude, int frequency_Hz) {
 	float angle = 0.0;
 
 	angle = 2.0 * M_PI * uz_SystemTime_GetInterruptCounter() * frequency_Hz / uz_SystemTime_GetIsrFrequencyInHz();
 
-	return (amplitude * sin(angle));
+	return (amplitude * sinf(angle));
 }
 
 float uz_wavegen_sawtooth(int amplitude, int frequency_Hz) {
@@ -15,4 +25,15 @@ float uz_wavegen_sawtooth(int amplitude, int frequency_Hz) {
 	sample = fmodf(uz_SystemTime_GetInterruptCounter(), (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz));
 
 	return (sample * amplitude / (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz));
+}
+
+float uz_wavegen_pulse(wavegen_pulse_settings pulse) {
+	float sample = 0.0f;
+
+	sample = fmodf(uz_SystemTime_GetInterruptCounter(), (uz_SystemTime_GetIsrFrequencyInHz() / pulse.frequency_Hz));
+
+	if (sample > (uz_SystemTime_GetIsrFrequencyInHz() / pulse.frequency_Hz) * pulse.DutyCycle)
+		pulse.amplitude = 0.0f;
+
+	return (pulse.amplitude);
 }
--- vitis/export/Baremetal/uz/uz_wavegen/uz_wavegen.h
+++ vitis/export/Baremetal/uz/uz_wavegen/uz_wavegen.h
@@ -1,5 +1,14 @@
 #pragma once
 #include "../uz_SystemTime/uz_SystemTime.h"
 
+typedef struct {
+	float amplitude;
+	float frequency_Hz;
+	float DutyCycle;
+
+} wavegen_pulse_settings;
+
 float uz_wavegen_sine(int amplitude, int frequency_Hz);
 float uz_wavegen_sawtooth(int amplitude, int frequency_Hz);
+float uz_wavegen_pulse(wavegen_pulse_settings pulse);
+

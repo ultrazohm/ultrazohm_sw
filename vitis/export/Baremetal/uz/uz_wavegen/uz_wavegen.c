24fdda990953053a137bc1453407fa51cb23dc52
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-04-29 11:57:41 +0200

    Improved waveform generation

--- vitis/export/Baremetal/uz/uz_wavegen/uz_wavegen.c
+++ vitis/export/Baremetal/uz/uz_wavegen/uz_wavegen.c
@@ -17,73 +17,80 @@ float uz_wavegen_sine_with_offset(float amplitude, float frequency_Hz, float off
 	return ((amplitude * sinf(angle)) + offset);
 }
 
-//float uz_wavegen_sawtooth(float amplitude, float frequency_Hz) {
-//
-//	float sample = 0.0;
-//
-//	uz_assert(frequency_Hz < 0.0);
-//
-//	sample = fmodf(uz_SystemTime_GetInterruptCounter(), (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz));
-//
-//	return (sample * amplitude / (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz));
-//}
-//
-//float uz_wavegen_pulse(wavegen_pulse_settings pulse) {
-//	float sample = 0.0f;
-//
-//	uz_assert(pulse.frequency_Hz < 0.0);
-//	uz_assert(pulse.DutyCycle < 0.0);
-//	uz_assert(pulse.DutyCycle > 1.0);
-//
-//	sample = fmodf(uz_SystemTime_GetInterruptCounter(), (uz_SystemTime_GetIsrFrequencyInHz() / pulse.frequency_Hz));
-//
-//	if (sample > (uz_SystemTime_GetIsrFrequencyInHz() / pulse.frequency_Hz) * pulse.DutyCycle) {
-//		pulse.amplitude = 0.0f;
-//	}
-//
-//	return (pulse.amplitude);
-//}
-//
-//float uz_wavegen_square(float amplitude, float frequency_Hz) {
-//
-//	float sample = 0.0;
-//
-//	uz_assert(frequency_Hz < 0.0);
-//
-//	sample = fmodf(uz_SystemTime_GetInterruptCounter(), (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz));
-//
-//	if (sample > (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz) * 0.5f) {
-//		amplitude = amplitude * -1.0f;
-//	}
-//
-//	return (amplitude);
-//}
-//
-//float uz_wavegen_triangle(float amplitude, float frequency_Hz) {
-//
-//	float sample = 0.0;
-//	float triangle_wave = 0.0;
-//
-//	uz_assert(frequency_Hz < 0.0);
-//
-//	sample = fmodf(uz_SystemTime_GetInterruptCounter(), (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz));
-//
-//	if (sample > (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz) * 0.5f) {
-//		triangle_wave = 2.0f * amplitude + -2.0f * sample * amplitude / (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz);
-//	} else {
-//		triangle_wave = 2.0f * sample * amplitude / (uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz);
-//	}
-//
-//	return (triangle_wave);
-//}
-//
-//float uz_wavegen_saturation(wavegen_saturation_settings saturation) {
-//
-//	if (saturation.input_signal > saturation.upper_limit) {
-//		saturation.input_signal = saturation.upper_limit;
-//	} else if (saturation.input_signal < saturation.lower_limit) {
-//		saturation.input_signal = saturation.lower_limit;
-//	}
-//
-//	return (saturation.input_signal);
-//}
+float uz_wavegen_sawtooth(float amplitude, float frequency_Hz) {
+	uz_assert(frequency_Hz > 0.0);
+	float counter = uz_SystemTime_GetInterruptCounter();
+	float frequency_adjusted = uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz;
+	float sample = fmodf(counter, frequency_adjusted);
+	return (sample * amplitude / frequency_adjusted);
+}
+
+float uz_wavegen_sawtooth_with_offset(float amplitude, float frequency_Hz, float offset) {
+	uz_assert(frequency_Hz > 0.0);
+	float counter = uz_SystemTime_GetInterruptCounter();
+	float frequency_adjusted = uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz;
+	float sample = fmodf(counter, frequency_adjusted);
+	return ((sample * amplitude / frequency_adjusted) + offset);
+}
+
+float uz_wavegen_pulse(float amplitude, float frequency_Hz, float duty_cycle) {
+	uz_assert(frequency_Hz > 0.0);
+	uz_assert(duty_cycle >= 0.0);
+	uz_assert(duty_cycle <= 1.0);
+	float counter = uz_SystemTime_GetInterruptCounter();
+	float frequency_adjusted = uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz;
+	float sample = fmodf(counter, frequency_adjusted);
+	if (sample > frequency_adjusted * duty_cycle) {
+		amplitude = 0.0f;
+	}
+	return (amplitude);
+}
+
+float uz_wavegen_square(float amplitude, float frequency_Hz) {
+	uz_assert(frequency_Hz > 0.0);
+	float counter = uz_SystemTime_GetInterruptCounter();
+	float frequency_adjusted = uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz;
+	float sample = fmodf(counter, frequency_adjusted);
+	if (sample > frequency_adjusted * 0.5f) {
+		amplitude = amplitude * -1.0f;
+	}
+	return (amplitude);
+}
+
+float uz_wavegen_triangle(float amplitude, float frequency_Hz) {
+	uz_assert(frequency_Hz > 0.0);
+	float triangle_wave = 0.0f;
+	float counter = uz_SystemTime_GetInterruptCounter();
+	float frequency_adjusted = uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz;
+	float sample = fmodf(counter, frequency_adjusted);
+	if (sample > frequency_adjusted * 0.5f) {
+		triangle_wave = 2.0f * amplitude - 2.0f * sample * amplitude / frequency_adjusted;
+	} else {
+		triangle_wave = 2.0f * sample * amplitude / frequency_adjusted;
+	}
+	return (triangle_wave);
+}
+
+float uz_wavegen_triangle_with_offset(float amplitude, float frequency_Hz, float offset) {
+	uz_assert(frequency_Hz > 0.0);
+	float triangle_wave = 0.0f;
+	float counter = uz_SystemTime_GetInterruptCounter();
+	float frequency_adjusted = uz_SystemTime_GetIsrFrequencyInHz() / frequency_Hz;
+	float sample = fmodf(counter, frequency_adjusted);
+	if (sample > frequency_adjusted * 0.5f) {
+		triangle_wave = 2.0f * amplitude - 2.0f * sample * amplitude / frequency_adjusted + offset;
+	} else {
+		triangle_wave = 2.0f * sample * amplitude / frequency_adjusted + offset;
+	}
+	return (triangle_wave);
+}
+
+
+float uz_wavegen_saturation(float signal, float upper_limit, float lower_limit) {
+	if (signal > upper_limit) {
+		signal = upper_limit;
+	} else if (signal < lower_limit) {
+		signal = lower_limit;
+	}
+	return (signal);
+}

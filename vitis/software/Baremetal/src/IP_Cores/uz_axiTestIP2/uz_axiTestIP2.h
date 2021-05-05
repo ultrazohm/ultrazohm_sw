89cf1d8f0a8edd273088e8de0fe1b807cd6b27f3
Tobias <tobias.schindler@th-nuernberg.de>
2021-05-05 10:08:18 +0200

    Integrates test ip

--- vitis/software/Baremetal/src/IP_Cores/uz_axiTestIP2/uz_axiTestIP2.h
+++
@@ -1,9 +0,0 @@
-#ifndef UZ_AXITESTIP2_H
-#define UZ_AXITESTIP2_H
-
-typedef struct uz_axiTestIP2 uz_axiTestIP2;
-
-uz_axiTestIP2* uz_axiTestIP2_init(uz_axiTestIP2* self);
-int uz_axiTestIP2_multiply(uz_axiTestIP2* self, int A, int B);
-
-#endif // UZ_AXITESTIP2_H

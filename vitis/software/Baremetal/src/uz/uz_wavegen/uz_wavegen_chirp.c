0868c0f01fab267075be60882af5b8c22a6ce61f
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-05-06 08:49:22 +0200

    Improved functionality in the chirp wavegen to read the systemTime

--- vitis/software/Baremetal/src/uz/uz_wavegen/uz_wavegen_chirp.c
+++ vitis/software/Baremetal/src/uz/uz_wavegen/uz_wavegen_chirp.c
@@ -6,6 +6,7 @@
 
 struct uz_wavegen {
 	bool is_ready;
+	bool read_system_time;
 	float elapsed_time_since_start;
 	float initial_global_time_Sec;
 	float transition_angle;
@@ -20,7 +21,7 @@ uz_wavegen* uz_wavegen_chirp_init(struct uz_wavegen_config config) {
 	uz_assert(self->is_ready == false);
 	counter += 1;
 	self->is_ready = true;
-
+	self->read_system_time = true;
 	uz_assert(config.start_frequency_Hz > 0);
 	uz_assert(config.end_frequency_Hz > 0);
 	uz_assert(config.end_frequency_Hz > config.start_frequency_Hz);
@@ -34,7 +35,7 @@ uz_wavegen* uz_wavegen_chirp_init(struct uz_wavegen_config config) {
 void uz_wavegen_chirp_reset(uz_wavegen* self) {
 	uz_assert_not_NULL(self);
 	uz_assert(self->is_ready == true);
-	self->initial_global_time_Sec = uz_SystemTime_GetGlobalTimeInSec();
+	self->read_system_time = true;
 	self->elapsed_time_since_start = 0.0f;
 	self->transition_angle = 0.0f;
 }
@@ -43,11 +44,15 @@ float uz_wavegen_chirp(uz_wavegen* self) {
 	uz_assert_not_NULL(self);
 	uz_assert(self->is_ready);
 	float remaining_delay_Sec = self->config.initial_delay_Sec - self->elapsed_time_since_start;
-	float system_time_Sec = uz_SystemTime_GetGlobalTimeInSec();
 	float t_Sec = self->elapsed_time_since_start - self->config.initial_delay_Sec;
 	float chirp_rate = (self->config.end_frequency_Hz - self->config.start_frequency_Hz) / self->config.duration_Sec;
 	float chirp_output = 0.0f;
+	float system_time_Sec = uz_SystemTime_GetGlobalTimeInSec();
 
+	if (self->read_system_time == true) {
+		self->initial_global_time_Sec = uz_SystemTime_GetGlobalTimeInSec();
+		self->read_system_time = false;
+	}
 	if (remaining_delay_Sec > 0) {
 		chirp_output = 0.0f;
 	} else {

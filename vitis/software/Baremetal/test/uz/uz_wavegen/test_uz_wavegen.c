0a244eca4b6c314c5ad2271690872f7fed43611a
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-05-06 15:34:19 +0200

    Added unit-test for wavegen

---
+++ vitis/software/Baremetal/test/uz/uz_wavegen/test_uz_wavegen.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+#include "uz_wavegen.h"
+#include <math.h>
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_uz_wavegen_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement uz_wavegen");
+}
+
+#endif // TEST

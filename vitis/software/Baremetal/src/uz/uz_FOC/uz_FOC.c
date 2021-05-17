d41e1152b797e8af34477d7bbed4db9627ab0c23
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-05-17 14:18:59 +0200

    Added first funtionality

--- vitis/software/Baremetal/src/uz/uz_FOC/uz_FOC.c
+++ vitis/software/Baremetal/src/uz/uz_FOC/uz_FOC.c
@@ -1,1 +1,18 @@
 #include "uz_FOC.h"
+#define max_FOC_CurrentControl_instances 1
+#define max_FOC_SpeedControl_instances 1
+
+static size_t counter_FOC_CurrentControl = 0;
+static size_t counter_FOC_SpeedControl = 0;
+
+static uz_FOC_CurrentControl_parameters instances_FOC_CurrentControl[max_FOC_CurrentControl_instances] = { 0 };
+static uz_FOC_SpeedControl_parameters instances_FOC_SpeedControl[max_FOC_SpeedControl_instances] = { 0 };
+
+uz_FOC_CurrentControl_parameters* uz_FOC_CurrentControl_init(uz_FOC_CurrentControl_config config) {
+	uz_assert(counter_FOC_CurrentControl < max_FOC_CurrentControl_instances);
+	uz_FOC_CurrentControl_parameters* self = &instances_FOC_CurrentControl[counter_FOC_CurrentControl];
+	uz_assert(self->is_ready == false);
+	counter_FOC_CurrentControl += 1;
+	self->is_ready = true;
+	return (self);
+}
--- vitis/software/Baremetal/src/uz/uz_FOC/uz_FOC.h
+++ vitis/software/Baremetal/src/uz/uz_FOC/uz_FOC.h
@@ -1,5 +1,53 @@
 #ifndef UZ_FOC_H
 #define UZ_FOC_H
 
-
 #endif // UZ_FOC_H
+
+#include "../uz_HAL.h"
+#include <math.h>
+#include <stdbool.h>
+
+typedef struct uz_FOC_CurrentControl_config {
+	float Rs_mOmega;
+	float Ld_mH;
+	float Lq_mH;
+	float Psi_PM_mVs;
+	float iq_soll_Ampere;
+	float id_soll_Ampere;
+} uz_FOC_CurrentControl_config;
+
+typedef struct uz_FOC_SpeedControl_config {
+	float Rs_mOmega;
+	float Ld_mH;
+	float Lq_mH;
+	float Psi_PM_mVs;
+	float n_soll_rpm;
+} uz_FOC_SpeedControl_config;
+
+
+typedef struct uz_FOC_CurrentControl_parameters {
+	bool is_ready;
+	float Kp_id;
+	float Tn_id;
+	float Kp_iq;
+	float Tn_iq;
+	uz_FOC_CurrentControl_config config;
+} uz_FOC_CurrentControl_parameters;
+
+typedef struct uz_FOC_SpeedControl_parameters {
+	bool is_ready;
+	float Kp_n;
+	float Tn_n;
+	float Kp_id;
+	float Tn_id;
+	float Kp_iq;
+	float Tn_iq;
+	uz_FOC_SpeedControl_config config;
+} uz_FOC_SpeedControl_parameters;
+
+
+uz_FOC_CurrentControl_parameters* uz_FOC_CurrentControl_init(uz_FOC_CurrentControl_config config);
+void uz_FOC_CurrentControl(uz_FOC_CurrentControl_parameters* self);
+
+uz_FOC_SpeedControl_parameters* uz_FOC_SpeedControl_init(uz_FOC_SpeedControl_config config);
+void uz_FOC_SpeedControl(uz_FOC_SpeedControl_parameters* self);

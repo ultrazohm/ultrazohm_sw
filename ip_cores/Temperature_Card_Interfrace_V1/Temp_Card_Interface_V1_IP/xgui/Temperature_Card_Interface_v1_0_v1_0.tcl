944ce13d312d81aff533bc548a99b48036ea9459
R_Zipprich <r.zipprich@uni-kassel.de>
2021-05-18 17:09:57 +0200

    Ip-Core written to handle the UZ-Temperature-Card. Start with the Registermap. Next Steps: - Testbench - Integration in the TopLVL - add Vios - programm CPLD to pass through the SPI-busses

---
+++ ip_cores/Temperature_Card_Interfrace_V1/Temp_Card_Interface_V1_IP/xgui/Temperature_Card_Interface_v1_0_v1_0.tcl
@@ -0,0 +1,49 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  ipgui::add_page $IPINST -name "Page 0"
+
+  set enable_FPGA_tempports [ipgui::add_param $IPINST -name "enable_FPGA_tempports"]
+  set_property tooltip {enables or disables Ports to get the temperaturedata inside the FPGA} ${enable_FPGA_tempports}
+
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_ADDR_WIDTH { PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to update C_S00_AXI_ADDR_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_ADDR_WIDTH { PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to validate C_S00_AXI_ADDR_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_DATA_WIDTH { PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to update C_S00_AXI_DATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_DATA_WIDTH { PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to validate C_S00_AXI_DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.enable_FPGA_tempports { PARAM_VALUE.enable_FPGA_tempports } {
+	# Procedure called to update enable_FPGA_tempports when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.enable_FPGA_tempports { PARAM_VALUE.enable_FPGA_tempports } {
+	# Procedure called to validate enable_FPGA_tempports
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH { MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S00_AXI_DATA_WIDTH}] ${MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH { MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S00_AXI_ADDR_WIDTH}] ${MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH}
+}
+

14fd7bd39f2aff549f9cddc4e6788e496cc2fbba
EykeLiegmann <eyke.liegmann@tum.de>
2021-06-24 20:48:33 +0200

    after zoom call with carolina: - applied preset from trenz board file - regenerated bitstream - exported new xsa file

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
---Date        : Tue May 25 12:56:14 2021
+--Date        : Thu Jun 24 19:52:33 2021
 --Host        : TUEIEAL-TM01 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

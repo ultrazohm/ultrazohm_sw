d20f4ca6f534cd003f6eb2f5c11c957bba17bd79
EykeLiegmann <eyke.liegmann@tum.de>
2021-06-25 18:30:56 +0200

    reducing the heap size of freertos to avoid running out of memory

--- tcl_scripts/vitis_generate_UltraZohm_workspace.tcl
+++ tcl_scripts/vitis_generate_UltraZohm_workspace.tcl
@@ -107,7 +107,7 @@ platform write
 bsp config lwip_dhcp true
 platform write 
 # increase heap size of freertos, to fix javascope glitches
-bsp config total_heap_size  1048576
+bsp config total_heap_size  524288
 platform write 
 
 puts "Info:(UltraZohm) regenerate FreeRTOS BSP"

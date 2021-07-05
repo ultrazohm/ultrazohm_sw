c17ff5e8dee4b744f3a2d294e38f836e5f0b2690
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-07-05 13:21:39 +0200

    Fixes ceedling pipeline

--- vitis/software/Baremetal/test/uz/uz_FOC/test_uz_FOC.c
+++ vitis/software/Baremetal/test/uz/uz_FOC/test_uz_FOC.c
@@ -3,7 +3,35 @@
 #include "unity.h"
 #include "test_assert_with_exception.h"
 #include "uz_FOC.h"
+TEST_FILE("uz_PI_controller.c")
 
 
+uz_PI_Controller_config config_id = {
+    .Kp = 6.75f,
+    .Ki = 2000.0f,
+    .samplingTime_sec = 0.001f,
+    .d_y_max = 10.0f,
+    .d_y_min = -10.0f 
+};
+
+uz_PI_Controller_config config_iq = {
+    .Kp = 6.75f,
+    .Ki = 2000.0f,
+    .samplingTime_sec = 0.001f,
+    .d_y_max = 10.0f,
+    .d_y_min = -10.0f 
+};
+
+uz_PI_Controller_config config_n = {
+    .Kp = 0.00864f,
+    .Ki = 0.0864f,
+    .samplingTime_sec = 0.001f,
+    .d_y_max = 10.0f,
+    .d_y_min = -10.0f 
+};
+
+void test_uz_FOC_init_L_d_negative(void){
+
+}
 
 #endif // TEST

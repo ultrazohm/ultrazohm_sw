503b049267d77cff6369d38ea0e335cf607c60dd
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-07-06 15:59:16 +0200

    Adds PI-Controller unit tests

--- vitis/software/Baremetal/test/uz/uz_piController/test_uz_piController.c
+++ vitis/software/Baremetal/test/uz/uz_piController/test_uz_piController.c
@@ -6,4 +6,91 @@
 #include "test_assert_with_exception.h"
 TEST_FILE("uz_signals.c")
 
+uz_PI_Controller_config config = {
+    .Kp = 10.0f,
+    .Ki = 10.0f,
+    .samplingTime_sec = 0.001f,
+    .upper_threshold = 10.0f,
+    .lower_threshold = -10.0f
+};
+
+void test_uz_PI_Controller_init_assert_Ki_negative(void){
+    config.Ki = -10.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_init(config));
+    config.Ki = 2000.0f;
+}
+
+void test_uz_PI_Controller_init_assert_Kp_negative(void){
+    config.Kp = -10.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_init(config));
+    config.Kp = 6.75f;
+}
+
+void test_uz_PI_Controller_init_assert_samplingTime_negative(void){
+    config.samplingTime_sec = -0.0001f;
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_init(config));
+}
+
+void test_uz_PI_Controller_init_assert_samplingTime_zero(void){
+    config.samplingTime_sec = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_init(config));
+    config.samplingTime_sec = 0.00001f;
+}
+
+void test_uz_PI_Controller_init_assert_upper_lower_threshold(void){
+    config.upper_threshold = -10.2f;
+    config.lower_threshold = 3.4f;
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_init(config));
+    config.upper_threshold = 10.0f;
+    config.lower_threshold = -10.0f;
+
+}
+void test_uz_PI_Controller_sample_NULL(void){
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_sample(NULL, 1.0f, 2.0f, false));
+}
+
+void test_uz_PI_Controller_sample_output(void){
+    uz_PI_Controller* variables = uz_PI_Controller_init(config);
+	//Compare values with Simulink Simulation
+	float values_id[11]={0.0f, 0.249f, 0.436f, 0.577f, 0.682f, 0.761f, 0.82f, 0.865f, 0.898f, 0.923f, 0.942f};
+	float values_output[11]={6.75f, 5.09f, 3.84f, 2.9f, 2.2f, 1.67f, 1.28f, 0.98f, 0.76f, 0.59f, 0.47f};
+	for(int i=0;i<11;i++){
+		TEST_ASSERT_EQUAL_FLOAT(values_output[i],roundf(uz_PI_Controller_sample(variables,1.0f,values_id[i],false)*100)/100);
+	}
+}
+
+void test_uz_PI_Controller_reset_assert_NULL(void){
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_reset(NULL));
+}
+
+void test_uz_PI_Controller_sample_output_Reset(void){
+    uz_PI_Controller* variables = uz_PI_Controller_init(config);
+	//Compare values with Simulink Simulation
+	float values_id[11]={0.0f, 0.249f, 0.436f, 0.577f, 0.682f, 0.761f, 0.82f, 0.865f, 0.898f, 0.923f, 0.942f};
+	float values_output[11]={6.75f, 5.09f, 3.84f, 2.9f, 2.2f, 1.67f, 1.28f, 0.98f, 0.76f, 0.59f, 0.47f};
+	for(int i=0;i<11;i++){
+		TEST_ASSERT_EQUAL_FLOAT(values_output[i],roundf(uz_PI_Controller_sample(variables,1.0f,values_id[i],false)*100)/100);
+	}
+    uz_PI_Controller_reset(variables);
+    TEST_ASSERT_EQUAL_FLOAT(0.32f,roundf(uz_PI_Controller_sample(variables,1.0f,0.953f,false)*100)/100);
+}   
+
+void test_uz_PI_Controller_set_Ki_assert_NULL(void){
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_set_Ki(NULL, 10.0f));
+}
+
+void test_uz_PI_Controller_set_Ki_assert_Ki_negative(void){
+    uz_PI_Controller* variables = uz_PI_Controller_init(config);
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_set_Ki(variables, -10.0f));
+}
+
+void test_uz_PI_Controller_set_Kp_assert_NULL(void){
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_set_Kp(NULL, 10.0f));
+}
+
+void test_uz_PI_Controller_set_Kp_assert_Kp_negative(void){
+    uz_PI_Controller* variables = uz_PI_Controller_init(config);
+    TEST_ASSERT_FAIL_ASSERT(uz_PI_Controller_set_Kp(variables, -10.0f));
+}
+
 #endif // TEST

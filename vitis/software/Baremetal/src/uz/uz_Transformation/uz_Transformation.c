87397d1be2dad2d04e1d73740c065a4992f562e2
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-07-21 13:51:23 +0200

    Addes doxygen comments & tiny refactoring & small doxygen fix in uz_signals

--- vitis/software/Baremetal/src/uz/uz_Transformation/uz_Transformation.c
+++ vitis/software/Baremetal/src/uz/uz_Transformation/uz_Transformation.c
@@ -3,27 +3,27 @@
 #include <math.h>
 
 
-struct uz_dq uz_dq_Transformation(struct uz_UVW input, float theta_el){
+struct uz_dq uz_dq_Transformation(struct uz_UVW input, float theta_el_rad){
     struct uz_dq output ={
         .d = 0.0f,
         .q = 0.0f,
         .zero = 0.0f
     };
-    output.d = (2.0f / 3.0f) * ( (cosf(theta_el) * input.U) + (cosf(theta_el -  ( (2.0f / 3.0f) * M_PI) ) * input.V) + (cosf(theta_el - ( (4.0f / 3.0f) * M_PI) ) * input.W) );
-    output.q = (2.0f / 3.0f) * ( -1.0f * (sinf(theta_el) * input.U) - (sinf(theta_el -  ( (2.0f / 3.0f) * M_PI) ) * input.V) - (sinf(theta_el - ( (4.0f / 3.0f) * M_PI) ) * input.W) );
+    output.d = (2.0f / 3.0f) * ( (cosf(theta_el_rad) * input.U) + (cosf(theta_el_rad -  ( (2.0f / 3.0f) * M_PI) ) * input.V) + (cosf(theta_el_rad - ( (4.0f / 3.0f) * M_PI) ) * input.W) );
+    output.q = (2.0f / 3.0f) * ( -1.0f * (sinf(theta_el_rad) * input.U) - (sinf(theta_el_rad -  ( (2.0f / 3.0f) * M_PI) ) * input.V) - (sinf(theta_el_rad - ( (4.0f / 3.0f) * M_PI) ) * input.W) );
     output.zero = (1.0f / 3.0f) * (input.U + input.V + input.W);
     return(output);
 }
 
-struct uz_UVW uz_inverse_dq_Transformation(struct uz_dq input, float theta_el){
+struct uz_UVW uz_inverse_dq_Transformation(struct uz_dq input, float theta_el_rad){
     struct uz_UVW output = {
         .U = 0.0f,
         .V = 0.0f,
         .W = 0.0f
     };
-    output.U = (cosf(theta_el) * input.d) - (sinf(theta_el) * input.q) + input.zero;
-    output.V = (cosf(theta_el -  ( (2.0f / 3.0f) * M_PI) ) * input.d) - (sinf(theta_el - ( (2.0f / 3.0f) * M_PI) ) * input.q) + input.zero;
-    output.W = (cosf(theta_el -  ( (4.0f / 3.0f ) * M_PI) ) * input.d) - (sinf(theta_el - ( (4.0f / 3.0f) * M_PI) ) * input.q) + input.zero;
+    output.U = (cosf(theta_el_rad) * input.d) - (sinf(theta_el_rad) * input.q) + input.zero;
+    output.V = (cosf(theta_el_rad -  ( (2.0f / 3.0f) * M_PI) ) * input.d) - (sinf(theta_el_rad - ( (2.0f / 3.0f) * M_PI) ) * input.q) + input.zero;
+    output.W = (cosf(theta_el_rad -  ( (4.0f / 3.0f ) * M_PI) ) * input.d) - (sinf(theta_el_rad - ( (4.0f / 3.0f) * M_PI) ) * input.q) + input.zero;
     return(output);
 }
 
--- vitis/software/Baremetal/src/uz/uz_Transformation/uz_Transformation.h
+++ vitis/software/Baremetal/src/uz/uz_Transformation/uz_Transformation.h
@@ -1,26 +1,66 @@
 #ifndef UZ_TRANSFORMATION_H
 #define UZ_TRANSFORMATION_H
 
+/**
+ * @brief Struct for the variables of a dq0-System
+ */
 struct uz_dq{
-    float d;
-    float q;
-    float zero;
+    float d;        /**< Amplitude of the direct axis component */ 
+    float q;        /**< Amplitude of the quadrature axis component */ 
+    float zero;     /**< Amplitude of the zero component */ 
 };
 
+/**
+ * @brief Struct for the variables of a alpha-beta-gamma-System
+ * 
+ */
 struct uz_alphabeta{
-    float alpha;
-    float beta;
-    float gamma;
+    float alpha;        /**< Amplitude of the alpha component */ 
+    float beta;         /**< Amplitude of the beta component */ 
+    float gamma;        /**< Amplitude of the gamma component */ 
 };
 
+/**
+ * @brief Struct for the variables of a three-phase-System
+ * 
+ */
 struct uz_UVW{
-    float U;
-    float V;
-    float W;
+    float U;        /**< Amplitude of the U phase */ 
+    float V;        /**< Amplitude of the V phase */ 
+    float W;        /**< Amplitude of the W phase */ 
 };
 
-struct uz_dq uz_dq_Transformation(struct uz_UVW input, float theta_el);
-struct uz_UVW uz_inverse_dq_Transformation(struct uz_dq input, float theta_el);
+/**
+ * @brief Calculates the dq0-components from the UVW-Phases
+ * 
+ * @param input uz_UVW struct
+ * @param theta_el_rad electrical theta in rad
+ * @return struct uz_dq Outputs the calculated dq0-components
+ */
+struct uz_dq uz_dq_Transformation(struct uz_UVW input, float theta_el_rad);
+
+/**
+ * @brief Calculates the UVW-Phases from the dq0-components  
+ * 
+ * @param input uz_dq struct
+ * @param theta_el_rad electrical theta in rad
+ * @return struct uz_UVW Outputs the calculated UVW-phases
+ */
+struct uz_UVW uz_inverse_dq_Transformation(struct uz_dq input, float theta_el_rad);
+
+/**
+ * @brief Calculates the alpha-beta-gamma-components from the UVW-phases
+ * 
+ * @param input uz_UVW struct
+ * @return struct uz_alphabeta Outputs the calculated alpha-beta-gamma-components
+ */
 struct uz_alphabeta uz_clarke_Transformation(struct uz_UVW input);
+
+/**
+ * @brief Calculates the UVW-phases from the alpha-beta-gamma-components
+ * 
+ * @param input uz_alphabeta struct
+ * @return struct uz_UVW Outputs the calculated UVW-phases
+ */
 struct uz_UVW uz_inverse_clarke_Transformation(struct uz_alphabeta input);
 #endif // UZ_TRANSFORMATION_H

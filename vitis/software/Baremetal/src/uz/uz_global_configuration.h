e80525c5ff179cc5a27457ce64114bd095ba8cc8
Sven Rosenkranz <rosenkranzsv67485@th-nuernberg.de>
2021-07-22 18:15:21 +0200

    fixed wrong config

--- vitis/software/Baremetal/src/uz/uz_global_configuration.h
+++ vitis/software/Baremetal/src/uz/uz_global_configuration.h
@@ -1,7 +1,7 @@
 #ifndef TEST
 
 // Hardware version of the UltraZohm
-#define UZ_HARDWARE_VERSION 4U
+#define UZ_HARDWARE_VERSION 3U
 
 /** ISR trigger source
  *
@@ -14,7 +14,7 @@
  * 5 for Interrupt_3L_center		
  * 6 for Interrupt_timer_fcc		
 */
-#define Interrupt_ISR_source_user_choice        3U
+#define Interrupt_ISR_source_user_choice        0U
 
 // Configuration defines for the number of used instances for testing with ceedling
 #define UZ_WAVEGEN_CHIRP_MAX_INSTANCES          2U

1a4fe9e742f46f5da203c1a276f263538f6b6db2
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-07-23 13:53:49 +0200

    Adds setup function to unit tests

--- vitis/software/Baremetal/test/uz/uz_FOC/test_uz_FOC.c
+++ vitis/software/Baremetal/test/uz/uz_FOC/test_uz_FOC.c
@@ -8,95 +8,87 @@ TEST_FILE("uz_signals.c")
 
 
 uz_PI_Controller_config config_id = {0};
-void setUp_PI_Controller_id(void)
+uz_PI_Controller_config config_iq = {0};
+uz_PI_Controller_config config_n = {0};
+uz_FOC_config config_FOC = {0};
+void setUp(void)
 {
     config_id.Kp = 6.75f;
     config_id.Ki = 2000.0f;
     config_id.samplingTime_sec = 0.0001f;
     config_id.upper_limit = 10.0f;
     config_id.lower_limit = -10.0f;
-}
-
-uz_PI_Controller_config config_iq = {0};
-void setUp_PI_Controller_iq(void)
-{
     config_iq.Kp = 6.75f;
     config_iq.Ki = 2000.0f;
     config_iq.samplingTime_sec = 0.0001f;
     config_iq.upper_limit = 10.0f;
     config_iq.lower_limit = -10.0f;
-}
-
-uz_PI_Controller_config config_n = {0};
-void setUp_PI_Controller_n(void)
-{
     config_n.Kp = 0.00864f,
     config_n.Ki = 0.0864f,
     config_n.samplingTime_sec = 0.0001f;
     config_n.upper_limit = 10.0f;
     config_n.lower_limit = -10.0f;
+    config_FOC.iq_ref_Ampere = 1.0;
+    config_FOC.id_ref_Ampere = 1.0f;
+    config_FOC.n_ref_rpm = 1000.0f;
+    config_FOC.FOC_Select = 1U;
+    config_FOC.polePairs = 4.0f;
+    config_FOC.L_q = 0.00027f;
+    config_FOC.L_d = 0.00027f;
+    config_FOC.psi_pm = 0.0082;
+    config_FOC.Reset = false;
 }
 
-uz_FOC_config config_FOC = {
-    .iq_ref_Ampere = 1.0f,
-    .id_ref_Ampere = 1.0f,
-    .n_ref_rpm = 1000.0f,
-    .FOC_Select = 1U,
-    .polePairs = 4.0f,
-    .L_q = 0.00027f,
-    .L_d = 0.00027f,
-    .psi_pm = 0.0082,
-    .Reset = false
-};
-
 void test_uz_FOC_init_L_d_negative(void){
-    setUp_PI_Controller_n();
-    setUp_PI_Controller_id();
-    setUp_PI_Controller_iq();
+    setUp();
     config_FOC.L_d = -0.1f;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
-    }
+}
 
 void test_uz_FOC_init_L_d_zero(void){
+    setUp();
     config_FOC.L_d = 0.0f;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
-    config_FOC.L_d = 0.00027f;
 }
 
 void test_uz_FOC_init_L_q_negative(void){
+    setUp();
     config_FOC.L_q = -0.1f;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
 }
 
 void test_uz_FOC_init_L_q_zero(void){
+    setUp();
     config_FOC.L_q = 0.0f;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
-    config_FOC.L_q = 0.00027f;
 }
 
 void test_uz_FOC_init_psi_pm_negative(void){
+    setUp();
     config_FOC.psi_pm = -0.1f;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
 }
 
 void test_uz_FOC_init_psi_pm_zero(void){
+    setUp();
     config_FOC.psi_pm = 0.0f;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
-    config_FOC.psi_pm = 0.0082f;
 }
 
 void test_uz_FOC_init_polePairs_negative(void){
+    setUp();
     config_FOC.polePairs = -2.0f;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
 }
 
 void test_uz_FOC_init_polePairs_zero(void){
+    setUp();
     config_FOC.polePairs = 0.0f;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
-    config_FOC.polePairs = 4.0f;
 }
 
 void test_uz_FOC_init_FOC_select_too_big(void){
+    setUp();
     config_FOC.FOC_Select = 20U;
     TEST_ASSERT_FAIL_ASSERT(uz_FOC_init(config_FOC,config_id, config_iq, config_n));
 }
@@ -122,9 +114,7 @@ void test_uz_FOC_linear_decouppling_FOC_NULL(void){
 }
 
 void test_uz_FOC_linear_decouppling_values_NULL(void){
-    setUp_PI_Controller_id();
-    setUp_PI_Controller_iq();
-    setUp_PI_Controller_n();
+    setUp();
     uz_FOC* FOC = uz_FOC_init(config_FOC,config_id, config_iq, config_n);
     float u_d_vor = 0.0f;
     float u_q_vor = 0.0f;
@@ -146,9 +136,7 @@ void test_uz_FOC_linear_decouppling_u_q_vor_NULL(void){
 }
 
 void test_uz_FOC_linear_decouppling_output(void){
-    setUp_PI_Controller_id();
-    setUp_PI_Controller_iq();
-    setUp_PI_Controller_n();
+    setUp();
 	//Values for comparision from simulation
 	uz_FOC_ActualValues* values = uz_FOC_ActualValues_init();
     uz_FOC* FOC = uz_FOC_init(config_FOC,config_id, config_iq, config_n);

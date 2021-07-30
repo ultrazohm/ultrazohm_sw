b9bf25c6acf5c7af569296476daa5d3357633ba0
schindlerto <tobias.schindler@th-nuernberg.de>
2021-07-30 11:30:47 +0000

    Updates script names

--- ip_cores/uz_pmsm_model/compare_simulation_to_measurement.m
+++ ip_cores/uz_pmsm_model/compare_simulation_to_measurement.m
@@ -3,52 +3,55 @@ clc
 clear
 close all
 set(0,'defaulttextinterpreter','latex')
+plot_figures=false;
+
+
 uz_pmsm_model_init_parameter;
+% Adjust simulation settings
+simulate_mechanical=true;
+simulate_open_loop=true;
+setpoint_step=10.0;
+
+if simulate_open_loop==true
+measruement_csv_name='open_loop_measruement.csv';
+else
+    measruement_csv_name='closed_loop_measruement.csv';
+end
+
+
+%
 mdl='uz_pmsm_model';
 out=sim(mdl);
 
 %% Plot Simulink
-
-
-%                         Name                   BlockPath                            
-%                         _____________________  ____________________________________ 
-%    1  [1x1 Signal]      u_d                    uz_pmsm_model/Data Type Conversion  
-%    2  [1x1 Signal]      u_q                    uz_pmsm_model/Data Type Conversion1 
-%    3  [1x1 Signal]      load_torque            uz_pmsm_model/Data Type Conversion15
-%    4  [1x1 Signal]      i_d_hdl                uz_pmsm_model/Demux                 
-%    5  [1x1 Signal]      i_q_hdl                uz_pmsm_model/Demux                 
-%    6  [1x1 Signal]      omega_mech_output_hdl  uz_pmsm_model/Demux                 
-%    7  [1x1 Signal]      torque_hdl             uz_pmsm_model/Demux                 
-%    8  [1x1 Signal]      i_d_ref                uz_pmsm_model/PMSM_flux_based       
-%    9  [1x1 Signal]      i_q_ref                uz_pmsm_model/PMSM_flux_based       
-%   10  [1x1 Signal]      ''                     uz_pmsm_model/Rate Transition7      
-%   11  [1x1 Signal]      u_d_input              uz_pmsm_model/Step2                 
-%   12  [1x1 Signal]      u_q_input              uz_pmsm_model/Step3                 
-%   13  [1x1 Signal]      error_i_d              uz_pmsm_model/Sum                   
-%   14  [1x1 Signal]      error_i_q              uz_pmsm_model/Sum1   
+%                         Name                   BlockPath
+%                         _____________________  ____________________________________
+%    1  [1x1 Signal]      u_d                    uz_pmsm_model/Data Type Conversion
+%    2  [1x1 Signal]      u_q                    uz_pmsm_model/Data Type Conversion1
+%    3  [1x1 Signal]      i_q_soll               uz_pmsm_model/Data Type Conversion10
+%    4  [1x1 Signal]      load_torque            uz_pmsm_model/Data Type Conversion15
+%    5  [1x1 Signal]      torque_hdl             uz_pmsm_model/Data Type Conversion5
+%    6  [1x1 Signal]      i_d_hdl                uz_pmsm_model/Data Type Conversion6
+%    7  [1x1 Signal]      i_q_hdl                uz_pmsm_model/Data Type Conversion7
+%    8  [1x1 Signal]      omega_mech_output_hdl  uz_pmsm_model/Data Type Conversion8
+%    9  [1x1 Signal]      i_d_soll               uz_pmsm_model/Data Type Conversion9
+%   10  [1x1 Signal]      ''                     uz_pmsm_model/PID Controller
+%   11  [1x1 Signal]      ''                     uz_pmsm_model/PID Controller1
+%   12  [1x1 Signal]      iq_regler              uz_pmsm_model/Rate Transition11
+%   13  [1x1 Signal]      id_regler              uz_pmsm_model/Rate Transition12
+%   14  [1x1 Signal]      u_q_input              uz_pmsm_model/Step1
+%   15  [1x1 Signal]      i_d                    uz_pmsm_model/uz_pmsm_model/pmsm
 sim.ud=out.logsout{1}.Values.Data;
 sim.uq=out.logsout{2}.Values.Data;
-sim.id=out.logsout{4}.Values.Data;
-sim.iq=out.logsout{5}.Values.Data;
-sim.speed=out.logsout{6}.Values.Data;
-sim.time=out.logsout{1}.Values.Time-0.000001;
-
-figure(1)
-subplot(2,1,1)
-title('Simulation results');
-grid on
-hold on
-
-plot(sim.time,sim.ud); % u_d input
-plot(sim.time,sim.id); % i_d
-legend('U_d', 'i_d');
-
-subplot(2,1,2)
-grid on
-hold on
-plot(sim.time,sim.uq); % u_d input
-plot(sim.time,sim.id); % i_d
-legend('U_q', 'i_q');
+sim.id=out.logsout{6}.Values.Data;
+sim.iq=out.logsout{7}.Values.Data;
+sim.speed=out.logsout{8}.Values.Data;
+sim.time=out.logsout{8}.Values.Time-0.00005; % Some magic number adjustments to align the steps
+sim.time=sim.time*1e3-199.8;
+sim.id_soll=out.logsout{9}.Values.Data;
+% There are two 'times' since one is 2 MHz and one is 20 kHz
+sim.id_soll_time=out.logsout{9}.Values.Time*1e3-199.8; % Scale time to ms and move to one time step before set-point step happens
+sim.iq_soll=out.logsout{3}.Values.Data;
 
 %% Plot FPGA version
 %
@@ -57,35 +60,108 @@ legend('U_q', 'i_q');
 % 3: i_d
 % 4: speed
 % 5: ud
-A=readmatrix('measurment_with_speed.csv');
-first_nonzero=find(measurements(:,5),1,'first');
-first_nonzero=first_nonzero;
+measurements=readmatrix(measruement_csv_name);
+first_nonzero=find(measurements(:,5),1,'first'); % Finds first nonzero in set-point to adjust time between measruement and simulation
 time_offset=measurements(first_nonzero,1)-0.2;
 measurements(:,1)=measurements(:,1)-time_offset;
 measurment.id=measurements(first_nonzero:end,3);
 measurment.iq=measurements(first_nonzero:end,2);
-measurment.time=measurements(first_nonzero:end,1);
-measurment.ud=measurements(first_nonzero:end,5);
+measurment.time=measurements(first_nonzero:end,1)*1e3-199.8; % Scale time to ms and move to one time step before set-point step happens
+measurment.sollwert=measurements(first_nonzero:end,5);
 measurment.speed=measurements(first_nonzero:end,4);
-measurment.uq=-measurment.ud;
+
+%% Matlab2Tikz options
+
+if plot_figures
+    extraaxisoptions = ['y tick label style={/pgf/number format/.cd, fixed,precision=2}'...
+        ,',ylabel style={yshift=-0.2cm,font=\small}'...
+        ,',xlabel style={font=\small}'...
+        ];
+    PPI = 600;
+end
+%% Compare for OPTIM Paper
+close all
+figure
+grid on
+hold on
+box on
+stairs(sim.id_soll_time,sim.id_soll,'Color', [0 127/255 0]); % u_d input
+plot(measurment.time, measurment.id,'rx'); % i_d
+plot(sim.time,sim.id,'b'); % i_d
+
+xlabel('Time (ms)');
+%ylabel('Current (A)');
+xlim([0.15 1.5]);
+ylim([-1.2 0]);
+title('')
+
+if plot_figures
+    cleanfigure('targetResolution',300);
+    matlab2tikz('id_step.tikz','showInfo', false,   ...
+        'width', '\fwidth', 'height', '\fheight', 'extraaxisoptions', extraaxisoptions);
+end
+
+figure
+grid on
+hold on
+box on
+stairs(sim.id_soll_time,sim.iq_soll,'Color', [0 127/255 0]); % u_d input
+plot(sim.time,sim.iq,'b'); % i_d
+plot(measurment.time, measurment.iq,'rx'); % i_d
+xlabel('Time (ms)');
+%ylabel('Current (A)');
+xlim([0.15 1.5]);
+ylim([-0.001 1.2]);
+title('')
+
+if plot_figures
+    cleanfigure('targetResolution',600);
+    matlab2tikz('iq_step.tikz','showInfo', false,  ...
+        'width', '\fwidth', 'height', '\fheight', 'extraaxisoptions', extraaxisoptions);
+end
+% calculate error
+sim_id=timeseries(sim.id,sim.time);
+meas_id=timeseries(measurment.id,measurment.time);
+[sim_id,meas_id]=synchronize(sim_id,meas_id,'union');
+
+sim_iq=timeseries(sim.iq,sim.time);
+meas_iq=timeseries(measurment.iq,measurment.time);
+[sim_iq,meas_iq]=synchronize(sim_iq,meas_iq,'union');
+
+sim_omega=timeseries(sim.speed,sim.time);
+meas_omega=timeseries(measurment.speed,measurment.time);
+[sim_omega,meas_omega]=synchronize(sim_omega,meas_omega,'union');
+
+abs_error_id=( sim_id-meas_id );
+abs_error_iq=( sim_iq-meas_iq );
+abs_error_omega=( sim_omega-meas_omega );
 %%
-figure(2)
-subplot(2,1,1)
-title('Measured values');
+figure
+plot(abs_error_id,'r');
 grid on
 hold on
+xlabel('Time (ms)');
+%ylabel('Current (A)');
+plot(abs_error_iq,'b');
+%legend('i_d error','i_q error','Location','eastoutside');
+xlabel('Time (ms)');
+ylabel('');
+xlim([0.199 5]);
+ylim([-4 4]*1e-3);
+title('')
 
-plot(measurment.time, measurment.ud); % u_d input
-plot(measurment.time, measurment.id); % i_d
-legend('U_d', 'i_d');
+if plot_figures
+    extraaxisoptions = ['scaled y ticks = true',...
+    'y tick label style={/pgf/number format/.cd, fixed,precision=2}'...
+    ,',ylabel style={yshift=-0.2cm,font=\small}'...
+    ,',xlabel style={font=\small}'...
+    ];
+    cleanfigure('targetResolution',600);
+    matlab2tikz('step_error.tikz','showInfo', false,  ...
+        'width', '\fwidth', 'height', '\fheight', 'extraaxisoptions', extraaxisoptions);
+end
 
-subplot(2,1,2)
-grid on
-hold on
-plot(measurment.time, measurment.uq); % u_d input
-plot(measurment.time, measurment.iq); % i_d
-legend('U_q', 'i_q');
-
+%%
 %% Compare
 f=figure(3);
 f.Position(3:4) = 2*[1024 768];
@@ -93,29 +169,25 @@ subplot(2,3,1)
 title('Compare simulation and measurement');
 grid on
 hold on
-plot(sim.time,sim.ud,'LineWidth',3); % u_d input
 plot(sim.time,sim.id,'LineWidth',3); % i_d
-plot(measurment.time, measurment.ud,'--','LineWidth',3); % u_d input
 plot(measurment.time, measurment.id,'--','LineWidth',3); % i_d
-legend('U_d sim', 'i_d sim','u_d meas','i_d meas', 'FontSize', 18);
-xlim([0.19 2]);
+legend('i_d sim','i_d meas', 'FontSize', 18,'Location','south');
+xlim([0.19 200]);
 title('Step Response $I_d$', 'FontSize', 20);
-xlabel('Time in s', 'FontSize', 18);
-ylabel('Current in A, Voltage in V', 'FontSize', 18);
+xlabel('Time (ms)', 'FontSize', 18);
+ylabel('Current (A)', 'FontSize', 18);
 set(gca,'fontsize',20);
 
 subplot(2,3,2)
 grid on
 hold on
-plot(sim.time,sim.uq,'LineWidth',3); % u_d input
 plot(sim.time,sim.iq,'LineWidth',3); % i_d
-plot(measurment.time, measurment.uq,'--','LineWidth',3); % u_d input
 plot(measurment.time, measurment.iq,'--','LineWidth',3); % i_d
-legend('U_q sim', 'i_q sim','u_q meas','i_q meas', 'FontSize', 18);
-xlim([0.19 2]);
+legend('i_q sim','i_q meas', 'FontSize', 18);
+xlim([0.19 200]);
 title('Step Response $I_q$', 'FontSize', 20);
-xlabel('Time in s', 'FontSize', 18);
-ylabel('Current in A, Voltage in V', 'FontSize', 18);
+xlabel('Time (ms)', 'FontSize', 18);
+ylabel('Current (A)', 'FontSize', 18);
 set(gca,'fontsize',20);
 
 subplot(2,3,3)
@@ -123,12 +195,11 @@ grid on
 hold on
 plot(sim.time,sim.speed,'LineWidth',3);
 plot(measurment.time, measurment.speed,'--','LineWidth',3);
-%legend('U_q sim', 'i_q sim','u_q meas','i_q meas');
-xlim([0.19 2]);
+xlim([0.19 200]);
 title('Rotational speed', 'FontSize', 20);
 legend('\omega_{mech} sim','\omega_{mech} meas', 'FontSize', 18);
-xlabel('Time in s', 'FontSize', 18);
-ylabel('Rotational speed in $\frac{1}{s}$', 'FontSize', 18);
+xlabel('Time (ms)', 'FontSize', 18);
+ylabel('Rotational speed ($\frac{1}{s}$)', 'FontSize', 18);
 set(gca,'fontsize',20);
 
 % calculate error
@@ -153,28 +224,29 @@ plot(abs_error_id,'LineWidth',3);
 grid on
 title('Error $I_d$', 'FontSize', 20);
 legend('i_d error', 'FontSize', 18);
-xlabel('Time in s', 'FontSize', 18);
-ylabel('Current in A', 'FontSize', 18);
+xlabel('Time (ms)', 'FontSize', 18);
+ylabel('Current (A)', 'FontSize', 18);
 set(gca,'fontsize',20);
-xlim([0.19 2]);
+xlim([0.19 200]);
 
 subplot(2,3,5)
 plot(abs_error_iq,'LineWidth',3);
 grid on
 title('Error $I_q$', 'FontSize', 20);
 legend('i_q error', 'FontSize', 18);
-xlabel('Time in s', 'FontSize', 18);
-ylabel('Current in A', 'FontSize', 18);
+xlabel('Time (ms)', 'FontSize', 18);
+ylabel('Current (A)', 'FontSize', 18);
 set(gca,'fontsize',20);
-xlim([0.19 2]);
+xlim([0.19 200]);
 
 subplot(2,3,6)
 plot(abs_error_omega,'LineWidth',3);
+set(0,'defaulttextinterpreter','latex')
 grid on
 title('Rotational speed', 'FontSize', 20);
 legend('\omega_{mech} error', 'FontSize', 18);
-xlabel('Time in s', 'FontSize', 18);
+xlabel('Time (ms)', 'FontSize', 18);
 ylabel('Rotational speed in $\frac{1}{s}$', 'FontSize', 18);
-xlim([0.19 2]);
+xlim([0.19 200]);
 set(gca,'fontsize',20);
 saveas(gcf,'referance_ip_core_measurment_comparision','svg');

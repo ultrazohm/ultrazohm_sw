5114af824ffb88668a632ba10f5f794db7ee9a7d
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-08-04 08:58:41 +0200

    Small refactor of SVL

--- vitis/software/Baremetal/src/uz/uz_FOC/uz_space_vector_limitation.c
+++ vitis/software/Baremetal/src/uz/uz_FOC/uz_space_vector_limitation.c
@@ -9,28 +9,28 @@ struct uz_dq_t uz_FOC_SpaceVector_Limitation(struct uz_dq_t u_dq_ref_Volts, floa
 	*ext_clamping = false;
 	struct uz_dq_t u_dq_limit_Volts = {0};
 
-  	float U_RZ_max =U_zk_Volts / sqrtf(3.0f);
-	float U_RZ_betrag = sqrtf(u_dq_ref_Volts.d * u_dq_ref_Volts.d + u_dq_ref_Volts.q * u_dq_ref_Volts.q);
+  	float U_SV_max =U_zk_Volts / sqrtf(3.0f);
+	float U_SV_abs = sqrtf(u_dq_ref_Volts.d * u_dq_ref_Volts.d + u_dq_ref_Volts.q * u_dq_ref_Volts.q);
 
-	if ( U_RZ_betrag > U_RZ_max ){
+	if ( U_SV_abs > U_SV_max ){
 		*ext_clamping = true;
 		if ((uz_signals_get_sign_of_value(omega_el_rad_per_sec) == uz_signals_get_sign_of_value(i_dq_meas_Ampere.q))) {
-			if ((fabsf(u_dq_ref_Volts.d) > 0.95f * U_RZ_max)) {
-				u_dq_limit_Volts.d = uz_signals_get_sign_of_value(u_dq_ref_Volts.d) * 0.95f * U_RZ_max;
-				u_dq_limit_Volts.q = uz_signals_get_sign_of_value(u_dq_ref_Volts.q) * sqrtf( (U_RZ_max * U_RZ_max) - (u_dq_limit_Volts.d * u_dq_limit_Volts.d) );
+			if ((fabsf(u_dq_ref_Volts.d) > 0.95f * U_SV_max)) {
+				u_dq_limit_Volts.d = uz_signals_get_sign_of_value(u_dq_ref_Volts.d) * 0.95f * U_SV_max;
+				u_dq_limit_Volts.q = uz_signals_get_sign_of_value(u_dq_ref_Volts.q) * sqrtf( (U_SV_max * U_SV_max) - (u_dq_limit_Volts.d * u_dq_limit_Volts.d) );
 			} else {
 				u_dq_limit_Volts.d = u_dq_ref_Volts.d;
-				u_dq_limit_Volts.q = uz_signals_get_sign_of_value(u_dq_ref_Volts.q) * sqrtf( (U_RZ_max * U_RZ_max) - (u_dq_limit_Volts.d * u_dq_limit_Volts.d) );
+				u_dq_limit_Volts.q = uz_signals_get_sign_of_value(u_dq_ref_Volts.q) * sqrtf( (U_SV_max * U_SV_max) - (u_dq_limit_Volts.d * u_dq_limit_Volts.d) );
 			}
 
 
 	     } else if ((uz_signals_get_sign_of_value(omega_el_rad_per_sec) != uz_signals_get_sign_of_value(i_dq_meas_Ampere.q))) {
-			if (fabsf(u_dq_ref_Volts.q) > 0.95f * U_RZ_max) {
-				u_dq_limit_Volts.q = uz_signals_get_sign_of_value(u_dq_ref_Volts.q) * 0.95f * U_RZ_max;
-				u_dq_limit_Volts.d = uz_signals_get_sign_of_value(u_dq_ref_Volts.d) * sqrtf((U_RZ_max * U_RZ_max) - (u_dq_limit_Volts.q * u_dq_limit_Volts.q));
+			if (fabsf(u_dq_ref_Volts.q) > 0.95f * U_SV_max) {
+				u_dq_limit_Volts.q = uz_signals_get_sign_of_value(u_dq_ref_Volts.q) * 0.95f * U_SV_max;
+				u_dq_limit_Volts.d = uz_signals_get_sign_of_value(u_dq_ref_Volts.d) * sqrtf((U_SV_max * U_SV_max) - (u_dq_limit_Volts.q * u_dq_limit_Volts.q));
 			} else {
 				u_dq_limit_Volts.q = u_dq_ref_Volts.q;
-				u_dq_limit_Volts.d = uz_signals_get_sign_of_value(u_dq_ref_Volts.d) * sqrtf((U_RZ_max * U_RZ_max) - (u_dq_limit_Volts.q * u_dq_limit_Volts.q));
+				u_dq_limit_Volts.d = uz_signals_get_sign_of_value(u_dq_ref_Volts.d) * sqrtf((U_SV_max * U_SV_max) - (u_dq_limit_Volts.q * u_dq_limit_Volts.q));
 			}
 		} else {
 			u_dq_limit_Volts.d = 0.0f;

7c19021a0b3a9f31e1223f597ef473a7a8b55ebd
Sven Rosenkranz <rosenkranzsv67485@th-nuernberg.de>
2021-08-17 14:17:26 +0200

    Changed current calculation for TFM (4windings, changed offset). FieldWeakening, VoltageLimitation, Decoupling und AnglePrediction default changed to on

--- vitis/software/Baremetal/src/sw/isr.c
+++ vitis/software/Baremetal/src/sw/isr.c
@@ -72,9 +72,9 @@ void ISR_Control(void *data)
 	Encoder_UpdateSpeedPosition(&Global_Data); 	//Read out speed and theta angle
 
 	//FOC_Strom Assign Signal-Values
-	codegenInstance.input.Act_Iu = (Global_Data.aa.A2.me.ADC_A1-2.5) * 80.0F/2.0F - 0.25F;		//A
-	codegenInstance.input.Act_Iv = (Global_Data.aa.A2.me.ADC_A2-2.5) * 80.0F/2.0F + 0.35F;		//A
-	codegenInstance.input.Act_Iw = (Global_Data.aa.A2.me.ADC_A3-2.5) * 80.0F/2.0F - 0.43F;		//A
+	codegenInstance.input.Act_Iu = (Global_Data.aa.A2.me.ADC_A1-2.5) * 80.0F/4.0F - 0.27F;		//A
+	codegenInstance.input.Act_Iv = (Global_Data.aa.A2.me.ADC_A2-2.5) * 80.0F/4.0F + 0.20F;		//A
+	codegenInstance.input.Act_Iw = (Global_Data.aa.A2.me.ADC_A3-2.5) * 80.0F/4.0F - 0.10F;		//A
 	codegenInstance.input.Act_U_ZK = Global_Data.aa.A2.me.ADC_A4 * 12.5F;			//V
 
 	codegenInstance.input.Act_n = Global_Data.av.mechanicalRotorSpeed; 				//[RPM]
@@ -107,6 +107,8 @@ void ISR_Control(void *data)
 	CheckForErrors();
 
 	//FOC_Strom Output
+
+
 	Global_Data.rasv.halfBridge1DutyCycle = codegenInstance.output.a_U;
 	Global_Data.rasv.halfBridge2DutyCycle = codegenInstance.output.a_V;
 	Global_Data.rasv.halfBridge3DutyCycle = codegenInstance.output.a_W;
@@ -166,10 +168,10 @@ int Initialize_ISR(){
 	codegenInstance.input.fl_power = 0U;
 	codegenInstance.input.fl_control_type = 0U;
 	codegenInstance.input.fl_integrator_reset = 0U;
-	codegenInstance.input.fl_field_weakening = 0U;
-	codegenInstance.input.fl_voltage_limitation = 0U;
-	codegenInstance.input.fl_decoupling = 0U;
-	codegenInstance.input.fl_angle_prediction = 0U;
+	codegenInstance.input.fl_field_weakening = 1U;
+	codegenInstance.input.fl_voltage_limitation = 2U;
+	codegenInstance.input.fl_decoupling = 1U;
+	codegenInstance.input.fl_angle_prediction = 1U;
 	codegenInstance.input.Ref_n = 0.0F;
 	codegenInstance.input.Ref_Id_ext = 0.0F;
 	codegenInstance.input.Ref_Iq_ext = 0.0F;

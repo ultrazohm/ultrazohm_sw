6602df7f7e3270099e19244575343d3b2fd06037
Tobias <tobias.schindler@th-nuernberg.de>
2021-09-08 16:01:01 +0200

    deletes sandbox for ceedling - not required anymore due to ceedling beeing integrated into the uz software

--- vitis/Sandbox/src/uz_ipCoreDriver_template/uz_ipCore_private.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include <stdbool.h>
-struct uz_ipCore{
-  const uint32_t base_address;
-  const uint32_t ip_clk_frequency_Hz;
-  bool is_ready;
-  int variable;
-};

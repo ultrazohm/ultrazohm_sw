c8041f92c9057ecc3109fb83bd8ccde1a2a324fd
Tobias <tobias.schindler@th-nuernberg.de>
2021-09-13 18:05:55 +0000

    Adjusts _hw part of incEncoder driver to handle fixed point conversion

--- vitis/software/Baremetal/src/IP_Cores/uz_incrementalEncoder/uz_incrementalEncoder_hw.h
+++ vitis/software/Baremetal/src/IP_Cores/uz_incrementalEncoder/uz_incrementalEncoder_hw.h
@@ -4,15 +4,15 @@
 #include "../../uz/uz_HAL.h"
 #include "../../uz/uz_AXI.h"
 
-void uz_incrementalEncoder_hw_set_timer_fpga_ms(uint32_t base_address, uint32_t timer_ms);
-void uz_incrementalEncoder_hw_set_pi2_inc(uint32_t base_address, uint32_t pi2_inc);
+void uz_incrementalEncoder_hw_set_timer_fpga_ms(uint32_t base_address, float timer_ms);
+void uz_incrementalEncoder_hw_set_pi2_inc(uint32_t base_address, float pi2_inc);
 void uz_incrementalEncoder_hw_set_increments_per_turn_mechanical(uint32_t base_address, uint32_t increments_per_turn);
 void uz_incrementalEncoder_hw_set_increments_per_turn_electric(uint32_t base_address, uint32_t increments_per_turn);
-void uz_incrementalEncoder_hw_set_omegaPerOverSampl(uint32_t base_address,uint32_t omega_per_over_sampl); 
+void uz_incrementalEncoder_hw_set_omegaPerOverSampl(uint32_t base_address,float omega_per_over_sampl); 
 
-uint32_t uz_incrementalEncoder_hw_get_omega(uint32_t base_address);
-uint32_t uz_incrementalEncoder_hw_get_direction(uint32_t base_address);
-uint32_t uz_incrementalEncoder_hw_get_theta_electric(uint32_t base_address);
+float uz_incrementalEncoder_hw_get_omega(uint32_t base_address);
+float uz_incrementalEncoder_hw_get_direction(uint32_t base_address);
+float uz_incrementalEncoder_hw_get_theta_electric(uint32_t base_address);
 uint32_t uz_incrementalEncoder_hw_get_position(uint32_t base_address);
 uint32_t uz_incrementalEncoder_hw_get_counterPerPeriod(uint32_t base_address);
 
--- vitis/software/Baremetal/test/IP_Cores/uz_incrementalEncoder/test_uz_incrementalEncoder_hw.c
+++ vitis/software/Baremetal/test/IP_Cores/uz_incrementalEncoder/test_uz_incrementalEncoder_hw.c
@@ -16,18 +16,21 @@ void tearDown(void)
 {
 }
 
-void test_uz_incrementalEncoder_hw_set_timer_fpga_ms(void)
-{
-    uint32_t expected=10;
-    uz_axi_write_uint32_Expect(BASE_ADDRESS+Timer_FPGA_ms_AXI4_Data_IncreEncoder_V24_ip,expected);
-    uz_incrementalEncoder_hw_set_timer_fpga_ms(BASE_ADDRESS,expected);
-}
-
 void test_uz_incrementalEncoder_hw_set_pi2_inc(void)
 {
-    uint32_t expected=1123;
-    uz_axi_write_uint32_Expect(BASE_ADDRESS+PI2_Inc_AXI4_Data_IncreEncoder_V24_ip,expected);
+    float expected=0.0012566447257996; //  (2*pi/(IncPerTurn*QuadratureFactor))*PolePair from Simulink model
+    uz_axi_write_uint32_Expect(BASE_ADDRESS+PI2_Inc_AXI4_Data_IncreEncoder_V24_ip, uz_convert_float_to_unsigned_fixed(expected,24) ); // Data type is ufix24_En24
+    TEST_ASSERT_EQUAL_HEX32(0x00525B,uz_convert_float_to_unsigned_fixed(expected,24) ); // 0x00525B is taken with a "display" from simulink using "hex (Stored Integer)" numeric display format 
     uz_incrementalEncoder_hw_set_pi2_inc(BASE_ADDRESS,expected);
+    
+}
+
+void test_uz_incrementalEncoder_hw_set_timer_fpga_ms(void)
+{
+    float expected=1.5915604308248e-5; //  (T_50MHz*IncPerTurn)/(2*pi) from Simulink model
+    uz_axi_write_uint32_Expect(BASE_ADDRESS+Timer_FPGA_ms_AXI4_Data_IncreEncoder_V24_ip, uz_convert_float_to_unsigned_fixed(expected,32) ); // Data type is ufix32_En32
+    TEST_ASSERT_EQUAL_HEX32(0x00010B05,uz_convert_float_to_unsigned_fixed(expected,32) ); // 0x00010B05 is taken with a "display" from simulink using "hex (Stored Integer)" numeric display format 
+    uz_incrementalEncoder_hw_set_timer_fpga_ms(BASE_ADDRESS, expected );
 }
 
 void test_uz_incrementalEncoder_hw_set_incPerTurn_mech(void){
@@ -43,30 +46,31 @@ void test_uz_incrementalEncoder_hw_set_incPerTurn_elec(void){
 }
 
 void test_uz_incrementalEncoder_hw_set_omegaPerOverSampl(void){
-    uint32_t expected=444;
-    uz_axi_write_uint32_Expect(BASE_ADDRESS+OmegaPerOverSampl_AXI4_Data_IncreEncoder_V24_ip,expected);
+    float expected=52.35986328125; // from Simulink model
+    uz_axi_write_int32_Expect(BASE_ADDRESS+OmegaPerOverSampl_AXI4_Data_IncreEncoder_V24_ip,uz_convert_float_to_sfixed(expected,11)); // datatype sfix24_en11
+    TEST_ASSERT_EQUAL_HEX32(0x01A2E1, uz_convert_float_to_sfixed(expected,11));
     uz_incrementalEncoder_hw_set_omegaPerOverSampl(BASE_ADDRESS,expected);
 }
 
 void test_uz_incrementalEncoder_hw_get_omega(void){
-    uint32_t expected_return_value=123;
-    uz_axi_read_uint32_ExpectAndReturn(BASE_ADDRESS+omega_AXI4_Data_IncreEncoder_V24_ip,expected_return_value);
-    uint32_t actual_return_value=uz_incrementalEncoder_hw_get_omega(BASE_ADDRESS);
-    TEST_ASSERT_EQUAL_UINT32(expected_return_value,actual_return_value);
+    float expected_return_value=52.3;
+    uz_axi_read_int32_ExpectAndReturn(BASE_ADDRESS+omega_AXI4_Data_IncreEncoder_V24_ip,uz_convert_float_to_sfixed(expected_return_value,11)); // datatype sfix24_en11
+    float actual_return_value=uz_incrementalEncoder_hw_get_omega(BASE_ADDRESS);
+    TEST_ASSERT_EQUAL_FLOAT(expected_return_value,actual_return_value);
 }
 
 void test_uz_incrementalEncoder_hw_get_direction(void){
-    uint32_t expected_return_value=123891239;
-    uz_axi_read_uint32_ExpectAndReturn(BASE_ADDRESS+direction_AXI4_Data_IncreEncoder_V24_ip,expected_return_value);
-    uint32_t actual_return_value=uz_incrementalEncoder_hw_get_direction(BASE_ADDRESS);
-    TEST_ASSERT_EQUAL_UINT32(expected_return_value,actual_return_value);
+    float expected_return_value=-1.0;
+    uz_axi_read_int32_ExpectAndReturn(BASE_ADDRESS+direction_AXI4_Data_IncreEncoder_V24_ip,uz_convert_float_to_sfixed(expected_return_value,11)); // data type is sfix24_en11
+    float actual_return_value=uz_incrementalEncoder_hw_get_direction(BASE_ADDRESS);
+    TEST_ASSERT_EQUAL_FLOAT(expected_return_value,actual_return_value);
 }
 
 void test_uz_incrementalEncoder_hw_get_theta_electric(void){
-    uint32_t expected_return_value=22;
-    uz_axi_read_uint32_ExpectAndReturn(BASE_ADDRESS+theta_el_AXI4_Data_IncreEncoder_V24_ip,expected_return_value);
-    uint32_t actual_return_value=uz_incrementalEncoder_hw_get_theta_electric(BASE_ADDRESS);
-    TEST_ASSERT_EQUAL_UINT32(expected_return_value,actual_return_value);
+    float expected_return_value=3.1;
+    uz_axi_read_int32_ExpectAndReturn(BASE_ADDRESS+theta_el_AXI4_Data_IncreEncoder_V24_ip, uz_convert_float_to_sfixed(expected_return_value,20) );
+    float actual_return_value=uz_incrementalEncoder_hw_get_theta_electric(BASE_ADDRESS);
+    TEST_ASSERT_EQUAL_FLOAT(expected_return_value,actual_return_value);
 }
 
 void test_uz_incrementalEncoder_hw_get_position(void){

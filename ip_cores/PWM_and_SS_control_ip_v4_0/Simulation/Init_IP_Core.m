3b6cd346b723f0b6ace3229b0837d2d4cda0777f
hoernermi <michael.hoerner@th-nuernberg.de>
2021-09-30 13:07:12 +0200

    Added IP core V4 of PWM_SS_control and included two instances into the block design

---
+++ ip_cores/PWM_and_SS_control_ip_v4_0/Simulation/Init_IP_Core.m
@@ -0,0 +1,19 @@
+% IP Core PWM and Switching Signal Control
+% Author:   Sebastian Wendel & Barnabas Haucke-Korber
+% Date:     04.09.2018
+% Version:  3.0
+% release notes: First Tests
+% Changed: Added the use for lower PWM frequencies and the Tri-State
+% function
+
+%%
+clc
+clear all
+
+hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath',...
+ 'C:\Xilinx\Vivado\2017.4\bin\vivado.bat'); % Hierdruch wird dem HDL-Coder gesagt welches Synthesetool er für die HDL-Generierung nutzen soll.
+
+
+%% Parameter PWM
+f_FPGA_clk=100e6; %Used FPGA-Clock
+T_FPGA_clk=1/f_FPGA_clk;

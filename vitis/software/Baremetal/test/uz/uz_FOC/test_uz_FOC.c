057f42c702d057dc3c8f477fece35971be4ae0d2
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2021-10-04 10:40:03 +0200

    Adds unit tests for DutyCycle function

--- vitis/software/Baremetal/test/uz/uz_FOC/test_uz_FOC.c
+++ vitis/software/Baremetal/test/uz/uz_FOC/test_uz_FOC.c
@@ -232,4 +232,32 @@ void test_uz_FOC_get_ext_clamping_output(void){
     uz_FOC_sample(instance, i_reference_Ampere, i_actual_Ampere, U_zk_Volts, omega_el_rad_per_sec);
     TEST_ASSERT_EQUAL_INT(1,uz_FOC_get_ext_clamping(instance));
 }
+
+void test_uz_FOC_generate_DutyCycles_output(void) {
+    setUp();
+    //Values for comparision from simulation
+    uz_FOC* instance = uz_FOC_init(config);
+    float theta_el_rad = M_PI;
+    struct uz_UVW_t UVW = uz_FOC_sample_UVW(instance, i_reference_Ampere, i_actual_Ampere, U_zk_Volts, omega_el_rad_per_sec, theta_el_rad);
+    struct uz_DutyCycle_t output = uz_FOC_generate_DutyCycles(UVW, U_zk_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 0.21875f, output.DutyCycle_U);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 0.39705f, output.DutyCycle_V);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 0.88420f, output.DutyCycle_W);
+    theta_el_rad = M_PI * 1.5f;
+    uz_FOC_reset(instance);
+    UVW = uz_FOC_sample_UVW(instance, i_reference_Ampere, i_actual_Ampere, U_zk_Volts, omega_el_rad_per_sec, theta_el_rad);
+    output = uz_FOC_generate_DutyCycles(UVW, U_zk_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-02, 0.78125f, output.DutyCycle_U);
+    TEST_ASSERT_FLOAT_WITHIN(1e-02, 0.1158f, output.DutyCycle_V);
+    TEST_ASSERT_FLOAT_WITHIN(1e-02, 0.6029f, output.DutyCycle_W);
+}
+
+void test_uz_FOC_generate_DutyCycles_limit(void) {
+    setUp();
+    struct uz_UVW_t UVW = {.U = 30.0f, .V = 35.32f, .W = -25.0f};
+    struct uz_DutyCycle_t output = uz_FOC_generate_DutyCycles(UVW, U_zk_Volts);
+    TEST_ASSERT_EQUAL_FLOAT(1.0f, output.DutyCycle_U);
+    TEST_ASSERT_EQUAL_FLOAT(1.0f, output.DutyCycle_V);
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, output.DutyCycle_W);
+}
 #endif // TEST

89d68a49fa92f425f44e6ffd0a79b0b38929718e
Tobias <tobias.schindler@th-nuernberg.de>
2021-11-16 19:17:45 +0000

    Uncomments test

--- vitis/software/Baremetal/test/helper/uz_fixedpoint_helper.c
+++ vitis/software/Baremetal/test/helper/uz_fixedpoint_helper.c
@@ -2,8 +2,7 @@
 #include "uz_fixedpoint_helper.h"
 
 void AssertEqual_uz_fixedpoint_definition_t(uz_fixedpoint_definition_t expected, uz_fixedpoint_definition_t actual, unsigned short line) {
-  TEST_ASSERT_EQUAL(0, 1); // This doesn't fails because this function never called
-//  UNITY_TEST_ASSERT_EQUAL_UINT8(expected.acceptance, actual.acceptance, line, "OtaAcceptance .acceptance comparison failed");
-//  UNITY_TEST_ASSERT_EQUAL_UINT8(expected.fwRegion, actual.fwRegion, line, "OtaAcceptance .fwRegion comparison failed");
-//  UNITY_TEST_ASSERT_EQUAL_UINT32(expected.version, actual.version, line, "OtaAcceptance .version comparison failed");
+  UNITY_TEST_ASSERT_EQUAL_INT32(expected.integer_bits, actual.integer_bits, line, "Fixed point definition .integer_bits comparison failed");
+  UNITY_TEST_ASSERT_EQUAL_INT32(expected.fractional_bits, actual.fractional_bits, line, "Fixed point definition .fractional_bits comparison failed");
+  UNITY_TEST_ASSERT_EQUAL_UINT32(expected.is_signed, actual.is_signed, line, "Fixed point definition .is_signed comparison failed");
 }

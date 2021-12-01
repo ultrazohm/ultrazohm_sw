8a70ddbfbeae148ff9339666c92ece19eef5ee3f
schindlerto <tobias.schindler@th-nuernberg.de>
2021-12-01 14:37:48 +0000

    Rebuilds vivado project to connect ADC A1 to interrupt

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Mon Nov 29 15:32:47 2021
---Host        : ws-wendt running 64-bit Ubuntu 20.04.3 LTS
+--Date        : Wed Dec  1 11:23:50 2021
+--Host        : ts-ThinkStation-P6203 running 64-bit Ubuntu 20.04.2 LTS
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

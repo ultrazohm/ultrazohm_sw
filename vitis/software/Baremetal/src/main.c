720e40d0ef1f2c5aa2789761c731db7618bfac7f
Tobias <tobias.schindler@th-nuernberg.de>
2022-01-18 10:30:24 +0000

    Introduces a chain of steps in main.c to explicitly do the init steps one after another

--- vitis/software/Baremetal/src/main.c
+++ vitis/software/Baremetal/src/main.c
@@ -17,76 +17,80 @@
 #include "main.h"
 
 // Initialize the global variables
-DS_Data Global_Data={
-    .rasv={
-        .halfBridge1DutyCycle=0.0f,
-        .halfBridge2DutyCycle=0.0f,
-        .halfBridge3DutyCycle=0.0f
+DS_Data Global_Data = {
+    .rasv = {
+        .halfBridge1DutyCycle = 0.0f,
+        .halfBridge2DutyCycle = 0.0f,
+        .halfBridge3DutyCycle = 0.0f
     },
-    .av.pwm_frequency_hz=UZ_PWM_FREQUENCY,
-    .av.isr_samplerate_s= (1.0f / UZ_PWM_FREQUENCY) * (Interrupt_ISR_freq_factor),
-    .aa={
-        .A1={
-            .cf.ADC_A1=10.0f,
-            .cf.ADC_A2=10.0f,
-            .cf.ADC_A3=10.0f,
-            .cf.ADC_A4=10.0f,
-            .cf.ADC_B5=10.0f,
-            .cf.ADC_B6=10.0f,
-            .cf.ADC_B7=10.0f,
-            .cf.ADC_B8=10.0f
-        },
-        .A2={
-            .cf.ADC_A1=10.0f,
-            .cf.ADC_A2=10.0f,
-            .cf.ADC_A3=10.0f,
-            .cf.ADC_A4=10.0f,
-            .cf.ADC_B5=10.0f,
-            .cf.ADC_B6=10.0f,
-            .cf.ADC_B7=10.0f,
-            .cf.ADC_B8=10.0f
-        },
-        .A3={
-            .cf.ADC_A1=10.0f,
-            .cf.ADC_A2=10.0f,
-            .cf.ADC_A3=10.0f,
-            .cf.ADC_A4=10.0f,
-            .cf.ADC_B5=10.0f,
-            .cf.ADC_B6=10.0f,
-            .cf.ADC_B7=10.0f,
-            .cf.ADC_B8=10.0f
-        }
+    .av.pwm_frequency_hz = UZ_PWM_FREQUENCY,
+    .av.isr_samplerate_s = (1.0f / UZ_PWM_FREQUENCY) * (Interrupt_ISR_freq_factor),
+    .aa = {.A1 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f},
+    	   .A2 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f},
+		   .A3 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f}
     }
 };
 
+enum init_chain
+{
+    init_assertions = 0,
+    init_gpios,
+    init_software,
+    init_ip_cores,
+    print_msg,
+    init_interrupts,
+    infinite_loop
+};
+enum init_chain initialization_chain = init_assertions;
+
 int main(void)
 {
-    uz_assert_configuration(); // This has to be the first line of code in main.c
-    Initialize_AXI_GPIO(); // This has to be the second line of code in main.c since the assertion callback uses the AXI_GPIO to disable the system
-    uz_frontplane_button_and_led_init(); // This has to be the third line of code since the assertion callback uses the LEDs to indicate an error
-
     int status = UZ_SUCCESS;
-    uz_printf("\r\n\r\n");
-    uz_printf("Welcome to the UltraZohm\r\n");
-    uz_printf("----------------------------------------\r\n");
-    uz_adcLtc2311_ip_core_init();
-    Global_Data.objects.deadtime_interlock_d1= uz_interlockDeadtime2L_staticAllocator_slotD1();
-    uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1, true);
-    Global_Data.objects.pwm_d1= initialize_pwm_2l_on_D1();
-    PWM_3L_Initialize(&Global_Data); // three-level modulator
-    initialize_incremental_encoder_ipcore_on_D5(UZ_D5_INCREMENTAL_ENCODER_RESOLUTION, UZ_D5_MOTOR_POLE_PAIR_NUMBER);
-
-    // Initialize Timer in order to Trigger the ISRs
-    Initialize_Timer();
-    uz_SystemTime_init();
-    JavaScope_initalize(&Global_Data);
-    uz_axigpio_enable_datamover();
-    Initialize_ISR(); // Initialize the Interrupts and enable them - last line of code before infinite loop
-
-    // Infinite loop
     while (1)
     {
-        ultrazohm_state_machine_step();
+        switch (initialization_chain)
+        {
+        case init_assertions:
+            uz_assert_configuration(); // This has to be the first line of code in main.c
+            initialization_chain = init_gpios;
+            break;
+        case init_gpios:
+            Initialize_AXI_GPIO();               // This has to be the second line of code in main.c since the assertion callback uses the AXI_GPIO to disable the system
+            uz_frontplane_button_and_led_init(); // This has to be the third line of code since the assertion callback uses the LEDs to indicate an error
+            initialization_chain = init_software;
+            break;
+        case init_software:
+            Initialize_Timer();
+            uz_SystemTime_init();
+            JavaScope_initalize(&Global_Data);
+            initialization_chain = init_ip_cores;
+            break;
+        case init_ip_cores:
+            uz_adcLtc2311_ip_core_init();
+            Global_Data.objects.deadtime_interlock_d1 = uz_interlockDeadtime2L_staticAllocator_slotD1();
+            uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1, true);
+            Global_Data.objects.pwm_d1 = initialize_pwm_2l_on_D1();
+            PWM_3L_Initialize(&Global_Data); // three-level modulator
+            initialize_incremental_encoder_ipcore_on_D5(UZ_D5_INCREMENTAL_ENCODER_RESOLUTION, UZ_D5_MOTOR_POLE_PAIR_NUMBER);
+            initialization_chain = print_msg;
+            break;
+        case print_msg:
+            uz_printf("\r\n\r\n");
+            uz_printf("Welcome to the UltraZohm\r\n");
+            uz_printf("----------------------------------------\r\n");
+            initialization_chain = init_interrupts;
+            break;
+        case init_interrupts:
+            uz_axigpio_enable_datamover();
+            Initialize_ISR(); // Initialize the Interrupts and enable them - last line of code before infinite loop
+            initialization_chain = infinite_loop;
+            break;
+        case infinite_loop:
+            ultrazohm_state_machine_step();
+            break;
+        default:
+            break;
+        }
     }
     return (status);
 }

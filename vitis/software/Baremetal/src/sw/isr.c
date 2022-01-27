7d7aadd4016e29d90fa8b459eb104093f617559e
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2022-01-27 14:33:24 +0100

    Adjusts ADC readout

--- vitis/software/Baremetal/src/sw/isr.c
+++ vitis/software/Baremetal/src/sw/isr.c
@@ -63,10 +63,15 @@ void ISR_Control(void *data)
     ReadAllADC();
     update_speed_and_position_of_encoder_on_D5(&Global_Data);
 	//Get values from ADCs
-	PID_Data.PID_ActualValues.I_UVW.U = (Global_Data.aa.A1.me.ADC_A2 - 2.5f) * (20.0f / 2.084f) / 3.0f;
-	PID_Data.PID_ActualValues.I_UVW.V = (Global_Data.aa.A1.me.ADC_A3 - 2.5f) * (20.0f / 2.084f) / 3.0f;
-	PID_Data.PID_ActualValues.I_UVW.W = (Global_Data.aa.A1.me.ADC_A4 - 2.5f) * (20.0f / 2.084f) / 3.0f;
+	PID_Data.PID_ActualValues.I_UVW.U = (Global_Data.aa.A1.me.ADC_B6 - 2.5f) * (20.0f / 2.084f) / 3.0f;
+	PID_Data.PID_ActualValues.I_UVW.V = (Global_Data.aa.A1.me.ADC_B7 - 2.5f) * (20.0f / 2.084f) / 3.0f;
+	PID_Data.PID_ActualValues.I_UVW.W = (Global_Data.aa.A1.me.ADC_B8 - 2.5f) * (20.0f / 2.084f) / 3.0f;
 	PID_Data.PID_ActualValues.V_DC = ((Global_Data.aa.A1.me.ADC_A1) * 20.05f) - 0.18f;
+	//PID_Data.PID_ActualValues.V_DC = 24.0f;
+	PID_Data.PID_ActualValues.V_UVW.U = Global_Data.aa.A1.me.ADC_A2;
+	PID_Data.PID_ActualValues.V_UVW.V = Global_Data.aa.A1.me.ADC_A3;
+	PID_Data.PID_ActualValues.V_UVW.W = Global_Data.aa.A1.me.ADC_A4;
+
 	PID_Data.PID_ActualValues.omega_m = (Global_Data.av.mechanicalRotorSpeed * 2.0f * UZ_PIf) / 60.0f;
 	PID_Data.PID_ActualValues.theta_el = Global_Data.av.theta_elec;
 
@@ -75,10 +80,11 @@ void ISR_Control(void *data)
 	PID_Data.PID_ActualValues.v_dq = uz_dq_transformation(PID_Data.PID_ActualValues.V_UVW, Global_Data.av.theta_elec);
 	PID_Data.PID_ActualValues.theta_m = Global_Data.av.theta_elec / PID_Data.PID_GlobalConfig.PMSM_config.polePairs;
 
-	platform_state_t current_state = ultrazohm_state_machine_get_state();
-    if (current_state==control_state)
+	uz_ParameterID_step(ParameterID, &PID_Data);
+
+	if (ultrazohm_state_machine_get_state() == control_state)
     {
-		uz_ParameterID_step(ParameterID, &PID_Data);
+
 		PID_v_dq = uz_ParameterID_Controller(&PID_Data, FOC_instance, SpeedControl_instance);
 		PID_DutyCycle = uz_ParameterID_generate_DutyCycle(&PID_Data, FOC_instance, SpeedControl_instance, PID_v_dq, Global_Data.objects.pwm_d1);
 		Global_Data.rasv.halfBridge1DutyCycle = PID_DutyCycle.DutyCycle_U;
@@ -86,9 +92,9 @@ void ISR_Control(void *data)
 		Global_Data.rasv.halfBridge3DutyCycle = PID_DutyCycle.DutyCycle_W;
 		PID_Data.PID_ActualValues.v_dq = PID_v_dq;
 	} else {
-		Global_Data.rasv.halfBridge1DutyCycle = 0.0f;
-		Global_Data.rasv.halfBridge2DutyCycle = 0.0f;
-		Global_Data.rasv.halfBridge3DutyCycle = 0.0f;
+		Global_Data.rasv.halfBridge1DutyCycle = 0.2f;
+		Global_Data.rasv.halfBridge2DutyCycle = 0.5f;
+		Global_Data.rasv.halfBridge3DutyCycle = 0.8f;
     }
     uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1, Global_Data.rasv.halfBridge1DutyCycle, Global_Data.rasv.halfBridge2DutyCycle, Global_Data.rasv.halfBridge3DutyCycle);
     // Set duty cycles for three-level modulator

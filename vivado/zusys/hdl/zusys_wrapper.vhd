c0c42b6d2d49e4d222753d2e4fb2d1aa05d8c38d
hoernermi <michael.hoerner@th-nuernberg.de>
2022-02-04 11:01:29 +0100

    exported xsa

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
---Date        : Thu Feb  3 16:45:29 2022
+--Date        : Fri Feb  4 10:12:55 2022
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

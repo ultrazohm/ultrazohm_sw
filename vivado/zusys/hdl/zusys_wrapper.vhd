b4e57ebc94f621fa25e22f2c35b4554f0d933310
Tobias <tobias.schindler@th-nuernberg.de>
2022-02-10 17:06:11 +0000

    Vivado project with mlp ip core

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Thu Dec  2 11:31:59 2021
---Host        : TS-WS running 64-bit Ubuntu 20.04.3 LTS
+--Date        : Fri Jan 28 15:14:30 2022
+--Host        : ts-ThinkPad-T14s-Gen-1 running 64-bit Ubuntu 21.04
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

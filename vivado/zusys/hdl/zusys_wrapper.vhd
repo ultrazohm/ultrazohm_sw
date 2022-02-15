d660adcf498c12a6e02b25ed30f1cc23a3650dc5
schindlerto <tobias.schindler@th-nuernberg.de>
2022-02-15 11:11:56 +0100

    Rebuilds bitstream with new ip-core version

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Mon Feb 14 17:25:19 2022
+--Date        : Tue Feb 15 10:00:41 2022
 --Host        : ts-ThinkStation-P6203 running 64-bit Ubuntu 20.04.3 LTS
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

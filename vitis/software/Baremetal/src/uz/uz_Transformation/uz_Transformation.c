fdca41bd69039884f33823392cd3d738520530fc
HoppeVa <hoppeva70971@th-nuernberg.de>
2022-02-16 15:20:53 +0100

    integrated added functions into current version of uz_Transformation

--- vitis/software/Baremetal/src/uz/uz_Transformation/uz_Transformation.c
+++ vitis/software/Baremetal/src/uz/uz_Transformation/uz_Transformation.c
@@ -61,3 +61,258 @@ uz_UVW_t uz_clarke_inverse_transformation(uz_alphabeta_t input){
 }
 
 
+float uz_9ph_arraymul(int line, float matrixval[9][9], float electricval[9]){
+    int i=0;
+    float output=0;
+    for(i=0;i<9;i++)
+        output = output + matrixval[line][i]*electricval[i];
+    return output;
+}
+
+
+struct uz_alphabeta_9ph_t uz_9ph_clarke_transformation(struct uz_abc_9ph_t input){
+    struct uz_alphabeta_9ph_t output;
+    float electricval[9], vsd_mat[9][9];
+
+    //write values of abc struct to array for easier usage
+    electricval[0] = input.a1;
+    electricval[1] = input.b1;
+    electricval[2] = input.c1;
+    electricval[3] = input.a2;
+    electricval[4] = input.b2;
+    electricval[5] = input.c2;
+    electricval[6] = input.a3;
+    electricval[7] = input.b3;
+    electricval[8] = input.c3;
+
+    //create transformationmatrix
+    vsd_mat[0][0] = 0.2222222f;
+    vsd_mat[0][1] = -0.1111111f;
+    vsd_mat[0][2] = -0.1111111f;
+    vsd_mat[0][3] = 0.2088206f;
+    vsd_mat[0][4] = -0.1702321f;
+    vsd_mat[0][5] = -0.0385885f;
+    vsd_mat[0][6] = 0.1702321f;
+    vsd_mat[0][7] = -0.2088206f;
+    vsd_mat[0][8] = 0.0385885f;
+    vsd_mat[1][0] = 0.0000000f;
+    vsd_mat[1][1] = 0.1924501f;
+    vsd_mat[1][2] = -0.1924501f;
+    vsd_mat[1][3] = 0.0760045f;
+    vsd_mat[1][4] = 0.1428417f;
+    vsd_mat[1][5] = -0.2188462f;
+    vsd_mat[1][6] = 0.1428417f;
+    vsd_mat[1][7] = 0.0760045f;
+    vsd_mat[1][8] = -0.2188462f;
+    vsd_mat[2][0] = 0.2222222f;
+    vsd_mat[2][1] = 0.2222222f;
+    vsd_mat[2][2] = 0.2222222f;
+    vsd_mat[2][3] = 0.1111111f;
+    vsd_mat[2][4] = 0.1111111f;
+    vsd_mat[2][5] = 0.1111111f;
+    vsd_mat[2][6] = -0.1111111f;
+    vsd_mat[2][7] = -0.1111111f;
+    vsd_mat[2][8] = -0.1111111f;
+    vsd_mat[3][0] = 0.0000000f;
+    vsd_mat[3][1] = -0.0000000f;
+    vsd_mat[3][2] = -0.0000000f;
+    vsd_mat[3][3] = 0.1924501f;
+    vsd_mat[3][4] = 0.1924501f;
+    vsd_mat[3][5] = 0.1924501f;
+    vsd_mat[3][6] = 0.1924501f;
+    vsd_mat[3][7] = 0.1924501f;
+    vsd_mat[3][8] = 0.1924501f;
+    vsd_mat[4][0] = 0.2222222f;
+    vsd_mat[4][1] = -0.1111111f;
+    vsd_mat[4][2] = -0.1111111f;
+    vsd_mat[4][3] = -0.0385885f;
+    vsd_mat[4][4] = 0.2088206f;
+    vsd_mat[4][5] = -0.1702321f;
+    vsd_mat[4][6] = -0.2088206f;
+    vsd_mat[4][7] = 0.0385885f;
+    vsd_mat[4][8] = 0.1702321f;
+    vsd_mat[5][0] = 0.0000000f;
+    vsd_mat[5][1] = -0.1924501f;
+    vsd_mat[5][2] = 0.1924501f;
+    vsd_mat[5][3] = 0.2188462f;
+    vsd_mat[5][4] = -0.0760045f;
+    vsd_mat[5][5] = -0.1428417f;
+    vsd_mat[5][6] = -0.0760045f;
+    vsd_mat[5][7] = 0.2188462f;
+    vsd_mat[5][8] = -0.1428417f;
+    vsd_mat[6][0] = 0.2222222f;
+    vsd_mat[6][1] = -0.1111111f;
+    vsd_mat[6][2] = -0.1111111f;
+    vsd_mat[6][3] = -0.1702321f;
+    vsd_mat[6][4] = -0.0385885f;
+    vsd_mat[6][5] = 0.2088206f;
+    vsd_mat[6][6] = 0.0385885f;
+    vsd_mat[6][7] = 0.1702321f;
+    vsd_mat[6][8] = -0.2088206f;
+    vsd_mat[7][0] = 0.0000000f;
+    vsd_mat[7][1] = 0.1924501f;
+    vsd_mat[7][2] = -0.1924501f;
+    vsd_mat[7][3] = 0.1428417f;
+    vsd_mat[7][4] = -0.2188462f;
+    vsd_mat[7][5] = 0.0760045f;
+    vsd_mat[7][6] = -0.2188462f;
+    vsd_mat[7][7] = 0.1428417f;
+    vsd_mat[7][8] = 0.0760045f;
+    vsd_mat[8][0] = 0.1111111f;
+    vsd_mat[8][1] = 0.1111111f;
+    vsd_mat[8][2] = 0.1111111f;
+    vsd_mat[8][3] = 0.1111111f;
+    vsd_mat[8][4] = 0.1111111f;
+    vsd_mat[8][5] = 0.1111111f;
+    vsd_mat[8][6] = 0.1111111f;
+    vsd_mat[8][7] = 0.1111111f;
+    vsd_mat[8][8] = 0.1111111f;
+
+    //apply transformation matrix
+    output.alpha = uz_9ph_arraymul(0, vsd_mat, electricval);
+    output.beta  = uz_9ph_arraymul(1, vsd_mat, electricval);
+    output.o1 = uz_9ph_arraymul(2, vsd_mat, electricval);
+    output.o2 = uz_9ph_arraymul(3, vsd_mat, electricval);
+    output.x1 = uz_9ph_arraymul(4, vsd_mat, electricval);
+    output.y1 = uz_9ph_arraymul(5, vsd_mat, electricval);
+    output.x2 = uz_9ph_arraymul(6, vsd_mat, electricval);
+    output.y2 = uz_9ph_arraymul(7, vsd_mat, electricval);
+    output.zero = uz_9ph_arraymul(8, vsd_mat, electricval);
+    return(output);
+}
+
+
+struct uz_abc_9ph_t uz_9ph_clarke_inverse_transformation(struct uz_alphabeta_9ph_t input){
+    struct uz_abc_9ph_t output;
+    float electricval[9], vsd_mat[9][9];
+
+    //write values of abc struct to array for easier usage
+    electricval[0] = input.alpha;
+    electricval[1] = input.beta;
+    electricval[2] = input.o1;
+    electricval[3] = input.o2;
+    electricval[4] = input.x1;
+    electricval[5] = input.y1;
+    electricval[6] = input.x2;
+    electricval[7] = input.y2;
+    electricval[8] = input.zero;
+
+    //create transformationmatrix, values imported from Matlab script LINK??
+vsd_mat[0][0] = 1.0000000f;
+vsd_mat[0][1] = 0.0000002f;
+vsd_mat[0][2] = -0.0000002f;
+vsd_mat[0][3] = -1.7320511f;
+vsd_mat[0][4] = 0.9999998f;
+vsd_mat[0][5] = 0.0000001f;
+vsd_mat[0][6] = 1.0000000f;
+vsd_mat[0][7] = -0.0000000f;
+vsd_mat[0][8] = 3.0000005f;
+vsd_mat[1][0] = -0.5000001f;
+vsd_mat[1][1] = 0.8660257f;
+vsd_mat[1][2] = 0.0000000f;
+vsd_mat[1][3] = -1.7320509f;
+vsd_mat[1][4] = -0.5000000f;
+vsd_mat[1][5] = -0.8660253f;
+vsd_mat[1][6] = -0.4999999f;
+vsd_mat[1][7] = 0.8660253f;
+vsd_mat[1][8] = 3.0000000f;
+vsd_mat[2][0] = -0.5000000f;
+vsd_mat[2][1] = -0.8660253f;
+vsd_mat[2][2] = 0.0000000f;
+vsd_mat[2][3] = -1.7320509f;
+vsd_mat[2][4] = -0.5000000f;
+vsd_mat[2][5] = 0.8660254f;
+vsd_mat[2][6] = -0.5000001f;
+vsd_mat[2][7] = -0.8660254f;
+vsd_mat[2][8] = 3.0000000f;
+vsd_mat[3][0] = 0.9396926f;
+vsd_mat[3][1] = 0.3420202f;
+vsd_mat[3][2] = 1.5000002f;
+vsd_mat[3][3] = 2.5980763f;
+vsd_mat[3][4] = -0.1736481f;
+vsd_mat[3][5] = 0.9848076f;
+vsd_mat[3][6] = -0.7660445f;
+vsd_mat[3][7] = 0.6427875f;
+vsd_mat[3][8] = -3.0000005f;
+vsd_mat[4][0] = -0.7660444f;
+vsd_mat[4][1] = 0.6427875f;
+vsd_mat[4][2] = 1.4999998f;
+vsd_mat[4][3] = 2.5980761f;
+vsd_mat[4][4] = 0.9396926f;
+vsd_mat[4][5] = -0.3420202f;
+vsd_mat[4][6] = -0.1736481f;
+vsd_mat[4][7] = -0.9848077f;
+vsd_mat[4][8] = -2.9999995f;
+vsd_mat[5][0] = -0.1736481f;
+vsd_mat[5][1] = -0.9848077f;
+vsd_mat[5][2] = 1.5000002f;
+vsd_mat[5][3] = 2.5980763f;
+vsd_mat[5][4] = -0.7660444f;
+vsd_mat[5][5] = -0.6427878f;
+vsd_mat[5][6] = 0.9396926f;
+vsd_mat[5][7] = 0.3420201f;
+vsd_mat[5][8] = -3.0000005f;
+vsd_mat[6][0] = 0.7660444f;
+vsd_mat[6][1] = 0.6427878f;
+vsd_mat[6][2] = -1.4999998f;
+vsd_mat[6][3] = -0.8660253f;
+vsd_mat[6][4] = -0.9396927f;
+vsd_mat[6][5] = -0.3420200f;
+vsd_mat[6][6] = 0.1736483f;
+vsd_mat[6][7] = -0.9848078f;
+vsd_mat[6][8] = 2.9999995f;
+vsd_mat[7][0] = -0.9396926f;
+vsd_mat[7][1] = 0.3420202f;
+vsd_mat[7][2] = -1.4999999f;
+vsd_mat[7][3] = -0.8660253f;
+vsd_mat[7][4] = 0.1736481f;
+vsd_mat[7][5] = 0.9848078f;
+vsd_mat[7][6] = 0.7660445f;
+vsd_mat[7][7] = 0.6427876f;
+vsd_mat[7][8] = 2.9999998f;
+vsd_mat[8][0] = 0.1736481f;
+vsd_mat[8][1] = -0.9848076f;
+vsd_mat[8][2] = -1.5000000f;
+vsd_mat[8][3] = -0.8660254f;
+vsd_mat[8][4] = 0.7660444f;
+vsd_mat[8][5] = -0.6427876f;
+vsd_mat[8][6] = -0.9396925f;
+vsd_mat[8][7] = 0.3420201f;
+vsd_mat[8][8] = 3.0000000f;
+
+    //apply transformation matrix
+    output.a1 = uz_9ph_arraymul(0, vsd_mat, electricval);
+    output.b1 = uz_9ph_arraymul(1, vsd_mat, electricval);
+    output.c1 = uz_9ph_arraymul(2, vsd_mat, electricval);
+    output.a2 = uz_9ph_arraymul(3, vsd_mat, electricval);
+    output.b2 = uz_9ph_arraymul(4, vsd_mat, electricval);
+    output.c2 = uz_9ph_arraymul(5, vsd_mat, electricval);
+    output.a3 = uz_9ph_arraymul(6, vsd_mat, electricval);
+    output.b3 = uz_9ph_arraymul(7, vsd_mat, electricval);
+    output.c3 = uz_9ph_arraymul(8, vsd_mat, electricval);
+    return(output);
+}
+
+
+struct uz_dq_t uz_ab_to_dq_transformation(struct uz_alphabeta_t input, float theta_el_rad){
+    float cos_theta = 0, sin_theta = 0;
+    struct uz_dq_t output;
+    cos_theta = cosf(theta_el_rad);
+    sin_theta = sinf(theta_el_rad);
+    output.d =    cos_theta*input.alpha + sin_theta*input.beta;
+    output.q = -1*sin_theta*input.alpha + cos_theta*input.beta;
+    output.zero = -999999.0f;
+    return output;
+}
+
+struct uz_alphabeta_t uz_dq_to_ab_inverse_transformation(struct uz_dq_t input, float theta_el_rad){
+    float cos_theta = 0, sin_theta = 0;
+    struct uz_alphabeta_t output;
+    cos_theta = cosf(theta_el_rad);
+    sin_theta = sinf(theta_el_rad);
+    output.alpha = cos_theta*input.d - sin_theta*input.q;
+    output.beta  = sin_theta*input.d + cos_theta*input.q;
+    output.gamma = -999999.0f;
+    return output;
+}
+
--- vitis/software/Baremetal/src/uz/uz_Transformation/uz_Transformation.h
+++ vitis/software/Baremetal/src/uz/uz_Transformation/uz_Transformation.h
@@ -31,6 +31,64 @@ typedef struct uz_UVW_t{
 }uz_UVW_t;
 
 /**
+ * @brief Struct for the variables of an alpha-beta 9 phase system
+ * 
+ */
+struct uz_alphabeta_9ph_t{
+    float alpha;        /**< Amplitude of the alpha component */ 
+    float beta;         /**< Amplitude of the beta component */ 
+    float o1;           /**< Amplitude of the o1 component */ 
+    float o2;           /**< Amplitude of the o2 component */
+    float x1;           /**< Amplitude of the x1 component */
+    float y1;           /**< Amplitude of the y1 component */
+    float x2;           /**< Amplitude of the x2 component */
+    float y2;           /**< Amplitude of the y2 component */
+    float zero;         /**< Amplitude of the zero component */
+};
+
+/**
+ * @brief Struct for the variables of an alpha-beta 6 phase system
+ * 
+ */
+struct uz_alphabeta_6ph_t{
+    float alpha;        /**< Amplitude of the alpha component */ 
+    float beta;         /**< Amplitude of the beta component */ 
+    float o1;           /**< Amplitude of the o1 component */ 
+    float o2;           /**< Amplitude of the o2 component */
+    float x1;           /**< Amplitude of the x1 component */
+    float y1;           /**< Amplitude of the y1 component */
+};
+
+/**
+ * @brief Struct for the variables of a nine-phase-System
+ * 
+ */
+struct uz_abc_9ph_t{
+    float a1;        /**< Amplitude of the a1 phase */ 
+    float b1;        /**< Amplitude of the b1 phase */ 
+    float c1;        /**< Amplitude of the c1 phase */ 
+    float a2;        /**< Amplitude of the a2 phase */ 
+    float b2;        /**< Amplitude of the b2 phase */ 
+    float c2;        /**< Amplitude of the c2 phase */ 
+    float a3;        /**< Amplitude of the a3 phase */ 
+    float b3;        /**< Amplitude of the b3 phase */ 
+    float c3;        /**< Amplitude of the c3 phase */ 
+};
+
+/**
+ * @brief Struct for the variables of a six-phase-System
+ * 
+ */
+struct uz_abc_6ph_t{
+    float a1;        /**< Amplitude of the a1 phase */ 
+    float b1;        /**< Amplitude of the b1 phase */ 
+    float c1;        /**< Amplitude of the c1 phase */ 
+    float a2;        /**< Amplitude of the a2 phase */ 
+    float b2;        /**< Amplitude of the b2 phase */ 
+    float c2;        /**< Amplitude of the c2 phase */ 
+};
+
+/**
  * @brief Calculates the dq0-components from the UVW-Phases
  * 
  * @param input uz_UVW struct
@@ -63,4 +121,65 @@ uz_alphabeta_t uz_clarke_transformation(uz_UVW_t input);
  * @return uz_UVW_t Outputs the calculated UVW-phases
  */
 uz_UVW_t uz_clarke_inverse_transformation(uz_alphabeta_t input);
+
+/**
+ * @brief Calculates the dq0-components from the alpha-beta components
+ * 
+ * @param matrixval linematrix containing the values of the transformation matrix
+ * @param electricval electrical values (either alpha bete or abc) input as array
+ * @return float that is the return value for alpha beta transformation or inverse transformation depending on linematrix
+ */
+float uz_9ph_arraymul(int line, float matrixval[9][9], float electricval[9]);
+
+/**
+ * @brief Calculates the dq0-components from the alpha-beta components
+ * 
+ * @param input uz_alphabeta_t struct where gamma is unused
+ * @param theta_el_rad electrical theta in rad
+ * @return struct uz_dq_t Outputs the calculated dq0-components
+ */
+uz_dq_t uz_ab_to_dq_transformation(uz_alphabeta_t input, float theta_el_rad);
+
+/**
+ * @brief Calculates the alpha-beta components from the dq0-components, ignoring the zero component
+ * 
+ * @param input uz_dq struct
+ * @param theta_el_rad electrical theta in rad
+ * @return struct uz_alphabeta_t Outputs the calculated UVW-phases
+ */
+uz_alphabeta_t uz_dq_to_ab_inverse_transformation(uz_dq_t input, float theta_el_rad);
+
+/**
+ * @brief Calculates the alpha-beta-gamma-components from the nine phase abc-phases
+ * 
+ * @param input uz_abc_9ph_t
+ * @return struct uz_alphabeta_9ph_t Outputs the calculated alpha-beta-gamma-components
+ */
+struct uz_alphabeta_9ph_t uz_9ph_clarke_transformation(struct uz_abc_9ph_t input);
+
+/**
+ * @brief Calculates the abc-phases from the alpha-beta-gamma-components
+ * 
+ * @param input uz_alphabeta_9ph_t struct
+ * @return struct uz_Uabc_t Outputs the calculated abc-phases
+ */
+struct uz_abc_9ph_t uz_9ph_clarke_inverse_transformation(struct uz_alphabeta_9ph_t input);
+
+/**
+ * @brief Calculates the alpha-beta-gamma-components from the six phase abc-phases
+ * 
+ * @param input uz_abc_6ph_t
+ * @return struct uz_alphabeta_9ph_t Outputs the calculated alpha-beta-gamma-components
+ */
+struct uz_alphabeta_6ph_t uz_6ph_clarke_transformation(struct uz_abc_6ph_t input);
+
+/**
+ * @brief Calculates the abc-phases from the alpha-beta-gamma-components
+ * 
+ * @param input uz_alphabeta_6ph_t struct
+ * @return struct uz_abc_t Outputs the calculated abc-phases
+ */
+struct uz_abc_6ph_t uz_6ph_clarke_inverse_transformation(struct uz_alphabeta_6ph_t input);
+
+
 #endif // UZ_TRANSFORMATION_H

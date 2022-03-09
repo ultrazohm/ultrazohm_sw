a2755ba5e6bc36ff5bae295164bd7fecd78171d7
Tomás P. Corrêa <tpcorrea@ufmg.br>
2022-03-09 17:33:17 +0100

    Python script to update .xdc files to a new board seems to work. It still need to handle a few exceptions.

--- vivado/constraints/process_xdc.py
+++ vivado/constraints/process_xdc.py
@@ -62,19 +62,43 @@ def extract_info_from_xdc(filename):
                     xdc_dict[port].iostandard = sl[2]
     return xdc_dict
                 
-
+def read_pinout(filename):
+    with open(filename) as f:
+        obj = csv.DictReader(f, delimiter=";")
+        output = []
+        for item in obj:         
+            if item["FPGA Pin Name"] != "---" and item["FPGA Pin Name"] != "--" :
+                output.append(item)
+    return output
+    
 
 # get all xdc files in folder
 xdc_files = [f for f in listdir(getcwd()) if isfile(join(getcwd(), f)) and f.endswith((".xdc",".XDC"))]
 
+# Package pins and B2B connector matching extracted from Trenz pinout sheet
+# https://shop.trenz-electronic.de/Download/?path=Trenz_Electronic/Pinout
+origin_filename = "TE0808_REV02.csv"
+target_filename = "TE0803_REV02.csv"
+
+dict_origin = read_pinout(origin_filename)
+dict_target = read_pinout(target_filename)
+
 # process one xdc at a time, extracting the relevant info and re-writing to a new .xdc
 for xdc_input in xdc_files:
     f = open("new/"+xdc_input,'w')
-
+    
     print(xdc_input)
     xdc_dict = extract_info_from_xdc(xdc_input)
 
     for port in xdc_dict.items():
+
+        # find dict on the origin list that has the same package pin
+        origin_info = [item for item in dict_origin if item["FPGA Pin Name"] == port[1].package_pin][0]
+        # find dict on the target list that is connected to the same B2B connector pin
+        target_info = [item for item in dict_target if item["C-Name"] == origin_info["C-Name"] and item["C-Pin"] == origin_info["C-Pin"]][0]
+        # replace package_pin with the information of the target board
+        port[1].package_pin = target_info["FPGA Pin Name"]
+      
         f.writelines(port[1].write())
         f.write("\n")
 

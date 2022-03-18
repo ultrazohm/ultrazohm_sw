23396765aa61395c8b079645bbbc5cd58e564c24
Tobias <tobias.schindler@th-nuernberg.de>
2022-03-18 11:10:26 +0100

    restores develop vivado files

--- vivado/zusys/hdl/zusys_wrapper.vhd
+++ vivado/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Thu Mar 17 17:00:07 2022
---Host        : ts-ThinkStation-P6203 running 64-bit Ubuntu 20.04.3 LTS
+--Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
+--Date        : Fri Jan 28 14:39:50 2022
+--Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

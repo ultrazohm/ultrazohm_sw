317fb5d9b0198c6701c34ef96fdd28542dd8f5d9
Tobias <tobias.schindler@th-nuernberg.de>
2022-03-25 13:58:33 +0100

    Adds swap line file and update update_tcl_scripts_for_vc.tcl with if/catch version for python/python3

--- vivado/update_tcl_scripts_for_vc.tcl
+++ vivado/update_tcl_scripts_for_vc.tcl
@@ -14,14 +14,35 @@ write_bd_tcl -force ./bd/_zusys.tcl
 
 # fix python error 
 # https://support.xilinx.com/s/article/72570?language=en_US
+# On second run, unset returns an error since the env variables are already unset -> ignore the error with catch
 catch {unset ::env(PYTHONPATH)}
 catch {unset ::env(PYTHONHOME)}
 
+# For some reason, the line
+# "set_property -name "board_part" -value "trenz.biz:te0808_9eg_1e:part0:3.0" -objects $obj" (line 131)
+# of _build.tcl is before the line:
+# set_property -name "board_part_repo_paths" -value "[file normalize "$origin_dir/board_files"]" -objects $obj (line 132)
+# leading the errors when rebuilding the project.
+# Thus, if the vivado version is 2020.1 or 2020.1.1, the lines are swapped.
+set vivado_version [version -short]
+if {$vivado_version == "2020.1" || $vivado_version == "2020.1.1"} {
+	if { [catch {exec python swap_board_part_line.py} result] } {
+		exec python3 swap_board_part_line.py
+		puts "Using python3 since python was not found"
+	}
+ 	puts "Swaps board part line since 2020.1 or 2020.1.1 is used"
+}
+
 # run python script to modify the generated output and generate the build.tcl script
-catch {exec python modify_build_script.py}
-catch {exec python3 modify_build_script.py}
+if { [catch {exec python modify_build_script.py} result] } {
+	exec python3 modify_build_script.py
+	puts "Using python3 since python was not found"
+}
 # run python script to modify the generated output and generate the zusys.tcl script
 # inside this files is defined the board files path (later we should pass it as argument)
-catch {exec python modify_bd_script.py}
-catch {exec python3 modify_bd_script.py}
+if { [catch {exec python modify_bd_script.py} result] } {
+	exec python3 modify_bd_script.py
+	puts "Using python3 since python was not found"
+}
 
+puts "Updated tcl scripts!"

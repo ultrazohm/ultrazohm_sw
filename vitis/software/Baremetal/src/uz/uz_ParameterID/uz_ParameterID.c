03d8feb7defae8ed09c7f061d444346c3d881ebd
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2022-03-25 15:22:44 +0100

    Changes motor-related params

--- vitis/software/Baremetal/src/uz/uz_ParameterID/uz_ParameterID.c
+++ vitis/software/Baremetal/src/uz/uz_ParameterID/uz_ParameterID.c
@@ -437,21 +437,21 @@ static void uz_ParameterID_initialize_data_structs(uz_ParameterID_t *self, uz_Pa
 	Data->GlobalConfig.sampleTimeISR = 50.0e-06f;
 
 	//Initialize motor-related parameters inside Global-Config
-	Data->GlobalConfig.Ki_id = 100.8f;
-	Data->GlobalConfig.Ki_iq = 100.8f;
-	Data->GlobalConfig.Ki_n = 0.6f;
-	Data->GlobalConfig.Kp_id = 0.1f;
-	Data->GlobalConfig.Kp_iq = 0.2f;
+	Data->GlobalConfig.Ki_id = 158.8f;
+	Data->GlobalConfig.Ki_iq = 158.8f;
+	Data->GlobalConfig.Ki_n = 0.8f;
+	Data->GlobalConfig.Kp_id = 0.25;
+	Data->GlobalConfig.Kp_iq = 0.25f;
 	Data->GlobalConfig.Kp_n = 0.04f;
-	Data->GlobalConfig.PMSM_config.Ld_Henry = 1.00e-04f;
-	Data->GlobalConfig.PMSM_config.Lq_Henry = 2.00e-04f;
-	Data->GlobalConfig.PMSM_config.R_ph_Ohm = 0.095f;
-	Data->GlobalConfig.PMSM_config.Psi_PM_Vs = 0.0057f;
+    Data->GlobalConfig.PMSM_config.Ld_Henry = 2.90e-04f;
+	Data->GlobalConfig.PMSM_config.Lq_Henry = 3.00e-04f;
+	Data->GlobalConfig.PMSM_config.R_ph_Ohm = 0.105f;
+	Data->GlobalConfig.PMSM_config.Psi_PM_Vs = 0.0075f;
 	Data->GlobalConfig.PMSM_config.polePairs = 4.0f;
-	Data->GlobalConfig.PMSM_config.J_kg_m_squared = 1.9e-05f;
-	Data->GlobalConfig.PMSM_config.I_max_Ampere = 20.0f;
-	Data->GlobalConfig.ratCurrent = 8.5f;
-	Data->GlobalConfig.ratSpeed = 4000.0f;
+	Data->GlobalConfig.PMSM_config.J_kg_m_squared = 3.24e-05f;
+	Data->GlobalConfig.PMSM_config.I_max_Ampere = 15.0f;
+	Data->GlobalConfig.ratCurrent = 8.0f;
+	Data->GlobalConfig.ratSpeed = 3000.0f;
 
 	//Initialize ElectricalID-Config
 	Data->ElectricalID_Config.goertzlFreq = 0.0f;

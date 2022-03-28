155266158a73dc052c06584d7ce8a621736c4e7b
Tobias <tobias.schindler@th-nuernberg.de>
2022-03-28 11:31:26 +0200

    rebuild scripts again

--- vivado/modify_build_script.py
+++ vivado/modify_build_script.py
@@ -26,19 +26,6 @@ with open("_build.tcl",'r') as f_in:
                 idx = line.lower().find(repo_dir.lower())
                 line = line[:idx] + "${origin_dir}/.." + line[idx+len(repo_dir):]
 
-            # process the lines, adapting what is necessary
-            # skips an entire region when skip_till_next_comment 
-            if skip_till_next_comment:
-                if "# " in line:
-                    f_out.write(line)
-                    skip_till_next_comment = False
-            # related to .bd file (that is not added, because we use the zusys.tcl instead)
-            elif "Add local files from the original project (-no_copy_sources specified)" in line:
-                skip_till_next_comment = True
-            elif "Set 'sources_1' fileset file properties for local files" in line:
-                skip_till_next_comment = True
-            elif "zusys.bd" in line:
-                pass
             # set default board variable
             elif "set origin_dir \".\"" in line:
                 f_out.write(line + "\n")

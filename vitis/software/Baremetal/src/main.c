3c29994d2c3eed49277cb9cbb48729b2c14e45fd
hoernermi <michael.hoerner@th-nuernberg.de>
2022-03-30 17:19:29 +0200

    some changes

--- vitis/software/Baremetal/src/main.c
+++ vitis/software/Baremetal/src/main.c
@@ -75,9 +75,9 @@ int main(void)
             initialize_incremental_encoder_ipcore_on_D5(UZ_D5_INCREMENTAL_ENCODER_RESOLUTION, UZ_D5_MOTOR_POLE_PAIR_NUMBER);
 
             //debug
-            uz_axi_write_uint32(XPAR_UZ_PU_CON_IP_0_BASEADDR + 0x104, 26214); //26214 = 0.1f (Q18)
-            uz_axi_write_uint32(XPAR_UZ_PU_CON_IP_0_BASEADDR + 0x108, 2621); //2621 = 0.01f (Q18)
-            uz_axi_write_uint32(XPAR_UZ_PU_CON_IP_0_BASEADDR + 0x10C, 262); //262 = 0.001f (Q18)
+            uz_axi_write_uint32(XPAR_UZ_PU_CON_IP_0_BASEADDR + 0x100, 26214); //26214 = 0.1f (Q18)
+            uz_axi_write_uint32(XPAR_UZ_PU_CON_IP_0_BASEADDR + 0x104, 2621); //2621 = 0.01f (Q18)
+            uz_axi_write_uint32(XPAR_UZ_PU_CON_IP_0_BASEADDR + 0x108, 262); //262 = 0.001f (Q18)
 
 
             initialization_chain = print_msg;

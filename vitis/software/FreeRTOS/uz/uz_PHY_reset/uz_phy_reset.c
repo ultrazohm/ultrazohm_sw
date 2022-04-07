7ba6b2e3bdbcd6d10fa6dc0f4d47e2f296c788ea
Tobias <tobias.schindler@th-nuernberg.de>
2022-04-07 10:43:22 +0200

    Adds reset for PHY1 and renames PHY to PHY0 - both phy are resetted - PHY0 reset tested on hardware and works

--- vitis/software/FreeRTOS/uz/uz_PHY_reset/uz_phy_reset.c
+++ vitis/software/FreeRTOS/uz/uz_PHY_reset/uz_phy_reset.c
@@ -9,14 +9,16 @@
 #define ENABLE_PIN				1 								//This Pin is enabled
 #define DISABLE_PIN				0 								//This Pin is disabled
 
-#define PHY_RESET_MIO_PIN		26U
+#define PHY0_RESET_MIO_PIN		26U
+#define PHY1_RESET_MIO_PIN		27U
+
 
 static void uz_phy_reset_init();
 static void InitializeXilinxMioGpioInstance();
 
 static XGpioPs Gpio_inst;
-static uz_gpio MIO_PHY_Reset;
-
+static uz_gpio MIO_PHY0_Reset;
+static uz_gpio MIO_PHY1_Reset;
 
 void uz_phy_reset(void)
 {
@@ -24,22 +26,30 @@ void uz_phy_reset(void)
 	int phy_reset_high_time_us = 20e3; //20ms = 20,000 us
 
 	// enable apply reset
-	uz_gpio_set_enable_output(&MIO_PHY_Reset, true);
-	uz_gpio_write_pin(&MIO_PHY_Reset, false);
+	uz_gpio_set_enable_output(&MIO_PHY0_Reset, true);
+	uz_gpio_write_pin(&MIO_PHY0_Reset, false);
+
+	uz_gpio_set_enable_output(&MIO_PHY1_Reset, true);
+	uz_gpio_write_pin(&MIO_PHY1_Reset, false);
 
 	// wait for some time as defined in data sheet
 	usleep(phy_reset_high_time_us);
 
 	// release reset and disable MIO pin
-	uz_gpio_write_pin(&MIO_PHY_Reset, true);
-	uz_gpio_set_enable_output(&MIO_PHY_Reset, false);
+	uz_gpio_write_pin(&MIO_PHY0_Reset, true);
+	uz_gpio_set_enable_output(&MIO_PHY0_Reset, false);
+
+	uz_gpio_write_pin(&MIO_PHY1_Reset, true);
+	uz_gpio_set_enable_output(&MIO_PHY1_Reset, false);
+
 
 	xil_printf("PHY reset successful \n\r");
 }
 
 static void uz_phy_reset_init() {
 	InitializeXilinxMioGpioInstance();
-	uz_gpio_init(&MIO_PHY_Reset, &Gpio_inst, PHY_RESET_MIO_PIN, OUTPUT_PIN);
+	uz_gpio_init(&MIO_PHY0_Reset, &Gpio_inst, PHY0_RESET_MIO_PIN, OUTPUT_PIN);
+	uz_gpio_init(&MIO_PHY1_Reset, &Gpio_inst, PHY1_RESET_MIO_PIN, OUTPUT_PIN);
 }
 
 static void InitializeXilinxMioGpioInstance() {

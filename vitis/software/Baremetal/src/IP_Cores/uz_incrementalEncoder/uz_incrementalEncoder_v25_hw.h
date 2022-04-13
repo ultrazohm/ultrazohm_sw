bcf8a03d5f8fb7b5f98f8010cbbbca0ad7d030e9
EykeLiegmann <eyke.liegmann@tum.de>
2022-04-13 20:05:47 +0200

    preliminary software driver

---
+++ vitis/software/Baremetal/src/IP_Cores/uz_incrementalEncoder/uz_incrementalEncoder_v25_hw.h
@@ -0,0 +1,17 @@
+#ifndef UZ_INCREMENTALENCODER_v25_HW_H
+#define UZ_INCREMENTALENCODER_v25_HW_H
+#include <stdint.h>
+#include "../../uz/uz_HAL.h"
+#include "../../uz/uz_AXI.h"
+
+void uz_incrementalEncoder_hw_set_speed_timeout(uint32_t base_address,uint32_t speed_timeout);
+
+float uz_incrementalEncoder_hw_get_omega_oversampled_N4(uint32_t base_address);
+float uz_incrementalEncoder_hw_get_omega_oversampled_N8(uint32_t base_address);
+
+float uz_incrementalEncoder_hw_get_NOTB(uint32_t base_address);
+float uz_incrementalEncoder_hw_get_NOTA(uint32_t base_address);
+float uz_incrementalEncoder_hw_get_A(uint32_t base_address);
+float uz_incrementalEncoder_hw_get_B(uint32_t base_address);
+
+#endif // UZ_INCREMENTALENCODER_HW_H

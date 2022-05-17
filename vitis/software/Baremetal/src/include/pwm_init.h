3dfcf687813304d6152d9b58f5e0ace6f0144789
hoernermi <michael.hoerner@th-nuernberg.de>
2022-05-17 16:21:29 +0200

    added software instances of PWM IP core and interlock drivers for additional modules in the block design

--- vitis/software/Baremetal/src/include/pwm_init.h
+++ vitis/software/Baremetal/src/include/pwm_init.h
@@ -3,3 +3,5 @@
 
 uz_PWM_SS_2L_t* initialize_pwm_2l_on_D1_pin_0_to_5(void);
 uz_PWM_SS_2L_t* initialize_pwm_2l_on_D1_pin_6_to_11(void);
+uz_PWM_SS_2L_t* initialize_pwm_2l_on_D1_pin_12_to_17(void);
+uz_PWM_SS_2L_t* initialize_pwm_2l_on_D1_pin_18_to_23(void);

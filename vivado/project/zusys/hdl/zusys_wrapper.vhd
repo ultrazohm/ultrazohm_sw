65a7f3dc9f65a9be4fda930c2ba70f902835636e
Tobias <tobias.schindler@th-nuernberg.de>
2022-06-09 09:35:13 +0000

    Bitstream with improved ip-cores

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Tue Jun  7 14:50:34 2022
---Host        : TS-WS running 64-bit Ubuntu 20.04.4 LTS
+--Date        : Thu Jun  9 09:56:40 2022
+--Host        : TS-WS running 64-bit Ubuntu 21.10
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

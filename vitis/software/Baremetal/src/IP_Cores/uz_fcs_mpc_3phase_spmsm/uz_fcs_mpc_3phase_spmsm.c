40fb52046ba334b4cb25260c1830e4ed5195aca4
merxan71462 <merxan71462@th-nuernberg.de>
2022-06-10 14:29:38 +0200

    IP-Core driver and tests created

--- vitis/software/Baremetal/src/IP_Cores/uz_fcs_mpc_3phase_spmsm/uz_fcs_mpc_3phase_spmsm.c
+++ vitis/software/Baremetal/src/IP_Cores/uz_fcs_mpc_3phase_spmsm/uz_fcs_mpc_3phase_spmsm.c
@@ -1,1 +1,40 @@
+#include "../../uz/uz_global_configuration.h"
+#if UZ_FCS_MPC_3PHASE_SPMSM_MAX_INSTANCES > 0U
+#include <stdbool.h>
+#include "../../uz/uz_HAL.h"
 #include "uz_fcs_mpc_3phase_spmsm.h"
+#include "uz_fcs_mpc_3phase_spmsm_hw.h"
+
+struct uz_fcs_mpc_3phase_spmsm_t {
+    bool is_ready;
+    struct uz_fcs_mpc_3phase_spmsm_config_t config;
+};
+
+static uint32_t instance_counter = 0U;
+static uz_fcs_mpc_3phase_spmsm_t instances[UZ_FCS_MPC_3PHASE_SPMSM_MAX_INSTANCES] = { 0 };
+
+static uz_fcs_mpc_3phase_spmsm_t* uz_fcs_mpc_3phase_spmsm_allocation(void);
+
+static uz_fcs_mpc_3phase_spmsm_t* uz_fcs_mpc_3phase_spmsm_allocation(void){
+    uz_assert(instance_counter < UZ_FCS_MPC_3PHASE_SPMSM_MAX_INSTANCES);
+    uz_fcs_mpc_3phase_spmsm_t* self = &instances[instance_counter];
+    uz_assert_false(self->is_ready);
+    instance_counter++;
+    self->is_ready = true;
+    return (self);
+}
+
+uz_fcs_mpc_3phase_spmsm_t* uz_fcs_mpc_3phase_spmsm_init(struct uz_fcs_mpc_3phase_spmsm_config_t config){
+    uz_fcs_mpc_3phase_spmsm_t* self = uz_fcs_mpc_3phase_spmsm_allocation();
+    uz_assert_not_zero_uint32(config.base_address);
+    uz_assert_not_zero_uint32(config.ip_clk_frequency_Hz);
+    self->config = config;
+    uz_fcs_mpc_3phase_spmsm_hw_set_u_dc_link(self->config.base_address, self->config.u_dc_link);
+    uz_fcs_mpc_3phase_spmsm_hw_set_SampleTime(self->config.base_address, self->config.SampleTime);
+    uz_fcs_mpc_3phase_spmsm_hw_set_Rs(self->config.base_address, self->config.Rs);
+    uz_fcs_mpc_3phase_spmsm_hw_set_Ld(self->config.base_address, self->config.Ld);
+    uz_fcs_mpc_3phase_spmsm_hw_set_Lq(self->config.base_address, self->config.Lq);
+    uz_fcs_mpc_3phase_spmsm_hw_set_psiPM(self->config.base_address, self->config.psiPM);
+    return (self);
+}
+#endif
--- vitis/software/Baremetal/test/IP_Cores/uz_fcs_mpc_3phase_spmsm/test_uz_fcs_mpc_3phase_spmsm.c
+++ vitis/software/Baremetal/test/IP_Cores/uz_fcs_mpc_3phase_spmsm/test_uz_fcs_mpc_3phase_spmsm.c
@@ -1,20 +1,49 @@
 #ifdef TEST
 
 #include "unity.h"
-
+#include "test_assert_with_exception.h"
+#include <stdint.h>
+#define TEST_BASE_ADDRESS 0x000BADBAD
+#define TEST_IP_CORE_FRQ 100000000U
 #include "uz_fcs_mpc_3phase_spmsm.h"
+#include "mock_uz_fcs_mpc_3phase_spmsm_hw.h"
+
+static struct uz_fcs_mpc_3phase_spmsm_config_t config={
+   .base_address= TEST_BASE_ADDRESS,
+   .ip_clk_frequency_Hz=TEST_IP_CORE_FRQ,
+   .u_dc_link = 1.5f,
+   .SampleTime=100.8f,
+   .Rs=0.4f,
+   .Ld=0.00003f,
+   .Lq=0.00005f,
+   .psiPM=0.007f
+};
 
 void setUp(void)
 {
+    config.base_address= TEST_BASE_ADDRESS;
+    config.ip_clk_frequency_Hz=TEST_IP_CORE_FRQ;
+    config.u_dc_link = 1.5f;
+    config.SampleTime=100.8f;
+    config.Rs=0.4f;
+    config.Ld=0.00003f;
+    config.Lq=0.00005f;
+    config.psiPM=0.007f;
 }
 
 void tearDown(void)
 {
 }
 
-void test_uz_fcs_mpc_3phase_spmsm_NeedToImplement(void)
+void test_uz_fcs_mpc_3phase_spmsm_init_test(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement uz_fcs_mpc_3phase_spmsm");
+   uz_fcs_mpc_3phase_spmsm_hw_set_u_dc_link_Expect(config.base_address, config.u_dc_link);
+   uz_fcs_mpc_3phase_spmsm_hw_set_SampleTime_Expect(config.base_address, config.SampleTime);
+   uz_fcs_mpc_3phase_spmsm_hw_set_Rs_Expect(config.base_address, config.Rs);
+   uz_fcs_mpc_3phase_spmsm_hw_set_Ld_Expect(config.base_address, config.Ld);
+   uz_fcs_mpc_3phase_spmsm_hw_set_Lq_Expect(config.base_address, config.Lq);
+   uz_fcs_mpc_3phase_spmsm_hw_set_psiPM_Expect(config.base_address, config.psiPM);
+   uz_fcs_mpc_3phase_spmsm_init(config);
 }
 
 #endif // TEST

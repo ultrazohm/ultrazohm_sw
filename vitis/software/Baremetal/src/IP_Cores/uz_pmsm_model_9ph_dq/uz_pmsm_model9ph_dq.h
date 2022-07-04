750736c24e75bbd2866e1f19d70ca09ea96c7c74
HoppeVa <hoppeva70971@th-nuernberg.de>
2022-07-04 16:00:07 +0200

    Add comments to functions

--- vitis/software/Baremetal/src/IP_Cores/uz_pmsm_model_9ph_dq/uz_pmsm_model9ph_dq.h
+++ vitis/software/Baremetal/src/IP_Cores/uz_pmsm_model_9ph_dq/uz_pmsm_model9ph_dq.h
@@ -31,7 +31,7 @@ struct uz_pmsm_model9ph_dq_config_t
 };
 
 /**
- * @brief Struct to return and read the outputs of the PMSM Model
+ * @brief Struct to return and read the general outputs of the PMSM Model
  * 
  */
 struct uz_pmsm_model9ph_dq_outputs_general_t
@@ -70,30 +70,84 @@ void uz_pmsm_model9ph_trigger_voltage_output_strobe(uz_pmsm_model9ph_dq_t *self)
  */
 void uz_pmsm_model9ph_trigger_current_output_strobe(uz_pmsm_model9ph_dq_t *self);
 
-
-void uz_pmsm_model9ph_dq_set_voltage(uz_pmsm_model9ph_dq_t *self, uz_9ph_dq_t voltages);
-
-void uz_pmsm_model9ph_dq_set_voltage_unsafe(uz_pmsm_model9ph_dq_t *self, uz_9ph_dq_t voltages);
-
-
-uz_9ph_dq_t uz_pmsm_model9ph_dq_get_input_voltages(uz_pmsm_model9ph_dq_t *self);
-
-uz_9ph_dq_t uz_pmsm_model9ph_dq_get_input_voltages_unsafe(uz_pmsm_model9ph_dq_t *self);
-
+/**
+ * @brief Set general inputs of the model and write them to the PMSM model
+ * 
+ * @param self Pointer to driver instance
+ * @param omega_mech value for fixed omega if simulate_mechanical_system if false
+ * @param load_torque value for load torque if simulate_mechanical_system if true
+ */
 void uz_pmsm_model9ph_dq_set_inputs_general(uz_pmsm_model9ph_dq_t *self, float omega_mech, float load_torque);
 
+/**
+ * @brief Returns general outputs of PMSM model
+ * 
+ * @param self Pointer to driver instance
+ * @return struct uz_pmsm_model9ph_dq_outputs_general_t Output values
+ */
 struct uz_pmsm_model9ph_dq_outputs_general_t uz_pmsm_model9ph_dq_get_outputs_general(uz_pmsm_model9ph_dq_t *self);
 
+/**
+ * @brief Safe function to set the input voltages of PMSM if PS is selected as source for voltages
+ * 
+ * @param self
+ * @param voltages struct of type uz_9ph_dq_t containing the voltages to set
+ */
+void uz_pmsm_model9ph_dq_set_voltage(uz_pmsm_model9ph_dq_t *self, uz_9ph_dq_t voltages);
 
+/**
+ * @brief Unsafe function to set the input voltages of PMSM if PS is selected as source for voltages
+ * 
+ * @param self
+ * @param voltages struct of type uz_9ph_dq_t containing the voltages to set
+ */
+void uz_pmsm_model9ph_dq_set_voltage_unsafe(uz_pmsm_model9ph_dq_t *self, uz_9ph_dq_t voltages);
 
+/**
+ * @brief Safe function to read out the set input voltages of PMSM. Works for voltages coming from PS and PL.
+ * 
+ * @param self
+ * @return struct of type uz_9ph_dq_t containing the set voltages
+ */
+uz_9ph_dq_t uz_pmsm_model9ph_dq_get_input_voltages(uz_pmsm_model9ph_dq_t *self);
 
+/**
+ * @brief Unsafe function to read out the set input voltages of PMSM. Works for voltages coming from PS and PL.
+ * 
+ * @param self
+ * @return struct of type uz_9ph_dq_t containing the set voltages
+ */
+uz_9ph_dq_t uz_pmsm_model9ph_dq_get_input_voltages_unsafe(uz_pmsm_model9ph_dq_t *self);
+
+/**
+ * @brief Safe function to read out the actual currents of PMSM
+ * 
+ * @param self
+ * @return struct of type uz_9ph_dq_t containing the actual currents
+ */
 uz_9ph_dq_t uz_pmsm_model9ph_dq_get_output_currents(uz_pmsm_model9ph_dq_t *self);
 
+/**
+ * @brief Unsafe function to read out the actual currents of PMSM
+ * 
+ * @param self
+ * @return struct of type uz_9ph_dq_t containing the actual currents
+ */
+uz_9ph_dq_t uz_pmsm_model9ph_dq_get_output_currents_unsafe(uz_pmsm_model9ph_dq_t *self);
 
+/**
+ * @brief Resets the PMSM model by writing zero to all inputs and sets integrators to zero
+ * 
+ * @param self Pointer to driver instance
+ */
 void uz_pmsm_model9ph_dq_reset(uz_pmsm_model9ph_dq_t *self);
 
-
-
+/**
+ * @brief Change input source for voltages (PL or PS) during runtime
+ * 
+ * @param self Pointer to driver instance
+ * @param use_axi true: voltages from PS, false: voltages from PL
+ */
 void uz_pmsm_model9ph_dq_set_use_axi_input(uz_pmsm_model9ph_dq_t *self, bool use_axi);
 
 

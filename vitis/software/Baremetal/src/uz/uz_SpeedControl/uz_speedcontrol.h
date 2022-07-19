f29c095a3c03ac9d6f26f6d25c60c5b5a6e92f60
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2022-07-19 08:43:07 +0200

    Removes all field-weakening functions from the speedcontrol
    It is, in essence, a PI-Controller wich outputs a reference torque

--- vitis/software/Baremetal/src/uz/uz_SpeedControl/uz_speedcontrol.h
+++ vitis/software/Baremetal/src/uz/uz_SpeedControl/uz_speedcontrol.h
@@ -15,8 +15,6 @@ typedef struct uz_SpeedControl_t uz_SpeedControl_t;
  * @brief Configuration struct for SpeedControl. Accessible by the user
  */
 struct uz_SpeedControl_config {
-    bool is_field_weakening_active; /**< flag to enable field_weaking */
-    uz_PMSM_t config_PMSM; /**< PMSM struct which carries necessary motor related parameters for field weakening */
 	struct uz_PI_Controller_config config_controller; /**< Configuration struct for speed-Controller */
 };
 
@@ -29,16 +27,14 @@ struct uz_SpeedControl_config {
 uz_SpeedControl_t* uz_SpeedControl_init(struct uz_SpeedControl_config config);
 
 /**
- * @brief Calculates reference currents for a SpeedControl
+ * @brief Calculates reference torque 
  * 
  * @param self pointer to uz_SpeedControl_t object
  * @param omega_m_rad_per_sec mechanical rotational speed in 1/rad
  * @param n_ref_rpm reference speed in 1/min
- * @param V_dc_volts DC-link voltage in volts
- * @param id_ref_Ampere reference d-axis current
- * @return uz_dq_t reference currents in Ampere
+ * @return reference torque in Nm
  */
-uz_3ph_dq_t uz_SpeedControl_sample(uz_SpeedControl_t* self, float omega_m_rad_per_sec, float n_ref_rpm, float V_dc_volts, float id_ref_Ampere);
+float uz_SpeedControl_sample(uz_SpeedControl_t* self, float omega_m_rad_per_sec, float n_ref_rpm);
 
 /**
  * @brief Resets the PI-Controller inside the uz_SpeedControl_t 
@@ -54,14 +50,6 @@ void uz_SpeedControl_reset(uz_SpeedControl_t* self);
  * @param ext_clamping external clamping signal. True=clamping gets activated, false=clamping gets disabled
  */
 void uz_SpeedControl_set_ext_clamping(uz_SpeedControl_t* self, bool ext_clamping);
-
-/**
- * @brief Enables or disables the field weakening
- * 
- * @param self pointer to uz_SpeedControl_t object
- * @param is_field_weakening_active status flag if field_weakening should be enabled = true or disabled
- */
-void uz_SpeedControl_set_field_weakening(uz_SpeedControl_t* self, bool is_field_weakening_active);
 
 /**
  * @brief  Function to change the Ki-value of the PI-Controller during runtime
@@ -87,13 +75,5 @@ void uz_SpeedControl_set_Kp(uz_SpeedControl_t* self, float new_Kp);
  * @param lower_limit new value for lower limit
  */
 void uz_SpeedControl_update_limits(uz_SpeedControl_t* self, float upper_limit, float lower_limit);
-
-/**
- * @brief Updates the motor related params in the uz_PMSM_t struct
- * 
- * @param self pointer to uz_SpeedControl_t object
- * @param input uz_PMSM_t struct with new values
- */
-void uz_SpeedControl_set_PMSM_config(uz_SpeedControl_t* self, uz_PMSM_t input);
 
 #endif // UZ_SPEEDCONTROL_H

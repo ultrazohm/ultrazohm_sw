2b2cffb65668101e0ff65d3b148546f236788cc7
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2022-07-26 14:14:14 +0200

    Adds unit-tests

--- vitis/software/Baremetal/src/uz/uz_setpoint/uz_setpoint.c
+++ vitis/software/Baremetal/src/uz/uz_setpoint/uz_setpoint.c
@@ -40,6 +40,7 @@ static uz_3ph_dq_t uz_SetPoint_field_weakening(uz_SetPoint_t* self, float id_ref
 static float uz_SetPoint_decide_id_ref(float I_max, float id_ref_Ampere, float id_field_weakening_Ampere, bool fw_flag);
 static float uz_SetPoint_calculate_omega_cut(uz_PMSM_t config_PMSM, float V_SV_max);
 static uz_3ph_dq_t uz_SetPoint_calculate_fw_currents(uz_SetPoint_t* self, float omega_el_rad_per_sec, float omega_cut, float V_SV_max);
+static void uz_SetPoint_assert_motor_parameters(uz_PMSM_t input, enum uz_Setpoint_motor_type motor_type);
 
 static uz_SetPoint_t* uz_SetPoint_allocation(void){
  uz_assert(instance_counter < UZ_SETPOINT_MAX_INSTANCES);
@@ -52,14 +53,15 @@ static uz_SetPoint_t* uz_SetPoint_allocation(void){
 
 uz_SetPoint_t* uz_SetPoint_init(struct uz_SetPoint_t_config config){
     uz_SetPoint_t* self = uz_SetPoint_allocation();
+    uz_SetPoint_assert_motor_parameters(config.config_PMSM, config.motor_type);
     self->config = config;
-    uz_SetPoint_set_field_weakening(self, config.is_field_weakening_active);
     return(self);
 }
 
 uz_3ph_dq_t uz_SetPoint_sample(uz_SetPoint_t* self, float omega_m_rad_per_sec, float M_ref_Nm, float id_ref_Ampere, float V_DC_Volts) {
     uz_assert_not_NULL(self);
     uz_assert(self->is_ready);
+    uz_assert(V_DC_Volts > 0.0f);
     uz_3ph_dq_t output_currents = {0};
 
     float i_ref = M_ref_Nm / (1.5f * self->config.config_PMSM.polePairs * self->config.config_PMSM.Psi_PM_Vs);
@@ -74,7 +76,7 @@ uz_3ph_dq_t uz_SetPoint_sample(uz_SetPoint_t* self, float omega_m_rad_per_sec, f
             break;
 
         case (IPMSM):
-            if(fabsf(id_ref_Ampere > 0.0f)) {
+            if(fabsf(id_ref_Ampere) > 0.0f) {
                 output_currents.q = i_ref;
                 output_currents.d = uz_signals_saturation(id_ref_Ampere, id_limit, -id_limit);
             } else { //If no input d-current is requested, calculate id-ref via MTPA
@@ -109,36 +111,13 @@ uz_3ph_dq_t uz_SetPoint_sample(uz_SetPoint_t* self, float omega_m_rad_per_sec, f
 void uz_SetPoint_set_field_weakening(uz_SetPoint_t* self, bool is_field_weakening_active) {
     uz_assert_not_NULL(self);
     uz_assert(self->is_ready);
-    //Since the object could've been initialized with the members of the uz_PMSM_t struct set to 0, all members of this struct have to be asserted again.
-    if(is_field_weakening_active) {
-        uz_assert(self->config.config_PMSM.polePairs > 0.0f);
-	    uz_assert(fmodf(self->config.config_PMSM.polePairs, 1.0f) == 0);
-	    uz_assert(self->config.config_PMSM.R_ph_Ohm > 0.0f);
-	    uz_assert(self->config.config_PMSM.I_max_Ampere > 0.0f);
-	    uz_assert(self->config.config_PMSM.Ld_Henry > 0.0f);
-	    uz_assert(self->config.config_PMSM.Lq_Henry > 0.0f);
-	    uz_assert(self->config.config_PMSM.Psi_PM_Vs >= 0.0f);
-        //Makes sure that, if IPMSM is selected, that a salience exists
-        if(self->config.motor_type == IPMSM) {
-            uz_assert(self->config.config_PMSM.Ld_Henry != self->config.config_PMSM.Lq_Henry);
-        }
-    }
 	self->config.is_field_weakening_active = is_field_weakening_active;
 }
 
 void uz_SetPoint_set_PMSM_config(uz_SetPoint_t* self, uz_PMSM_t input) {
     uz_assert_not_NULL(self);
     uz_assert(self->is_ready);
-    uz_assert(input.polePairs > 0.0f);
-	uz_assert(fmodf(input.polePairs, 1.0f) == 0);
-	uz_assert(input.R_ph_Ohm > 0.0f);
-	uz_assert(input.I_max_Ampere > 0.0f);
-	uz_assert(input.Ld_Henry > 0.0f);
-	uz_assert(input.Lq_Henry > 0.0f);
-	uz_assert(input.Psi_PM_Vs >= 0.0f);
-    if(self->config.motor_type == IPMSM) {
-            uz_assert(self->config.config_PMSM.Ld_Henry != self->config.config_PMSM.Lq_Henry);
-        }
+    uz_SetPoint_assert_motor_parameters(input, self->config.motor_type);
     self->config.config_PMSM = input;
 }
 
@@ -147,6 +126,20 @@ bool uz_SetPoint_get_ext_clamping(uz_SetPoint_t* self){
     uz_assert(self->is_ready);
     return(self->ext_clamping);
 }
+
+static void uz_SetPoint_assert_motor_parameters(uz_PMSM_t input, enum uz_Setpoint_motor_type motor_type) {
+    uz_assert(input.polePairs > 0.0f);
+	uz_assert(fmodf(input.polePairs, 1.0f) == 0);
+	uz_assert(input.R_ph_Ohm > 0.0f);
+	uz_assert(input.I_max_Ampere > 0.0f);
+	uz_assert(input.Ld_Henry > 0.0f);
+	uz_assert(input.Lq_Henry > 0.0f);
+	uz_assert(input.Psi_PM_Vs >= 0.0f);
+    if(motor_type == IPMSM) {
+        uz_assert(input.Ld_Henry != input.Lq_Henry);
+    }
+}
+
 static uz_3ph_dq_t uz_SetPoint_field_weakening(uz_SetPoint_t* self, float id_ref_Ampere, float omega_m_rad_per_sec, float V_dc_volts){
     uz_3ph_dq_t output = {0};
     bool fw_flag = false;
@@ -161,20 +154,22 @@ static uz_3ph_dq_t uz_SetPoint_field_weakening(uz_SetPoint_t* self, float id_ref
     }
 	output.d = uz_SetPoint_decide_id_ref(self->config.config_PMSM.I_max_Ampere * 0.95f, id_ref_Ampere, output.d, fw_flag);//field-weakening current for d-axis
     output.q = sqrtf(powf(self->config.config_PMSM.I_max_Ampere, 2.0f) - powf(output.d, 2.0f));//new max. ampere limit for q-axis current
+            
     return(output);
 }
 
 static float uz_SetPoint_decide_id_ref(float I_max, float id_ref_Ampere, float id_field_weakening_Ampere, bool fw_flag){
 	//Gives out the input id_ref, as long as fw is off, or the input value is lower than the needed id_fw value to reach its n_ref speed
 	float output = 0.0f;
+    
     bool id_ref_smaller_than_id_fw = id_ref_Ampere <= id_field_weakening_Ampere;
     bool id_ref_valid = fabsf(id_ref_Ampere) < I_max;
     if(fw_flag == false){
-		output = uz_signals_saturation(id_ref_Ampere, I_max, -I_max);
+		output = uz_signals_saturation(id_ref_Ampere, I_max, -I_max);      
 	} else if ( (fw_flag == true) && (id_ref_smaller_than_id_fw == true) && (id_ref_valid == true)) {
 		output = uz_signals_saturation(id_ref_Ampere, I_max, -I_max);
-    }else{
-	    output = id_field_weakening_Ampere;
+    } else {
+	    output = id_field_weakening_Ampere;    
     }
     return(output);
 }
@@ -198,12 +193,13 @@ switch (self->config.motor_type)
             output.d = uz_signals_saturation(output.d, self->config.config_PMSM.I_max_Ampere, -self->config.config_PMSM.I_max_Ampere);
             break;
 
-        case (IPMSM):
-            output.d = ((-self->config.config_PMSM.Psi_PM_Vs * self->config.config_PMSM.Ld_Henry) + (sqrtf(powf(self->config.config_PMSM.Psi_PM_Vs * self->config.config_PMSM.Ld_Henry, 2.0f) -
-             (powf(self->config.config_PMSM.Ld_Henry, 2.0f) - powf(self->config.config_PMSM.Lq_Henry, 2.0f)) * 
-             (powf(self->config.config_PMSM.Psi_PM_Vs, 2.0f) + (powf(self->config.config_PMSM.Lq_Henry, 2.0f) * powf(self->config.config_PMSM.I_max_Ampere, 2.0f)) - 
-             (powf(V_SV_max, 2.0f) / powf(omega_el_rad_per_sec, 2.0f)))))) / (powf(self->config.config_PMSM.Ld_Henry, 2.0f) - powf(self->config.config_PMSM.Lq_Henry, 2.0f));
-            break;
+        case (IPMSM):;
+        float Ld_minus_Lq = powf(self->config.config_PMSM.Ld_Henry, 2.0f) - powf(self->config.config_PMSM.Lq_Henry, 2.0f);
+        float psi_pm_times_Ld = self->config.config_PMSM.Psi_PM_Vs * self->config.config_PMSM.Ld_Henry;
+        float Lq_times_I_max = powf(self->config.config_PMSM.Lq_Henry, 2.0f) * powf(self->config.config_PMSM.I_max_Ampere, 2.0f);
+        float V_max_diff_omega = powf(V_SV_max, 2.0f) / powf(omega_el_rad_per_sec, 2.0f); 
+        output.d = (-psi_pm_times_Ld + sqrtf(powf(psi_pm_times_Ld, 2.0f) - (Ld_minus_Lq * (powf(self->config.config_PMSM.Psi_PM_Vs, 2.0f) + Lq_times_I_max - V_max_diff_omega)))) / (Ld_minus_Lq);
+        break;            
 
         default:
             uz_assert(0);
---
+++ vitis/software/Baremetal/test/uz/uz_setpoint/test_uz_setpoint.c
@@ -0,0 +1,340 @@
+#ifdef TEST
+
+#include "test_assert_with_exception.h"
+#include "unity.h"
+#include "uz_setpoint.h"
+#include "../uz_signals/uz_signals.h"
+
+struct uz_SetPoint_t_config config = {0};
+float omega_m_rad_per_sec = 0.0f;
+float M_ref_Nm = 0.045f;
+float id_ref_Ampere = 0.0f;
+float V_DC_Volts = 24.0f;
+void setUp(void)
+{
+    M_ref_Nm = 0.045f;
+    id_ref_Ampere = 0.0f;
+    config.config_PMSM.I_max_Ampere = 15.0f;
+    config.config_PMSM.Ld_Henry = 0.0003f;
+    config.config_PMSM.Lq_Henry = 0.0003f;
+    config.config_PMSM.R_ph_Ohm = 0.1f;
+    config.config_PMSM.polePairs = 4.0f;
+    config.config_PMSM.Psi_PM_Vs = 0.0075f;
+    config.motor_type = SMPMSM;
+    config.is_field_weakening_active = false;
+}
+
+void test_uz_SetPoint_init_assert_Rph_negative(void){
+    config.config_PMSM.R_ph_Ohm = -0.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_Rph_zero(void){
+    config.config_PMSM.R_ph_Ohm = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_Ld_negative(void){
+    config.config_PMSM.Ld_Henry = -0.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_Ld_zero(void){
+    config.config_PMSM.Ld_Henry = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_Lq_negative(void){
+    config.config_PMSM.Lq_Henry = -0.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_Lq_zero(void){
+    config.config_PMSM.Lq_Henry = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_Psi_pm(void){
+    config.config_PMSM.Psi_PM_Vs = -0.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_polePairs_negative(void){
+    config.config_PMSM.polePairs = -2.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_polePairs_zero(void){
+    config.config_PMSM.polePairs = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_polePairs_decimal_value(void){
+    config.config_PMSM.polePairs = 0.5f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_I_max_negative(void){
+    config.config_PMSM.I_max_Ampere = -5.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_I_max_zero(void){
+    config.config_PMSM.I_max_Ampere = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_init_assert_Ld_non_equal_Lq_for_IPMSM(void){
+    config.motor_type = IPMSM;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_init(config));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_Rph_negative(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.R_ph_Ohm = -0.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_Rph_zero(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.R_ph_Ohm = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_Ld_negative(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.Ld_Henry = -0.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_Ld_zero(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.Ld_Henry = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_Lq_negative(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.Lq_Henry = -0.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_Lq_zero(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.Lq_Henry = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_Psi_pm(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.Psi_PM_Vs = -0.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_polePairs_negative(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.polePairs = -2.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_polePairs_zero(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.polePairs = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_polePairs_decimal_value(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.polePairs = 0.5f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_I_max_negative(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.I_max_Ampere = -5.08f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_I_max_zero(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.I_max_Ampere = 0.0f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_Ld_non_equal_Lq_for_IPMSM(void){
+    config.motor_type = IPMSM;
+    config.config_PMSM.Lq_Henry = 0.0002f;
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    config.config_PMSM.Lq_Henry = 0.0003f;
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(instance, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_PMSM_config_assert_NULL(void) {
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_PMSM_config(NULL, config.config_PMSM));
+}
+
+void test_uz_SetPoint_set_field_weakening_assert_NULL(void) {
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_set_field_weakening(NULL, true));
+}
+
+void test_uz_SetPoint_sample_NULL(void){
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_sample(NULL, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts));
+}
+
+void test_uz_SetPoint_sample_V_DC_greater_than_zero(void){
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    TEST_ASSERT_FAIL_ASSERT(uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, 0.0f));
+}
+
+void test_uz_SetPoint_sample_MTPA_SMPMSM_operation_no_id(void){
+    //Results for comparision from simulation
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_EQUAL_FLOAT(1.0f, output.q);
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, output.d);
+    M_ref_Nm = 0.09f;
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_EQUAL_FLOAT(2.0f, output.q);
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, output.d);
+}
+
+void test_uz_SetPoint_sample_MTPA_SMPMSM_operation_manual_id(void){
+    //Results for comparision from simulation
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    id_ref_Ampere = -1.23f;
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_EQUAL_FLOAT(1.0f, output.q);
+    TEST_ASSERT_EQUAL_FLOAT(-1.23f, output.d);
+    M_ref_Nm = 0.09f;
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_EQUAL_FLOAT(2.0f, output.q);
+    TEST_ASSERT_EQUAL_FLOAT(-1.23f, output.d);
+}
+
+void test_uz_SetPoint_sample_MTPA_IPMSM_operation_no_id(void){
+    //Results for comparision from simulation
+    config.motor_type = IPMSM;
+    config.config_PMSM.Ld_Henry = 0.0002f;
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 1.0f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03,-0.0133f, output.d);
+    M_ref_Nm = 0.09f;
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 2.0f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03,-0.0533f, output.d);
+}
+
+void test_uz_SetPoint_sample_MTPA_IPMSM_operation_manual_id(void){
+    //Results for comparision from simulation
+    config.motor_type = IPMSM;
+    config.config_PMSM.Ld_Henry = 0.0002f;
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    id_ref_Ampere = -5.23f;
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 1.0f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03,-5.23f, output.d);
+    M_ref_Nm = 0.09f;
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 2.0f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03,-5.23f, output.d);
+}
+
+void test_uz_SetPoint_sample_MTPA_SMPMSM_operation_limit_iq_id(void){
+    //Results for comparision from simulation
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    id_ref_Ampere = -5.23f;
+    M_ref_Nm = 2.0f;
+    //Since reference torque is too high, output will be limited. Priority is given to q-current
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 15.0f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03,0.0f, output.d);
+}
+
+void test_uz_SetPoint_sample_MTPA_IPMSM_operation_limit_iq_id(void){
+    //Results for comparision from simulation
+    config.motor_type = IPMSM;
+    config.config_PMSM.Ld_Henry = 0.0002f;
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    id_ref_Ampere = -5.23f;
+    M_ref_Nm = 2.0f;
+    //Since reference torque is too high, output will be limited. Priority is given to q-current
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 15.0f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03,0.0f, output.d);
+    id_ref_Ampere = 0.0f;
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 14.7379f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03,-2.7921f, output.d);
+}
+
+void test_uz_SetPoint_sample_field_weakening_SMPMSM_operation(void){
+    //Results for comparision from simulation
+    config.is_field_weakening_active = true;
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    M_ref_Nm = 0.09275f;
+    omega_m_rad_per_sec = 418.75f;
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 2.061f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -5.007f, output.d);
+    //Negative speed
+    M_ref_Nm = -0.09275f;
+    omega_m_rad_per_sec = -418.75f;
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -2.061f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -5.007f, output.d);  
+}
+
+void test_uz_SetPoint_sample_field_weakening_SMPMSM_manual_id(void){
+    //Results for comparision from simulation
+    config.is_field_weakening_active = true;
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    M_ref_Nm = 0.09275f;
+    omega_m_rad_per_sec = 418.75f;
+    id_ref_Ampere = 5.0f; //Should be rejected, since its above the id_fw current
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 2.061f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -5.007f, output.d);
+    id_ref_Ampere = -10.0f; //Should be accepted, since its below the id_fw current
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 2.061f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -10.0f, output.d);  
+}
+
+void test_uz_SetPoint_sample_field_weakening_IPMSM_operation(void){
+    //Results for comparision from simulation
+    config.is_field_weakening_active = true;
+    config.config_PMSM.Ld_Henry = 0.0002f;
+    config.motor_type = IPMSM;
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    M_ref_Nm = 0.08705f;
+    omega_m_rad_per_sec = 419.0f;
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 1.934f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -4.9582f, output.d);
+    //Negative speed
+    M_ref_Nm = -0.08705f;
+    omega_m_rad_per_sec = -419.0f;
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -1.934f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -4.9582f, output.d);  
+}
+
+void test_uz_SetPoint_sample_field_weakening_IPMSM_operation_manual_id(void){
+    //Results for comparision from simulation
+    config.is_field_weakening_active = true;
+    config.config_PMSM.Ld_Henry = 0.0002f;
+    config.motor_type = IPMSM;
+    uz_SetPoint_t* instance = uz_SetPoint_init(config);
+    M_ref_Nm = 0.08705f;
+    omega_m_rad_per_sec = 419.0f;
+    id_ref_Ampere = 5.0f; //Should be rejected, since its above the id_fw current
+    uz_3ph_dq_t output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 1.934f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -4.9582f, output.d);
+    id_ref_Ampere = -10.0f; //Should be accepted, since its below the id_fw current
+    output = uz_SetPoint_sample(instance, omega_m_rad_per_sec, M_ref_Nm, id_ref_Ampere, V_DC_Volts);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, 1.934f, output.q);
+    TEST_ASSERT_FLOAT_WITHIN(1e-03, -10.0f, output.d);  
+}
+#endif // TEST

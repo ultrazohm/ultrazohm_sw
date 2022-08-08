4d884c171578108da7c1d01ceb8f431452f7faed
hoernermi <michael.hoerner@th-nuernberg.de>
2022-08-08 10:16:22 +0200

    added vsd_6ph ip core for testing purposes into block design

---
+++ ip_cores/uz_vsd_6ph_30deg/hdl_prj/hdlsrc/VSD_6ph_30deg_HDL/VSD_6ph_ip_src_VSD_6ph_30deg_compile.do
@@ -0,0 +1,5 @@
+vlib work
+vcom  VSD_6ph_ip_src_VSD_6ph_30deg_pkg.vhd
+vcom  VSD_6ph_ip_src_Detect_Rise_Positive.vhd
+vcom  VSD_6ph_ip_src_VSD_6ph_30deg_block.vhd
+vcom  VSD_6ph_ip_src_VSD_6ph_30deg.vhd
---
+++ ip_cores/uz_vsd_6ph_30deg/hdl_prj/hdlsrc/VSD_6ph_30deg_HDL/html/VSD_6ph_30deg_HDL_traceInfo.js
@@ -0,0 +1,173 @@
+function RTW_Sid2UrlHash() {
+	this.urlHashMap = new Array();
+	/* <S2>/Delay */
+	this.urlHashMap["VSD_6ph_30deg_HDL:71"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:183,184,185,186,187,188,189,190,191,192";
+	/* <S2>/Delay1 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:73"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:205,206,207,208,209,210,211,212,213,214";
+	/* <S2>/Delay2 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:75"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:227,228,229,230,231,232,233,234,235,236";
+	/* <S2>/Delay3 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:77"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:249,250,251,252,253,254,255,256,257,258";
+	/* <S2>/Delay4 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:79"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:271,272,273,274,275,276,277,278,279,280";
+	/* <S2>/Delay5 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:81"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:293,294,295,296,297,298,299,300,301,302";
+	/* <S2>/Detect Rise
+Positive */
+	this.urlHashMap["VSD_6ph_30deg_HDL:40"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:152,153,154,155,156,157,158";
+	/* <S2>/Switch */
+	this.urlHashMap["VSD_6ph_30deg_HDL:72"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:196,197";
+	/* <S2>/Switch1 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:74"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:218,219";
+	/* <S2>/Switch2 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:76"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:240,241";
+	/* <S2>/Switch3 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:78"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:262,263";
+	/* <S2>/Switch4 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:80"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:284,285";
+	/* <S2>/Switch5 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:82"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:306,307";
+	/* <S2>/VSD_6ph_30deg */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30"] = "VSD_6ph_ip_src_VSD_6ph_30deg.vhd:160,161,162,163,164,165,166,167,168,169,170,171,172,173";
+	/* <S17>/Delay Input1 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:40:2"] = "VSD_6ph_ip_src_Detect_Rise_Positive.vhd:49,50,51,52,53,54,55,56,57,58";
+	/* <S17>/FixPt
+Relational
+Operator */
+	this.urlHashMap["VSD_6ph_30deg_HDL:40:3"] = "VSD_6ph_ip_src_Detect_Rise_Positive.vhd:62,63";
+	/* <S17>/Positive */
+	this.urlHashMap["VSD_6ph_30deg_HDL:40:4"] = "VSD_6ph_ip_src_Detect_Rise_Positive.vhd:45,46";
+	/* <S18>:1 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30:1"] = "VSD_6ph_ip_src_VSD_6ph_30deg_block.vhd:90";
+	/* <S18>:1:6 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30:1:6"] = "VSD_6ph_ip_src_VSD_6ph_30deg_block.vhd:91,92,93,94,95,96,98,99,101,102,103,104,105,106,107,108,109,110,111,112,113,115";
+	/* <S18>:1:13 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30:1:13"] = "msg=rtwMsg_optimizedSfObject&block=VSD_6ph_30deg_HDL:30:1:13";
+	/* <S18>:1:14 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30:1:14"] = "msg=rtwMsg_optimizedSfObject&block=VSD_6ph_30deg_HDL:30:1:14";
+	/* <S18>:1:15 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30:1:15"] = "msg=rtwMsg_optimizedSfObject&block=VSD_6ph_30deg_HDL:30:1:15";
+	/* <S18>:1:16 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30:1:16"] = "msg=rtwMsg_optimizedSfObject&block=VSD_6ph_30deg_HDL:30:1:16";
+	/* <S18>:1:17 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30:1:17"] = "msg=rtwMsg_optimizedSfObject&block=VSD_6ph_30deg_HDL:30:1:17";
+	/* <S18>:1:18 */
+	this.urlHashMap["VSD_6ph_30deg_HDL:30:1:18"] = "VSD_6ph_ip_src_VSD_6ph_30deg_block.vhd:117,118,119,120,121,122";
+	/* <S19>/Compare */
+	this.urlHashMap["VSD_6ph_30deg_HDL:40:4:2"] = "msg=rtwMsg_notTraceable&block=VSD_6ph_30deg_HDL:40:4:2";
+	/* <S19>/Constant */
+	this.urlHashMap["VSD_6ph_30deg_HDL:40:4:3"] = "msg=rtwMsg_notTraceable&block=VSD_6ph_30deg_HDL:40:4:3";
+	this.getUrlHash = function(sid) { return this.urlHashMap[sid];}
+}
+RTW_Sid2UrlHash.instance = new RTW_Sid2UrlHash();
+function RTW_rtwnameSIDMap() {
+	this.rtwnameHashMap = new Array();
+	this.sidHashMap = new Array();
+	this.rtwnameHashMap["<Root>"] = {sid: "VSD_6ph_30deg_HDL"};
+	this.sidHashMap["VSD_6ph_30deg_HDL"] = {rtwname: "<Root>"};
+	this.rtwnameHashMap["<S2>/a1"] = {sid: "VSD_6ph_30deg_HDL:8"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:8"] = {rtwname: "<S2>/a1"};
+	this.rtwnameHashMap["<S2>/b1"] = {sid: "VSD_6ph_30deg_HDL:9"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:9"] = {rtwname: "<S2>/b1"};
+	this.rtwnameHashMap["<S2>/c1"] = {sid: "VSD_6ph_30deg_HDL:36"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:36"] = {rtwname: "<S2>/c1"};
+	this.rtwnameHashMap["<S2>/a2"] = {sid: "VSD_6ph_30deg_HDL:37"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:37"] = {rtwname: "<S2>/a2"};
+	this.rtwnameHashMap["<S2>/b2"] = {sid: "VSD_6ph_30deg_HDL:38"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:38"] = {rtwname: "<S2>/b2"};
+	this.rtwnameHashMap["<S2>/c2"] = {sid: "VSD_6ph_30deg_HDL:39"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:39"] = {rtwname: "<S2>/c2"};
+	this.rtwnameHashMap["<S2>/pu_conv_valid"] = {sid: "VSD_6ph_30deg_HDL:41"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:41"] = {rtwname: "<S2>/pu_conv_valid"};
+	this.rtwnameHashMap["<S2>/Delay"] = {sid: "VSD_6ph_30deg_HDL:71"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:71"] = {rtwname: "<S2>/Delay"};
+	this.rtwnameHashMap["<S2>/Delay1"] = {sid: "VSD_6ph_30deg_HDL:73"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:73"] = {rtwname: "<S2>/Delay1"};
+	this.rtwnameHashMap["<S2>/Delay2"] = {sid: "VSD_6ph_30deg_HDL:75"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:75"] = {rtwname: "<S2>/Delay2"};
+	this.rtwnameHashMap["<S2>/Delay3"] = {sid: "VSD_6ph_30deg_HDL:77"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:77"] = {rtwname: "<S2>/Delay3"};
+	this.rtwnameHashMap["<S2>/Delay4"] = {sid: "VSD_6ph_30deg_HDL:79"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:79"] = {rtwname: "<S2>/Delay4"};
+	this.rtwnameHashMap["<S2>/Delay5"] = {sid: "VSD_6ph_30deg_HDL:81"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:81"] = {rtwname: "<S2>/Delay5"};
+	this.rtwnameHashMap["<S2>/Detect Rise Positive"] = {sid: "VSD_6ph_30deg_HDL:40"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40"] = {rtwname: "<S2>/Detect Rise Positive"};
+	this.rtwnameHashMap["<S2>/Switch"] = {sid: "VSD_6ph_30deg_HDL:72"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:72"] = {rtwname: "<S2>/Switch"};
+	this.rtwnameHashMap["<S2>/Switch1"] = {sid: "VSD_6ph_30deg_HDL:74"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:74"] = {rtwname: "<S2>/Switch1"};
+	this.rtwnameHashMap["<S2>/Switch2"] = {sid: "VSD_6ph_30deg_HDL:76"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:76"] = {rtwname: "<S2>/Switch2"};
+	this.rtwnameHashMap["<S2>/Switch3"] = {sid: "VSD_6ph_30deg_HDL:78"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:78"] = {rtwname: "<S2>/Switch3"};
+	this.rtwnameHashMap["<S2>/Switch4"] = {sid: "VSD_6ph_30deg_HDL:80"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:80"] = {rtwname: "<S2>/Switch4"};
+	this.rtwnameHashMap["<S2>/Switch5"] = {sid: "VSD_6ph_30deg_HDL:82"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:82"] = {rtwname: "<S2>/Switch5"};
+	this.rtwnameHashMap["<S2>/VSD_6ph_30deg"] = {sid: "VSD_6ph_30deg_HDL:30"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30"] = {rtwname: "<S2>/VSD_6ph_30deg"};
+	this.rtwnameHashMap["<S2>/alpha"] = {sid: "VSD_6ph_30deg_HDL:10"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:10"] = {rtwname: "<S2>/alpha"};
+	this.rtwnameHashMap["<S2>/beta"] = {sid: "VSD_6ph_30deg_HDL:31"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:31"] = {rtwname: "<S2>/beta"};
+	this.rtwnameHashMap["<S2>/x1"] = {sid: "VSD_6ph_30deg_HDL:32"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:32"] = {rtwname: "<S2>/x1"};
+	this.rtwnameHashMap["<S2>/y1"] = {sid: "VSD_6ph_30deg_HDL:33"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:33"] = {rtwname: "<S2>/y1"};
+	this.rtwnameHashMap["<S2>/z1"] = {sid: "VSD_6ph_30deg_HDL:34"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:34"] = {rtwname: "<S2>/z1"};
+	this.rtwnameHashMap["<S2>/z2"] = {sid: "VSD_6ph_30deg_HDL:35"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:35"] = {rtwname: "<S2>/z2"};
+	this.rtwnameHashMap["<S2>/vsd_done"] = {sid: "VSD_6ph_30deg_HDL:42"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:42"] = {rtwname: "<S2>/vsd_done"};
+	this.rtwnameHashMap["<S2>/alpha_AXI"] = {sid: "VSD_6ph_30deg_HDL:97"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:97"] = {rtwname: "<S2>/alpha_AXI"};
+	this.rtwnameHashMap["<S2>/beta_AXI"] = {sid: "VSD_6ph_30deg_HDL:98"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:98"] = {rtwname: "<S2>/beta_AXI"};
+	this.rtwnameHashMap["<S2>/x_AXI"] = {sid: "VSD_6ph_30deg_HDL:99"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:99"] = {rtwname: "<S2>/x_AXI"};
+	this.rtwnameHashMap["<S2>/y_AXI"] = {sid: "VSD_6ph_30deg_HDL:100"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:100"] = {rtwname: "<S2>/y_AXI"};
+	this.rtwnameHashMap["<S2>/z1_AXI"] = {sid: "VSD_6ph_30deg_HDL:101"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:101"] = {rtwname: "<S2>/z1_AXI"};
+	this.rtwnameHashMap["<S2>/z2_AXI"] = {sid: "VSD_6ph_30deg_HDL:102"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:102"] = {rtwname: "<S2>/z2_AXI"};
+	this.rtwnameHashMap["<S17>/U"] = {sid: "VSD_6ph_30deg_HDL:40:1"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:1"] = {rtwname: "<S17>/U"};
+	this.rtwnameHashMap["<S17>/Delay Input1"] = {sid: "VSD_6ph_30deg_HDL:40:2"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:2"] = {rtwname: "<S17>/Delay Input1"};
+	this.rtwnameHashMap["<S17>/FixPt Relational Operator"] = {sid: "VSD_6ph_30deg_HDL:40:3"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:3"] = {rtwname: "<S17>/FixPt Relational Operator"};
+	this.rtwnameHashMap["<S17>/Positive"] = {sid: "VSD_6ph_30deg_HDL:40:4"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:4"] = {rtwname: "<S17>/Positive"};
+	this.rtwnameHashMap["<S17>/Y"] = {sid: "VSD_6ph_30deg_HDL:40:5"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:5"] = {rtwname: "<S17>/Y"};
+	this.rtwnameHashMap["<S18>:1"] = {sid: "VSD_6ph_30deg_HDL:30:1"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30:1"] = {rtwname: "<S18>:1"};
+	this.rtwnameHashMap["<S18>:1:6"] = {sid: "VSD_6ph_30deg_HDL:30:1:6"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30:1:6"] = {rtwname: "<S18>:1:6"};
+	this.rtwnameHashMap["<S18>:1:13"] = {sid: "VSD_6ph_30deg_HDL:30:1:13"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30:1:13"] = {rtwname: "<S18>:1:13"};
+	this.rtwnameHashMap["<S18>:1:14"] = {sid: "VSD_6ph_30deg_HDL:30:1:14"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30:1:14"] = {rtwname: "<S18>:1:14"};
+	this.rtwnameHashMap["<S18>:1:15"] = {sid: "VSD_6ph_30deg_HDL:30:1:15"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30:1:15"] = {rtwname: "<S18>:1:15"};
+	this.rtwnameHashMap["<S18>:1:16"] = {sid: "VSD_6ph_30deg_HDL:30:1:16"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30:1:16"] = {rtwname: "<S18>:1:16"};
+	this.rtwnameHashMap["<S18>:1:17"] = {sid: "VSD_6ph_30deg_HDL:30:1:17"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30:1:17"] = {rtwname: "<S18>:1:17"};
+	this.rtwnameHashMap["<S18>:1:18"] = {sid: "VSD_6ph_30deg_HDL:30:1:18"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:30:1:18"] = {rtwname: "<S18>:1:18"};
+	this.rtwnameHashMap["<S19>/u"] = {sid: "VSD_6ph_30deg_HDL:40:4:1"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:4:1"] = {rtwname: "<S19>/u"};
+	this.rtwnameHashMap["<S19>/Compare"] = {sid: "VSD_6ph_30deg_HDL:40:4:2"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:4:2"] = {rtwname: "<S19>/Compare"};
+	this.rtwnameHashMap["<S19>/Constant"] = {sid: "VSD_6ph_30deg_HDL:40:4:3"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:4:3"] = {rtwname: "<S19>/Constant"};
+	this.rtwnameHashMap["<S19>/y"] = {sid: "VSD_6ph_30deg_HDL:40:4:5"};
+	this.sidHashMap["VSD_6ph_30deg_HDL:40:4:5"] = {rtwname: "<S19>/y"};
+	this.getSID = function(rtwname) { return this.rtwnameHashMap[rtwname];}
+	this.getRtwname = function(sid) { return this.sidHashMap[sid];}
+}
+RTW_rtwnameSIDMap.instance = new RTW_rtwnameSIDMap();

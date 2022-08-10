a00d36a42d0c99bc9a11492aac1be6f7f33b4424
hoernermi <michael.hoerner@th-nuernberg.de>
2022-08-10 16:13:28 +0200

    new bitstream with correct adc slicing

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
---Date        : Wed Aug 10 10:26:45 2022
+--Date        : Wed Aug 10 15:38:40 2022
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

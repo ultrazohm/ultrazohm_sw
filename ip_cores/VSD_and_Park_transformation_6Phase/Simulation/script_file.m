9ef8b71bcd3ea2971fbcade5a1d6541b050b4230
merxan71462 <merxan71462@th-nuernberg.de>
2022-08-12 14:21:44 +0200

    no message

--- ip_cores/VSD_and_Park_transformation_6Phase/Simulation/script_file.m
+++ ip_cores/VSD_and_Park_transformation_6Phase/Simulation/script_file.m
@@ -1,6 +1,5 @@
 clc;        % command window löschen 
 clear;      % alle Variablen löschen 
-close all; % alle Fenster schließen
 
 hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath',...
  'C:\Xilinx\Vivado\2020.1\bin\vivado.bat');

44eb3f407b9f0e669c96e7b3cb39ae266f1ab8fb
merxan71462 <merxan71462@th-nuernberg.de>
2022-08-22 13:25:41 +0200

    Started writing IP-Core driver and tests

---
+++ vitis/software/Baremetal/test/IP_Cores/uz_delay_compensation_fcs_mpc_6phase_pmsm/test_uz_delay_compensation_fcs_mpc_6phase_pmsm.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "uz_delay_compensation_fcs_mpc_6phase_pmsm.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_uz_delay_compensation_fcs_mpc_6phase_pmsm_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement uz_delay_compensation_fcs_mpc_6phase_pmsm");
+}
+
+#endif // TEST

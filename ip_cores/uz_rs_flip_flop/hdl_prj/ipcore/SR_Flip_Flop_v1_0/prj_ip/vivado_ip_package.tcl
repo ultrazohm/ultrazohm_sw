d94a3d757c09ea692ec3ded833f3da00ac005b4c
HoppeVa <hoppeva70971@th-nuernberg.de>
2022-09-27 19:40:22 +0200

    change naming to uz_rs_flip_flop

---
+++ ip_cores/uz_rs_flip_flop/hdl_prj/ipcore/SR_Flip_Flop_v1_0/prj_ip/vivado_ip_package.tcl
@@ -0,0 +1,95 @@
+create_project prj_ip {} -part xazu11eg-ffvf1517-1-i -force
+set_property ip_repo_paths {../../} [current_fileset]
+
+# Add HDL source files to project
+add_files -norecurse {../hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop_pkg.vhd}
+add_files -norecurse {../hdl/vhdl/SR_Flip_Flop_src_S_R_Flip_Flop.vhd}
+add_files -norecurse {../hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop.vhd}
+add_files -norecurse {../hdl/vhdl/SR_Flip_Flop_reset_sync.vhd}
+add_files -norecurse {../hdl/vhdl/SR_Flip_Flop_dut.vhd}
+add_files -norecurse {../hdl/vhdl/SR_Flip_Flop.vhd}
+set_property top SR_Flip_Flop [get_filesets sources_1]
+set_property top SR_Flip_Flop [get_filesets sim_1]
+update_compile_order -fileset sources_1
+update_compile_order -fileset sim_1
+
+# Package IP from project
+ipx::package_project -root_dir {../} -force
+
+# Set IP properties
+set_property name SR_Flip_Flop [ipx::current_core]
+set_property display_name SR_Flip_Flop [ipx::current_core]
+set_property version {1.0} [ipx::current_core]
+set_property library {ip} [ipx::current_core]
+set Families [split [lsort -uniq [get_property C_FAMILY [get_parts]]]]
+set IPSupportedFamily {}
+foreach family $Families {append IPSupportedFamily "{$family} {Production} "}
+set_property supported_families $IPSupportedFamily [ipx::current_core]
+set_property taxonomy {{/HDL Coder Generated IP}} [ipx::current_core]
+set_property description {HDL Coder generated IP} [ipx::current_core]
+set_property core_revision 0001000000 [ipx::current_core]
+
+# Add HDL source files to IP
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop_pkg.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop_pkg.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop_pkg.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop_pkg.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_src_S_R_Flip_Flop.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_src_S_R_Flip_Flop.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_src_S_R_Flip_Flop.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_src_S_R_Flip_Flop.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_src_uz_rs_flip_flop.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_reset_sync.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_reset_sync.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_reset_sync.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_reset_sync.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_dut.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_dut.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop_dut.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop_dut.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/SR_Flip_Flop.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/SR_Flip_Flop.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+
+# Add bus interfaces
+ipx::remove_all_bus_interface [ipx::current_core]
+ipx::remove_all_memory_map [ipx::current_core]
+ipx::remove_all_address_space [ipx::current_core]
+
+# Add IP clock and reset definition
+ipx::add_bus_interface {IPCORE_CLK} [ipx::current_core]
+set_property abstraction_type_vlnv {xilinx.com:signal:clock_rtl:1.0} [ipx::get_bus_interfaces IPCORE_CLK -of_objects [ipx::current_core]]
+set_property bus_type_vlnv {xilinx.com:signal:clock:1.0} [ipx::get_bus_interfaces IPCORE_CLK -of_objects [ipx::current_core]]
+ipx::add_port_map {CLK} [ipx::get_bus_interfaces IPCORE_CLK -of_objects [ipx::current_core]]
+set_property physical_name {IPCORE_CLK} [ipx::get_port_maps CLK -of_objects [ipx::get_bus_interfaces IPCORE_CLK -of_objects [ipx::current_core]]]
+ipx::add_bus_interface {IPCORE_RESETN} [ipx::current_core]
+set_property abstraction_type_vlnv {xilinx.com:signal:reset_rtl:1.0} [ipx::get_bus_interfaces IPCORE_RESETN -of_objects [ipx::current_core]]
+set_property bus_type_vlnv {xilinx.com:signal:reset:1.0} [ipx::get_bus_interfaces IPCORE_RESETN -of_objects [ipx::current_core]]
+ipx::add_port_map {RST} [ipx::get_bus_interfaces IPCORE_RESETN -of_objects [ipx::current_core]]
+set_property physical_name {IPCORE_RESETN} [ipx::get_port_maps RST -of_objects [ipx::get_bus_interfaces IPCORE_RESETN -of_objects [ipx::current_core]]]
+ipx::add_bus_parameter {ASSOCIATED_RESET} [ipx::get_bus_interfaces IPCORE_CLK -of_objects [ipx::current_core]]
+set_property value {IPCORE_RESETN} [ipx::get_bus_parameters ASSOCIATED_RESET -of_objects [ipx::get_bus_interfaces IPCORE_CLK -of_objects [ipx::current_core]]]
+ipx::add_bus_parameter {POLARITY} [ipx::get_bus_interfaces IPCORE_RESETN -of_objects [ipx::current_core]]
+set_property value {ACTIVE_LOW} [ipx::get_bus_parameters POLARITY -of_objects [ipx::get_bus_interfaces IPCORE_RESETN -of_objects [ipx::current_core]]]
+
+
+# Add report files
+ipx::add_file_group -type {product_guide} {} [ipx::current_core]
+ipx::add_file {doc/doc_arch_axi4_lite.jpg} [ipx::get_file_groups xilinx_productguide -of_objects [ipx::current_core]]
+set_property type {{image}} [ipx::get_files {doc/doc_arch_axi4_lite.jpg} -of_objects [ipx::get_file_groups xilinx_productguide -of_objects [ipx::current_core]]]
+ipx::add_file {doc/free_running.jpg} [ipx::get_file_groups xilinx_productguide -of_objects [ipx::current_core]]
+set_property type {{image}} [ipx::get_files {doc/free_running.jpg} -of_objects [ipx::get_file_groups xilinx_productguide -of_objects [ipx::current_core]]]
+ipx::add_file {doc/uz_rs_flip_flop_ip_core_report.html} [ipx::get_file_groups xilinx_productguide -of_objects [ipx::current_core]]
+set_property type {{html}} [ipx::get_files {doc/uz_rs_flip_flop_ip_core_report.html} -of_objects [ipx::get_file_groups xilinx_productguide -of_objects [ipx::current_core]]]
+ipx::create_xgui_files [ipx::current_core]
+ipx::check_integrity -quiet [ipx::current_core]
+ipx::save_core [ipx::current_core]
+
+# Package IP
+ipx::archive_core {../SR_Flip_Flop_v1_0.zip} [ipx::current_core]
+close_project
+exit

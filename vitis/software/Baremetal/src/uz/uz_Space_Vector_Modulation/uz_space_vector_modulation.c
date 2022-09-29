88811b729e8910033f2bcbe93934cc69de06efa7
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2022-09-29 09:44:41 +0200

    Adds define for constants & small improvements

--- vitis/software/Baremetal/src/uz/uz_Space_Vector_Modulation/uz_space_vector_modulation.c
+++ vitis/software/Baremetal/src/uz/uz_Space_Vector_Modulation/uz_space_vector_modulation.c
@@ -19,115 +19,121 @@
 #include "../uz_signals/uz_signals.h"
 #include <math.h>
 
+#define ONE_DIVIDED_BY_SQRT_THREE  (1.0f / sqrtf(3.0f))
+#define TWO_DIVIDED_BY_SQRT_THREE  (2.0f / sqrtf(3.0f))
+#define TWO_DIVIDED_BY_THREE (2.0f/3.0f) 
+
 struct uz_DutyCycle_t  uz_Space_Vector_Modulation(uz_3ph_dq_t v_ref_Volts, float V_DC_Volts, float theta_el_rad) {
+	struct uz_DutyCycle_t output = {0};
+	uz_3ph_alphabeta_t reference = uz_transformation_3ph_dq_to_alphabeta(v_ref_Volts, theta_el_rad);
+	float V_DC_scaling = 1.0f / (V_DC_Volts * TWO_DIVIDED_BY_THREE);
+	float abs_alpha = fabsf(reference.alpha);
+	float abs_beta = fabsf(reference.beta);
+	float a = (abs_alpha + (ONE_DIVIDED_BY_SQRT_THREE * abs_beta)) * V_DC_scaling;
+	float b = (abs_alpha - (ONE_DIVIDED_BY_SQRT_THREE * abs_beta)) * V_DC_scaling;
+	float c = (TWO_DIVIDED_BY_SQRT_THREE * abs_beta) * V_DC_scaling;
+	uint32_t quadrant = 0U;
+	uint32_t sector = 0U;
 
-struct uz_DutyCycle_t output = {0};   
-uz_3ph_alphabeta_t reference = uz_transformation_3ph_dq_to_alphabeta(v_ref_Volts, theta_el_rad);
-float V_DC_scaling = V_DC_Volts * (2.0f / 3.0f);	
-float a = (fabsf(reference.alpha) + (1.0f / sqrtf(3.0f)) * fabsf(reference.beta)) / V_DC_scaling;
-float b = (fabsf(reference.alpha) - (1.0f / sqrtf(3.0f)) * fabsf(reference.beta)) / V_DC_scaling;
-float c = (2.0f / sqrtf(3.0f)) * fabsf(reference.beta) / V_DC_scaling;
-uint16_t quadrant = 0U;
-uint16_t sector = 0U;
+	//Determine the sector&quadrant of the space vector
+	if(reference.beta < 0.0f) {
+		if(reference.alpha < 0.0f) {
+			quadrant = 3U;
+			if(b < 0.0f) {
+				sector = 5U;
+			} else {
+				sector = 4U;
+			}
+		} else {
+			quadrant = 4U;
+			if(b < 0.0f) {
+				sector = 5U;
+			} else {
+				sector = 6U;
+			}
+		}
+	} else {
+		if(reference.alpha < 0.0f) {
+			quadrant = 2U;
+			if(b < 0.0f) {
+				sector = 2U;
+			} else {
+            	ector = 3U;
+			}
+		} else {
+			quadrant = 1U;
+			if(b < 0.0f) {
+				sector = 2U;
+			} else {
+				sector = 1U;
+			}
+		}
+	}
 
-//Determine the sector&quadrant of the space vector 
-if(reference.beta < 0.0f) {
-    if(reference.alpha < 0.0f) {
-        quadrant = 3U;
-        if(b < 0.0f) {
-            sector = 5U;
-        } else {
-            sector = 4U;
-        }
-    } else {
-        quadrant = 4U;
-        if(b < 0.0f) {
-            sector = 5U;
-        } else {
-            sector = 6U;
-        } 
+	//Calculate output DutyCycle
+	switch(sector) {
+
+    	case 1U:
+    		output.DutyCycle_A = (1.0f + b + c);
+    		output.DutyCycle_B = (1.0f - b + c);
+    		output.DutyCycle_C = (1.0f - b - c);
+        break;
+        
+        case 2U:
+            if(quadrant == 1U) {
+                output.DutyCycle_A = (1.0f + a + b);
+                output.DutyCycle_B = (1.0f + a - b);
+                output.DutyCycle_C = (1.0f - a + b);
+            } else {
+                output.DutyCycle_A = (1.0f - a - b);
+                output.DutyCycle_B = (1.0f + a - b);
+                output.DutyCycle_C = (1.0f - a + b);
+            }
+        break;
+
+        case 3U:
+            output.DutyCycle_A = (1.0f - b - c);
+            output.DutyCycle_B = (1.0f + b + c);
+            output.DutyCycle_C = (1.0f + b - c);
+        break;
+
+        case 4U:
+            output.DutyCycle_A = (1.0f - b - c);
+            output.DutyCycle_B = (1.0f + b - c);
+            output.DutyCycle_C = (1.0f + b + c);
+        break;
+
+        case 5U:
+            if(quadrant == 3U) {
+                output.DutyCycle_A = (1.0f - a - b);
+                output.DutyCycle_B = (1.0f - a + b);
+                output.DutyCycle_C = (1.0f + a - b);
+            } else {
+                output.DutyCycle_A = (1.0f + b + a);
+                output.DutyCycle_B = (1.0f + b - a);
+                output.DutyCycle_C = (1.0f - b + a);
+            }
+        break;
+
+        case 6U:
+            output.DutyCycle_A = (1.0f + b + c);
+            output.DutyCycle_B = (1.0f - b - c);
+            output.DutyCycle_C = (1.0f - b + c);
+        break;
+
+        default:
+            uz_assert(0);
+        break;
     }
-} else {
-    if(reference.alpha < 0.0f) {
-        quadrant = 2U;
-        if(b < 0.0f) {
-            sector = 2U;
-        } else {
-            sector = 3U;
-        }
-    } else {
-        quadrant = 1U;
-        if(b < 0.0f) {
-            sector = 2U;
-        } else {
-            sector = 1U;
-        }
-    }
-}
 
-//Calculate output DutyCycle
-switch(sector) {
+    output.DutyCycle_A *= 0.5f;
+    output.DutyCycle_B *= 0.5f;
+    output.DutyCycle_C *= 0.5f;
 
-    case 1U:
-        output.DutyCycle_A = (1.0f + b + c);
-        output.DutyCycle_B = (1.0f - b + c);
-        output.DutyCycle_C = (1.0f - b - c);
-        break;
-    
-    case 2U:
-        if(quadrant == 1U) {
-            output.DutyCycle_A = (1.0f + a + b);
-            output.DutyCycle_B = (1.0f + a - b);
-            output.DutyCycle_C = (1.0f - a + b);
-        } else {
-            output.DutyCycle_A = (1.0f - a - b);
-            output.DutyCycle_B = (1.0f + a - b);
-            output.DutyCycle_C = (1.0f - a + b);
-        }
-        break;
+    output.DutyCycle_A = uz_signals_saturation(output.DutyCycle_A, 1.0f, 0.0f);
+    output.DutyCycle_B = uz_signals_saturation(output.DutyCycle_B, 1.0f, 0.0f);
+    output.DutyCycle_C = uz_signals_saturation(output.DutyCycle_C, 1.0f, 0.0f);
 
-    case 3U:
-        output.DutyCycle_A = (1.0f - b - c);
-        output.DutyCycle_B = (1.0f + b + c);
-        output.DutyCycle_C = (1.0f + b - c);
-        break;
-
-    case 4U:
-        output.DutyCycle_A = (1.0f - b - c);
-        output.DutyCycle_B = (1.0f + b - c);
-        output.DutyCycle_C = (1.0f + b + c);
-        break;
-
-    case 5U:
-        if(quadrant == 3U) {
-            output.DutyCycle_A = (1.0f - a - b);
-            output.DutyCycle_B = (1.0f - a + b);
-            output.DutyCycle_C = (1.0f + a - b);
-        } else {
-            output.DutyCycle_A = (1.0f + b + a);
-            output.DutyCycle_B = (1.0f + b - a);
-            output.DutyCycle_C = (1.0f - b + a);
-        }
-        break;
-
-    case 6U:
-        output.DutyCycle_A = (1.0f + b + c);
-        output.DutyCycle_B = (1.0f - b - c);
-        output.DutyCycle_C = (1.0f - b + c);
-        break;
-
-    default:
-        uz_assert(0);
-}
-
-output.DutyCycle_A = output.DutyCycle_A * 0.5f;
-output.DutyCycle_B = output.DutyCycle_B * 0.5f;
-output.DutyCycle_C = output.DutyCycle_C * 0.5f;
-
-output.DutyCycle_A = uz_signals_saturation(output.DutyCycle_A, 1.0f, 0.0f);
-output.DutyCycle_B = uz_signals_saturation(output.DutyCycle_B, 1.0f, 0.0f);
-output.DutyCycle_C = uz_signals_saturation(output.DutyCycle_C, 1.0f, 0.0f);
-
-return (output);
+    return (output);
 }
 

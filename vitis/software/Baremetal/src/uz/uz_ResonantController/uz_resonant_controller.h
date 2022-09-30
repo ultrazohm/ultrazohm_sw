a43aef50321640f3649c71c30e4643162912bf5a
Josef Knoblach <knoblachjo83518@th-nuernberg.de>
2022-09-30 13:32:23 +0000

    added comment in resonant controller header file

--- vitis/software/Baremetal/src/uz/uz_ResonantController/uz_resonant_controller.h
+++ vitis/software/Baremetal/src/uz/uz_ResonantController/uz_resonant_controller.h
@@ -1,15 +1,25 @@
+/**
+ * @file uz_resonant_controller.h
+ * @author Josef Knoblach
+ * @brief Resonant-controller, created with Matlab/Simulink Embedded-Coder from a resonant-controller implemented in Simulink
+ * @version 0.1
+ * @date 2022-09-30
+ * 
+ */
+
 #ifndef UZ_RESONANTCONTROLLER_H
 #define UZ_RESONANTCONTROLLER_H
 
 #include "Resonant_Controller_ert_rtw/Resonant_Controller.h"
 
+
+
 /**
  * @brief Struct for Resonant-Controller
- * @var asdf
  */
 typedef struct uz_resonantController{
-	ExtU_Resonant_Controller_T input;	/**< Input struct */
-	ExtY_Resonant_Controller_T output;	/**< Output struct */
+	ExtU_Resonant_Controller_T input;	/**< Input struct of the resonant controller*/
+	ExtY_Resonant_Controller_T output;	/**< Output struct of the resonant controller*/
 	DW_Resonant_Controller_T dwork;
 	RT_MODEL_Resonant_Controller_T modelData;
 	RT_MODEL_Resonant_Controller_T *PtrToModelData;

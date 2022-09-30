85674d436625ba308c9cff169c727170bd9d3d78
merxan71462 <merxan71462@th-nuernberg.de>
2022-09-30 13:36:18 +0200

    Test IP-Core parallel_prediction_and_cost_function_6phase created

---
+++ ip_cores/parallel_Prediction_and_cost_function_6phase/ipcore/parallel_prediction_and_cost_function_6phase_v1_0/include/parallel_prediction_and_cost_function_6phase_addr.h
@@ -0,0 +1,37 @@
+/*
+ * File Name:         C:\ultrazohm_sw\ip_cores\parallel_Prediction_and_cost_function_6phase\ipcore\parallel_prediction_and_cost_function_6phase_v1_0\include\parallel_prediction_and_cost_function_6phase_addr.h
+ * Description:       C Header File
+ * Created:           2022-09-30 13:33:13
+*/
+
+#ifndef PARALLEL_PREDICTION_AND_COST_FUNCTION_6PHASE_H_
+#define PARALLEL_PREDICTION_AND_COST_FUNCTION_6PHASE_H_
+
+#define  IPCore_Reset_parallel_prediction_and_cost_function_6phase                                   0x0  //write 0x1 to bit 0 to reset IP core
+#define  IPCore_Enable_parallel_prediction_and_cost_function_6phase                                  0x4  //enabled (by default) when bit 0 is 0x1
+#define  IPCore_Timestamp_parallel_prediction_and_cost_function_6phase                               0x8  //contains unique IP timestamp (yymmddHHMM): 2209301328: 2209301333
+#define  Lq_AXI_Data_parallel_prediction_and_cost_function_6phase                                    0x100  //data register for Inport Lq_AXI
+#define  Ld_AXI_Data_parallel_prediction_and_cost_function_6phase                                    0x104  //data register for Inport Ld_AXI
+#define  SampleTime_div_Ld_AXI_Data_parallel_prediction_and_cost_function_6phase                     0x108  //data register for Inport SampleTime_div_Ld_AXI
+#define  SampleTime_div_Lq_AXI_Data_parallel_prediction_and_cost_function_6phase                     0x10C  //data register for Inport SampleTime_div_Lq_AXI
+#define  SampleTime_div_Lx_AXI_Data_parallel_prediction_and_cost_function_6phase                     0x110  //data register for Inport SampleTime_div_Lx_AXI
+#define  pole_pairs_AXI_Data_parallel_prediction_and_cost_function_6phase                            0x114  //data register for Inport pole_pairs_AXI
+#define  SampleTime_div_Ly_AXI_Data_parallel_prediction_and_cost_function_6phase                     0x118  //data register for Inport SampleTime_div_Ly_AXI
+#define  Rs_AXI_Data_parallel_prediction_and_cost_function_6phase                                    0x11C  //data register for Inport Rs_AXI
+#define  psiPM_AXI_Data_parallel_prediction_and_cost_function_6phase                                 0x120  //data register for Inport psiPM_AXI
+#define  id_ref_AXI_Data_parallel_prediction_and_cost_function_6phase                                0x124  //data register for Inport id_ref_AXI
+#define  iq_ref_AXI_Data_parallel_prediction_and_cost_function_6phase                                0x128  //data register for Inport iq_ref_AXI
+#define  ix_ref_AXI_Data_parallel_prediction_and_cost_function_6phase                                0x12C  //data register for Inport ix_ref_AXI
+#define  iy_ref_AXI_Data_parallel_prediction_and_cost_function_6phase                                0x130  //data register for Inport iy_ref_AXI
+#define  use_AXI_Data_parallel_prediction_and_cost_function_6phase                                   0x134  //data register for Inport use_AXI
+#define  id_k_1_AXI_Data_parallel_prediction_and_cost_function_6phase                                0x138  //data register for Inport id_k_1_AXI
+#define  iq_k_1_AXI_Data_parallel_prediction_and_cost_function_6phase                                0x13C  //data register for Inport iq_k_1_AXI
+#define  ix_k_1_AXI_Data_parallel_prediction_and_cost_function_6phase                                0x140  //data register for Inport ix_k_1_AXI
+#define  iy_k_1_AXI_Data_parallel_prediction_and_cost_function_6phase                                0x144  //data register for Inport iy_k_1_AXI
+#define  omega_m_measured_AXI_Data_parallel_prediction_and_cost_function_6phase                      0x148  //data register for Inport omega_m_measured_AXI
+#define  d_voltage_per_switching_state_AXI_61_64_Data_parallel_prediction_and_cost_function_6phase   0x14C  //data register for Inport d_voltage_per_switching_state_AXI_61_64
+#define  q_voltage_per_switching_state_AXI_61_64_Data_parallel_prediction_and_cost_function_6phase   0x150  //data register for Inport q_voltage_per_switching_state_AXI_61_64
+#define  x_voltage_per_switching_state_AXI_61_64_Data_parallel_prediction_and_cost_function_6phase   0x154  //data register for Inport x_voltage_per_switching_state_AXI_61_64
+#define  y_volatge_per_switching_state_AXI_61_64_Data_parallel_prediction_and_cost_function_6phase   0x158  //data register for Inport y_volatge_per_switching_state_AXI_61_64
+
+#endif /* PARALLEL_PREDICTION_AND_COST_FUNCTION_6PHASE_H_ */

6106186189f4dac324abf60e63d311129e830138
hoernermi <michael.hoerner@th-nuernberg.de>
2022-10-04 14:16:33 +0200

    updated block design but still data type mismatches

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
---Date        : Fri Sep 30 14:13:37 2022
+--Date        : Tue Oct  4 14:07:49 2022
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

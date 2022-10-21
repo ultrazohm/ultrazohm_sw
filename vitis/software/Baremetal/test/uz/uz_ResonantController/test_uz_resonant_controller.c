f13007b378c42315dbdd38d896e094f9285bca63
Josef Knoblach <knoblachjo83518@th-nuernberg.de>
2022-10-21 09:37:22 +0000

    added tests for different config parameter and tests for asserts in set config and init

--- vitis/software/Baremetal/test/uz/uz_ResonantController/test_uz_resonant_controller.c
+++ vitis/software/Baremetal/test/uz/uz_ResonantController/test_uz_resonant_controller.c
@@ -5,6 +5,7 @@
 #include "uz_resonant_controller.h"
 #include "../../../src/uz/uz_ResonantController/Resonant_Controller_ert_rtw/Resonant_Controller.h"
 #include "../uz_global_configuration.h"
+#include "test_assert_with_exception.h"
 
 #include <stdio.h>
 
@@ -27,6 +28,21 @@ void setUp(void)
 }
 
 
+void test_uz_resonant_controller_init(void){
+	
+	// tests asserts in init-function
+
+	config.sampling_time = -0.0001f;
+	TEST_ASSERT_FAIL_ASSERT(uz_resonantController_init(config));
+	config.sampling_time = 0.0001f;
+	config.upper_limit = 0.0f;
+	config.lower_limit = 1.3f;
+	TEST_ASSERT_FAIL_ASSERT(uz_resonantController_init(config));
+	config.upper_limit = 2.0f;
+	config.lower_limit = -2.0f;	
+
+}
+
 
 void test_uz_resonant_controller_Reset(void)
 {
@@ -38,18 +54,12 @@ void test_uz_resonant_controller_Reset(void)
 	float omega_el = 10.0f;
 	float output = 0.0f;
 	
-
 	output = uz_resonantController_step(test_R_controller, in_ref, in_m, omega_el);
 	TEST_ASSERT_EQUAL_FLOAT(-0.008f,output);
     uz_resonantController_reset(test_R_controller);
 	output = uz_resonantController_get_output(test_R_controller);
     TEST_ASSERT_EQUAL_FLOAT(0.0f,output);
 }
-
-
-
-
-
 
 // Test multiple consecutive Steps with reset inbetween
 void test_uz_resonant_controller_StepAndReset(void)
@@ -100,15 +110,80 @@ void test_uz_resonant_controller_StepAndReset(void)
 
 void test_uz_resonantController_set_config(void)
 {
-	
+	float in_ref = 3.0f;
+	float in_m = 2.0f;
+	float omega_el = 10.0f;
+	float output = 0.0f;
+
 	uz_resonantController_t* test_R_controller_config;
 	test_R_controller_config = uz_resonantController_init(config);
 
-	config.harmonic_order = 7.0f;
+	output = uz_resonantController_step(test_R_controller_config, in_ref, in_m, omega_el);
+    TEST_ASSERT_FLOAT_WITHIN (0.016f -1e-06f, 0.016f+ 1e-06f, output);
+	uz_resonantController_reset(test_R_controller_config);
+
+	// test setting to different config parameters
+	config.harmonic_order = 900.0f;
 
 	uz_resonantController_set_config(test_R_controller_config, config);
 
+	output = uz_resonantController_step(test_R_controller_config, in_ref, in_m, omega_el);
+    TEST_ASSERT_FLOAT_WITHIN (-0.003635f -1e-06f, -0.003635f+ 1e-06f, output);
+
+	uz_resonantController_reset(test_R_controller_config);
+
+	config.harmonic_order = 2.0f;
+	config.fundamental_frequency = 100.0f;
+	config.gain = 1.0f;
+
+	uz_resonantController_set_config(test_R_controller_config, config);
+
+	output = uz_resonantController_step(test_R_controller_config, in_ref, in_m, omega_el);
+    TEST_ASSERT_FLOAT_WITHIN (0.0009992f -1e-08f, 0.0009992f+ 1e-08f, output);
+
+	uz_resonantController_reset(test_R_controller_config);
+
+
+	// test limits:
+	config.gain = 1000.0f;
+	config.upper_limit = 0.5f;
+	config.lower_limit = -0.5f;
+	in_m = 0.0f;
+
+	uz_resonantController_set_config(test_R_controller_config, config);
+	output = uz_resonantController_step(test_R_controller_config, in_ref, in_m, omega_el);
+	output = uz_resonantController_step(test_R_controller_config, in_ref, in_m, omega_el);
+
+	TEST_ASSERT_FLOAT_WITHIN (0.5f -1e-08f, 0.5f+ 1e-08f, output);
+
+	uz_resonantController_reset(test_R_controller_config);
+
+	in_ref = -3.0f;
+
+	uz_resonantController_set_config(test_R_controller_config, config);
+	output = uz_resonantController_step(test_R_controller_config, in_ref, in_m, omega_el);
+	output = uz_resonantController_step(test_R_controller_config, in_ref, in_m, omega_el);
+
+
+
+	TEST_ASSERT_FLOAT_WITHIN (-0.5f -1e-08f, -0.5f+ 1e-08f, output);
+
+	uz_resonantController_reset(test_R_controller_config);
+
+
+
+	// test asserts of set_config:
+	config.sampling_time = -0.0001f;
+	TEST_ASSERT_FAIL_ASSERT(uz_resonantController_set_config(test_R_controller_config, config));
+	config.sampling_time = 0.0001f;
+	config.upper_limit = 0.0f;
+	config.lower_limit = 1.3f;
+	TEST_ASSERT_FAIL_ASSERT(uz_resonantController_set_config(test_R_controller_config, config));
+	config.upper_limit = 2.0f;
+	config.lower_limit = -2.0f;
+
 }
+
 
 
 #endif // TEST

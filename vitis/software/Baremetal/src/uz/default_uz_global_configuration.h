8710bc183111cb3240d692abe0f3d0f5b54f87fd
HoppeVa <hoppeva70971@th-nuernberg.de>
2022-10-31 09:45:35 +0100

    Add instances to default_global_config

--- vitis/software/Baremetal/src/uz/default_uz_global_configuration.h
+++ vitis/software/Baremetal/src/uz/default_uz_global_configuration.h
@@ -43,6 +43,7 @@
 #define UZ_PMSM9PH_TRANSFORMATION_MAX_INSTANCES         0U
 #define UZ_PMSM_MODEL9PH_DQ_MAX_INSTANCES               1U
 #define UZ_INVERTER_3PH_MAX_INSTANCES                   3U
+#define UZ_PMSM3PH_TRANSFORMATION_MAX_INSTANCES         1U
 
 #define UZ_INVERTER_ADAPTER_MAX_INSTANCES               3U
 #endif
@@ -74,6 +75,7 @@
     #define UZ_PMSM_MODEL9PH_DQ_MAX_INSTANCES               20U
     #define UZ_INVERTER_3PH_MAX_INSTANCES                   20U
     #define UZ_SUM_MAX_INSTANCES                            5U
+    #define UZ_PMSM3PH_TRANSFORMATION_MAX_INSTANCES         20U
 
     #define UZ_INVERTER_ADAPTER_MAX_INSTANCES               20U
 #endif

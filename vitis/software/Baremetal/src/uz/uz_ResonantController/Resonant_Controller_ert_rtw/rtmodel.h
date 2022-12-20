f3134b90248d31f381b1ce9781dcc8c42d069184
merxan71462 <merxan71462@th-nuernberg.de>
2022-12-20 18:38:21 +0100

    More Driver adjustments

--- vitis/software/Baremetal/src/uz/uz_ResonantController/Resonant_Controller_ert_rtw/rtmodel.h
+++ vitis/software/Baremetal/src/uz/uz_ResonantController/Resonant_Controller_ert_rtw/rtmodel.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Academic License - for use in teaching, academic research, and meeting
  * course requirements at degree granting institutions only.  Not for
@@ -50,56 +49,3 @@
  *
  * [EOF]
  */
-=======
-/*
- * Academic License - for use in teaching, academic research, and meeting
- * course requirements at degree granting institutions only.  Not for
- * government, commercial, or other organizational use.
- *
- * File: rtmodel.h
- *
- * Code generated for Simulink model 'Resonant_Controller'.
- *
- * Model version                  : 4.4
- * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
- * C/C++ source code generated on : Wed Oct 12 11:19:47 2022
- *
- * Target selection: ert.tlc
- * Embedded hardware selection: ARM Compatible->ARM Cortex-R
- * Code generation objectives:
- *    1. Execution efficiency
- *    2. Traceability
- * Validation result: Passed (10), Warnings (2), Error (0)
- */
-
-#ifndef RTW_HEADER_rtmodel_h_
-#define RTW_HEADER_rtmodel_h_
-#include "Resonant_Controller.h"
-
-/*
- * ROOT_IO_FORMAT: 0 (Individual arguments)
- * ROOT_IO_FORMAT: 1 (Structure reference)
- * ROOT_IO_FORMAT: 2 (Part of model data structure)
- */
-#define ROOT_IO_FORMAT                 2
-
-/* Macros generated for backwards compatibility  */
-#ifndef rtmGetErrorStatus
-#define rtmGetErrorStatus(rtm)         ((void*) 0)
-#endif
-
-#ifndef rtmSetErrorStatus
-#define rtmSetErrorStatus(rtm, val)    ((void) 0)
-#endif
-
-#ifndef rtmGetStopRequested
-#define rtmGetStopRequested(rtm)       ((void*) 0)
-#endif
-#endif                                 /* RTW_HEADER_rtmodel_h_ */
-
-/*
- * File trailer for generated code.
- *
- * [EOF]
- */
->>>>>>> feature/Test_6Phase_FCS_MPC

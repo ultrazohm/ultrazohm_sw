0c613d126973fc04aad07b92998b58ba9acc3da6
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-01-09 11:25:54 +0100

    renew adresses

--- vitis/software/Baremetal/src/IP_Cores/uz_pmsm_model_3ph_dq/uz_pmsm_model3ph_dq_hw.c
+++ vitis/software/Baremetal/src/IP_Cores/uz_pmsm_model_3ph_dq/uz_pmsm_model3ph_dq_hw.c
@@ -21,79 +21,79 @@
 float uz_pmsm_model3ph_hw_read_torque(uint32_t base_address)
 {
     uz_assert_not_zero(base_address);
-    return uz_axi_read_float(base_address + torque_Data_uz_pmsm_model3ph);
+    return uz_axi_read_float(base_address + outputs_Data_uz_pmsm_model_3ph_dq + 0x08);
 }
 
 float uz_pmsm_model3ph_hw_read_i_q(uint32_t base_address)
 {
     uz_assert_not_zero(base_address);
-    return (uz_axi_read_float(base_address + i_q_Data_uz_pmsm_model3ph));
+    return (uz_axi_read_float(base_address + outputs_Data_uz_pmsm_model_3ph_dq + 0x04));
 }
 
 float uz_pmsm_model3ph_hw_read_i_d(uint32_t base_address)
 {
     uz_assert_not_zero(base_address);
-    return (uz_axi_read_float(base_address + i_d_Data_uz_pmsm_model3ph));
+    return (uz_axi_read_float(base_address + outputs_Data_uz_pmsm_model_3ph_dq));
 }
 
 float uz_pmsm_model3ph_hw_read_theta_el(uint32_t base_address)
 {
     uz_assert_not_zero(base_address);
-    return (uz_axi_read_float(base_address + theta_el_Data_uz_pmsm_model3ph));
+    return (uz_axi_read_float(base_address + outputs_Data_uz_pmsm_model_3ph_dq + 0x10));
 }
 
 void uz_pmsm_model3ph_hw_write_reset(uint32_t base_address, bool reset)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_bool(base_address + reset_integrators_Data_uz_pmsm_model3ph, reset);
+    uz_axi_write_bool(base_address + reset_integrators_Data_uz_pmsm_model_3ph_dq, reset);
 }
 
 void uz_pmsm_model3ph_hw_write_omega_mech(uint32_t base_address, float omega_mech)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address + in_omega_mech_Data_uz_pmsm_model3ph, omega_mech);
+    uz_axi_write_float(base_address + inputs_Data_uz_pmsm_model_3ph_dq + 0x08, omega_mech);
 }
 
 void uz_pmsm_model3ph_hw_write_v_q(uint32_t base_address, float v_q)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address + v_q_Data_uz_pmsm_model3ph, v_q);
+    uz_axi_write_float(base_address + inputs_Data_uz_pmsm_model_3ph_dq + 0x04, v_q);
 }
 
 void uz_pmsm_model3ph_hw_write_v_d(uint32_t base_address, float v_d)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address + v_d_Data_uz_pmsm_model3ph, v_d);
+    uz_axi_write_float(base_address + inputs_Data_uz_pmsm_model_3ph_dq, v_d);
 }
 
 void uz_pmsm_model3ph_hw_write_friction_coefficient(uint32_t base_address, float mu)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address + mu_Data_uz_pmsm_model3ph, mu);
+    uz_axi_write_float(base_address + mu_Data_uz_pmsm_model_3ph_dq, mu);
 }
 
 void uz_pmsm_model3ph_hw_write_coulomb_friction_constant(uint32_t base_address, float m_c)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address + M_n0_Data_uz_pmsm_model3ph, m_c);
+    uz_axi_write_float(base_address + M_n0_Data_uz_pmsm_model_3ph_dq, m_c);
 }
 
 void uz_pmsm_model3ph_hw_write_r_1(uint32_t base_address, float r_1)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address + R1_Data_uz_pmsm_model3ph, r_1);
+    uz_axi_write_float(base_address + R1_Data_uz_pmsm_model_3ph_dq, r_1);
 }
 
 void uz_pmsm_model3ph_hw_write_polepairs(uint32_t base_address, float polepairs)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address + polepairs_Data_uz_pmsm_model3ph, polepairs);
+    uz_axi_write_float(base_address + polepairs_Data_uz_pmsm_model_3ph_dq, polepairs);
 }
 
 void uz_pmsm_model3ph_hw_write_psi_pm(uint32_t base_address, float psi_pm)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address + psi_pm_Data_uz_pmsm_model3ph, psi_pm);
+    uz_axi_write_float(base_address + psi_pm_Data_uz_pmsm_model_3ph_dq, psi_pm);
 }
 
 void uz_pmsm_model3ph_hw_write_L_d(uint32_t base_address, float L_d)
@@ -101,7 +101,7 @@ void uz_pmsm_model3ph_hw_write_L_d(uint32_t base_address, float L_d)
     uz_assert_not_zero(base_address);
     uz_assert(L_d > 0.0f); // prevent division by zero & negative inductance makes no sense
     // Hardware expects recirpocal of L_d to not have to invert L_d in the IP-Core
-    uz_axi_write_float(base_address + reciprocal_L_d_Data_uz_pmsm_model3ph, (1.0f / L_d));
+    uz_axi_write_float(base_address + reciprocal_L_d_Data_uz_pmsm_model_3ph_dq, (1.0f / L_d));
 }
 
 void uz_pmsm_model3ph_hw_write_L_q(uint32_t base_address, float L_q)
@@ -109,61 +109,61 @@ void uz_pmsm_model3ph_hw_write_L_q(uint32_t base_address, float L_q)
     uz_assert_not_zero(base_address);
     uz_assert(L_q > 0.0f); // prevent division by zero & negative inductance makes no sense
     // Hardware expects recirpocal of L_q to not have to invert it in the IP-Core
-    uz_axi_write_float(base_address + reciprocal_L_q_Data_uz_pmsm_model3ph, (1.0f / L_q));
+    uz_axi_write_float(base_address + reciprocal_L_q_Data_uz_pmsm_model_3ph_dq, (1.0f / L_q));
 }
 
 float uz_pmsm_model3ph_hw_read_omega_mech(uint32_t base_address){
     uz_assert_not_zero(base_address);
-    return uz_axi_read_float(base_address+out_omega_mech_Data_uz_pmsm_model3ph);
+    return uz_axi_read_float(base_address + outputs_Data_uz_pmsm_model_3ph_dq + 0x0C);
 }
 
 void uz_pmsm_model3ph_hw_write_simulate_mechanical(uint32_t base_address, bool simulate_mechanical){
     uz_assert_not_zero(base_address);
-    uz_axi_write_bool(base_address+simulate_mechanical_Data_uz_pmsm_model3ph,simulate_mechanical);
+    uz_axi_write_bool(base_address+simulate_mechanical_Data_uz_pmsm_model_3ph_dq,simulate_mechanical);
 }
 
 void uz_pmsm_model3ph_hw_write_inertia(uint32_t base_address, float intertia){
     uz_assert_not_zero(base_address);
     uz_assert(intertia > 0.0f); // prevent division by zero and no negative inertia exists
-    uz_axi_write_float(base_address+reciprocal_J_Data_uz_pmsm_model3ph,(1.0f/intertia));
+    uz_axi_write_float(base_address+reciprocal_J_Data_uz_pmsm_model_3ph_dq,(1.0f/intertia));
 }
 
 void uz_pmsm_model3ph_hw_write_load_torque(uint32_t base_address, float load_torque){
     uz_assert_not_zero(base_address);
-    uz_axi_write_float(base_address+load_torque_Data_uz_pmsm_model3ph,load_torque);
+    uz_axi_write_float(base_address+inputs_Data_uz_pmsm_model_3ph_dq + 0x0C ,load_torque);
 }
 
 void uz_pmsm_model3ph_hw_write_switch_pspl(uint32_t base_address, bool switch_pspl)
 {
     uz_assert_not_zero(base_address);
-    uz_axi_write_bool(base_address + use_axi_input_Data_uz_pmsm_model3ph, switch_pspl);
+    uz_axi_write_bool(base_address + use_axi_input_Data_uz_pmsm_model_3ph_dq, switch_pspl);
 }
 
 // Voltage Input readout
 float uz_pmsm_model3ph_hw_read_v_d(uint32_t base_address){
     uz_assert_not_zero(base_address);
-    return uz_axi_read_float(base_address + voltage_input_dq_out_axi_fb_Data_uz_pmsm_model3ph);
+    return uz_axi_read_float(base_address + voltage_input_dq_out_axi_fb_Data_uz_pmsm_model_3ph_dq);
 }
 
 float uz_pmsm_model3ph_hw_read_v_q(uint32_t base_address){
     uz_assert_not_zero(base_address);
-    return uz_axi_read_float(base_address + voltage_input_dq_out_axi_fb_Data_uz_pmsm_model3ph+0x04);
+    return uz_axi_read_float(base_address + voltage_input_dq_out_axi_fb_Data_uz_pmsm_model_3ph_dq+0x04);
 }
 
 void uz_pmsm_model3ph_hw_trigger_input_strobe(uint32_t base_address){
     uz_assert_not_zero_uint32(base_address);
-    uz_axi_write_bool(base_address+inputs_Strobe_uz_pmsm_model3ph,true);
-    uz_axi_write_bool(base_address+inputs_Strobe_uz_pmsm_model3ph,false);
+    uz_axi_write_bool(base_address+inputs_Strobe_uz_pmsm_model_3ph_dq,true);
+    uz_axi_write_bool(base_address+inputs_Strobe_uz_pmsm_model_3ph_dq,false);
 }
 
 void uz_pmsm_model3ph_hw_trigger_output_strobe(uint32_t base_address){
     uz_assert_not_zero_uint32(base_address);
-    uz_axi_write_bool(base_address+outputs_Strobe_uz_pmsm_model3ph,true);
-    uz_axi_write_bool(base_address+outputs_Strobe_uz_pmsm_model3ph,false);
+    uz_axi_write_bool(base_address+outputs_Strobe_uz_pmsm_model_3ph_dq,true);
+    uz_axi_write_bool(base_address+outputs_Strobe_uz_pmsm_model_3ph_dq,false);
 }
 
 void uz_pmsm_model3ph_hw_trigger_fb_strobe(uint32_t base_address){
     uz_assert_not_zero_uint32(base_address);
-    uz_axi_write_bool(base_address+voltage_input_dq_out_axi_fb_Strobe_uz_pmsm_model3ph,true);
-    uz_axi_write_bool(base_address+voltage_input_dq_out_axi_fb_Strobe_uz_pmsm_model3ph,false);
+    uz_axi_write_bool(base_address+voltage_input_dq_out_axi_fb_Strobe_uz_pmsm_model_3ph_dq,true);
+    uz_axi_write_bool(base_address+voltage_input_dq_out_axi_fb_Strobe_uz_pmsm_model_3ph_dq,false);
 }
--- vitis/software/Baremetal/test/IP_Cores/uz_pmsm_model_3ph_dq/test_uz_pmsm_model3ph_dq_hw.c
+++ vitis/software/Baremetal/test/IP_Cores/uz_pmsm_model_3ph_dq/test_uz_pmsm_model3ph_dq_hw.c
@@ -19,7 +19,7 @@ void tearDown(void)
 void test_uz_pmsm_model3ph_hw_read_torque(void)
 {
     float torque_expect=4.2f;
-    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+torque_Data_uz_pmsm_model3ph,torque_expect);
+    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+outputs_Data_uz_pmsm_model_3ph_dq + 0x08,torque_expect);
     float torque_read=uz_pmsm_model3ph_hw_read_torque(BASE_ADDRESS);
     TEST_ASSERT_EQUAL_FLOAT(torque_expect,torque_read);
 }
@@ -27,149 +27,149 @@ void test_uz_pmsm_model3ph_hw_read_torque(void)
 void test_uz_pmsm_model3ph_hw_read_i_q(void)
 {
     float i_q_val=4.2f;
-    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+i_q_Data_uz_pmsm_model3ph,i_q_val);
+    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+outputs_Data_uz_pmsm_model_3ph_dq + 0x04,i_q_val);
     float readback=uz_pmsm_model3ph_hw_read_i_q(BASE_ADDRESS);
     TEST_ASSERT_EQUAL_FLOAT(i_q_val,readback);
 }
 
 void test_uz_pmsm_model3ph_hw_read_i_d(void){
     float i_d_val=13.1f;
-    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+i_d_Data_uz_pmsm_model3ph,i_d_val);
+    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+outputs_Data_uz_pmsm_model_3ph_dq,i_d_val);
     float readback=uz_pmsm_model3ph_hw_read_i_d(BASE_ADDRESS);
     TEST_ASSERT_EQUAL_FLOAT(i_d_val,readback);
 }
 
 void test_uz_pmsm_model3ph_hw_read_theta_el(void){
     float theta_el=13.1f;
-    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+theta_el_Data_uz_pmsm_model3ph,theta_el);
+    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+outputs_Data_uz_pmsm_model_3ph_dq + 0x10,theta_el);
     float readback=uz_pmsm_model3ph_hw_read_theta_el(BASE_ADDRESS);
     TEST_ASSERT_EQUAL_FLOAT(theta_el,readback);
 }
 
 void test_uz_pmsm_model3ph_hw_write_reset(void){
-    uz_axi_write_bool_Expect(BASE_ADDRESS+reset_integrators_Data_uz_pmsm_model3ph,true);
+    uz_axi_write_bool_Expect(BASE_ADDRESS+reset_integrators_Data_uz_pmsm_model_3ph_dq,true);
     uz_pmsm_model3ph_hw_write_reset(BASE_ADDRESS,true);
 }
 
 void test_uz_pmsm_model3ph_hw_write_omega_mech(void){
     float omega_mech=123.4f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+in_omega_mech_Data_uz_pmsm_model3ph,omega_mech);
+    uz_axi_write_float_Expect(BASE_ADDRESS+inputs_Data_uz_pmsm_model_3ph_dq+0x08,omega_mech);
     uz_pmsm_model3ph_hw_write_omega_mech(BASE_ADDRESS,omega_mech);
 }
 
 void test_uz_pmsm_model3ph_hw_write_v_q(void){
     float v_q_expect=123.1f;
-    uz_axi_write_float_Expect(BASE_ADDRESS + v_q_Data_uz_pmsm_model3ph,v_q_expect);
+    uz_axi_write_float_Expect(BASE_ADDRESS + inputs_Data_uz_pmsm_model_3ph_dq+0x04,v_q_expect);
     uz_pmsm_model3ph_hw_write_v_q(BASE_ADDRESS,v_q_expect);
 }
 
 void test_uz_pmsm_model3ph_hw_write_v_d(void){
     float v_d_expect=-3.1f;
-    uz_axi_write_float_Expect(BASE_ADDRESS + v_d_Data_uz_pmsm_model3ph,v_d_expect);
+    uz_axi_write_float_Expect(BASE_ADDRESS + inputs_Data_uz_pmsm_model_3ph_dq,v_d_expect);
     uz_pmsm_model3ph_hw_write_v_d(BASE_ADDRESS,v_d_expect);
 }
 
 void test_uz_pmsm_model3ph_hw_write_friction_coefficient(void){
     float mu=1.1f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+mu_Data_uz_pmsm_model3ph,mu);
+    uz_axi_write_float_Expect(BASE_ADDRESS+mu_Data_uz_pmsm_model_3ph_dq,mu);
     uz_pmsm_model3ph_hw_write_friction_coefficient(BASE_ADDRESS,mu);
 }
 
 void test_uz_pmsm_model3ph_hw_write_coulomb_friction(void){
     float m_c=1.1f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+M_n0_Data_uz_pmsm_model3ph,m_c);
+    uz_axi_write_float_Expect(BASE_ADDRESS+M_n0_Data_uz_pmsm_model_3ph_dq,m_c);
     uz_pmsm_model3ph_hw_write_coulomb_friction_constant(BASE_ADDRESS,m_c);
 }
 
 void test_uz_pmsm_model3ph_hw_write_r1(void){
     float r_1=11.2f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+R1_Data_uz_pmsm_model3ph,r_1);
+    uz_axi_write_float_Expect(BASE_ADDRESS+R1_Data_uz_pmsm_model_3ph_dq,r_1);
     uz_pmsm_model3ph_hw_write_r_1(BASE_ADDRESS,r_1);
 }
 
 void test_uz_pmsm_model3ph_hw_write_poleparis(void){
     float polepairs=3.0f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+polepairs_Data_uz_pmsm_model3ph,polepairs);
+    uz_axi_write_float_Expect(BASE_ADDRESS+polepairs_Data_uz_pmsm_model_3ph_dq,polepairs);
     uz_pmsm_model3ph_hw_write_polepairs(BASE_ADDRESS,polepairs);
 }
 
 void test_uz_pmsm_model3ph_hw_write_psi_pm(void){
     float psi_pm=0.001f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+psi_pm_Data_uz_pmsm_model3ph,psi_pm);
+    uz_axi_write_float_Expect(BASE_ADDRESS+psi_pm_Data_uz_pmsm_model_3ph_dq,psi_pm);
     uz_pmsm_model3ph_hw_write_psi_pm(BASE_ADDRESS,psi_pm);
 }
 
 void test_uz_pmsm_model3ph_hw_write_L_d(void){
     float L_d=0.003f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+reciprocal_L_d_Data_uz_pmsm_model3ph,1.0f/L_d);
+    uz_axi_write_float_Expect(BASE_ADDRESS+reciprocal_L_d_Data_uz_pmsm_model_3ph_dq,1.0f/L_d);
     uz_pmsm_model3ph_hw_write_L_d(BASE_ADDRESS,L_d);
 }
 
 void test_uz_pmsm_model3ph_hw_write_L_q(void){
     float L_q=0.001f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+reciprocal_L_q_Data_uz_pmsm_model3ph,1.0f/L_q);
+    uz_axi_write_float_Expect(BASE_ADDRESS+reciprocal_L_q_Data_uz_pmsm_model_3ph_dq,1.0f/L_q);
     uz_pmsm_model3ph_hw_write_L_q(BASE_ADDRESS,L_q);
 }
 
 void test_uz_pmsm_model3ph_hw_read_omega_mech(void){
     float omega_mech_expected=100.2f;
-    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+out_omega_mech_Data_uz_pmsm_model3ph,omega_mech_expected);
+    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+outputs_Data_uz_pmsm_model_3ph_dq+0x0C,omega_mech_expected);
     float omega_mech=uz_pmsm_model3ph_hw_read_omega_mech(BASE_ADDRESS);
     TEST_ASSERT_EQUAL_FLOAT(omega_mech_expected,omega_mech);
 }
 
 void test_uz_pmsm_model3ph_hw_write_simulate_mechanical(void){
     _Bool simulate_mech=true;
-    uz_axi_write_bool_Expect(BASE_ADDRESS+simulate_mechanical_Data_uz_pmsm_model3ph,simulate_mech);
+    uz_axi_write_bool_Expect(BASE_ADDRESS+simulate_mechanical_Data_uz_pmsm_model_3ph_dq,simulate_mech);
     uz_pmsm_model3ph_hw_write_simulate_mechanical(BASE_ADDRESS,simulate_mech);
 }
 
 void test_uz_pmsm_model3ph_hw_write_inertia(void){
     float inertia=0.00031f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+reciprocal_J_Data_uz_pmsm_model3ph,1.0f/inertia);
+    uz_axi_write_float_Expect(BASE_ADDRESS+reciprocal_J_Data_uz_pmsm_model_3ph_dq,1.0f/inertia);
     uz_pmsm_model3ph_hw_write_inertia(BASE_ADDRESS,inertia);
 }
 
 void test_uz_pmsm_model3ph_hw_write_load_torque(void){
     float load_torque=10.5f;
-    uz_axi_write_float_Expect(BASE_ADDRESS+load_torque_Data_uz_pmsm_model3ph,load_torque);
+    uz_axi_write_float_Expect(BASE_ADDRESS+inputs_Data_uz_pmsm_model_3ph_dq+0x0C,load_torque);
     uz_pmsm_model3ph_hw_write_load_torque(BASE_ADDRESS,load_torque);
 }
 
 void test_uz_pmsm_model3ph_hw_write_switch_pspl(void){
     bool switch_pspl=true;
-    uz_axi_write_bool_Expect(BASE_ADDRESS + use_axi_input_Data_uz_pmsm_model3ph,switch_pspl);
+    uz_axi_write_bool_Expect(BASE_ADDRESS + use_axi_input_Data_uz_pmsm_model_3ph_dq,switch_pspl);
     uz_pmsm_model3ph_hw_write_switch_pspl(BASE_ADDRESS,switch_pspl);
 }
 
 void test_uz_pmsm_model3ph_hw_trigger_input_strobe(void){
-    uz_axi_write_bool_Expect(BASE_ADDRESS+inputs_Strobe_uz_pmsm_model3ph,true);
-    uz_axi_write_bool_Expect(BASE_ADDRESS+inputs_Strobe_uz_pmsm_model3ph,false);
+    uz_axi_write_bool_Expect(BASE_ADDRESS+inputs_Strobe_uz_pmsm_model_3ph_dq,true);
+    uz_axi_write_bool_Expect(BASE_ADDRESS+inputs_Strobe_uz_pmsm_model_3ph_dq,false);
     uz_pmsm_model3ph_hw_trigger_input_strobe(BASE_ADDRESS);
 }
 
 void test_uz_pmsm_model3ph_hw_trigger_output_strobe(void){
-    uz_axi_write_bool_Expect(BASE_ADDRESS+outputs_Strobe_uz_pmsm_model3ph,true);
-    uz_axi_write_bool_Expect(BASE_ADDRESS+outputs_Strobe_uz_pmsm_model3ph,false);
+    uz_axi_write_bool_Expect(BASE_ADDRESS+outputs_Strobe_uz_pmsm_model_3ph_dq,true);
+    uz_axi_write_bool_Expect(BASE_ADDRESS+outputs_Strobe_uz_pmsm_model_3ph_dq,false);
     uz_pmsm_model3ph_hw_trigger_output_strobe(BASE_ADDRESS);
 }
 
 void test_uz_pmsm_model3ph_hw_trigger_fb_strobe(void){
-    uz_axi_write_bool_Expect(BASE_ADDRESS+voltage_input_dq_out_axi_fb_Strobe_uz_pmsm_model3ph,true);
-    uz_axi_write_bool_Expect(BASE_ADDRESS+voltage_input_dq_out_axi_fb_Strobe_uz_pmsm_model3ph,false);
+    uz_axi_write_bool_Expect(BASE_ADDRESS+voltage_input_dq_out_axi_fb_Strobe_uz_pmsm_model_3ph_dq,true);
+    uz_axi_write_bool_Expect(BASE_ADDRESS+voltage_input_dq_out_axi_fb_Strobe_uz_pmsm_model_3ph_dq,false);
     uz_pmsm_model3ph_hw_trigger_fb_strobe(BASE_ADDRESS);
 }
 
 void test_uz_pmsm_model3ph_hw_read_v_d(void){
     float testvolt=13.1f;
-    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+voltage_input_dq_out_axi_fb_Data_uz_pmsm_model3ph,testvolt);
+    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+voltage_input_dq_out_axi_fb_Data_uz_pmsm_model_3ph_dq,testvolt);
     float readback=uz_pmsm_model3ph_hw_read_v_d(BASE_ADDRESS);
     TEST_ASSERT_EQUAL_FLOAT(testvolt,readback);
 }
 
 void test_uz_pmsm_model3ph_hw_read_v_q(void){
     float testvolt=13.1f;
-    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+voltage_input_dq_out_axi_fb_Data_uz_pmsm_model3ph+0x04,testvolt);
+    uz_axi_read_float_ExpectAndReturn(BASE_ADDRESS+voltage_input_dq_out_axi_fb_Data_uz_pmsm_model_3ph_dq+0x04,testvolt);
     float readback=uz_pmsm_model3ph_hw_read_v_q(BASE_ADDRESS);
     TEST_ASSERT_EQUAL_FLOAT(testvolt,readback);
 }

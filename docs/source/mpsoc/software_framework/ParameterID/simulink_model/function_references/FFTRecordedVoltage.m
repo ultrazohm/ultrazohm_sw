acea23eaefb3c4bf2799c2dc57a776cae3188039
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-01-27 09:02:13 +0100

    working compensation and comparison

--- docs/source/mpsoc/software_framework/ParameterID/simulink_model/function_references/FFTRecordedVoltage.m
+++ docs/source/mpsoc/software_framework/ParameterID/simulink_model/function_references/FFTRecordedVoltage.m
@@ -1,6 +1,7 @@
-function [frequencies,amplitudes,angles] = FFTRecordedVoltage(data,sampletime)
+function [output] = FFTRecordedVoltage(data,sampletime)
 %FFTRecordedVoltage performs FFT
 %   Detailed explanation goes here
+%% fft functionality
 Y = single(zeros(10000,1));%1000 depends on the size of recorded data!
 L = length(data);
 Y = fft(data);
@@ -11,9 +12,9 @@ for i=1:10000%1000 depends on the size of recorded data!
     end
 end
 P2 = abs(Y/L);
-amplitudes = P2(1:L/2+1);
-amplitudes(2:end-1) = 2*amplitudes(2:end-1);
-frequencies = (0:(L/2))/L/sampletime;
-angles=angle(Y);
-angles=angles(1:L/2+1);
+output.amplitudes = P2(1:L/2+1);
+output.amplitudes(2:end-1) = 2*output.amplitudes(2:end-1);
+output.frequencies = (0:(L/2))/L/sampletime;
+output.angles=angle(Y);
+output.angles=output.angles(1:L/2+1);
 end
--- docs/source/mpsoc/software_framework/ParameterID/simulink_model/function_references/FindPsiPMs.m
+++ docs/source/mpsoc/software_framework/ParameterID/simulink_model/function_references/FindPsiPMs.m
@@ -1,28 +1,21 @@
-function [psi_pm,psi_freq,angle] = FindPsiPMs(frequencies,amplitudes,angles,compensate,fc)
+function [output] = FindPsiPMs(input)
 %FindPsiPMs Finds PSI_PMs of higher orders
 %   Detailed explanation goes here
-[ampl_1,index_1] = max(amplitudes);
-omega_el1 = 2*pi*frequencies(index_1);
+%% for testing, can delete in application or compiler does
+output = input;
+%% find fundamental
+[ampl_1,index_1] = max(input.amplitudes);
+omega_el1 = 2*pi*input.frequencies(index_1);
 %% initialize arrays
-psi_pm = single(zeros(5,1));
-psi_freq = psi_pm;
-angle = psi_pm;
+output.psi_pm = single(zeros(5,1));
+output.psi_freq = output.psi_pm;
+output.angle = output.psi_pm;
 order = single([1;3;5;7;11]);
 %% calculate results
 % calculate frequencies to orders
-psi_freq = order*frequencies(index_1);
+output.psi_freq = order*input.frequencies(index_1);
 % calculate psi magnitude
-if(compensate)
-    factor = sqrt(1+(2*pi*psi_freq))
-    psi_pm = amplitudes(index_1*order)./(omega_el1*order)*factor;
-else
-    psi_pm = amplitudes(index_1*order)./(omega_el1*order);
-end
+output.psi_pm = input.amplitudes(index_1*order)./(omega_el1*order);
 % calculate angles
-if(compensate)
-    correction = atan(psi_freq/fc);
-    angle = angles(order*index_1) - angles(index_1) + correction;
-else
-   angle = angles(order*index_1) - angles(index_1);
-end
+output.psi_angle = wrapTo2Pi(input.angles(order*index_1));% - angles(index_1);
 end

3825a44cb02f8edcc55f30c80c0176bb35c44c60
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-02-01 16:04:33 +0100

    properly protects AutoRefCurrents & adds get&set functions

--- vitis/software/Baremetal/src/uz/uz_ParameterID/OnlineID/AutoRefCurrents/uz_ParaID_AutoRefCurrents.c
+++ vitis/software/Baremetal/src/uz/uz_ParameterID/OnlineID/AutoRefCurrents/uz_ParaID_AutoRefCurrents.c
@@ -18,8 +18,16 @@
 #include "uz_ParaID_AutoRefCurrents.h"
 #include "../../../uz_HAL.h"
 
-static uint32_t instances_counter_ParaID_AutoRefCurrents = 0;
+struct uz_ParaID_AutoRefCurrents_t{
+	bool is_ready;
+	ExtY_AutoRefCurrents_t output;
+	ExtU_AutoRefCurrents_t input;
+	DW_AutoRefCurrents_t rtDW; /* Observable states */
+	RT_MODEL_AutoRefCurrents_t modelData;
+	RT_MODEL_AutoRefCurrents_t *PtrToModelData;
+};
 
+static uint32_t instances_counter_ParaID_AutoRefCurrents = 0;
 static uz_ParaID_AutoRefCurrents_t instances_ParaID_AutoRefCurrents[UZ_PARAMETERID_MAX_INSTANCES] = { 0 };
 
 static uz_ParaID_AutoRefCurrents_t* uz_ParaID_AutoRefCurrents_allocation(void);
@@ -27,7 +35,9 @@ static uz_ParaID_AutoRefCurrents_t* uz_ParaID_AutoRefCurrents_allocation(void);
 static uz_ParaID_AutoRefCurrents_t* uz_ParaID_AutoRefCurrents_allocation(void) {
 	uz_assert(instances_counter_ParaID_AutoRefCurrents < UZ_PARAMETERID_MAX_INSTANCES);
 	uz_ParaID_AutoRefCurrents_t* self = &instances_ParaID_AutoRefCurrents[instances_counter_ParaID_AutoRefCurrents];
+	uz_assert_false(self->is_ready);
 	instances_counter_ParaID_AutoRefCurrents++;
+	self->is_ready = true;
 	return (self);
 }
 uz_ParaID_AutoRefCurrents_t* uz_AutoRefCurrents_init(void) {
@@ -42,7 +52,37 @@ uz_ParaID_AutoRefCurrents_t* uz_AutoRefCurrents_init(void) {
 
 void uz_AutoRefCurrents_step(uz_ParaID_AutoRefCurrents_t *self) {
 	uz_assert_not_NULL(self);
+	uz_assert(self->is_ready);
 	AutoRefCurrents_step(self->PtrToModelData);
 }
 
+void uz_AutoRefCurrents_set_Config(uz_ParaID_AutoRefCurrents_t *self, uz_ParaID_AutoRefCurrentsConfig_t Config) {
+	uz_assert_not_NULL(self);
+	uz_assert(self->is_ready);
+	self->input.AutoRefCurrentsConfig = Config;
+}
+
+void uz_AutoRefCurrents_set_ActualValues(uz_ParaID_AutoRefCurrents_t *self, uz_ParaID_ActualValues_t ActualValues) {
+	uz_assert_not_NULL(self);
+	uz_assert(self->is_ready);
+	self->input.ActualValues = ActualValues;
+}
+
+void uz_AutoRefCurrents_set_GlobalConfig(uz_ParaID_AutoRefCurrents_t *self, uz_ParaID_GlobalConfig_t GlobalConfig) {
+	uz_assert_not_NULL(self);
+	uz_assert(self->is_ready);
+	self->input.GlobalConfig_out = GlobalConfig;
+}
+
+void uz_AutoRefCurrents_set_ControlFlags(uz_ParaID_AutoRefCurrents_t *self, uz_ParaID_ControlFlags_t ControlFlags) {
+	uz_assert_not_NULL(self);
+	uz_assert(self->is_ready);
+	self->input.ControlFlags = ControlFlags;
+}
+
+uz_ParaID_AutoRefCurrents_output_t* uz_AutoRefCurrents_get_output(uz_ParaID_AutoRefCurrents_t *self) {
+	uz_assert_not_NULL(self);
+	uz_assert(self->is_ready);
+	return(&self->output.AutoRefCurrents_output);
+}
 #endif

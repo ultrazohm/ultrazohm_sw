6395de19e468dc2be7c2cebcca8f7edc933d8cb0
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-02-07 11:00:30 +0100

    add fft function

--- docs/source/mpsoc/software_framework/ParameterID/simulink_model/function_references/FFTRecordedVoltage.m
+++ docs/source/mpsoc/software_framework/ParameterID/simulink_model/function_references/FFTRecordedVoltage.m
@@ -1,24 +1,27 @@
-function [output] = FFTRecordedVoltage(input,data,sampletime)
+function [frequencies,amplitudes,angles] = FFTRecordedVoltage(data,sampletime) %#codegen
 %FFTRecordedVoltage performs FFT
 %   Detailed explanation goes here
-%% carry datatype
-output = input;
-%% fft functionality
+%% init
 Y = single(zeros(10000,1));%1000 depends on the size of recorded data!
-L = length(data);
-Y = fft(data);
+L = single(length(data));
 temp_amplitude = single(zeros(5001,1));
 temp_angle = single(zeros(10000,1));
 tol = single(1e-7);
-for i=1:10000%1000 depends on the size of recorded data!
+frequencies = single(zeros(1,5001));
+amplitudes = single(zeros(1,5001));
+angles = single(zeros(1,5001));
+%% FFT
+Y = fft(data);
+
+for i=1:10000%depends on the size of recorded data!
     if(abs(Y(i))<tol)
         Y(i) = single(0);
     end
 end
 P2 = abs(Y/L);
 temp_amplitude = P2(1:L/2+1);
-output.amplitudes(2:end-1) = 2*temp_amplitude(2:end-1);
-output.frequencies = (0:(L/2))/L/sampletime;
+amplitudes(2:end-1) = 2*temp_amplitude(2:end-1);
+frequencies = (0:(L/2))/L/sampletime;
 temp_angle=angle(Y);
-output.angles=temp_angle(1:L/2+1);
+angles=temp_angle(1:L/2+1);
 end

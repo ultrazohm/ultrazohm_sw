abd5e92fb952324bcf1e7a4200a664fe48dbf6ac
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-02-13 14:52:20 +0100

    add functions and tests

---
+++ vitis/software/Baremetal/src/uz/uz_complex/uz_complex.h
@@ -0,0 +1,69 @@
+#ifndef UZ_COMPLEX_H
+#define UZ_COMPLEX_H
+
+#include "../uz_math_constants.h"
+#include <stdint.h>
+#include <math.h>
+
+/**
+ * @brief struct for complex number in cartesian form
+ *
+ */
+typedef struct uz_complex_cartesian_t{
+    float real; /**< real component */
+    float imaginary; /**< imaginary component */
+}uz_complex_cartesian_t;
+
+/**
+ * @brief struct for complex number in exponantial form
+ *
+ */
+typedef struct uz_complex_exponential_t{
+    float absolute; /**< absolute of exponential complex number */
+    float phase; /**< phase of exponential complex number in rad */
+}uz_complex_exponential_t;
+
+/**
+ * @brief multiplication for complex numbers
+ * 
+ * @param arg1 first factor
+ * @param arg2 second factor
+ * @return the complex multiplication of both factors
+ */
+uz_complex_cartesian_t uz_complex_multiplication(uz_complex_cartesian_t arg1, uz_complex_cartesian_t arg2);
+
+/**
+ * @brief addition for complex numbers
+ * 
+ * @param arg1 first summand
+ * @param arg2 second summand
+ * @return the complex addition of both summands
+ */
+uz_complex_cartesian_t uz_complex_addition(uz_complex_cartesian_t arg1, uz_complex_cartesian_t arg2);
+
+/**
+ * @brief division for complex numbers
+ * 
+ * @param dividend number to get divided
+ * @param divisor number to divide with
+ * @return the complex division of dividend/divisor
+*/
+uz_complex_cartesian_t uz_complex_division(uz_complex_cartesian_t dividend, uz_complex_cartesian_t divisor);
+
+/**
+ * @brief transformation from complex cartesian to complex exponential
+ * 
+ * @param in cartesian complex number
+ * @return complex number in exponential form
+*/
+uz_complex_exponential_t uz_complex_cartesian_to_exponential(uz_complex_cartesian_t in);
+
+/**
+ * @brief transformation from complex exponential to complex cartesian
+ * 
+ * @param in exponential complex number
+ * @return complex number in cartesian form
+*/
+uz_complex_cartesian_t uz_complex_exponential_to_cartesian(uz_complex_exponential_t in);
+
+#endif // UZ_COMPLEX_H

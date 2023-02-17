b4888fb7c912479f7317e9496d12373605242fc7
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-02-17 08:04:54 +0100

    generate code

--- vitis/software/Baremetal/src/uz/uz_ParameterID/ElectricalID_6ph/ElectricalID_6ph_codegen.h
+++ vitis/software/Baremetal/src/uz/uz_ParameterID/ElectricalID_6ph/ElectricalID_6ph_codegen.h
@@ -7,9 +7,9 @@
  *
  * Code generated for Simulink model 'ElectricalID_6ph_codegen'.
  *
- * Model version                  : 3.44
+ * Model version                  : 3.48
  * Simulink Coder version         : 9.6 (R2021b) 14-May-2021
- * C/C++ source code generated on : Mon Feb 13 11:54:04 2023
+ * C/C++ source code generated on : Fri Feb 17 08:00:25 2023
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex-R
@@ -63,7 +63,6 @@
 
 /* Forward declaration for rtModel */
 typedef struct tag_RTM_ElectricalID_6ph_code_t RT_MODEL_ElectricalID_6ph_cod_t;
-
 
 /* Block signals and states (default storage) for system '<Root>' */
 typedef struct {

1c71a1094a246d556f552451bd3da467f13d3916
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-02-28 14:49:32 +0100

    add functions for current control flux map

--- vitis/software/Baremetal/src/uz/uz_ParameterID/FluxMapID_6ph/uz_ParaID_FluxMapID_6ph.c
+++ vitis/software/Baremetal/src/uz/uz_ParameterID/FluxMapID_6ph/uz_ParaID_FluxMapID_6ph.c
@@ -72,4 +72,86 @@ uz_ParaID_FluxMapID_output_t* uz_get_FluxMapID_6ph_output(uz_ParaID_FluxMapID_6p
 	return(&self->output.FluxMapID_output);
 }
 
+void uz_FluxMapID_6ph_init_controllers(uz_ParameterID_Data_t* Data, uz_CurrentControl_t* CC_instance_1, uz_CurrentControl_t* CC_instance_2, uz_resonantController_t* res_instance_1, uz_resonantController_t* res_instance_2)
+{
+    struct uz_CurrentControl_config cc_config_1 = {
+        .decoupling_select = no_decoupling,
+        .config_id.Ki = PARAMETERID6PH_FLUXMAP_BANDWITH_CC*Data->GlobalConfig.PMSM_config.Ld_Henry,
+        .config_id.Kp = PARAMETERID6PH_FLUXMAP_BANDWITH_CC*Data->GlobalConfig.PMSM_config.R_ph_Ohm,
+        .config_id.samplingTime_sec = Data->GlobalConfig.sampleTimeISR,
+        .config_id.type = parallel,
+        .config_id.lower_limit = -1.0f*PARAMETERID6PH_FLUXMAP_LIMIT_PI,
+        .config_id.upper_limit = PARAMETERID6PH_FLUXMAP_LIMIT_PI,
+        .config_iq.Ki = PARAMETERID6PH_FLUXMAP_BANDWITH_CC*Data->GlobalConfig.PMSM_config.Lq_Henry,
+        .config_iq.Kp = PARAMETERID6PH_FLUXMAP_BANDWITH_CC*Data->GlobalConfig.PMSM_config.R_ph_Ohm,
+        .config_iq.samplingTime_sec = Data->GlobalConfig.sampleTimeISR,
+        .config_iq.type = parallel,
+        .config_iq.lower_limit = -1.0f*PARAMETERID6PH_FLUXMAP_LIMIT_PI,
+        .config_iq.upper_limit = PARAMETERID6PH_FLUXMAP_LIMIT_PI};
+    struct uz_CurrentControl_config cc_config_2 = {
+        .decoupling_select = no_decoupling,
+        //.config_id.Ki = PARAMETERID6PH_FLUXMAP_BANDWITH_CC*Data->GlobalConfig.Lx_Henry,
+        .config_id.Kp = PARAMETERID6PH_FLUXMAP_BANDWITH_CC*Data->GlobalConfig.PMSM_config.R_ph_Ohm,
+        .config_id.samplingTime_sec = Data->GlobalConfig.sampleTimeISR,
+        .config_id.type = parallel,
+        .config_id.lower_limit = -1.0f*PARAMETERID6PH_FLUXMAP_LIMIT_PI,
+        .config_id.upper_limit = PARAMETERID6PH_FLUXMAP_LIMIT_PI,
+        //.config_iq.Ki = PARAMETERID6PH_FLUXMAP_BANDWITH_CC*Data->GlobalConfig.Ly_Henry,
+        .config_iq.Kp = PARAMETERID6PH_FLUXMAP_BANDWITH_CC*Data->GlobalConfig.PMSM_config.R_ph_Ohm,
+        .config_iq.samplingTime_sec = Data->GlobalConfig.sampleTimeISR,
+        .config_iq.type = parallel,
+        .config_iq.lower_limit = -1.0f*PARAMETERID6PH_FLUXMAP_LIMIT_PI,
+        .config_iq.upper_limit = PARAMETERID6PH_FLUXMAP_LIMIT_PI};
+    struct uz_resonantController_config resonant_config_1 = {
+        .sampling_time = Data->GlobalConfig.sampleTimeISR,
+        //.gain = 1.0f,
+        //.harmonic_order = 99.0f,
+        .fundamental_frequency = 1.0f
+        .lower_limit = -1.0f*PARAMETERID6PH_FLUXMAP_LIMIT_PI,
+        .upper_limit = PARAMETERID6PH_FLUXMAP_LIMIT_PI,
+        //.antiwindup_gain = 1.0f,
+        .in_reference_value = 0.0f,
+        .in_measured_value = 0.0f};
+    struct uz_resonantController_config resonant_config_2 = {0};
+    CC_instance_1 = uz_CurrentControl_init(cc_config_1);
+    CC_instance_2 = uz_CurrentControl_init(cc_config_2);
+    res_instance_1 = uz_resonantController_init(resonant_config_1);
+    res_instance_2 = uz_resonantController_init(resonant_config_2);
+}
+
+uz_6ph_dq_t uz_FluxMapID_6ph_step_controllers(uz_ParameterID_Data_t* Data, uz_CurrentControl_t* CC_instance_1, uz_CurrentControl_t* CC_instance_2, uz_resonantController_t* resonant_1, uz_resonantController_t* resonant_2)
+{
+    // output struct
+    uz_6ph_dq_t out = {0};
+    // struct holding controller output for 1st and 2nd reference frames
+    uz_3ph_dq_t cc_out_ab = {0};
+    uz_3ph_dq_t cc_out_xy = {0};
+    float res_out_1 = 0.0f;
+    float res_out_2 = 0.0f;
+    // actual rotating 1st reference frame
+    uz_3ph_dq_t actual_ab = {
+        .d = Data->ActualValues.v_dq_6ph.d,
+        .q = Data->ActualValues.v_dq_6ph.q};
+    // actual stationary 2nd reference frame
+    uz_3ph_alphabeta_t actual_xy_stationary = {
+        .alpha = Data->ActualValues.v_dq_6ph.x,
+        .beta = Data->ActualValues.v_dq_6ph.y};
+    // actual rotating 2nd reference frame
+    uz_3ph_dq_t actual_xy = uz_transformation_3ph_alphabeta_to_dq(actual_xy_stationary, -1.0f*Data->ActualValues.theta_el);
+    switch(Data->FluxmapID_extended_controller_Output->selected_subsystem)
+    {
+        // control alpha beta system
+        case 1:
+        {
+            cc_out_ab = uz_CurrentControl_sample(CC_instance_1, Data->FluxmapID_extended_controller_Output->ab_i_dq_PI_ref, actual_ab, Data->ActualValues.V_DC, Data->ActualValues.omega_el);
+            cc_out_xy = uz_CurrentControl_sample(CC_instance_2, Data->FluxmapID_extended_controller_Output->xy_i_dq_PI_ref, actual_xy, Data->ActualValues.V_DC, Data->ActualValues.omega_el);
+            res_out_1 = uz_resonantController_step(resonant_1, 0.0f, Data->ActualValues.v_dq_6ph.alpha?, Data->ActualValues.omega_el);
+            res_out_2 = uz_resonantController_step(resonant_2, 0.0f, Data->ActualValues.v_dq_6ph.alpha?, Data->ActualValues.omega_el);
+            break;
+        }
+        default:
+            break:
+    }
+}
+
 #endif

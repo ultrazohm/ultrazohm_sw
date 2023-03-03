6484ee3452d4225e3c539b0c4ef0c2404376d58f
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-03-03 16:15:29 +0100

    Adds doxygen comments for CleanPsiArray and InterpMeshGrid

--- vitis/software/Baremetal/src/uz/uz_ParameterID/OnlineID/CleanPsiArray/uz_ParaID_CleanPsiArray.h
+++ vitis/software/Baremetal/src/uz/uz_ParameterID/OnlineID/CleanPsiArray/uz_ParaID_CleanPsiArray.h
@@ -38,10 +38,45 @@ uz_ParaID_CleanPsiArray_t* uz_CleanPsiArray_init(void);
  */
 void uz_CleanPsiArray_step(uz_ParaID_CleanPsiArray_t *self);
 
+/**
+ * @brief Sets the OnlineID output for the CleanPsiArray
+ * 
+ * @param self pointer to the uz_ParaID_CleanPsiArray_t object
+ * @param OnlineID_output pointer to the output struct of the OnlineID
+ */
 void uz_CleanPsiArray_set_OnlineID_output(uz_ParaID_CleanPsiArray_t* self, uz_ParaID_OnlineID_output_t* OnlineID_output);
+
+/**
+ * @brief Sets the eta_c of the CleanPsiArray. The function is searching for measuring pairs which are closer than "eta_c" to their neighbors. 
+ * In this case it is averaging this similar pairs to avoid measuring values containing the same information. 
+ * 
+ * @param self pointer to the uz_ParaID_CleanPsiArray_t object
+ * @param eta_c factor to determine the averaging threshold for close measuring points
+ */
 void uz_CleanPsiArray_set_eta_c(uz_ParaID_CleanPsiArray_t* self, float eta_c);
 
+/**
+ * @brief Gets the cleaned psi array
+ * 
+ * @param self pointer to the uz_ParaID_CleanPsiArray_t object
+ * @return float* returns pointer to the first array element
+ */
 float* uz_CleanPsiArray_get_psi_array_out(uz_ParaID_CleanPsiArray_t* self);
+
+/**
+ * @brief Gets the array cleaned flag. Signals, when the entire array has been cleaned
+ * 
+ * @param self pointer to the uz_ParaID_CleanPsiArray_t object
+ * @return true array has been cleaned
+ * @return false array hasn't been cleaned
+ */
 bool uz_CleanPsiArray_get_array_cleaned_flag(uz_ParaID_CleanPsiArray_t* self);
+
+/**
+ * @brief Gets the amount of measuring points from the cleaned array
+ * 
+ * @param self pointer to the uz_ParaID_CleanPsiArray_t object
+ * @return float amount of measuring points
+ */
 float uz_CleanPsiArray_get_n_flux_points(uz_ParaID_CleanPsiArray_t* self);
 #endif

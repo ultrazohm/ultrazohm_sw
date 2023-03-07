1af44b52aed99973ebe6c8756e5ecd4df857ddb5
Josef Knoblach <knoblachjo83518@th-nuernberg.de>
2023-03-07 15:44:08 +0000

    adjusted tests to code changes, added some more tests for get_k_parameter, added tests for uz_vsd_opf_6ph_faultdetection

--- vitis/software/Baremetal/test/uz/uz_VSD_6ph_FD_control/test_uz_VSD_6ph_FD.c
+++ vitis/software/Baremetal/test/uz/uz_VSD_6ph_FD_control/test_uz_VSD_6ph_FD.c
@@ -70,20 +70,24 @@ void test_uz_vsd_opf_6ph_no_fault(void)
     TEST_ASSERT_FLOAT_WITHIN(0.001,expected_indices.R6,indices.R6);
 }
 
-void test_uz_vsd_opf_6ph_fault_on_phase_1(void)
-{
 
+uz_6ph_abc_t get_vaild_abcdef_currents_without_fault(float d, float q, float theta){
     uz_6ph_dq_t dq_values={
-        .d=-1.0f,
-        .q=10.0f,
+        .d=d,
+        .q=q,
         .x=0.0f,
         .y=0.0f,
         .z1=0.0f,
         .z2=0.0f        
     };
-    float theta=2.0f;
+    return uz_transformation_asym30deg_6ph_dq_to_abc(dq_values,theta); // Get valid abc currents without fault
+}
 
-    uz_6ph_abc_t currents_abc=uz_transformation_asym30deg_6ph_dq_to_abc(dq_values,theta); // Get valid abc currents without fault
+
+void test_uz_vsd_opf_6ph_fault_on_phase_1(void)
+{
+    uz_6ph_abc_t currents_abc = get_vaild_abcdef_currents_without_fault(-1.0f, 10.0f, 2.0f);
+
     // Potentially add an error by setting a phase current to 0
     currents_abc.a1=0.0f; // open phase fault one phase 1
 
@@ -101,12 +105,127 @@ void test_uz_vsd_opf_6ph_fault_on_phase_1(void)
     };
 
     TEST_ASSERT_FLOAT_WITHIN(0.001,expected_indices.R1,indices.R1);
+}
+
+
+void test_uz_vsd_opf_6ph_fault_on_phase_2(void)
+{
+    uz_6ph_abc_t currents_abc = get_vaild_abcdef_currents_without_fault(-1.0f, 10.0f, 2.0f);
+
+    // Potentially add an error by setting a phase current to 0
+    currents_abc.b1=0.0f; // open phase fault one phase 1
+
+    uz_6ph_alphabeta_t currents_ab = uz_transformation_asym30deg_6ph_abc_to_alphabeta(currents_abc);
+    uz_6phFD_indices indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+
+    indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+    uz_6phFD_indices expected_indices={
+        .R1=0.0f,
+        .R2=1.0f,
+        .R3=0.0f,
+        .R4=0.0f,
+        .R5=0.0f,
+        .R6=0.0f
+    };
+
     TEST_ASSERT_FLOAT_WITHIN(0.001,expected_indices.R2,indices.R2);
+}
+
+void test_uz_vsd_opf_6ph_fault_on_phase_3(void)
+{
+    uz_6ph_abc_t currents_abc = get_vaild_abcdef_currents_without_fault(-1.0f, 10.0f, 2.0f);
+
+    // Potentially add an error by setting a phase current to 0
+    currents_abc.c1=0.0f; // open phase fault one phase 1
+
+    uz_6ph_alphabeta_t currents_ab = uz_transformation_asym30deg_6ph_abc_to_alphabeta(currents_abc);
+    uz_6phFD_indices indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+
+    indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+    uz_6phFD_indices expected_indices={
+        .R1=0.0f,
+        .R2=0.0f,
+        .R3=1.0f,
+        .R4=0.0f,
+        .R5=0.0f,
+        .R6=0.0f
+    };
+
     TEST_ASSERT_FLOAT_WITHIN(0.001,expected_indices.R3,indices.R3);
+}
+
+
+void test_uz_vsd_opf_6ph_fault_on_phase_4(void)
+{
+    uz_6ph_abc_t currents_abc = get_vaild_abcdef_currents_without_fault(-1.0f, 10.0f, 2.0f);
+
+    // Potentially add an error by setting a phase current to 0
+    currents_abc.a2=0.0f; // open phase fault one phase 1
+
+    uz_6ph_alphabeta_t currents_ab = uz_transformation_asym30deg_6ph_abc_to_alphabeta(currents_abc);
+    uz_6phFD_indices indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+
+    indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+    uz_6phFD_indices expected_indices={
+        .R1=0.0f,
+        .R2=0.0f,
+        .R3=0.0f,
+        .R4=1.0f,
+        .R5=0.0f,
+        .R6=0.0f
+    };
+
     TEST_ASSERT_FLOAT_WITHIN(0.001,expected_indices.R4,indices.R4);
+}
+
+
+void test_uz_vsd_opf_6ph_fault_on_phase_5(void)
+{
+    uz_6ph_abc_t currents_abc = get_vaild_abcdef_currents_without_fault(-1.0f, 10.0f, 2.0f);
+
+    // Potentially add an error by setting a phase current to 0
+    currents_abc.b2=0.0f; // open phase fault one phase 1
+
+    uz_6ph_alphabeta_t currents_ab = uz_transformation_asym30deg_6ph_abc_to_alphabeta(currents_abc);
+    uz_6phFD_indices indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+
+    indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+    uz_6phFD_indices expected_indices={
+        .R1=0.0f,
+        .R2=0.0f,
+        .R3=0.0f,
+        .R4=0.0f,
+        .R5=1.0f,
+        .R6=0.0f
+    };
+
     TEST_ASSERT_FLOAT_WITHIN(0.001,expected_indices.R5,indices.R5);
+}
+
+
+void test_uz_vsd_opf_6ph_fault_on_phase_6(void)
+{
+    uz_6ph_abc_t currents_abc = get_vaild_abcdef_currents_without_fault(-1.0f, 10.0f, 2.0f);
+
+    // Potentially add an error by setting a phase current to 0
+    currents_abc.c2=0.0f; // open phase fault one phase 1
+
+    uz_6ph_alphabeta_t currents_ab = uz_transformation_asym30deg_6ph_abc_to_alphabeta(currents_abc);
+    uz_6phFD_indices indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+
+    indices = uz_vsd_opf_6ph_faultdetection(currents_ab);
+    uz_6phFD_indices expected_indices={
+        .R1=0.0f,
+        .R2=0.0f,
+        .R3=0.0f,
+        .R4=0.0f,
+        .R5=0.0f,
+        .R6=1.0f
+    };
+
     TEST_ASSERT_FLOAT_WITHIN(0.001,expected_indices.R6,indices.R6);
 }
+
 
 
 void test_uz_vsd_fd_hysteresis_filter(void)
--- vitis/software/Baremetal/test/uz/uz_VSD_6ph_FD_control/test_uz_VSD_6ph_ML_MT_Control.c
+++ vitis/software/Baremetal/test/uz/uz_VSD_6ph_FD_control/test_uz_VSD_6ph_ML_MT_Control.c
@@ -12,6 +12,8 @@ void setUp(void)
 
 void test_get_k_parameter(void)
 {
+    // check for some selected fault scenarios if the correct k-parameters are selected
+
     uz_6ph_MLMT_kparameter test_k_parameter = {0};
 
 
@@ -20,7 +22,7 @@ void test_get_k_parameter(void)
     // no phase faulted 1N MT:
     test_k_parameter = get_k_parameter(FD_indices, N1, MT);
 
-    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.a, 1.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.deratingFaktor, 1.0f);
     TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k1, 0.0f);
     TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k2, 0.0f);
     TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k3, 0.0f);
@@ -34,7 +36,7 @@ void test_get_k_parameter(void)
     FD_indices.R1 = 1.0f;
     test_k_parameter = get_k_parameter(FD_indices, N1, MT);
 
-    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.a, 0.694444);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.deratingFaktor, 0.694444);
     TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k1, -0.641110f);
     TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k2, -0.208910f);
     TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k3, -0.754240f);
@@ -44,24 +46,117 @@ void test_get_k_parameter(void)
     TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k7, 0.358890f);
     TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k8, -0.208910f);
 
+    // more than 3 OPF 1N MT:
+    FD_indices.R2 = 1.0f;
+    FD_indices.R3 = 1.0f;
+    FD_indices.R4 = 1.0f;
+    test_k_parameter = get_k_parameter(FD_indices, N1, MT);
+
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.deratingFaktor, 1.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k1, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k2, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k3, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k4, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k5, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k6, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k7, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k8, 0.0f);
+
+
+    // 1 OPF 2N ML
+    FD_indices.R1 = 0.0f;
+    FD_indices.R2 = 0.0f;
+    FD_indices.R3 = 1.0f;
+    FD_indices.R4 = 0.0f;
+    FD_indices.R5 = 0.0f;
+    FD_indices.R6 = 0.0f;
+    test_k_parameter = get_k_parameter(FD_indices, N2, ML);
+
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.deratingFaktor, 0.554700f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k1, -0.250000f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k2, -0.433010f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k3, 0.433010f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k4, 0.750000f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k5, 0.0);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k6, 0.0);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k7, 0.0);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k8, 0.0f);
+
+    // 2 OPF 2N ML
+    FD_indices.R1 = 0.0f;
+    FD_indices.R2 = 1.0f;
+    FD_indices.R3 = 1.0f;
+    FD_indices.R4 = 0.0f;
+    FD_indices.R5 = 0.0f;
+    FD_indices.R6 = 0.0f;
+    test_k_parameter = get_k_parameter(FD_indices, N2, ML);
+
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.deratingFaktor, 0.5f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k1, -1.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k2, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k3, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k4, 1.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k5, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k6, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k7, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k8, 0.0f);
+
+    // 3 OPF 2N MT
+    FD_indices.R1 = 1.0f;
+    FD_indices.R2 = 1.0f;
+    FD_indices.R3 = 1.0f;
+    FD_indices.R4 = 0.0f;
+    FD_indices.R5 = 0.0f;
+    FD_indices.R6 = 0.0f;
+    test_k_parameter = get_k_parameter(FD_indices, N2, MT);
+
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.deratingFaktor, 0.5f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k1, -1.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k2, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k3, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k4, 1.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k5, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k6, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k7, 0.0f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k8, 0.0f);
+
+
+    FD_indices.R1 = 0.0f;
+    FD_indices.R2 = 0.0f;
+    FD_indices.R3 = 0.0f;
+    FD_indices.R4 = 0.0f;
+    FD_indices.R5 = 1.0f;
+    FD_indices.R6 = 1.0f;
+    test_k_parameter = get_k_parameter(FD_indices, N1, MT);
+
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.deratingFaktor, 0.557668f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k1, 0.543660f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k2, -0.796040f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k3, 0.263470f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k4, -0.540410f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k5, -0.263470f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k6, -0.459590f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k7, 0.263470f);
+    TEST_ASSERT_EQUAL_FLOAT(test_k_parameter.k8, 0.459590f);
+
 }
 
 void test_uz_get_k_parameter_num_opf(void){
     uz_6phFD_opf test_struct = {0};
     uz_6phFD_indices input = {0};
 
-    test_struct = uz_num_opf(input);
+    test_struct = uz_get_numbers_and_order_of_faulted_phases(input);
 
     TEST_ASSERT_EQUAL_INT(test_struct.num_opf, 0);
 
     input.R1 = 1.0f;
-    test_struct = uz_num_opf(input);
+    test_struct = uz_get_numbers_and_order_of_faulted_phases(input);
     TEST_ASSERT_EQUAL_INT(test_struct.num_opf, 1);
     TEST_ASSERT_EQUAL_INT(test_struct.opf1, 1);
 
     input.R1 = 1.0f;
     input.R4 = 1.0f;
-    test_struct = uz_num_opf(input);
+    test_struct = uz_get_numbers_and_order_of_faulted_phases(input);
     TEST_ASSERT_EQUAL_INT(test_struct.num_opf, 2);
     TEST_ASSERT_EQUAL_INT(test_struct.opf1, 1);
     TEST_ASSERT_EQUAL_INT(test_struct.opf2, 4);
@@ -70,7 +165,7 @@ void test_uz_get_k_parameter_num_opf(void){
     input.R1 = 0.0f;
     input.R4 = 1.0f;
     input.R6 = 1.0f;
-    test_struct = uz_num_opf(input);
+    test_struct = uz_get_numbers_and_order_of_faulted_phases(input);
     TEST_ASSERT_EQUAL_INT(test_struct.num_opf, 2);
     TEST_ASSERT_EQUAL_INT(test_struct.opf1, 4);
     TEST_ASSERT_EQUAL_INT(test_struct.opf2, 6);
@@ -79,7 +174,7 @@ void test_uz_get_k_parameter_num_opf(void){
     input.R4 = 1.0f;
     input.R2 = 1.0f;
     input.R6 = 1.0f;
-    test_struct = uz_num_opf(input);
+    test_struct = uz_get_numbers_and_order_of_faulted_phases(input);
     TEST_ASSERT_EQUAL_INT(test_struct.num_opf, 3);
     TEST_ASSERT_EQUAL_INT(test_struct.opf1, 2);
     TEST_ASSERT_EQUAL_INT(test_struct.opf2, 4);
@@ -91,7 +186,7 @@ void test_uz_get_k_parameter_num_opf(void){
     input.R6 = 1.0f;
     input.R1 = 1.0f;
     input.R3 = 1.0f;
-    test_struct = uz_num_opf(input);
+    test_struct = uz_get_numbers_and_order_of_faulted_phases(input);
     TEST_ASSERT_EQUAL_INT(test_struct.num_opf, 5);
     TEST_ASSERT_EQUAL_INT(test_struct.opf1, 1);
     TEST_ASSERT_EQUAL_INT(test_struct.opf2, 2);

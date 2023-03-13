f5d8741ea421ad1fd0d291825b9c328295322bea
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-03-13 14:33:50 +0100

    add transition to exit dq fluxmap and enter xy

--- vitis/software/Baremetal/src/uz/uz_ParameterID/FluxMapID_6ph/FluxMapID_6ph_codegen.h
+++ vitis/software/Baremetal/src/uz/uz_ParameterID/FluxMapID_6ph/FluxMapID_6ph_codegen.h
@@ -9,7 +9,7 @@
  *
  * Model version                  : 3.68
  * Simulink Coder version         : 9.6 (R2021b) 14-May-2021
- * C/C++ source code generated on : Mon Mar 13 12:28:56 2023
+ * C/C++ source code generated on : Mon Mar 13 14:16:36 2023
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex-R
@@ -63,7 +63,6 @@
 
 /* Forward declaration for rtModel */
 typedef struct tag_RTM_FluxMapID_6ph_codegen_t RT_MODEL_FluxMapID_6ph_codege_t;
-
 
 /* Block signals and states (default storage) for system '<S1>/AMMstate.IdentificationDQ.Fluxmap' */
 typedef struct {
@@ -136,6 +135,8 @@ typedef struct {
   boolean_T resetIntegrator_n;    /* '<S1>/AMMstate.IdentificationDQ.Fluxmap' */
   boolean_T finished_calculation_i;
                                   /* '<S1>/AMMstate.IdentificationDQ.Fluxmap' */
+  boolean_T finished_ab;               /* '<Root>/FluxMapID_6ph_codegen' */
+  boolean_T finished_xy;               /* '<Root>/FluxMapID_6ph_codegen' */
 } DW_FluxMapID_6ph_codegen_t;
 
 /* External inputs (root inport signals with default storage) */

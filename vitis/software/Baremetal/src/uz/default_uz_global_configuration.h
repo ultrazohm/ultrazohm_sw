3836b29778d0e7babdf318e3077aeddb0b0f2a85
hoernermi <michael.hoerner@th-nuernberg.de>
2023-03-16 19:11:12 +0100

    added pwm detection to global config

--- vitis/software/Baremetal/src/uz/default_uz_global_configuration.h
+++ vitis/software/Baremetal/src/uz/default_uz_global_configuration.h
@@ -49,6 +49,7 @@
 #define UZ_MOVINGAVERAGEFILTER_MAX_INSTANCES            0U
 #define UZ_INVERTER_ADAPTER_MAX_INSTANCES               3U
 #define UZ_RESOLVERIP_MAX_INSTANCES                     3U
+#define UZ_PWM_DUTY_FREQ_DETECTION_MAX_INSTANCES        0U
 #endif
 
 // Configuration defines for the number of used instances for testing with ceedling
@@ -85,4 +86,5 @@
     #define UZ_INVERTER_ADAPTER_MAX_INSTANCES               20U
     #define UZ_EXP_SMOOTH_MAX_INSTANCES                     5U
     #define UZ_RESOLVERIP_MAX_INSTANCES                     40U
+    #define UZ_PWM_DUTY_FREQ_DETECTION_MAX_INSTANCES        5U    
 #endif

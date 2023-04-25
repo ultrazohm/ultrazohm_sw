e0b0e7fac6958b1965ef60b04b340c0bfc40fd5d
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-04-25 12:41:12 +0200

    change reinit filter to reset

--- vitis/software/Baremetal/src/uz/uz_encoder_offset_estimation/uz_encoder_offset_estimation.c
+++ vitis/software/Baremetal/src/uz/uz_encoder_offset_estimation/uz_encoder_offset_estimation.c
@@ -138,8 +138,8 @@ void uz_encoder_offset_estimation_set_min_omega_el(uz_encoder_offset_estimation_
 
 void uz_encoder_offset_estimation_reset_states(uz_encoder_offset_estimation_t* self){
     // set default values
-    self->filter_u_q = uz_filter_cumulativeavg_init();
-    self->filter_omega_el = uz_filter_cumulativeavg_init();
+    uz_filter_cumulativeavg_reset(self->filter_u_q);
+    uz_filter_cumulativeavg_reset(self->filter_omega_el);
     self->encoderoffset_current_state_ll = encoderoffset_ll_init;
     self->encoderoffset_current_state_hl = encoderoffset_hl_positive_setpoint;
     *self->ptr_offset_angle = *self->ptr_offset_angle - OFFSET_RANGE_RAD;         // use initial offset minus the ranges lower end to start search

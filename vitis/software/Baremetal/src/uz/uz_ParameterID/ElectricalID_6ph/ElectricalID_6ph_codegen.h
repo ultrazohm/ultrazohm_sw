830739b79bdca723f02c512bd8191aca9474eb75
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-04-27 09:37:06 +0200

    fix setpoint for psipm

--- vitis/software/Baremetal/src/uz/uz_ParameterID/ElectricalID_6ph/ElectricalID_6ph_codegen.h
+++ vitis/software/Baremetal/src/uz/uz_ParameterID/ElectricalID_6ph/ElectricalID_6ph_codegen.h
@@ -7,9 +7,9 @@
  *
  * Code generated for Simulink model 'ElectricalID_6ph_codegen'.
  *
- * Model version                  : 3.90
+ * Model version                  : 3.93
  * Simulink Coder version         : 9.6 (R2021b) 14-May-2021
- * C/C++ source code generated on : Wed Apr 26 15:27:46 2023
+ * C/C++ source code generated on : Thu Apr 27 09:35:20 2023
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex-R

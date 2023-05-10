6b169eafe5dc92651eac6779e3d86c46800bf893
Tobias <tobias.schindler@th-nuernberg.de>
2023-05-10 16:21:31 +0000

    Implements integration with clamping

--- vitis/software/Baremetal/src/uz_integrator/uz_integrator.c
+++ vitis/software/Baremetal/src/uz_integrator/uz_integrator.c
@@ -2,10 +2,14 @@
 
 float uz_integrator_eulerforward(float current_value, float old_value, float time_step, bool clamping_active)
 {
-    float output_value=0.0f;
-    if (!clamping_active)
+    float output_value = 0.0f;
+    if (clamping_active)
     {
-        output_value = (current_value*time_step + old_value); 
+        output_value = old_value;
+    }
+    else
+    {
+        output_value = (current_value * time_step + old_value);
     }
     return output_value;
 }
--- vitis/software/Baremetal/test/uz_integrator/test_uz_integrator.c
+++ vitis/software/Baremetal/test/uz_integrator/test_uz_integrator.c
@@ -43,4 +43,14 @@ void test_uz_integrator_one_to_two(void)
     TEST_ASSERT_EQUAL_FLOAT(2.0f, result);
 }
 
+void test_uz_integrator_clamped(void)
+{
+    float input = 1.0f;
+    float old_value = 1.0f;
+    bool clamping = true;
+    float ts = 1.0f;
+    float result = uz_integrator_eulerforward(input, old_value, ts, clamping);
+    TEST_ASSERT_EQUAL_FLOAT(1.0f, result);
+}
+
 #endif // TEST

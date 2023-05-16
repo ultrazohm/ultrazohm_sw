659dc8118422d210ca6cebdbc54b8e80452fcd0c
Tobias <tobias.schindler@th-nuernberg.de>
2023-05-16 17:49:14 +0000

    [skip ci] Commit Vivado binarys from DroneCI

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Fri Jan 27 15:00:54 2023
---Host        : c7842239c97b running 64-bit unknown
+--Date        : Tue May 16 15:35:09 2023
+--Host        : 7720a1a1a302 running 64-bit unknown
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

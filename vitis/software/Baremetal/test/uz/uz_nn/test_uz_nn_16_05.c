f463a382aefd3a0b22c1b3fda3f880ee8f5e9a7f
Florian Rosner <rosnerfl82835@th-nuernberg.de>
2023-05-16 11:31:13 +0000

    add new stepwise test file for backprop

---
+++ vitis/software/Baremetal/test/uz/uz_nn/test_uz_nn_16_05.c
@@ -0,0 +1,207 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "uz_nn.h"
+#include "uz_nn_layer.h"
+#include "uz_nn_activation_functions.h"
+#include "uz_matrix.h"
+#include "uz_sum.h"
+#include "uz_codegen0_ert_rtw/uz_codegen0.h"
+#include <time.h>
+
+
+#define NUMBER_OF_INPUTS 2
+#define NUMBER_OF_OUTPUTS 1
+#define NUMBER_OF_HIDDEN_LAYER 2
+#define NUMBER_OF_NEURONS_IN_FIRST_LAYER 5
+// stuff for training and update
+// sumout
+float s_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+float s_2[NUMBER_OF_OUTPUTS] = {0};
+//derivate matrix activation, Dimension = Sumout x Sumout 50x50=2500 z.B.
+float d_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER * NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+float d_2[NUMBER_OF_OUTPUTS * NUMBER_OF_OUTPUTS] = {0};
+//deltas
+float delta_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+float delta_2[NUMBER_OF_OUTPUTS] = {0};
+
+//cache gradients, Gräße entspricht delta des aktuellen layers * größe des Outputs des vorherigen layers
+float cacheg_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER * NUMBER_OF_INPUTS] = {0};
+float cacheg_2[NUMBER_OF_OUTPUTS * NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+//Gradienten
+float g_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER + NUMBER_OF_NEURONS_IN_FIRST_LAYER * NUMBER_OF_INPUTS] = {0};
+float g_2[NUMBER_OF_OUTPUTS + NUMBER_OF_OUTPUTS * NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+//
+float gradtest1[21] = {
+#include "functions_weights/GWB_test1.csv"
+};
+float x[NUMBER_OF_INPUTS] = {
+#include "functions_weights/X_test1.csv"
+};
+
+float reference_output[NUMBER_OF_OUTPUTS]= {
+#include "functions_weights/T_test1.csv"
+};
+
+float w_1[NUMBER_OF_INPUTS * NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {
+#include "functions_weights/layer1_weights_test1.csv"
+};
+float b_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {
+#include "functions_weights/layer1_bias_test1.csv"
+};
+float y_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+float w_2[NUMBER_OF_NEURONS_IN_FIRST_LAYER * NUMBER_OF_OUTPUTS] = {
+#include "functions_weights/layer2_weights_test1.csv"
+};
+float b_2[NUMBER_OF_OUTPUTS] = {
+#include "functions_weights/layer2_bias_test1.csv"
+};
+float y_2[NUMBER_OF_OUTPUTS] = {0};
+// error
+float e_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER]={0.0f};
+float e_2[NUMBER_OF_OUTPUTS]={0.0f};
+
+// Temporary buffer storage
+
+float T1[NUMBER_OF_NEURONS_IN_FIRST_LAYER * NUMBER_OF_OUTPUTS] = {0};
+float T2[1] = {0}; // eigentlich nicht nötig da man cachebackprop im letzten layer nicht benötigt, aber fest definiert in layerconfig
+
+
+struct uz_nn_layer_config config[NUMBER_OF_HIDDEN_LAYER] = {
+    [0] = {
+        .activation_function = activation_ReLU,
+        .number_of_neurons = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+        .number_of_inputs = NUMBER_OF_INPUTS,
+        .number_of_cachegradrows = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+        .number_of_cachegradcolumns = NUMBER_OF_INPUTS,
+        .number_of_temporaryrows = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+        .number_of_temporarycolumns = NUMBER_OF_OUTPUTS,
+        .length_of_weights = UZ_MATRIX_SIZE(w_1),
+        .length_of_bias = UZ_MATRIX_SIZE(b_1),
+        .length_of_output = UZ_MATRIX_SIZE(y_1),
+        .length_of_sumout = UZ_MATRIX_SIZE(s_1),
+        .length_of_derivate_gradients= UZ_MATRIX_SIZE(d_1),
+        .length_of_delta = UZ_MATRIX_SIZE(delta_1),
+        .length_of_error = UZ_MATRIX_SIZE(e_1),
+        .length_of_gradients = UZ_MATRIX_SIZE(g_1),
+        .length_of_temporarybackprop = UZ_MATRIX_SIZE(T1),
+        .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_1),
+        .weights = w_1,
+        .bias = b_1,
+        .output = y_1,
+        .sumout = s_1,
+        .derivate_gradients = d_1,
+        .delta = delta_1,
+        .temporarybackprop = T1,
+        .gradients = g_1,
+        .cachegradients = cacheg_1,
+        .error = e_1},
+    [1] = {
+      .activation_function = activation_linear, 
+      .number_of_neurons = NUMBER_OF_OUTPUTS,
+      .number_of_inputs = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+      .number_of_cachegradrows = NUMBER_OF_OUTPUTS,
+      .number_of_cachegradcolumns = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+      .number_of_temporaryrows = NUMBER_OF_OUTPUTS,
+      .number_of_temporarycolumns = NUMBER_OF_OUTPUTS,
+      .length_of_weights = UZ_MATRIX_SIZE(w_2),
+      .length_of_bias = UZ_MATRIX_SIZE(b_2),
+      .length_of_output = UZ_MATRIX_SIZE(y_2),
+      .length_of_sumout = UZ_MATRIX_SIZE(s_2),
+      .length_of_derivate_gradients = UZ_MATRIX_SIZE(d_2),
+      .length_of_delta = UZ_MATRIX_SIZE(delta_2),
+      .length_of_gradients = UZ_MATRIX_SIZE(g_2),
+      .length_of_error = UZ_MATRIX_SIZE(e_2),
+      .length_of_temporarybackprop = UZ_MATRIX_SIZE(T2),
+      .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_2),
+      .weights = w_2,
+      .bias = b_2,
+      .output = y_2,
+      .sumout = s_2,
+      .derivate_gradients = d_2,
+      .delta = delta_2,
+      .temporarybackprop = T2,
+      .gradients = g_2,
+      .cachegradients = cacheg_2,
+      .error=e_2}
+  };
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+
+void test_uz_nn_checkgradients_test1(void)
+{
+    uz_nn_t *test = uz_nn_init(config, NUMBER_OF_HIDDEN_LAYER);
+    struct uz_matrix_t x_matrix={0};
+    uz_matrix_t* input=uz_matrix_init(&x_matrix,&x[0],2,1,NUMBER_OF_INPUTS);
+    uz_nn_ff(test,input);
+    uz_matrix_t* output=uz_nn_get_output_data(test);
+    float result1=uz_matrix_get_element_zero_based(output,0,0);
+    float error1 = reference_output[0]- result1;
+    uz_nn_backward_pass(test,&error1,input);
+    uz_matrix_t* gradhelptest1 = uz_nn_get_gradient_data(test,1); // index 1-2 verwenden für nn mit 2 layern
+    uz_matrix_t* gradhelptest2 = uz_nn_get_gradient_data(test,2);
+    float gradhelpertest1[UZ_MATRIX_SIZE(g_1)+UZ_MATRIX_SIZE(g_2)];
+     gradhelpertest1[0] = uz_matrix_get_element_zero_based(gradhelptest1,0,0);
+     gradhelpertest1[1] = uz_matrix_get_element_zero_based(gradhelptest1,1,0);
+     gradhelpertest1[2] = uz_matrix_get_element_zero_based(gradhelptest1,2,0);
+     gradhelpertest1[3] = uz_matrix_get_element_zero_based(gradhelptest1,3,0);
+     gradhelpertest1[4] = uz_matrix_get_element_zero_based(gradhelptest1,4,0);
+     gradhelpertest1[5] = uz_matrix_get_element_zero_based(gradhelptest1,5,0);
+     gradhelpertest1[6] = uz_matrix_get_element_zero_based(gradhelptest1,6,0);
+     gradhelpertest1[7] = uz_matrix_get_element_zero_based(gradhelptest1,7,0);
+     gradhelpertest1[8] = uz_matrix_get_element_zero_based(gradhelptest1,8,0);
+     gradhelpertest1[9] = uz_matrix_get_element_zero_based(gradhelptest1,9,0);
+     gradhelpertest1[10] = uz_matrix_get_element_zero_based(gradhelptest1,10,0);
+     gradhelpertest1[11] = uz_matrix_get_element_zero_based(gradhelptest1,11,0);
+     gradhelpertest1[12] = uz_matrix_get_element_zero_based(gradhelptest1,12,0);
+     gradhelpertest1[13] = uz_matrix_get_element_zero_based(gradhelptest1,13,0);
+     gradhelpertest1[14] = uz_matrix_get_element_zero_based(gradhelptest1,14,0);
+     gradhelpertest1[15] = uz_matrix_get_element_zero_based(gradhelptest2,0,0);
+     gradhelpertest1[16] = uz_matrix_get_element_zero_based(gradhelptest2,1,0);
+     gradhelpertest1[17] = uz_matrix_get_element_zero_based(gradhelptest2,2,0);
+     gradhelpertest1[18] = uz_matrix_get_element_zero_based(gradhelptest2,3,0);
+     gradhelpertest1[19] = uz_matrix_get_element_zero_based(gradhelptest2,4,0);
+     gradhelpertest1[20] = uz_matrix_get_element_zero_based(gradhelptest2,5,0);
+     for(int i=0;i< (int)(sizeof(gradhelpertest1) / sizeof(float));i++) {
+        TEST_ASSERT_FLOAT_WITHIN(1e-02f, gradtest1[i], gradhelpertest1[i]);
+    }
+}
+
+void test_uz_nn_checkgradients_test2(void)
+{
+// check cachegradients
+   uz_nn_t *test2 = uz_nn_init(config, NUMBER_OF_HIDDEN_LAYER);
+    struct uz_matrix_t x_matrix={0};
+    uz_matrix_t* input=uz_matrix_init(&x_matrix,&x[0],2,1,NUMBER_OF_INPUTS);
+    uz_nn_ff(test2,input);
+    uz_matrix_t* output=uz_nn_get_output_data(test2);
+    float result2=uz_matrix_get_element_zero_based(output,0,0);
+    float error2 = reference_output[0]- result2;
+    uz_nn_backward_pass(test2,&error2,input);
+    uz_matrix_t* cachegradhelp1= uz_nn_get_cachegradient_data(test2,1); // index 1-2 verwenden für nn mit 2 layern
+    uz_matrix_t* cachegradhelp2 = uz_nn_get_cachegradient_data(test2,2);
+}
+
+void test_uz_nn_checkgradients_test3(void)
+{
+    // check delta
+   uz_nn_t *test3 = uz_nn_init(config, NUMBER_OF_HIDDEN_LAYER);
+    struct uz_matrix_t x_matrix={0};
+    uz_matrix_t* input=uz_matrix_init(&x_matrix,&x[0],2,1,NUMBER_OF_INPUTS);
+    uz_nn_ff(test3,input);
+    uz_matrix_t* output=uz_nn_get_output_data(test3);
+    float result3=uz_matrix_get_element_zero_based(output,0,0);
+    float error3 = reference_output[0]- result3;
+    uz_nn_backward_pass(test3,&error3,input);
+    uz_matrix_t* deltagradhelp1 = uz_nn_get_delta_data(test3,1); // index 1-2 verwenden für nn mit 2 layern
+    uz_matrix_t* deltagradhelp2 = uz_nn_get_delta_data(test3,2);
+}
+#endif // TEST

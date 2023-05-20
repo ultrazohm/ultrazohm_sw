8cdfb2ccdd97a9066d80f3c349c46ab9f01ff381
JensWenzl <jens.wenzl@gmail.com>
2023-05-20 09:54:21 +0200

    Change: Decrease DHCP timeout to 1s

--- vitis/software/FreeRTOS/main.c
+++ vitis/software/FreeRTOS/main.c
@@ -276,7 +276,7 @@ int main_thread()
 			break;
 		}
 		mscnt += DHCP_FINE_TIMER_MSECS;
-		if (mscnt >=5000) { // define timeout time here
+		if (mscnt >= 1000) { // define timeout time here
 			uz_printf("APU: DHCP request timed out\r\n");
 			uz_printf("APU: Configuring default IP of 192.168.1.233\r\n");
 			IP4_ADDR(&(server_netif.ip_addr),  192, 168, 178, 233);	// 192, 168, 1, 233);

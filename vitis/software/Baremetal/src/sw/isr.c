f3cfb209606e7683a798d6c89cf0189270e633be
Michael Schmidt <michael.schmidt@th-nuernberg.de>
2023-05-23 15:58:53 +0200

    add Current Control in the isr.c

--- vitis/software/Baremetal/src/sw/isr.c
+++ vitis/software/Baremetal/src/sw/isr.c
@@ -38,6 +38,35 @@ XIpiPsu INTCInst_IPI; // Interrupt handler -> only instance one -> responsible f
 // Global variable structure
 extern DS_Data Global_Data;
 
+// Variables for Current Control and Speed Control
+struct uz_3ph_abc_t measurement_current = {.a = 0.0f, .b = 0.0f, .c = 0.0f};
+struct uz_3ph_abc_t measurement_voltage = {.a = 0.0f, .b = 0.0f, .c = 0.0f};
+
+struct uz_3ph_dq_t dq_measurement_current = {.d = 0.0f, .q = 0.0f, .zero = 0.0f};
+struct uz_3ph_dq_t dq_measurement_voltage = {.d = 0.0f, .q = 0.0f, .zero = 0.0f};
+struct uz_3ph_dq_t dq_reference_current = {.d = 0.0f, .q = 0.0f, .zero = 0.0f};
+struct uz_3ph_dq_t dq_reference_voltage = {.d = 0.0f, .q = 0.0f, .zero = 0.0f};
+
+struct uz_DutyCycle_t output = {
+		.DutyCycle_A = 0.0f,
+		.DutyCycle_B = 0.0f,
+		.DutyCycle_C = 0.0f,
+};
+
+float omega_el_rad_per_sec = 0.0f;
+float omega_m_rad_per_sec = 0.0f;
+
+// Conversion factors for current and voltage
+#define NUMBER_OF_TURNS_CURRENT_MEASURING 	1.0f 	// Number of turns Current Measuring FU
+#define ADC_CURRENT_SCALING 				80.0f	// Scaling factor for current measurement
+#define ADC_CURRENT_OFFSET 					2.5f	// Offset for LEM Sensors
+#define DC_VOLT_CONV						12.5f	// Scaling factor for voltage measurement
+#define PHASE_VOLT_CONV						12.5f	// Scaling factor for voltage measurement
+#define ADC_PH_VOLT_OFFSET					0.0f	// Offset for voltage sensors
+#define USE_RESOVER							0U		// 0u: Incremental Encoder on D5
+#define MAX_CURRENT_ASSERTION				55.0f	// Maximum Current
+#define MAX_SPEED_ASSERTION					2500.0f	// Maximum Speed
+
 //==============================================================================================================================================================
 //----------------------------------------------------
 // INTERRUPT HANDLER FUNCTIONS
@@ -52,15 +81,75 @@ void ISR_Control(void *data)
     ReadAllADC();
     update_speed_and_position_of_encoder_on_D5(&Global_Data);
 
+    // Calculate theta_elec
+    Global_Data.av.theta_elec = Global_Data.av.theta_elec - Global_Data.av.theta_offset;
+
+    // convert ADC currents
+    measurement_current.a = ADC_CURRENT_SCALING * (Global_Data.aa.A1.me.ADC_A1 - ADC_CURRENT_OFFSET);
+    measurement_current.b = ADC_CURRENT_SCALING * (Global_Data.aa.A1.me.ADC_A2 - ADC_CURRENT_OFFSET);
+    measurement_current.c = ADC_CURRENT_SCALING * (Global_Data.aa.A1.me.ADC_A3 - ADC_CURRENT_OFFSET);
+    dq_measurement_current = uz_transformation_3ph_abc_to_dq(measurement_current, Global_Data.av.theta_elec);
+
+    // Value to Scope
+    Global_Data.av.I_U = measurement_current.a;
+    Global_Data.av.I_V = measurement_current.b;
+    Global_Data.av.I_W = measurement_current.c;
+
+    Global_Data.av.I_d = dq_measurement_current.d;
+    Global_Data.av.I_q = dq_measurement_current.q;
+
+    // Check if maximum current or maximum speed is reached
+    if (fabs(measurement_current.a) > MAX_CURRENT_ASSERTION || fabs(measurement_current.b) > MAX_CURRENT_ASSERTION || fabs(measurement_current.c) > MAX_CURRENT_ASSERTION || fabs(Global_Data.av.mechanicalRotorSpeed) > MAX_SPEED_ASSERTION)
+    {
+    	// Assertion to Stop Machine if max. Current or max. Speed
+    	output.DutyCycle_A = 0.0f;
+    	output.DutyCycle_B = 0.0f;
+    	output.DutyCycle_C = 0.0f;
+    	ultrazohm_state_machine_set_stop(true);
+    }
+
+    // convert ADC voltages
+    measurement_voltage.a = PHASE_VOLT_CONV * (Global_Data.aa.A1.me.ADC_B5 - ADC_PH_VOLT_OFFSET);
+    measurement_voltage.b = PHASE_VOLT_CONV * (Global_Data.aa.A1.me.ADC_B6 - ADC_PH_VOLT_OFFSET);
+    measurement_voltage.c = PHASE_VOLT_CONV * (Global_Data.aa.A1.me.ADC_B7 - ADC_PH_VOLT_OFFSET);
+    dq_measurement_voltage = uz_transformation_3ph_abc_to_dq(measurement_voltage, Global_Data.av.theta_elec);
+
+    // Value to Scope
+    Global_Data.av.U_U = measurement_voltage.a;
+    Global_Data.av.U_V = measurement_voltage.b;
+    Global_Data.av.U_W = measurement_voltage.c;
+
+    Global_Data.av.U_d = dq_measurement_voltage.d;
+    Global_Data.av.U_q = dq_measurement_voltage.q;
+
+    // calculating values needed for current control
+    omega_m_rad_per_sec = Global_Data.av.mechanicalRotorSpeed * (2.0f * M_PI) / 60.0f; // omega_mech
+    omega_el_rad_per_sec = omega_m_rad_per_sec * Global_Data.av.polepairs;
+
+    // Value to Scope
+
+
     platform_state_t current_state=ultrazohm_state_machine_get_state();
     if (current_state==control_state)
     {
-        // Start: Control algorithm - only if ultrazohm is in control state
+        if (Global_Data.av.flg_speed_control){
+        	//dq_reference_current = uz_SpeedControl_sample(Global_Data.objects.Speed_instance, omega_m_rad_per_sec, Global_Data.rasv.n_ref_rpm);
+        }else{
+        	// Set I_d and I_q currents for current control
+        	dq_reference_current.d = Global_Data.rasv.i_d_ref;
+        	dq_reference_current.q = Global_Data.rasv.i_q_ref;
+        	dq_reference_current.zero = 0.0f;
+        }
+
+        // FOC - get U_d and U_q as controlled variables
+        dq_reference_voltage = uz_CurrentControl_sample(Global_Data.objects.CurrentControl_instance, dq_reference_current, dq_measurement_current, Global_Data.av.U_ZK, omega_el_rad_per_sec);
+        // Generate PWM Signal for each phase
+        output = uz_Space_Vector_Modulation(dq_reference_voltage, Global_Data.av.U_ZK, Global_Data.av.theta_elec);
     }
     uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_0_to_5, Global_Data.rasv.halfBridge1DutyCycle, Global_Data.rasv.halfBridge2DutyCycle, Global_Data.rasv.halfBridge3DutyCycle);
-    uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_6_to_11, Global_Data.rasv.halfBridge4DutyCycle, Global_Data.rasv.halfBridge5DutyCycle, Global_Data.rasv.halfBridge6DutyCycle);
-    uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_12_to_17, Global_Data.rasv.halfBridge7DutyCycle, Global_Data.rasv.halfBridge8DutyCycle, Global_Data.rasv.halfBridge9DutyCycle);
-    uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_18_to_23, Global_Data.rasv.halfBridge10DutyCycle, Global_Data.rasv.halfBridge11DutyCycle, Global_Data.rasv.halfBridge12DutyCycle);
+    // uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_6_to_11, Global_Data.rasv.halfBridge4DutyCycle, Global_Data.rasv.halfBridge5DutyCycle, Global_Data.rasv.halfBridge6DutyCycle);
+    // uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_12_to_17, Global_Data.rasv.halfBridge7DutyCycle, Global_Data.rasv.halfBridge8DutyCycle, Global_Data.rasv.halfBridge9DutyCycle);
+    // uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_18_to_23, Global_Data.rasv.halfBridge10DutyCycle, Global_Data.rasv.halfBridge11DutyCycle, Global_Data.rasv.halfBridge12DutyCycle);
 
     // Set duty cycles for three-level modulator
     PWM_3L_SetDutyCycle(Global_Data.rasv.halfBridge1DutyCycle,

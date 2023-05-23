3c9d5a8c56da13fb89dfe03ea87e86b948b2eae3
Michael Schmidt <michael.schmidt@th-nuernberg.de>
2023-05-23 08:55:45 +0200

    generate bitstream of the modified block design and export bitstream to the vitis folder

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
---Date        : Tue May 16 15:30:53 2023
---Host        : hufnagelde-TP-T14s running 64-bit Ubuntu 22.04.2 LTS
+--Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+--Date        : Tue May 23 07:56:18 2023
+--Host        : Michael_Schmidt running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

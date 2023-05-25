2155a1996beef9fba8683e8c5b4996200f7b2720
Martin Geier <martin.geier@zohm-control.com>
2023-05-25 23:45:00 +0200

    Add (missing) whitespace

--- tcl_scripts/vitis_generate_UltraZohm_workspace.tcl
+++ tcl_scripts/vitis_generate_UltraZohm_workspace.tcl
@@ -73,203 +73,203 @@ proc app_build {{name *}} {
 
 proc vitis_main {} {
 
-set WS_PATH [getws]
-cd $WS_PATH
-cd ..
-set FOLDER_PATH [pwd]
-cd $WS_PATH
+  set WS_PATH [getws]
+  cd $WS_PATH
+  cd ..
+  set FOLDER_PATH [pwd]
+  cd $WS_PATH
 
-set PLATFORM_NAME 	UltraZohm
-set XSA_FOLDER 	[file join $FOLDER_PATH vivado_exported_xsa]
-set EXPORT_FOLDER [file join $FOLDER_PATH software]
-set SHARED_FOLDER [file join $EXPORT_FOLDER shared]
-
-
-set filename_Baremetal [file join $EXPORT_FOLDER Baremetal/src]
-set filename_FreeRTOS  [file join $EXPORT_FOLDER FreeRTOS]
-set filename_FSBL      [file join $EXPORT_FOLDER FSBL]
-set filename_FSBLelf   [file join $EXPORT_FOLDER BootImage]
-
-puts "Info (UltraZohm): Path to exports to be imported:"
-puts stdout $EXPORT_FOLDER
-
-####################################################
-puts "Info (UltraZohm): create Platform Project"
-if {[catch {set XSA_FILES [glob -join -dir ${XSA_FOLDER} *.xsa]} ]} {puts "Error:(UltraZohm) update of ${PLATFORM_NAME} failed: .xsa does not exist in ${XSA_FOLDER}."}
-#
-set XSA_FILE [lindex $XSA_FILES 0]
-puts "WARNING (UltraZohm): Make sure there is only one xsa file in ${XSA_FOLDER} "
-puts "Info (UltraZohm): using {$XSA_FILE}"
-
-#create platform
-platform create -name $PLATFORM_NAME -hw $XSA_FILE -no-boot-bsp
-
-#Domain FreeRTOS A53_0
-####################################################
-puts "Info (UltraZohm): create FreeRTOS_domain"
-#add freertos domain
-domain create -name FreeRTOS_domain -os freertos10_xilinx -proc psu_cortexa53_0
-
-puts "Info (UltraZohm): change FreeRTOS BSP settings"
-#add liIP lib to BSP
-bsp setlib -name lwip211
-
-# get list of configurable parameters for lwip lib
-#bsp listparams -lib lwip211
-bsp config api_mode SOCKET_API
-platform write
-bsp config dhcp_does_arp_check true
-platform write
-bsp config lwip_dhcp true
-platform write
-# increase heap size of freertos, to fix javascope glitches
-bsp config total_heap_size 200000000
-platform write
-
-puts "Info (UltraZohm): regenerate FreeRTOS BSP"
-#regenerate board support package
-bsp regenerate
+  set PLATFORM_NAME 	UltraZohm
+  set XSA_FOLDER 	[file join $FOLDER_PATH vivado_exported_xsa]
+  set EXPORT_FOLDER [file join $FOLDER_PATH software]
+  set SHARED_FOLDER [file join $EXPORT_FOLDER shared]
 
 
-#Domain Baremetal R5_0
-#####################################################
-puts "Info (UltraZohm): create Baremetal_domain"
-#create Baremetal domain
-domain create -name Baremetal_domain -os standalone -proc psu_cortexr5_0
-#save changes
-platform write
+  set filename_Baremetal [file join $EXPORT_FOLDER Baremetal/src]
+  set filename_FreeRTOS  [file join $EXPORT_FOLDER FreeRTOS]
+  set filename_FSBL      [file join $EXPORT_FOLDER FSBL]
+  set filename_FSBLelf   [file join $EXPORT_FOLDER BootImage]
+
+  puts "Info (UltraZohm): Path to exports to be imported:"
+  puts stdout $EXPORT_FOLDER
+
+  ####################################################
+  puts "Info (UltraZohm): create Platform Project"
+  if {[catch {set XSA_FILES [glob -join -dir ${XSA_FOLDER} *.xsa]} ]} {puts "Error:(UltraZohm) update of ${PLATFORM_NAME} failed: .xsa does not exist in ${XSA_FOLDER}."}
+  #
+  set XSA_FILE [lindex $XSA_FILES 0]
+  puts "WARNING (UltraZohm): Make sure there is only one xsa file in ${XSA_FOLDER} "
+  puts "Info (UltraZohm): using {$XSA_FILE}"
+
+  #create platform
+  platform create -name $PLATFORM_NAME -hw $XSA_FILE -no-boot-bsp
+
+  #Domain FreeRTOS A53_0
+  ####################################################
+  puts "Info (UltraZohm): create FreeRTOS_domain"
+  #add freertos domain
+  domain create -name FreeRTOS_domain -os freertos10_xilinx -proc psu_cortexa53_0
+
+  puts "Info (UltraZohm): change FreeRTOS BSP settings"
+  #add liIP lib to BSP
+  bsp setlib -name lwip211
+
+  # get list of configurable parameters for lwip lib
+  #bsp listparams -lib lwip211
+  bsp config api_mode SOCKET_API
+  platform write
+  bsp config dhcp_does_arp_check true
+  platform write
+  bsp config lwip_dhcp true
+  platform write
+  # increase heap size of freertos, to fix javascope glitches
+  bsp config total_heap_size 200000000
+  platform write
+
+  puts "Info (UltraZohm): regenerate FreeRTOS BSP"
+  #regenerate board support package
+  bsp regenerate
 
 
-##Domain FSBL (Standalone) A53_0
-#####################################################
-#puts "Info (UltraZohm): create FSBL_domain"
-##add FSBL domain
-#domain create -name FSBL_domain -os standalone -proc psu_cortexa53_0
-#
-#puts "Info (UltraZohm): change FSBL BSP settings"
-##add xilffs lib to BSP
-#bsp setlib -name xilffs
-##add xilpm lib to BSP
-#bsp setlib -name xilpm
-##add xilsecure lib to BSP
-#bsp setlib -name xilsecure
-#
-## get list of configurable parameters for xilffs lib
-##bsp listparams -lib xilffs
-#bsp config zynqmp_fsbl_bsp true
-#platform write
-#
-#puts "Info (UltraZohm): regenerate FSBL BSP"
-#regenerate board support package
-#bsp regenerate
-
-#Regenerate platform
-#####################################################
-puts "Info (UltraZohm): generate Platform project"
-platform generate
+  #Domain Baremetal R5_0
+  #####################################################
+  puts "Info (UltraZohm): create Baremetal_domain"
+  #create Baremetal domain
+  domain create -name Baremetal_domain -os standalone -proc psu_cortexr5_0
+  #save changes
+  platform write
 
 
-#Application Baremetal R5_0
-#####################################################
-puts "Info (UltraZohm): create Baremetal Application"
-# create application
-app create -name Baremetal -template {Empty Application} -platform $PLATFORM_NAME -domain Baremetal_domain
+  ##Domain FSBL (Standalone) A53_0
+  #####################################################
+  #puts "Info (UltraZohm): create FSBL_domain"
+  ##add FSBL domain
+  #domain create -name FSBL_domain -os standalone -proc psu_cortexa53_0
+  #
+  #puts "Info (UltraZohm): change FSBL BSP settings"
+  ##add xilffs lib to BSP
+  #bsp setlib -name xilffs
+  ##add xilpm lib to BSP
+  #bsp setlib -name xilpm
+  ##add xilsecure lib to BSP
+  #bsp setlib -name xilsecure
+  #
+  ## get list of configurable parameters for xilffs lib
+  ##bsp listparams -lib xilffs
+  #bsp config zynqmp_fsbl_bsp true
+  #platform write
+  #
+  #puts "Info (UltraZohm): regenerate FSBL BSP"
+  #regenerate board support package
+  #bsp regenerate
 
-puts "Info (UltraZohm): import Baremetal Application sources"
-# import sources to baremetal project
-# first the source files are linked
-importsources -name Baremetal -path $filename_Baremetal -soft-link
-# add shared folder
-importsources -name Baremetal -path $SHARED_FOLDER -soft-link
-# link to linker-script instead of copying it
-app config -name Baremetal -set linker-script $filename_Baremetal/lscript.ld
-
-#add math library to linker option
-app config -name Baremetal -add libraries m
-
-
-#Application FreeRTOS A53_0
-####################################################
-puts "Info (UltraZohm): create FreeRTOS Application"
-#create application
-app create -name FreeRTOS -template {Empty Application} -platform $PLATFORM_NAME -domain FreeRTOS_domain
-
-puts "Info (UltraZohm): import FreeRTOS Application sources"
-#import sources to freertos project
-
-puts "Info (UltraZohm): Path to FreeRTOS source files:"
-puts stdout $filename_FreeRTOS
-
-# first the source files are linked
-importsources -name FreeRTOS -path $filename_FreeRTOS -soft-link
-importsources -name FreeRTOS -path $SHARED_FOLDER -soft-link
-#link to linker-script instead of copying it
-app config -name FreeRTOS -set linker-script $filename_FreeRTOS/lscript.ld
-
-# add shared folder to build directory
-# this is a bit of hack, since it is not possible to add a compiler directory using the TCL script
-# we have to add it with -I"path" which results in the same
-# but in Vitis in the compiler settings, it is listed under miscellaneous instead of directories
-app config -name Baremetal compiler-misc -I"$SHARED_FOLDER"
-app config -name FreeRTOS compiler-misc -I"$SHARED_FOLDER"
-
-# set optimization level
-app config -name FreeRTOS -set compiler-optimization {Optimize most (-O3)}
-app config -name Baremetal -set compiler-optimization {Optimize more (-O2)}
-
-##Application FSBL (Standalone) A53_0
-#####################################################
-#puts "Info (UltraZohm): create FSBL Application"
-##create standalone app based on {Zynq MP FSBL}
-#app create -name FSBL -template {Zynq MP FSBL} -platform $PLATFORM_NAME -domain FSBL_domain
-##app create -name FSBL -template {Empty Application} -platform $PLATFORM_NAME -domain FSBL_domain
-#
-#puts "Path to FSBL:"
-#puts stdout $filename_FSBL
-#
-#puts "Info (UltraZohm): import FSBL Application sources"
-##import sources to FSBL project
-#importsources -name FSBL -path $filename_FSBL -soft-link
-#importsources -name FSBL -path $filename_FSBL/lscript.ld -linker-script
-
-puts "Info (UltraZohm): add standard FSBL.elf"
-platform config -remove-boot-bsp
-platform config -fsbl-elf $filename_FSBLelf/FSBL.elf
-platform write
-
-#Clean all
-####################################################
-puts "Info (UltraZohm): clean platform and all application projects"
-platform clean
-app_clean
-puts "Info (UltraZohm): build platform and all application projects"
-platform generate
-app_build
+  #Regenerate platform
+  #####################################################
+  puts "Info (UltraZohm): generate Platform project"
+  platform generate
 
 
-puts "========================================"
-# copy debug files
-set filename_meta [file join $WS_PATH .metadata]
-set filename_plugins [file join $filename_meta .plugins]
-set filename_eclipse [file join $filename_plugins org.eclipse.debug.core]
-set filename_launches [file join $filename_eclipse .launches]
-puts "Info (UltraZohm): Path to launches:"
-puts stdout $filename_launches
+  #Application Baremetal R5_0
+  #####################################################
+  puts "Info (UltraZohm): create Baremetal Application"
+  # create application
+  app create -name Baremetal -template {Empty Application} -platform $PLATFORM_NAME -domain Baremetal_domain
 
-file mkdir $filename_launches
-set DebugBaremetal [file join $EXPORT_FOLDER DebugBaremetal.launch]
-set DebugFreeRTOS [file join $EXPORT_FOLDER DebugFreeRTOS.launch]
-set DebugAll [file join $EXPORT_FOLDER Debug_FreeRTOS_Baremetal_FPGA.launch]
-#     file copy ?-force? ?--? source ?source ...? targetDir
-#file copy -force -- $DebugBaremetal $filename_launches
-file copy -force -- $DebugFreeRTOS $filename_launches
-file copy -force -- $DebugAll $filename_launches
-puts "========================================"
-puts "Info (UltraZohm): debug files copied"
-puts "Info (UltraZohm): debug configurations are visible after restarting Vitis"
-puts "========================================"
-puts "Info (UltraZohm): generate_UltraZohm_workspace.tcl script finished successfully"
-puts "========================================"
+  puts "Info (UltraZohm): import Baremetal Application sources"
+  # import sources to baremetal project
+  # first the source files are linked
+  importsources -name Baremetal -path $filename_Baremetal -soft-link
+  # add shared folder
+  importsources -name Baremetal -path $SHARED_FOLDER -soft-link
+  # link to linker-script instead of copying it
+  app config -name Baremetal -set linker-script $filename_Baremetal/lscript.ld
+
+  #add math library to linker option
+  app config -name Baremetal -add libraries m
+
+
+  #Application FreeRTOS A53_0
+  ####################################################
+  puts "Info (UltraZohm): create FreeRTOS Application"
+  #create application
+  app create -name FreeRTOS -template {Empty Application} -platform $PLATFORM_NAME -domain FreeRTOS_domain
+
+  puts "Info (UltraZohm): import FreeRTOS Application sources"
+  #import sources to freertos project
+
+  puts "Info (UltraZohm): Path to FreeRTOS source files:"
+  puts stdout $filename_FreeRTOS
+
+  # first the source files are linked
+  importsources -name FreeRTOS -path $filename_FreeRTOS -soft-link
+  importsources -name FreeRTOS -path $SHARED_FOLDER -soft-link
+  #link to linker-script instead of copying it
+  app config -name FreeRTOS -set linker-script $filename_FreeRTOS/lscript.ld
+
+  # add shared folder to build directory
+  # this is a bit of hack, since it is not possible to add a compiler directory using the TCL script
+  # we have to add it with -I"path" which results in the same
+  # but in Vitis in the compiler settings, it is listed under miscellaneous instead of directories
+  app config -name Baremetal compiler-misc -I"$SHARED_FOLDER"
+  app config -name FreeRTOS compiler-misc -I"$SHARED_FOLDER"
+
+  # set optimization level
+  app config -name FreeRTOS -set compiler-optimization {Optimize most (-O3)}
+  app config -name Baremetal -set compiler-optimization {Optimize more (-O2)}
+
+  ##Application FSBL (Standalone) A53_0
+  #####################################################
+  #puts "Info (UltraZohm): create FSBL Application"
+  ##create standalone app based on {Zynq MP FSBL}
+  #app create -name FSBL -template {Zynq MP FSBL} -platform $PLATFORM_NAME -domain FSBL_domain
+  ##app create -name FSBL -template {Empty Application} -platform $PLATFORM_NAME -domain FSBL_domain
+  #
+  #puts "Path to FSBL:"
+  #puts stdout $filename_FSBL
+  #
+  #puts "Info (UltraZohm): import FSBL Application sources"
+  ##import sources to FSBL project
+  #importsources -name FSBL -path $filename_FSBL -soft-link
+  #importsources -name FSBL -path $filename_FSBL/lscript.ld -linker-script
+
+  puts "Info (UltraZohm): add standard FSBL.elf"
+  platform config -remove-boot-bsp
+  platform config -fsbl-elf $filename_FSBLelf/FSBL.elf
+  platform write
+
+  #Clean all
+  ####################################################
+  puts "Info (UltraZohm): clean platform and all application projects"
+  platform clean
+  app_clean
+  puts "Info (UltraZohm): build platform and all application projects"
+  platform generate
+  app_build
+
+
+  puts "========================================"
+  # copy debug files
+  set filename_meta [file join $WS_PATH .metadata]
+  set filename_plugins [file join $filename_meta .plugins]
+  set filename_eclipse [file join $filename_plugins org.eclipse.debug.core]
+  set filename_launches [file join $filename_eclipse .launches]
+  puts "Info (UltraZohm): Path to launches:"
+  puts stdout $filename_launches
+
+  file mkdir $filename_launches
+  set DebugBaremetal [file join $EXPORT_FOLDER DebugBaremetal.launch]
+  set DebugFreeRTOS [file join $EXPORT_FOLDER DebugFreeRTOS.launch]
+  set DebugAll [file join $EXPORT_FOLDER Debug_FreeRTOS_Baremetal_FPGA.launch]
+  #     file copy ?-force? ?--? source ?source ...? targetDir
+  #file copy -force -- $DebugBaremetal $filename_launches
+  file copy -force -- $DebugFreeRTOS $filename_launches
+  file copy -force -- $DebugAll $filename_launches
+  puts "========================================"
+  puts "Info (UltraZohm): debug files copied"
+  puts "Info (UltraZohm): debug configurations are visible after restarting Vitis"
+  puts "========================================"
+  puts "Info (UltraZohm): generate_UltraZohm_workspace.tcl script finished successfully"
+  puts "========================================"
 }
 
 

32fe1dae03e9b867291cd326b2edbbbe2122317b
serayozcan <serayyozcann@gmail.com>
2023-06-06 12:44:55 +0200

    Cleans vivado folder

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Mon Jun  5 17:20:10 2023
---Host        : LAPTOP-ME665JGB running 64-bit major release  (build 9200)
+--Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+--Date        : Tue May 16 15:30:53 2023
+--Host        : hufnagelde-TP-T14s running 64-bit Ubuntu 22.04.2 LTS
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

b8206f1d83d097040ce5ddcab6a796d1fa3d0e78
Florian Rosner <rosnerfl82835@th-nuernberg.de>
2023-06-07 13:40:46 +0000

    fixed configs for all nn/mlp files, all tests work

--- vitis/software/Baremetal/test/IP_Cores/uz_mlp_three_layer/test_uz_mlp_three_layer.c
+++ vitis/software/Baremetal/test/IP_Cores/uz_mlp_three_layer/test_uz_mlp_three_layer.c
@@ -25,6 +25,30 @@ void tearDown(void)
 #define NUMBER_OF_OUTPUTS 4
 #define NUMBER_OF_HIDDEN_LAYER 3
 
+// stuff for training and update
+// sumout
+float s_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+float s_2[NUMBER_OF_NEURONS_IN_SECOND_LAYER] = {0};
+float s_3[NUMBER_OF_NEURONS_IN_THIRD_LAYER] = {0};
+float s_4[NUMBER_OF_OUTPUTS] = {0};
+
+float delta_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+float delta_2[NUMBER_OF_NEURONS_IN_SECOND_LAYER] = {0};
+float delta_3[NUMBER_OF_NEURONS_IN_THIRD_LAYER] = {0};
+float delta_4[NUMBER_OF_OUTPUTS] = {0};
+
+//cache gradients, Gräße entspricht delta des aktuellen layers * größe des Outputs des vorherigen layers
+float cacheg_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER * NUMBER_OF_INPUTS] = {0};
+float cacheg_2[NUMBER_OF_NEURONS_IN_SECOND_LAYER * NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+float cacheg_3[NUMBER_OF_NEURONS_IN_THIRD_LAYER * NUMBER_OF_NEURONS_IN_SECOND_LAYER] = {0};
+float cacheg_4[NUMBER_OF_OUTPUTS * NUMBER_OF_NEURONS_IN_THIRD_LAYER] = {0};
+
+//Gradienten
+float g_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER + NUMBER_OF_NEURONS_IN_FIRST_LAYER * NUMBER_OF_INPUTS] = {0};
+float g_2[NUMBER_OF_NEURONS_IN_SECOND_LAYER + NUMBER_OF_NEURONS_IN_SECOND_LAYER * NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {0};
+float g_3[NUMBER_OF_NEURONS_IN_THIRD_LAYER + NUMBER_OF_NEURONS_IN_THIRD_LAYER * NUMBER_OF_NEURONS_IN_SECOND_LAYER] = {0};
+float g_4[NUMBER_OF_OUTPUTS+NUMBER_OF_OUTPUTS * NUMBER_OF_NEURONS_IN_THIRD_LAYER] = {0};
+
 float x[NUMBER_OF_INPUTS] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f};
 float w_1[NUMBER_OF_INPUTS * NUMBER_OF_NEURONS_IN_FIRST_LAYER] = {
 #include "layer1_weights.csv"
@@ -58,20 +82,119 @@ float b_4[NUMBER_OF_OUTPUTS] = {
 };
 float y_4[NUMBER_OF_OUTPUTS] = {0};
 float mlp_ip_output[NUMBER_OF_OUTPUTS] = {0};
+
+float e_1[NUMBER_OF_NEURONS_IN_FIRST_LAYER]={0.0f};
+float e_2[NUMBER_OF_NEURONS_IN_SECOND_LAYER]={0.0f};
+float e_3[NUMBER_OF_NEURONS_IN_THIRD_LAYER]={0.0f};
+float e_4[NUMBER_OF_OUTPUTS]={0.0f};
+
+float T1[NUMBER_OF_NEURONS_IN_FIRST_LAYER * NUMBER_OF_NEURONS_IN_SECOND_LAYER] = {0};
+float T2[NUMBER_OF_NEURONS_IN_SECOND_LAYER * NUMBER_OF_NEURONS_IN_THIRD_LAYER] = {0};
+float T3[NUMBER_OF_NEURONS_IN_THIRD_LAYER * NUMBER_OF_OUTPUTS] = {0};
+float T4[4] = {0};
+
 struct uz_nn_layer_config software_nn_config[4] = {
     [0] = {
         .activation_function = activation_ReLU,
         .number_of_neurons = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
         .number_of_inputs = NUMBER_OF_INPUTS,
+        .number_of_cachegradrows = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+        .number_of_cachegradcolumns = NUMBER_OF_INPUTS,
+        .number_of_temporaryrows = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+        .number_of_temporarycolumns = NUMBER_OF_NEURONS_IN_SECOND_LAYER,
         .length_of_weights = UZ_MATRIX_SIZE(w_1),
         .length_of_bias = UZ_MATRIX_SIZE(b_1),
         .length_of_output = UZ_MATRIX_SIZE(y_1),
+        .length_of_sumout = UZ_MATRIX_SIZE(s_1),
+        .length_of_delta = UZ_MATRIX_SIZE(delta_1),
+        .length_of_error = UZ_MATRIX_SIZE(e_1),
+        .length_of_gradients = UZ_MATRIX_SIZE(g_1),
+        .length_of_temporarybackprop = UZ_MATRIX_SIZE(T1),
+        .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_1),
         .weights = w_1,
         .bias = b_1,
-        .output = y_1},
-    [1] = {.activation_function = activation_ReLU, .number_of_neurons = NUMBER_OF_NEURONS_IN_SECOND_LAYER, .number_of_inputs = NUMBER_OF_NEURONS_IN_SECOND_LAYER, .length_of_weights = UZ_MATRIX_SIZE(w_2), .length_of_bias = UZ_MATRIX_SIZE(b_2), .length_of_output = UZ_MATRIX_SIZE(y_2), .weights = w_2, .bias = b_2, .output = y_2},
-    [2] = {.activation_function = activation_ReLU, .number_of_neurons = NUMBER_OF_NEURONS_IN_THIRD_LAYER, .number_of_inputs = NUMBER_OF_NEURONS_IN_THIRD_LAYER, .length_of_weights = UZ_MATRIX_SIZE(w_3), .length_of_bias = UZ_MATRIX_SIZE(b_3), .length_of_output = UZ_MATRIX_SIZE(y_3), .weights = w_3, .bias = b_3, .output = y_3},
-    [3] = {.activation_function = activation_linear, .number_of_neurons = NUMBER_OF_OUTPUTS, .number_of_inputs = NUMBER_OF_NEURONS_IN_THIRD_LAYER, .length_of_weights = UZ_MATRIX_SIZE(w_4), .length_of_bias = UZ_MATRIX_SIZE(b_4), .length_of_output = UZ_MATRIX_SIZE(y_4), .weights = w_4, .bias = b_4, .output = y_4}};
+        .output = y_1,
+        .sumout = s_1,
+        .delta = delta_1,
+        .temporarybackprop = T1,
+        .gradients = g_1,
+        .cachegradients = cacheg_1,
+        .error = e_1},
+    [1] = {.activation_function = activation_ReLU,       
+      .number_of_neurons = NUMBER_OF_NEURONS_IN_SECOND_LAYER,
+      .number_of_inputs = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+      .number_of_cachegradrows = NUMBER_OF_NEURONS_IN_SECOND_LAYER,
+      .number_of_cachegradcolumns = NUMBER_OF_NEURONS_IN_FIRST_LAYER,
+      .number_of_temporaryrows = NUMBER_OF_NEURONS_IN_SECOND_LAYER,
+      .number_of_temporarycolumns = NUMBER_OF_NEURONS_IN_THIRD_LAYER,
+      .length_of_weights = UZ_MATRIX_SIZE(w_2),
+      .length_of_bias = UZ_MATRIX_SIZE(b_2),
+      .length_of_output = UZ_MATRIX_SIZE(y_2),
+      .length_of_sumout = UZ_MATRIX_SIZE(s_2),
+      .length_of_delta = UZ_MATRIX_SIZE(delta_2),
+      .length_of_gradients = UZ_MATRIX_SIZE(g_2),
+      .length_of_error = UZ_MATRIX_SIZE(e_2),
+      .length_of_temporarybackprop = UZ_MATRIX_SIZE(T2),
+      .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_2),
+      .weights = w_2,
+      .bias = b_2,
+      .output = y_2,
+      .sumout = s_2,
+      .delta = delta_2,
+      .temporarybackprop = T2,
+      .gradients = g_2,
+      .cachegradients = cacheg_2,
+      .error=e_2},
+    [2] = {.activation_function = activation_ReLU,    
+   .number_of_neurons = NUMBER_OF_NEURONS_IN_THIRD_LAYER,
+   .number_of_inputs = NUMBER_OF_NEURONS_IN_SECOND_LAYER,
+   .number_of_cachegradrows = NUMBER_OF_NEURONS_IN_THIRD_LAYER,
+   .number_of_cachegradcolumns = NUMBER_OF_NEURONS_IN_SECOND_LAYER,
+   .number_of_temporarycolumns = NUMBER_OF_NEURONS_IN_THIRD_LAYER,
+   .number_of_temporaryrows = NUMBER_OF_OUTPUTS,
+   .length_of_weights = UZ_MATRIX_SIZE(w_3),
+   .length_of_bias = UZ_MATRIX_SIZE(b_3),
+   .length_of_output = UZ_MATRIX_SIZE(y_3),
+   .length_of_sumout = UZ_MATRIX_SIZE(s_3),
+   .length_of_delta = UZ_MATRIX_SIZE(delta_3),
+   .length_of_gradients = UZ_MATRIX_SIZE(g_3),
+   .length_of_error = UZ_MATRIX_SIZE(e_3),
+   .length_of_temporarybackprop = UZ_MATRIX_SIZE(T3),
+   .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_3),
+   .weights = w_3,
+   .bias = b_3,
+   .output = y_3,
+   .sumout = s_3,
+   .delta = delta_3, 
+   .temporarybackprop = T3,
+   .gradients = g_3,
+   .cachegradients = cacheg_3,
+   .error= e_3},
+    [3] = {.activation_function = activation_linear,    
+    .number_of_neurons = NUMBER_OF_OUTPUTS,
+   .number_of_inputs = NUMBER_OF_NEURONS_IN_THIRD_LAYER,
+   .number_of_cachegradrows = NUMBER_OF_OUTPUTS,
+   .number_of_cachegradcolumns = NUMBER_OF_NEURONS_IN_THIRD_LAYER,
+   .number_of_temporarycolumns = 2,
+   .number_of_temporaryrows = 2,
+   .length_of_weights = UZ_MATRIX_SIZE(w_4),
+   .length_of_bias = UZ_MATRIX_SIZE(b_4),
+   .length_of_output = UZ_MATRIX_SIZE(y_4),
+   .length_of_sumout = UZ_MATRIX_SIZE(s_4),
+   .length_of_delta = UZ_MATRIX_SIZE(delta_4),
+   .length_of_gradients = UZ_MATRIX_SIZE(g_4),
+   .length_of_error = UZ_MATRIX_SIZE(e_4),
+   .length_of_temporarybackprop = UZ_MATRIX_SIZE(T4),
+   .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_4),
+   .weights = w_4,
+   .bias = b_4,
+   .output = y_4,
+   .sumout = s_4,
+   .delta = delta_4, 
+   .temporarybackprop = T4,
+   .gradients = g_4,
+   .cachegradients = cacheg_4,
+   .error= e_4}};
 
 float w_data[32] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 32, 32};
 float w_data_short[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
--- vitis/software/Baremetal/test/uz/uz_nn/test_uz_nn.c
+++ vitis/software/Baremetal/test/uz/uz_nn/test_uz_nn.c
@@ -11,6 +11,21 @@
 #define NUMBER_OF_OUTPUTS 1
 #define NUMBER_OF_NEURONS_IN_HIDDEN_LAYER 3
 
+float s_1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+float s_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+float s_3[NUMBER_OF_OUTPUTS] = {0};
+float delta_1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+float delta_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+float delta_3[NUMBER_OF_OUTPUTS] = {0};
+float cacheg_1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_INPUTS] = {0};
+float cacheg_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+float cacheg_3[NUMBER_OF_OUTPUTS * NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+
+//Gradienten
+float g_1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER + NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_INPUTS] = {0};
+float g_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER + NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+float g_3[NUMBER_OF_OUTPUTS+NUMBER_OF_OUTPUTS * NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+
 float x[NUMBER_OF_INPUTS] = {1, 2};
 float w_1[NUMBER_OF_INPUTS * NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {1, 2, 3, 4, 5, 6};
 float b_1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {1, 2, 3};
@@ -21,6 +36,13 @@ float y_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
 float w_3[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_OUTPUTS] = {16, 17, -18};
 float b_3[NUMBER_OF_OUTPUTS] = {7};
 float y_3[NUMBER_OF_OUTPUTS] = {0};
+
+float e_1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER]={0.0f};
+float e_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER]={0.0f};
+float e_3[NUMBER_OF_OUTPUTS]={0.0f};
+float T1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
+float T2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_OUTPUTS] = {0};
+float T3[4] = {0}; // eigentlich nicht nötig da man cachebackprop im letzten layer nicht benötigt, aber fest definiert in layerconfig
 
 void setUp(void)
 {
@@ -35,15 +57,78 @@ struct uz_nn_layer_config config[3] = {
         .activation_function = activation_ReLU,
         .number_of_neurons = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
         .number_of_inputs = NUMBER_OF_INPUTS,
+        .number_of_cachegradrows = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+        .number_of_cachegradcolumns = NUMBER_OF_INPUTS,
+        .number_of_temporaryrows = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+        .number_of_temporarycolumns = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
         .length_of_weights = UZ_MATRIX_SIZE(w_1),
         .length_of_bias = UZ_MATRIX_SIZE(b_1),
         .length_of_output = UZ_MATRIX_SIZE(y_1),
+        .length_of_sumout = UZ_MATRIX_SIZE(s_1),
+        .length_of_delta = UZ_MATRIX_SIZE(delta_1),
+        .length_of_error = UZ_MATRIX_SIZE(e_1),
+        .length_of_gradients = UZ_MATRIX_SIZE(g_1),
+        .length_of_temporarybackprop = UZ_MATRIX_SIZE(T1),
+        .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_1),
         .weights = w_1,
         .bias = b_1,
-        .output = y_1},
-    [1] = {.activation_function = activation_ReLU, .number_of_neurons = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER, .number_of_inputs = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER, .length_of_weights = UZ_MATRIX_SIZE(w_2), .length_of_bias = UZ_MATRIX_SIZE(b_2), .length_of_output = UZ_MATRIX_SIZE(y_2), .weights = w_2, .bias = b_2, .output = y_2},
-    [2] = {.activation_function = activation_linear, .number_of_neurons = NUMBER_OF_OUTPUTS, .number_of_inputs = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER, .length_of_weights = UZ_MATRIX_SIZE(w_3), .length_of_bias = UZ_MATRIX_SIZE(b_3), .length_of_output = UZ_MATRIX_SIZE(y_3), .weights = w_3, .bias = b_3, .output = y_3}};
-
+        .output = y_1,
+        .sumout = s_1,
+        .delta = delta_1,
+        .temporarybackprop = T1,
+        .gradients = g_1,
+        .cachegradients = cacheg_1,
+        .error = e_1},
+    [1] = {.activation_function = activation_ReLU,      
+      .number_of_neurons = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+      .number_of_inputs = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+      .number_of_cachegradrows = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+      .number_of_cachegradcolumns = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+      .number_of_temporaryrows = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+      .number_of_temporarycolumns = NUMBER_OF_OUTPUTS,
+      .length_of_weights = UZ_MATRIX_SIZE(w_2),
+      .length_of_bias = UZ_MATRIX_SIZE(b_2),
+      .length_of_output = UZ_MATRIX_SIZE(y_2),
+      .length_of_sumout = UZ_MATRIX_SIZE(s_2),
+      .length_of_delta = UZ_MATRIX_SIZE(delta_2),
+      .length_of_gradients = UZ_MATRIX_SIZE(g_2),
+      .length_of_error = UZ_MATRIX_SIZE(e_2),
+      .length_of_temporarybackprop = UZ_MATRIX_SIZE(T2),
+      .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_2),
+      .weights = w_2,
+      .bias = b_2,
+      .output = y_2,
+      .sumout = s_2,
+      .delta = delta_2,
+      .temporarybackprop = T2,
+      .gradients = g_2,
+      .cachegradients = cacheg_2,
+      .error=e_2},
+    [2] = {.activation_function = activation_linear,    
+   .number_of_neurons = NUMBER_OF_OUTPUTS,
+   .number_of_inputs = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+   .number_of_cachegradrows = NUMBER_OF_OUTPUTS,
+   .number_of_cachegradcolumns = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+   .number_of_temporarycolumns = 2,
+   .number_of_temporaryrows = 2,
+   .length_of_weights = UZ_MATRIX_SIZE(w_3),
+   .length_of_bias = UZ_MATRIX_SIZE(b_3),
+   .length_of_output = UZ_MATRIX_SIZE(y_3),
+   .length_of_sumout = UZ_MATRIX_SIZE(s_3),
+   .length_of_delta = UZ_MATRIX_SIZE(delta_3),
+   .length_of_gradients = UZ_MATRIX_SIZE(g_3),
+   .length_of_error = UZ_MATRIX_SIZE(e_3),
+   .length_of_temporarybackprop = UZ_MATRIX_SIZE(T3),
+   .length_of_cachegradients = UZ_MATRIX_SIZE(cacheg_3),
+   .weights = w_3,
+   .bias = b_3,
+   .output = y_3,
+   .sumout = s_3,
+   .delta = delta_3, 
+   .temporarybackprop = T3,
+   .gradients = g_3,
+   .cachegradients = cacheg_3,
+   .error= e_3}};
 void test_uz_nn_init(void)
 {
     uz_nn_init(config, 3);

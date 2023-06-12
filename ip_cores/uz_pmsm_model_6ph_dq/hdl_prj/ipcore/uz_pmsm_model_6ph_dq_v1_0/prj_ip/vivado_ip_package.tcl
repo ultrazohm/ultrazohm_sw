b24fd992b916c61e32e3c996ff529620e721f2eb
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-06-12 14:10:16 +0200

    make 6ph pmsm single precision

--- ip_cores/uz_pmsm_model_6ph_dq/hdl_prj/ipcore/uz_pmsm_model_6ph_dq_v1_0/prj_ip/vivado_ip_package.tcl
+++ ip_cores/uz_pmsm_model_6ph_dq/hdl_prj/ipcore/uz_pmsm_model_6ph_dq_v1_0/prj_ip/vivado_ip_package.tcl
@@ -4,16 +4,14 @@ set_property ip_repo_paths {../../} [current_fileset]
 # Add HDL source files to project
 add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_uz_pmsm_model_6ph_dq_pkg.vhd}
 add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_24_En11.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double2single.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_27_En18.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_double.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_double.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_18_En14.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single2double.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_double.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double_block.vhd}
-add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_double.vhd}
+add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_27_En18.vhd}
+add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_single.vhd}
+add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_single.vhd}
+add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_18_En14.vhd}
+add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single.vhd}
+add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_single.vhd}
+add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single_block.vhd}
+add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_single.vhd}
 add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_uz_pmsm_model_6ph_dq_tc.vhd}
 add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_src_uz_pmsm_model_6ph_dq.vhd}
 add_files -norecurse {../hdl/vhdl/uz_pmsm_model_6ph_dq_pkg.vhd}
@@ -47,7 +45,7 @@ foreach family $Families {append IPSupportedFamily "{$family} {Production} "}
 set_property supported_families $IPSupportedFamily [ipx::current_core]
 set_property taxonomy {{/HDL Coder Generated IP}} [ipx::current_core]
 set_property description {HDL Coder generated IP} [ipx::current_core]
-set_property core_revision 2112822800 [ipx::current_core]
+set_property core_revision 2112972913 [ipx::current_core]
 
 # Add HDL source files to IP
 ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_uz_pmsm_model_6ph_dq_pkg.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
@@ -58,46 +56,38 @@ ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_24_E
 set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_24_En11.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
 ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_24_En11.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
 set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_24_En11.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double2single.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double2single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double2single.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double2single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_27_En18.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_27_En18.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_27_En18.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_27_En18.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_double.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_double.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_double.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_double.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_18_En14.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_18_En14.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_18_En14.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_double_to_fixed_18_En14.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single2double.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single2double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single2double.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single2double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_double.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_double.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double_block.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double_block.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double_block.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_double_block.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_double.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
-ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_double.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
-set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_double.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_27_En18.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_27_En18.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_27_En18.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_27_En18.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_single.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_single.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_add_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_single.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_single.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_mul_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_18_En14.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_18_En14.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_18_En14.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_single_to_sfix_18_En14.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_single.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_single.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_sub_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single_block.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single_block.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single_block.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_relop_single_block.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_single.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
+ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_single.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]
+set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_nfp_convert_sfix_27_En16_to_single.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]]
 ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_uz_pmsm_model_6ph_dq_tc.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]
 set_property type {{vhdlSource}} [ipx::get_files {hdl/vhdl/uz_pmsm_model_6ph_dq_src_uz_pmsm_model_6ph_dq_tc.vhd} -of_objects [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]]
 ipx::add_file {hdl/vhdl/uz_pmsm_model_6ph_dq_src_uz_pmsm_model_6ph_dq_tc.vhd} [ipx::get_file_groups xilinx_anylanguagebehavioralsimulation -of_objects [ipx::current_core]]

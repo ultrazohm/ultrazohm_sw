3d3320ca970f01879cce1a9d3a8d38be02f28e5c
hoernermi <michael.hoerner@th-nuernberg.de>
2023-06-15 14:59:56 +0200

    changes according to review

--- vitis/software/Baremetal/src/IP_Cores/uz_resolver_pl_interface/uz_resolver_pl_interface_hw.c
+++ vitis/software/Baremetal/src/IP_Cores/uz_resolver_pl_interface/uz_resolver_pl_interface_hw.c
@@ -5,7 +5,7 @@
 
 void uz_resolver_pl_interface_hw_write_resolver_polepairs(uint32_t base_address, int32_t resolver_polepairs) {
     uz_assert_not_zero(base_address);
-    uz_assert(resolver_polepairs>0);
+    uz_assert(resolver_polepairs > 0);
     uz_axi_write_int32(base_address + resolver_polepairs_AXI_Data_uz_resolver_pl_interface, resolver_polepairs);
 }
 
@@ -21,13 +21,13 @@ void uz_resolver_pl_interface_hw_write_position_intmax(uint32_t base_address, in
 
 void uz_resolver_pl_interface_hw_write_machine_polepairs(uint32_t base_address, int32_t machine_polepairs) {
     uz_assert_not_zero(base_address);
-    uz_assert(machine_polepairs>0);
+    uz_assert(machine_polepairs > 0);
     uz_axi_write_int32(base_address + machine_polepairs_AXI_Data_uz_resolver_pl_interface, machine_polepairs);
 }
 
 void uz_resolver_pl_interface_hw_write_bitToRPS_factor(uint32_t base_address, float bitToRPS_factor) {
     uz_assert_not_zero(base_address);
-    uz_assert(bitToRPS_factor>0.0f);
+    uz_assert(bitToRPS_factor > 0.0f);
     uz_axi_write_float(base_address + bitToRPS_Factor_AXI_Data_uz_resolver_pl_interface, bitToRPS_factor);
 }
 

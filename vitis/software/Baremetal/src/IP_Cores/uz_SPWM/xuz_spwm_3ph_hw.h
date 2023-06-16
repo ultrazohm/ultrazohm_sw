732d94a00a8c253569f4619c6bb9fdb4f5609f16
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-06-16 10:58:58 +0200

    add drivers spwm

---
+++ vitis/software/Baremetal/src/IP_Cores/uz_SPWM/xuz_spwm_3ph_hw.h
@@ -0,0 +1,18 @@
+// ==============================================================
+// Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2022.2 (64-bit)
+// Tool Version Limit: 2019.12
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+// control
+// 0x00 : reserved
+// 0x04 : reserved
+// 0x08 : reserved
+// 0x0c : reserved
+// 0x10 : Data signal of u_dc
+//        bit 31~0 - u_dc[31:0] (Read/Write)
+// 0x14 : reserved
+// (SC = Self Clear, COR = Clear on Read, TOW = Toggle on Write, COH = Clear on Handshake)
+
+#define XUZ_SPWM_3PH_CONTROL_ADDR_U_DC_DATA 0x10
+#define XUZ_SPWM_3PH_CONTROL_BITS_U_DC_DATA 32
+

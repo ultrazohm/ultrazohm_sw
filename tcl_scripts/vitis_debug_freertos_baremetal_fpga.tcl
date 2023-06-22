cadb4b7a0b4d7224c22af96142fbb8ecdd4a9018
EykeAufderheide <eyke.aufderheide@tum.de>
2023-06-22 17:04:15 +0200

    change default breakpoint to exit of main instead of entrence

--- tcl_scripts/vitis_debug_freertos_baremetal_fpga.tcl
+++ tcl_scripts/vitis_debug_freertos_baremetal_fpga.tcl
@@ -67,7 +67,8 @@ dow Baremetal/Debug/Baremetal.elf
 puts "INFO: The application 'workspace/Baremetal/Debug/Baremetal.elf' is downloaded to processor 'psu_cortexr5_0'."
 configparams force-mem-access 0
 puts "INFO: 'configparams force-mem-access 0' command is executed."
-bpadd -addr &main
+#bpadd -addr &main
+bpadd -addr &exit
 puts "INFO: 'Enabled debug mode."
 targets -set -nocase -filter {name =~ "*A53*#0"}
 con

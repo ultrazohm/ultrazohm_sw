c808de8642276f0c9b20f735429d92e052b4035d
Josef_Knoblach <josef.knoblach@th-nuernberg.de>
2023-06-26 16:46:28 +0200

    added init for fault detection

--- vitis/software/Baremetal/src/main.c
+++ vitis/software/Baremetal/src/main.c
@@ -94,6 +94,63 @@ struct uz_temperaturecard_config_t t_config = {
 	.Configdata_B = {0},
 	.Configdata_C = {0}};
 
+
+// fault detection:
+// config for moving average filter
+
+struct uz_movingAverageFilter_config movAvF_config = {
+    .filterLength = 300U
+};
+
+
+// moving average filter for 6 phases
+uz_movingAverageFilter_t* movAvFilter_R1;
+uz_movingAverageFilter_t* movAvFilter_R2;
+uz_movingAverageFilter_t* movAvFilter_R3;
+uz_movingAverageFilter_t* movAvFilter_R4;
+uz_movingAverageFilter_t* movAvFilter_R5;
+uz_movingAverageFilter_t* movAvFilter_R6;
+
+// circular Buffers for 6 moving average filters
+float dataR1 [500] = {0};
+uz_array_float_t circularBuffer_R1 = {
+  .length = UZ_ARRAY_SIZE(dataR1),
+  .data = &dataR1[0]
+};
+
+float dataR2 [500] = {0};
+uz_array_float_t circularBuffer_R2 = {
+  .length = UZ_ARRAY_SIZE(dataR2),
+  .data = &dataR2[0]
+};
+
+float dataR3 [500] = {0};
+uz_array_float_t circularBuffer_R3 = {
+  .length = UZ_ARRAY_SIZE(dataR3),
+  .data = &dataR3[0]
+};
+
+float dataR4 [500] = {0};
+uz_array_float_t circularBuffer_R4 = {
+  .length = UZ_ARRAY_SIZE(dataR4),
+  .data = &dataR4[0]
+};
+
+float dataR5 [500] = {0};
+uz_array_float_t circularBuffer_R5 = {
+  .length = UZ_ARRAY_SIZE(dataR5),
+  .data = &dataR5[0]
+};
+
+float dataR6 [500] = {0};
+uz_array_float_t circularBuffer_R6 = {
+  .length = UZ_ARRAY_SIZE(dataR6),
+  .data = &dataR6[0]
+};
+
+
+
+
 int main(void)
 {
     int status = UZ_SUCCESS;
@@ -184,7 +241,7 @@ int main(void)
 			uz_ParameterID_6ph_initialize_encoder_offset_estimation(&ParaID_Data, &Global_Data.av.theta_elec_rad, &controller_out.q);
 			// ParaID inits end
 
-			// normal 6ph-current control
+			// 6ph-current control
 
 			Global_Data.objects.CC_instance_dq = uz_CurrentControl_init(cc_config_1);
 			Global_Data.objects.CC_instance_xy = uz_CurrentControl_init(cc_config_2);
@@ -199,7 +256,34 @@ int main(void)
 
 			// end current control
 
+			// initialize moving average filter
+			movAvFilter_R1 =  uz_movingAverageFilter_init(movAvF_config, circularBuffer_R1);
+			movAvFilter_R2 =  uz_movingAverageFilter_init(movAvF_config, circularBuffer_R2);
+			movAvFilter_R3 =  uz_movingAverageFilter_init(movAvF_config, circularBuffer_R3);
+			movAvFilter_R4 =  uz_movingAverageFilter_init(movAvF_config, circularBuffer_R4);
+			movAvFilter_R5 =  uz_movingAverageFilter_init(movAvF_config, circularBuffer_R5);
+			movAvFilter_R6 =  uz_movingAverageFilter_init(movAvF_config, circularBuffer_R6);
 
+			// config for OPF fault detection
+			struct uz_VSD_6ph_FD_config OPF_FD_config = {
+			    .upperlimit = 1.1f,
+			    .lowerlimit = 0.9f,
+			    .threshold = 0.4f,
+			    .mov_average_filter_length = 500,
+			    .sample_frequency_Hz = 1000,
+			    .percent_of_el_period = 0.4f,
+			    .movingAverageFilter_R1 = movAvFilter_R1,
+			    .movingAverageFilter_R2 = movAvFilter_R2,
+			    .movingAverageFilter_R3 = movAvFilter_R3,
+			    .movingAverageFilter_R4 = movAvFilter_R4,
+			    .movingAverageFilter_R5 = movAvFilter_R5,
+			    .movingAverageFilter_R6 = movAvFilter_R6,
+			};
+
+			//fault detection:
+			Global_Data.objects.OPF_FD = uz_VSD_6ph_FD_init(OPF_FD_config);
+
+			//
 
 			Initialize_Timer();
 			uz_SystemTime_init();

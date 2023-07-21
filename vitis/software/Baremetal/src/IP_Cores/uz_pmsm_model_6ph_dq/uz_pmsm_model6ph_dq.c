49adaec1c50504b455857755431cdc6506c8244e
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-07-21 10:22:14 +0200

    Fixes ceedling tests

--- vitis/software/Baremetal/src/IP_Cores/uz_pmsm_model_6ph_dq/uz_pmsm_model6ph_dq.c
+++ vitis/software/Baremetal/src/IP_Cores/uz_pmsm_model_6ph_dq/uz_pmsm_model6ph_dq.c
@@ -88,6 +88,7 @@ void uz_pmsm_model6ph_dq_reset(uz_pmsm_model6ph_dq_t *self)
     if(self->config.switch_pspl){
         uz_6ph_dq_t zero_voltages = {0};
         uz_pmsm_model6ph_dq_set_voltage(self, zero_voltages);
+        uz_pmsm_model6ph_trigger_voltage_input_strobe(self);
     }
     uz_pmsm_model6ph_hw_write_reset(self->config.base_address, false);
     uz_sleep_useconds(1U);
@@ -144,7 +145,6 @@ uz_6ph_dq_t uz_pmsm_model6ph_dq_get_input_voltages(uz_pmsm_model6ph_dq_t *self){
     uz_assert_not_NULL(self);
     uz_assert(self->is_ready);
     uz_6ph_dq_t out = {0};
-    uz_pmsm_model6ph_trigger_voltage_output_strobe(self);
     out.d = uz_pmsm_model6ph_hw_read_u_d(self->config.base_address);
     out.q = uz_pmsm_model6ph_hw_read_u_q(self->config.base_address);
     out.x = uz_pmsm_model6ph_hw_read_u_x(self->config.base_address);

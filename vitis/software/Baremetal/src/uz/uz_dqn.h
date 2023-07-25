45958361016c9c20e9a4bc29cbce7400e423a3d5
Tobias Schindler <tobias.schindler@th-nuernberg.de>
2023-07-25 07:31:06 +0000

    Pseudocode sample and buffer

--- vitis/software/Baremetal/src/uz/uz_dqn.h
+++ vitis/software/Baremetal/src/uz/uz_dqn.h
@@ -17,8 +17,29 @@ struct uz_dqn_expirience_replay_t{
    struct uz_matrix_t observations_k;
    struct uz_matrix_t observations_k_plus_one; 
    uint32_t action_k; 
-   struct uz_matrix_t reward_k; 
+   float reward_k; 
 };
+
+#define SIZE_OF_BUFFER 1000000
+
+uint32_t index=0U;
+struct uz_dqn_expirience_replay_t buffer[SIZE_OF_BUFFER]={0};
+
+void dqn_push_to_buffer(struct uz_dqn_expirience_replay_t sample)
+{
+   if(index==(SIZE_OF_BUFFER-1)){
+      index=0U;
+   }
+   buffer[index] = sample;
+   index++;
+}
+
+void dqn_get_random_samples(){
+   indexes[64]=randi(64);
+   buffer[indexes]
+   buffer_ptr=[&buffer[index(1) &buffer[index(2) ]         ]
+}
+
 
 
 #endif // UZ_DQN_H

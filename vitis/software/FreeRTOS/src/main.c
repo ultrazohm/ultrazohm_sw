9579fb0fc8253e537999b68da9111b7224d1e3f6
Jens Wenzl <jens_wenzl@t-online.de>
2023-08-06 17:10:42 +0200

    Change: Set BoardIP to 169.254.1.1 for DHCP timeout

--- vitis/software/FreeRTOS/src/main.c
+++ vitis/software/FreeRTOS/src/main.c
@@ -281,10 +281,12 @@ int main_thread()
 		mscnt += DHCP_FINE_TIMER_MSECS;
 		if (mscnt >=1000) { // define timeout time here
 			uz_printf("APU: DHCP request timed out\r\n");
-			uz_printf("APU: Configuring default IP of 192.168.1.233\r\n");
-			IP4_ADDR(&(server_netif.ip_addr),  192, 168, 1, 233);
-			IP4_ADDR(&(server_netif.netmask), 255, 255, 255,  0);
-			IP4_ADDR(&(server_netif.gw),  192, 168, 1, 1);
+			/* The 169.254.0.0/16 IP address range are link-local addresses. It will be active if
+			 * no DHCP server is in the network (e.g. UZ Board is directly connected to PC).
+			 * This allows connecting the UZ board to a PC without changing the PC network config. */
+			IP4_ADDR(&(server_netif.ip_addr),  169, 254, 1, 1);
+			IP4_ADDR(&(server_netif.netmask), 255, 255, 0,  0);
+			IP4_ADDR(&(server_netif.gw),  169, 254, 0, 1);
 			print_ip_settings(&(server_netif.ip_addr), &(server_netif.netmask), &(server_netif.gw));
 			/* print all application headers */
 			uz_printf("\r\n");

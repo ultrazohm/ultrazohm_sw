e815d758d86b955282e1a1de6585858c0a8f9465
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-08-14 11:28:22 +0200

    adds first test

--- vitis/software/Baremetal/test/uz/uz_CurrentControl/test_uz_space_vector_limitation.c
+++ vitis/software/Baremetal/test/uz/uz_CurrentControl/test_uz_space_vector_limitation.c
@@ -187,4 +187,31 @@ void test_uz_6ph_Space_Vector_Limitation_V_dc_zero(void){
     TEST_ASSERT_FAIL_ASSERT(uz_6ph_Space_Vector_Limitation(v_input_6ph_Volts,V_dc_volts, max_modulation_index, omega_el_rad_per_sec, i_actual_6ph_Ampere, &ext_clamping));
 }
 
+void test_uz_6ph_Space_Vector_Limitation_output(void){
+    //Values for comparision from simulation
+    float ud_in[5]={0.0f, -0.316f, -0.616f, -0.817f, -0.930f};
+    float uq_in[5]={0.0f, 0.333f, 0.682f, 0.918f, 1.033f};
+    float ux_in[5]={0.0f, 0.300f, 0.592f, 0.785f, 0.886f};
+    float uy_in[5]={0.0f, 0.313f, 0.645f, 0.896f, 1.049f};
+    float ud_out[5]={0.0f, -0.316f, -0.616f, -0.817f, -0.930f};
+    float uq_out[5]={0.0f, 0.333f, 0.682f, 0.918f, 1.033f};
+    float ux_out[5]={0.0f, 0.300f, 0.592f, 0.785f, 0.886f};
+    float uy_out[5]={0.0f, 0.313f, 0.645f, 0.896f, 1.049f};
+    max_modulation_index = 0.5f;
+    for(int i=0;i<5;i++){
+        i_actual_6ph_Ampere.q = 1.0f;
+        omega_el_rad_per_sec = 523.598f;
+        v_input_6ph_Volts.d = ud_in[i];
+        v_input_6ph_Volts.q = uq_in[i];
+        v_input_6ph_Volts.x = ux_in[i];
+        v_input_6ph_Volts.y = uy_in[i];
+        uz_6ph_dq_t output = uz_6ph_Space_Vector_Limitation(v_input_6ph_Volts,V_dc_volts, max_modulation_index, omega_el_rad_per_sec, i_actual_6ph_Ampere, &ext_clamping);
+		TEST_ASSERT_FLOAT_WITHIN(1e-02, ud_out[i], output.d);
+	    TEST_ASSERT_FLOAT_WITHIN(1e-02, uq_out[i], output.q);
+        TEST_ASSERT_FLOAT_WITHIN(1e-02, ux_out[i], output.x);
+        TEST_ASSERT_FLOAT_WITHIN(1e-02, uy_out[i], output.y);
+    }
+}
+
+
 #endif // TEST

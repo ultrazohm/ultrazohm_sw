fec1b4014129b8eb1dc7c452182163c822db8403
Jens Wenzl <jens_wenzl@t-online.de>
2023-08-19 09:06:08 +0200

    AddILA for control bits

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
---Date        : Sun Aug  6 16:44:43 2023
+--Date        : Sun Aug 13 21:00:28 2023
 --Host        : Dell-G3 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

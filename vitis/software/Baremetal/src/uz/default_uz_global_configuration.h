d0e518d82e4c2505800351b1957331f26163bd17
HoppeVa <hoppeva70971@th-nuernberg.de>
2023-08-29 15:34:08 +0200

    set all para id global config max instances 0, makes more sense

--- vitis/software/Baremetal/src/uz/default_uz_global_configuration.h
+++ vitis/software/Baremetal/src/uz/default_uz_global_configuration.h
@@ -46,8 +46,8 @@
 #define UZ_MUX_AXI_MAX_INSTANCES                        1U
 #define UZ_SPEEDCONTROL_MAX_INSTANCES                   1U
 #define UZ_IIR_FILTER_MAX_INSTANCES                     1U
-#define UZ_PARAMETERID_MAX_INSTANCES					1U
-#define UZ_PARAMETERID_6PH_MAX_INSTANCES                1U
+#define UZ_PARAMETERID_MAX_INSTANCES					0U
+#define UZ_PARAMETERID_6PH_MAX_INSTANCES                0U
 #define UZ_DAC_INTERFACE_MAX_INSTANCES                  0U
 #define UZ_INVERTER_3PH_MAX_INSTANCES                   3U
 #define UZ_PMSM6PH_TRANSFORMATION_MAX_INSTANCES         0U
@@ -65,10 +65,10 @@
 #define UZ_RESOLVER_PL_INTERFACE_MAX_INSTANCES          3U
 #define UZ_AXI_GPIO_MAX_INSTANCES                       0U
 #define UZ_6PH_OPEN_PHASE_FAULT_DETECTION               2U
-#define UZ_ENCODER_OFFSET_ESTIMATION_MAX_INSTANCES      1U
-#define UZ_FILTER_CUMULATIVEAVG_MAX_INSTANCES           5U
-#define UZ_DQ_SETPOINT_FILTER_MAX_INSTANCES             3U
-#define UZ_SUBSYSTEM_RESONANT_CONTROL_MAX_INSTANCES     3U
+#define UZ_ENCODER_OFFSET_ESTIMATION_MAX_INSTANCES      0U
+#define UZ_FILTER_CUMULATIVEAVG_MAX_INSTANCES           0U
+#define UZ_DQ_SETPOINT_FILTER_MAX_INSTANCES             0U
+#define UZ_SUBSYSTEM_RESONANT_CONTROL_MAX_INSTANCES     0U
 #endif
 
 // Configuration defines for the number of used instances for testing with ceedling

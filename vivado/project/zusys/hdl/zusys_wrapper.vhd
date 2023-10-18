6017250fddc6aff84e97a43bcb50ea2d4c850d30
hoernermi <michael.hoerner@th-nuernberg.de>
2023-10-18 14:31:50 +0200

    exported bitstream with improved OC shut down behaviour

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1.1 (win64) Build 2960000 Wed Aug  5 22:57:20 MDT 2020
---Date        : Fri Oct 13 14:17:54 2023
+--Date        : Wed Oct 18 13:24:58 2023
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

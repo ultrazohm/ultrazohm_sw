e4c8cb22dfb5538a887518c1d1b309d9566ae2c4
hoernermi <michael.hoerner@th-nuernberg.de>
2023-10-18 22:06:46 +0200

    exported bitstream with synchronizable pwm modules for testing

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Wed Oct 18 15:08:40 2023
+--Date        : Wed Oct 18 20:46:47 2023
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

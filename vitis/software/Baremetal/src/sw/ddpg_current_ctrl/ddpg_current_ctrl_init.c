d2ae4b4b983ceb2866abece2cb7081242b83f392
larabroghammer <lara.broghammer@th-nuernberg.de>
2023-10-18 17:34:46 +0200

    fix number of neurons

--- vitis/software/Baremetal/src/sw/ddpg_current_ctrl/ddpg_current_ctrl_init.c
+++ vitis/software/Baremetal/src/sw/ddpg_current_ctrl/ddpg_current_ctrl_init.c
@@ -3,7 +3,7 @@ extern DS_Data Global_Data;
 
 #define NUMBER_OF_INPUTS 9
 #define NUMBER_OF_OUTPUTS 2
-#define NUMBER_OF_NEURONS_IN_HIDDEN_LAYER 1
+#define NUMBER_OF_NEURONS_IN_HIDDEN_LAYER 64
 
 static float x[NUMBER_OF_INPUTS] = {0};
 static float w_1[NUMBER_OF_INPUTS * NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {
@@ -13,10 +13,10 @@ static float b_1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {
 	#include "ac_layer1_bias.csv"
 };
 static float y_1[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {0};
-static float w_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {
+static float w_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER * NUMBER_OF_OUTPUTS] = {
 	#include "ac_layer_out_weights.csv"
 };
-static float b_2[NUMBER_OF_NEURONS_IN_HIDDEN_LAYER] = {
+static float b_2[NUMBER_OF_OUTPUTS] = {
 	#include "ac_layer_out_bias.csv"
 };
 static float y_2[NUMBER_OF_OUTPUTS] = {0};
@@ -33,7 +33,7 @@ struct uz_nn_layer_config config[2] = {
     .bias = b_1,
     .output = y_1},
 [1] = {.activation_function = activation_tanh,
-        .number_of_neurons = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
+        .number_of_neurons = NUMBER_OF_OUTPUTS,
         .number_of_inputs = NUMBER_OF_NEURONS_IN_HIDDEN_LAYER,
         .length_of_weights = UZ_MATRIX_SIZE(w_2),
         .length_of_bias = UZ_MATRIX_SIZE(b_2),

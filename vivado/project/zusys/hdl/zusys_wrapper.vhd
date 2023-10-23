c3370c3692d2a9bea756a93366a8a3f44fb49227
Philipp Doelger <doelgerph90359@th-nuernberg.de>
2023-10-23 17:36:34 +0200

    generated bitstream static LUTs and singeprecision

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
---Date        : Wed Oct 11 19:20:40 2023
---Host        : TS-WS running 64-bit Ubuntu 22.04.3 LTS
+--Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+--Date        : Thu Oct 19 15:53:32 2023
+--Host        : DESKTOP-MRHTEUR running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

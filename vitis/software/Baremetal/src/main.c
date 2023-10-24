d921fcb142b4f182057cf23075acaa2dfbb8ea16
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-10-24 16:13:52 +0200

    Inits the arrays with values

--- vitis/software/Baremetal/src/main.c
+++ vitis/software/Baremetal/src/main.c
@@ -43,7 +43,7 @@ struct uz_matrix_t A_input = {0};
 struct uz_matrix_t B_input = {0};
 struct uz_matrix_t C_output = {0};
 float A_matrix[5] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f};
-float B_matrix[25] = {1.0f, 2.0f};
+float B_matrix[25] = {1.0f, 2.0f,3.0f,4.0f,5.0f,6.0f,7.0f,8.0f,9.0f,10.0f,11.0f,12.0f,13.0f,14.0f,15.0f,16.0f,17.0f,18.0f,19.0f,20.0f,21.0f,22.0f,23.0f,24.0f,25.0f};
 float C_matrix[5] = {0};
 
 struct uz_Matrix_Multi_config config = {

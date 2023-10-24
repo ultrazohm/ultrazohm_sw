1a47ecdcb926705bffcb3e5409b0aeebfe3e5333
hoernermi <michael.hoerner@th-nuernberg.de>
2023-10-24 11:27:46 +0200

    Adds improved ip-core to bitsream

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
---Date        : Tue Oct 17 14:34:50 2023
---Host        : hufnagelde-TP-T14s running 64-bit Ubuntu 22.04.3 LTS
+--Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+--Date        : Tue Oct 24 10:31:36 2023
+--Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

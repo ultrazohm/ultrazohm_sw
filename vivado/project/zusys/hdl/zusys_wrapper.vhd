85df9273d32c7bd0ad1d6bd8e4e5fa993ee86a2b
hoernermi <michael.hoerner@th-nuernberg.de>
2023-10-25 14:49:27 +0200

    exports bitstream

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Tue Oct 24 10:31:36 2023
+--Date        : Wed Oct 25 13:56:26 2023
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

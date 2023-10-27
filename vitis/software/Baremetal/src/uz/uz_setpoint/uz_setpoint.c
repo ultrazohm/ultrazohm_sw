f91d233fa16da30fec8239ffe56aa9ddea5e490d
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-10-27 12:43:12 +0200

    Adds as placeholder separate function with the proper omega_cut calculation

--- vitis/software/Baremetal/src/uz/uz_setpoint/uz_setpoint.c
+++ vitis/software/Baremetal/src/uz/uz_setpoint/uz_setpoint.c
@@ -48,6 +48,7 @@ static uz_3ph_dq_t uz_SetPoint_FOC_control(uz_SetPoint_t* self, float omega_m_ra
 static uz_3ph_dq_t uz_SetPoint_field_weakening(uz_SetPoint_t* self, float omega_m_rad_per_sec, float V_dc_volts, float i_ref, float M_ref_Nm);
 static uz_3ph_dq_t uz_SetPoint_MTPA(uz_SetPoint_t* self, float i_ref_Ampere, float M_ref_Nm);
 static void uz_SetPoint_calculate_omega_cut_rad_per_sec(uz_SetPoint_t* self, float V_FE_max, uz_3ph_dq_t actual_currents_Ampere);
+static void uz_SetPoint_calculate_omega_cut_rad_per_sec_improved(uz_SetPoint_t* self, float V_FE_max, uz_3ph_dq_t actual_currents_Ampere);
 static void uz_SetPoint_assert_motor_parameters(uz_PMSM_t input, enum uz_Setpoint_motor_type motor_type);
 static float uz_SetPoint_newton_MTPA_raphson_iq_approximation(uz_SetPoint_t* self, float i_ref_Ampere, float M_ref_Nm);
 static void uz_SetPoint_newton_raphson_MTPA_check(uz_SetPoint_t* self, float iq_ref_Ampere, float Ld_Lq_squared, float M_ref_Nm); 
@@ -298,6 +299,21 @@ static void uz_SetPoint_calculate_omega_cut_rad_per_sec(uz_SetPoint_t* self, flo
     self->omega_cut_rad_per_sec = (-b_omega + sqrtf((b_omega * b_omega) - (4.0f * a_omega * c_omega) )) / (2.0f * a_omega);
 }
 
+static void uz_SetPoint_calculate_omega_cut_rad_per_sec_improved(uz_SetPoint_t* self, float V_FE_max, uz_3ph_dq_t actual_currents_Ampere) {
+    float Id_squared = actual_currents_Ampere.d * actual_currents_Ampere.d;
+    float Iq_squared = actual_currents_Ampere.q * actual_currents_Ampere.q;
+    float Rs_squared = self->config.config_PMSM.R_ph_Ohm * self->config.config_PMSM.R_ph_Ohm;
+    float Ld_squared = self->config.config_PMSM.Ld_Henry * self->config.config_PMSM.Ld_Henry;
+    float Lq_squared = self->config.config_PMSM.Lq_Henry * self->config.config_PMSM.Lq_Henry;
+    float psi_pm_squared = self->config.config_PMSM.Psi_PM_Vs * self->config.config_PMSM.Psi_PM_Vs;
+    float a_omega = (Lq_squared * Iq_squared) + psi_pm_squared + ((self->config.config_PMSM.Ld_Henry * actual_currents_Ampere.d) * 
+                    ((2.0f * self->config.config_PMSM.Psi_PM_Vs) + (self->config.config_PMSM.Ld_Henry * actual_currents_Ampere.d))) + (Ld_squared * Id_squared);
+    float b_omega = (2.0f * self->config.config_PMSM.R_ph_Ohm) * ((-actual_currents_Ampere.d * self->config.config_PMSM.Lq_Henry * actual_currents_Ampere.q) + 
+                    (actual_currents_Ampere.q * self->config.config_PMSM.Psi_PM_Vs) + (actual_currents_Ampere.q * self->config.config_PMSM.Ld_Henry * actual_currents_Ampere.d));
+    float c_omega = (Rs_squared * (Id_squared * Iq_squared)) - (V_FE_max * V_FE_max);
+    self->omega_cut_rad_per_sec = (-b_omega + sqrtf((b_omega * b_omega) - (4.0f * a_omega * c_omega) )) / (2.0f * a_omega);
+}
+
 static void uz_SetPoint_newton_raphson_MTPA_check(uz_SetPoint_t* self, float iq_ref_Ampere, float Ld_Lq_squared, float M_ref_Nm) {
     float iq_ref_squared = iq_ref_Ampere * iq_ref_Ampere;
     float psi_pm_squared = self->config.config_PMSM.Psi_PM_Vs * self->config.config_PMSM.Psi_PM_Vs;
@@ -319,4 +335,6 @@ static void uz_SetPoint_newton_raphson_field_weakening_check(uz_SetPoint_t* self
         uz_assert(0);
     }
 }
+
+
 #endif

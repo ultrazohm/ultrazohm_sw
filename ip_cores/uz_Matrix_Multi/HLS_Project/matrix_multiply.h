88b692782b489b1948a3f78aaccef2427bbf522c
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-11-24 13:14:01 +0100

    Changes array to axi ports

--- ip_cores/uz_Matrix_Multi/HLS_Project/matrix_multiply.h
+++ ip_cores/uz_Matrix_Multi/HLS_Project/matrix_multiply.h
@@ -1,4 +1,3 @@
 #include <stdint.h>
 #include <stdio.h>
-void MatrixMultiplication(float const A[5], float const B[25], float C_out[5], uint_fast32_t A_rows, uint_fast32_t B_rows, uint_fast32_t B_columns);
-//void MatrixMultiplication_Pointer(float const *A_pointer, float const *B_pointer, float *C_out_pointer, uint_fast32_t A_rows, uint_fast32_t B_rows, uint_fast32_t B_columns, bool trigger, bool* is_done);
+void MatrixMultiplication(float *A_input, float *B_input, float *C_output, uint_fast32_t A_rows, uint_fast32_t B_rows, uint_fast32_t B_columns);
--- ip_cores/uz_Matrix_Multi/HLS_Project/tb_matrix_multiply.cpp
+++ ip_cores/uz_Matrix_Multi/HLS_Project/tb_matrix_multiply.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void MatrixMultiplication(float const A[5], float const B[25], float C_out[5], uint_fast32_t A_rows, uint_fast32_t B_rows, uint_fast32_t B_columns);
+void MatrixMultiplication(float *A_input, float *B_input, float *C_output, uint_fast32_t A_rows, uint_fast32_t B_rows, uint_fast32_t B_columns);
 
 #define A_rows 1
 #define B_rows 4
@@ -12,7 +12,7 @@ int main() {
    float B[B_rows * B_columns] = {5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
    float C_out[A_rows * B_columns] = {0};
    float expected[A_rows * B_columns]={130,140,150,160};
-   MatrixMultiplication(A, B, C_out, A_rows, B_rows, B_columns);
+   MatrixMultiplication(&A[0], &B[0], &C_out[0], A_rows, B_rows, B_columns);
    bool result=false;
 
    for (uint_fast32_t i=0;i<4; i++) {

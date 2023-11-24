a3346d3e5a456550287a213850d263ccaf384b64
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-11-24 09:03:26 +0100

    increase max instances in default config

--- vitis/software/Baremetal/src/uz/default_uz_global_configuration.h
+++ vitis/software/Baremetal/src/uz/default_uz_global_configuration.h
@@ -70,7 +70,7 @@
 #define UZ_DQ_SETPOINT_FILTER_MAX_INSTANCES             0U
 #define UZ_SUBSYSTEM_RESONANT_CONTROL_MAX_INSTANCES     0U
 #define UZ_TRAJECTORY_MAX_INSTANCES                     0U
-#define UZ_MATRIX_MULTIPLICATION_MAX_INSTANCES			0U
+#define UZ_MATRIX_MULTIPLICATION_MAX_INSTANCES			1U
 #endif
 
 // Configuration defines for the number of used instances for testing with ceedling

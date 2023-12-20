f47687c43e107f6d284295e071f1261d07823729
hoernermi <michael.hoerner@th-nuernberg.de>
2023-12-20 16:09:35 +0100

    new bitstream with fixed ip-core name

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Tue Dec  5 13:11:20 2023
+--Date        : Wed Dec 20 14:39:39 2023
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

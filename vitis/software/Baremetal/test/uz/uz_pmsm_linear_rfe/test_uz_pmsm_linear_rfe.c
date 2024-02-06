b06a1dc73c2a020222ab1b63b5d2e5ea47c75433
diringerni90344 <diringerni90344@th-nuernberg.de>
2024-02-06 11:04:51 +0000

    adds some tests for model with rfe

--- vitis/software/Baremetal/test/uz/uz_pmsm_linear_rfe/test_uz_pmsm_linear_rfe.c
+++ vitis/software/Baremetal/test/uz/uz_pmsm_linear_rfe/test_uz_pmsm_linear_rfe.c
@@ -15,16 +15,16 @@ void tearDown(void)
 {
 }
 
-void test_uz_pmsm_linear_rfe_ones(void)
+void test_uz_pmsm_linear_rfe_ones_omega_null(void)
 {
     float R = 1.0f;
     float L_d = 1.0f;
     float L_q = 1.0f;
     float rfe_d= 1.0f;
     float rfe_q= 1.0f;
-     float psi_pm = 0.001f;
+    float psi_pm = 0.001f;
     float ts = 1.0f / 10000.0f;
-    float omega_el = 0;
+    float omega_el = 0.0f;
     uz_pmsm_linear_rfe_t *pmsm = uz_pmsm_linear_rfe_init(R, psi_pm, L_d, L_q, rfe_d, rfe_q, ts);
     uz_3ph_dq_t v = {
         .d = 1.0f,
@@ -37,6 +37,54 @@ void test_uz_pmsm_linear_rfe_ones(void)
     // at standstill, R=1 und v_dq= 1 & -1, the currents must be 1 & -1 in steady state
     TEST_ASSERT_FLOAT_WITHIN(1e-3, 1.0f, current.d);
     TEST_ASSERT_FLOAT_WITHIN(1e-3, -1.0f, current.q);
+}
+
+void test_uz_pmsm_linear_rfe_ones(void)
+{
+    float R = 1.0f;
+    float L_d = 1.0f;
+    float L_q = 1.0f;
+    float rfe_d= 1.0f;
+    float rfe_q= 1.0f;
+    float psi_pm = 1.0f;
+    float ts = 1.0f / 10000.0f;
+    float omega_el = 1.0f;
+    uz_pmsm_linear_rfe_t *pmsm = uz_pmsm_linear_rfe_init(R, psi_pm, L_d, L_q, rfe_d, rfe_q, ts);
+    uz_3ph_dq_t v = {
+        .d = -1.0f,
+        .q = 1.0f};
+    uz_3ph_dq_t current = {0};
+    for (size_t i = 0; i < 1000000; i++)
+    {
+        current = uz_pmsm_linear_rfe_step(pmsm, v, omega_el);
+    }
+    // at standstill, R=1 und v_dq= 1 & -1, the currents must be 1 & -1 in steady state
+    TEST_ASSERT_FLOAT_WITHIN(1e-3, -0.8f, current.d);
+    TEST_ASSERT_FLOAT_WITHIN(1e-3, 0.4f, current.q);
+}
+
+void test_uz_pmsm_linear_rfe_r_fe_inf(void)
+{
+    float R = 1.0f;
+    float L_d = 1.0f;
+    float L_q = 1.0f;
+    float rfe_d= 10000000.0f;
+    float rfe_q= 10000000.0f;
+    float psi_pm = 1.0f;
+    float ts = 1.0f / 10000.0f;
+    float omega_el = 1.0f;
+    uz_pmsm_linear_rfe_t *pmsm = uz_pmsm_linear_rfe_init(R, psi_pm, L_d, L_q, rfe_d, rfe_q, ts);
+    uz_3ph_dq_t v = {
+        .d = -1.0f,
+        .q = 1.0f};
+    uz_3ph_dq_t current = {0};
+    for (size_t i = 0; i < 1000000; i++)
+    {
+        current = uz_pmsm_linear_rfe_step(pmsm, v, omega_el);
+    }
+    // at standstill, R=1 und v_dq= 1 & -1, the currents must be 1 & -1 in steady state
+    TEST_ASSERT_FLOAT_WITHIN(1e-3, -0.5f, current.d);
+    TEST_ASSERT_FLOAT_WITHIN(1e-3, 0.5f, current.q);
 }
 
 #endif // TEST

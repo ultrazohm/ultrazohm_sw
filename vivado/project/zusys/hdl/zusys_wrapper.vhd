da9bcd5f50de1b3deb8c106b6487e5fc19024aeb
hoernermi <michael.hoerner@th-nuernberg.de>
2024-03-06 08:49:07 +0100

    routed gate signals to D3 and axi GPIOs to D4, exported bitstream

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Tue Mar  5 20:47:01 2024
+--Date        : Tue Mar  5 22:45:41 2024
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

477eb6f6c49aa3a379d412d2fb1a2ef2206f3bec
Nikita Votintsev <votintsevni75814@th-nuernberg.de>
2024-03-23 23:01:37 +0100

    updated model

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Sat Mar 23 17:34:09 2024
+--Date        : Sat Mar 23 21:23:53 2024
 --Host        : LAPTOP-QM3EKBPJ running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

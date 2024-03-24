a5916e83ddbbebb5e9d7bce0327913b58c5ba0e2
Nikita Votintsev <votintsevni75814@th-nuernberg.de>
2024-03-24 16:55:30 +0100

    bugfix

--- vitis/software/Baremetal/src/hw_init/uz_EnDat_IP_Core_init.c
+++ vitis/software/Baremetal/src/hw_init/uz_EnDat_IP_Core_init.c
@@ -44,9 +44,9 @@ uz_EnDat_t* uz_EnDat_IP_core_custom_init(void) {
 }
 
 
-*//////////////////////////////////////////*
-*/////////////FOR EXPERTS ONLY////////////*
-*/////////////////////////////////////////*
+/*////////////////////////////////////////*/
+/*////////////FOR EXPERTS ONLY///////////*/
+/*///////////////////////////////////////*/
 uz_EnDat_t* uz_EnDat_IP_core_expert_init(uint32_t baseaddress, uint32_t ipcorefreq, int16_t fct1, int16_t fct2, int16_t fct3, int16_t fct4, int16_t fct5, int16_t fct6, int16_t fct7, controlword ctrl, uint8_t dvder) {
     uz_EnDat_t* EnDat_temp_ptr = uz_EnDat_IP_core_init_helper(baseaddress, ipcorefreq);
 

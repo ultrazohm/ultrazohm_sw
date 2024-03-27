1bb98b51fcd9ddce362ed50500e1630a5e827c38
Tobias Schindler <tobias.schindler@th-nuernberg.de>
2024-03-27 13:27:44 +0100

    Hacks tristate into assertion callback

--- vitis/software/Baremetal/src/hw_init/uz_assertion_configuration.c
+++ vitis/software/Baremetal/src/hw_init/uz_assertion_configuration.c
@@ -3,7 +3,9 @@
 
 #include "../uz/uz_HAL.h"
 #include "../include/uz_platform_state_machine.h"
-
+#include "../IP_Cores/uz_PWM_SS_2L/uz_PWM_SS_2L.h"
+#include "../../globalData.h"
+#include <stdbool.h>
 
 
 static void uz_assertCallback(const char8 *file, s32 line);
@@ -15,6 +17,8 @@ void uz_assert_configuration(void){
 
 static void uz_assertCallback(const char8 *file, s32 line)
 {
+    uz_PWM_SS_2L_set_tristate(Global_Data.objects.pwm_d1_pin_0_to_5, true, true, true);
+    uz_PWM_SS_2L_set_tristate(Global_Data.objects.pwm_d1_pin_6_to_11, true, true, true);
     uz_printf("\r\n RPU: Assertion in file %s on line %d\r\n", file, line);
     ultrazohm_state_machine_set_error(true);
 }

96b8f16b67d496a37e9d38760fbc810ea3bc6947
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2024-04-04 14:19:47 +0200

    Includes changes regarding i_q_ref usage for SVLimitation

--- vitis/software/Baremetal/src/uz/uz_CurrentControl/uz_space_vector_limitation.h
+++ vitis/software/Baremetal/src/uz/uz_CurrentControl/uz_space_vector_limitation.h
@@ -12,11 +12,11 @@
  * @param V_dc_volts DC-link voltage
  * @param max_modulation_index Max possible modulation index for the chosen modulation method. I.e. 1/sqrt(3) for Space-Vector-Modulation. Must be greater than 0.0f
  * @param omega_el_rad_per_sec electrical rotational speed in rad/s
- * @param i_actual_Ampere uz_3ph_dq_t struct for measured dq-currents in Ampere
+ * @param i_ref_Ampere uz_3ph_dq_t struct for reference dq-currents in Ampere
  * @param ext_clamping flag which states, if the SVL is active
  * @return uz_3ph_dq_t
  */
-uz_3ph_dq_t uz_CurrentControl_SpaceVector_Limitation(uz_3ph_dq_t v_input_Volts, float V_dc_volts, float max_modulation_index, float omega_el_rad_per_sec, uz_3ph_dq_t i_actual_Ampere, bool* ext_clamping);
+uz_3ph_dq_t uz_CurrentControl_SpaceVector_Limitation(uz_3ph_dq_t v_input_Volts, float V_dc_volts, float max_modulation_index, float omega_el_rad_per_sec, uz_3ph_dq_t i_ref_Ampere, bool* ext_clamping);
 
 /**
  * @brief function to limit the voltages of a 6ph PMSM. Only applicable if the XY subspace is transformed with -theta_el and the 6ph-PMSM is in the 2N-configuration (separated neutral points).
@@ -25,10 +25,10 @@ uz_3ph_dq_t uz_CurrentControl_SpaceVector_Limitation(uz_3ph_dq_t v_input_Volts, 
  * @param V_dc_volts DC-link voltage
  * @param max_modulation_index Max possible modulation index for the chosen modulation method. I.e. 1/sqrt(3) for Space-Vector-Modulation. Must be greater than 0.0f
  * @param omega_el_rad_per_sec electrical rotational speed in rad/s
- * @param i_actual_Ampere uz_6ph_dq_t struct for measured dq-currents in Ampere
+ * @param i_ref_Ampere uz_6ph_dq_t struct for reference dq-currents in Ampere
  * @param ext_clamping flag which states, if the SVL is active
  * @return uz_6ph_dq_t struct with limited voltages
  */
-uz_6ph_dq_t uz_6ph_Space_Vector_Limitation(uz_6ph_dq_t v_input_Volts, float V_dc_volts, float max_modulation_index, float omega_el_rad_per_sec, uz_6ph_dq_t i_actual_Ampere, bool* ext_clamping);
+uz_6ph_dq_t uz_6ph_Space_Vector_Limitation(uz_6ph_dq_t v_input_Volts, float V_dc_volts, float max_modulation_index, float omega_el_rad_per_sec, uz_6ph_dq_t i_ref_Ampere, bool* ext_clamping);
 
 #endif // UZ_SPACE_VECTOR_LIMITATION_H

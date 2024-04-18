da7e43cee8d7adf2f90888436a0ec4b6db7c128d
Tobias Schindler <tobias.schindler@th-nuernberg.de>
2024-04-18 06:09:18 +0000

    Some simple ideas for python gui

---
+++ javascope/dash_test.py
@@ -0,0 +1,24 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df=pd.read_parquet('Log_2024-04-17_17-17-02.parquet')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.columns.values.tolist(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=True)

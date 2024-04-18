ef58438d94f57f4c5bb6aa5c6ad64b83d9156048
Tobias <tobias.schindler@th-nuernberg.de>
2024-04-18 12:08:30 +0000

    Dataviewer using dash and semi-serious version with fft

--- javascope/dataviewer_fft2.py
+++ javascope/dataviewer_fft2.py
@@ -2,6 +2,8 @@ from dash import Dash, html, dcc, callback, Output, Input, callback_context
 import plotly.graph_objects as go
 import pandas as pd
 from plotly_resampler import FigureResampler
+import numpy as np
+from scipy.fft import fft
 
 app = Dash(__name__)
 fig = FigureResampler()
@@ -22,7 +24,16 @@ app.layout = html.Div([
             clearable=False,
             style={'width': '100%', 'margin': '20px auto'}
         ),
-        dcc.Graph(id='graph-content', style={'width': '100%', 'margin': '0 auto'})
+        dcc.Graph(id='graph-content', style={'width': '100%', 'margin': '0 auto'}),
+        html.Div([
+            dcc.Dropdown(
+                id='fft-dropdown-selection',
+                multi=True,
+                clearable=False,
+                style={'width': '100%', 'margin': '20px auto'}
+            ),
+            dcc.Graph(id='fft-content', style={'width': '100%', 'margin': '0 auto'})
+        ])
     ], style={'width': '90%', 'margin': 'auto'})
 ], style={'max-width': '10000px', 'margin': '0 auto', 'padding': '20px'})
 
@@ -49,13 +60,48 @@ def update_graph(value, filename):
     global df
     # df = pd.read_parquet(filename)
     fig.replace(go.Figure())  # Clear the existing figure
+    fft_fig = go.Figure()
+
     if value:
         for col in value:
             fig.add_trace(go.Scattergl(name=col, x=df['time'], y=df[col]))
 
+            # Calculate FFT
+            signal = np.array(df[col])
+            sampling_freq = 1 / (df['time'][1] - df['time'][0])  # Calculate sampling frequency
+            freqs = np.fft.fftfreq(len(signal), d=1/sampling_freq)
+            fft_vals = np.abs(fft(signal))
+
+            fft_fig.add_trace(go.Scattergl(name=f'{col} FFT', x=freqs[:len(freqs)//2], y=fft_vals[:len(freqs)//2]))
+
         fig.update_layout(template="simple_white", xaxis=dict(showgrid=True), yaxis=dict(showgrid=True))
+        fft_fig.update_layout(template="simple_white", xaxis=dict(showgrid=True), yaxis=dict(showgrid=True))
 
     return fig
+
+@app.callback(
+    Output('fft-content', 'figure'),
+    Input('fft-dropdown-selection', 'value'),
+    prevent_initial_call=True
+)
+def update_fft_graph(value):
+    global df
+    fft_fig = go.Figure()
+
+    if value:
+        for col in value:
+            # Calculate FFT
+            signal = np.array(df[col])
+            sampling_freq = 1 / (df['time'][1] - df['time'][0])  # Calculate sampling frequency
+            freqs = np.fft.fftfreq(len(signal), d=1/sampling_freq)
+            fft_vals = np.abs(fft(signal))
+
+            fft_fig.add_trace(go.Scattergl(name=f'{col} FFT', x=freqs[:len(freqs)//2], y=fft_vals[:len(freqs)//2]))
+
+        fft_fig.update_layout(template="simple_white", xaxis=dict(showgrid=True), yaxis=dict(showgrid=True))
+        fft_fig.update_layout(title='FFT')
+
+    return fft_fig
 
 fig.register_update_graph_callback(app=app, graph_id="graph-content")
 

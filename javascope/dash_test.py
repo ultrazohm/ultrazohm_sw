5569d754a7431710425db2b357fc9fb9bc55cf93
Tobias Schindler <tobias.schindler@th-nuernberg.de>
2024-04-18 07:05:05 +0000

    dropdown test but resample does not work

--- javascope/dash_test.py
+++ javascope/dash_test.py
@@ -1,14 +1,20 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go;
+import numpy as np
+from plotly_resampler import register_plotly_resampler
+from plotly_resampler import FigureResampler, FigureWidgetResampler
 
 df=pd.read_parquet('Log_2024-04-17_17-17-02.parquet')
 
 app = Dash(__name__)
+register_plotly_resampler(mode='auto')
+fig = FigureResampler(px.line(df, x='time', y='time'))
 
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.columns.values.tolist(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(df.columns.values.tolist(), 'time', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
 
@@ -17,8 +23,10 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+    # dff = df.iloc[:, value]
+    return px.line(df, x='time', y=value)
+    # return fig.add_trace({"x": df.time,"y":df.iloc[:, 1]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)

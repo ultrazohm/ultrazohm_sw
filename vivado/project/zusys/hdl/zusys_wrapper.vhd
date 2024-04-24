1a89d4f21a3c902f7e9b6a232c493ab3cf4b37c0
Scaglione96 <gioacchino.scaglione@unipa.it>
2024-04-24 14:12:42 +0200

    created bitstream and add to Vitis

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
---Date        : Wed Jul 12 17:21:32 2023
---Host        : hufnagelde-TP-T14s running 64-bit Ubuntu 22.04.2 LTS
+--Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+--Date        : Wed Apr 24 10:52:13 2024
+--Host        : Grifis running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

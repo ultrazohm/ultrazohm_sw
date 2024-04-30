985c38d2d31e6181ae3cddc6b8c4c5b9ed55d334
Tobias <tobias.schindler@th-nuernberg.de>
2024-04-30 11:51:15 +0200

    adds files for testcase

--- vitis/software/Baremetal/src/main.c
+++ vitis/software/Baremetal/src/main.c
@@ -15,30 +15,27 @@
 
 // Includes from own files
 #include "main.h"
+static void ReadAllADC();
+bool do_control=false;
 
-// Initialize the global variables
-DS_Data Global_Data = {
-    .rasv = {
-        .halfBridge1DutyCycle = 0.0f,
-        .halfBridge2DutyCycle = 0.0f,
-        .halfBridge3DutyCycle = 0.0f,
-        .halfBridge4DutyCycle = 0.0f,
-        .halfBridge5DutyCycle = 0.0f,
-        .halfBridge6DutyCycle = 0.0f,
-		.halfBridge7DutyCycle = 0.0f,
-		.halfBridge8DutyCycle = 0.0f,
-		.halfBridge9DutyCycle = 0.0f,
-		.halfBridge10DutyCycle = 0.0f,
-		.halfBridge11DutyCycle = 0.0f,
-		.halfBridge12DutyCycle = 0.0f
-    },
-    .av.pwm_frequency_hz = UZ_PWM_FREQUENCY,
-    .av.isr_samplerate_s = (1.0f / UZ_PWM_FREQUENCY) * (Interrupt_ISR_freq_factor),
-    .aa = {.A1 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f},
-    	   .A2 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f},
-		   .A3 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f}
-    }
-};
+    // Initialize the global variables
+    DS_Data Global_Data = {
+        .rasv = {
+            .halfBridge1DutyCycle = 0.0f,
+            .halfBridge2DutyCycle = 0.0f,
+            .halfBridge3DutyCycle = 0.0f,
+            .halfBridge4DutyCycle = 0.0f,
+            .halfBridge5DutyCycle = 0.0f,
+            .halfBridge6DutyCycle = 0.0f,
+            .halfBridge7DutyCycle = 0.0f,
+            .halfBridge8DutyCycle = 0.0f,
+            .halfBridge9DutyCycle = 0.0f,
+            .halfBridge10DutyCycle = 0.0f,
+            .halfBridge11DutyCycle = 0.0f,
+            .halfBridge12DutyCycle = 0.0f},
+        .av.pwm_frequency_hz = UZ_PWM_FREQUENCY,
+        .av.isr_samplerate_s = (1.0f / UZ_PWM_FREQUENCY) * (Interrupt_ISR_freq_factor),
+        .aa = {.A1 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f}, .A2 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f}, .A3 = {.cf.ADC_A1 = 10.0f, .cf.ADC_A2 = 10.0f, .cf.ADC_A3 = 10.0f, .cf.ADC_A4 = 10.0f, .cf.ADC_B5 = 10.0f, .cf.ADC_B6 = 10.0f, .cf.ADC_B7 = 10.0f, .cf.ADC_B8 = 10.0f}}};
 
 enum init_chain
 {
@@ -51,66 +48,104 @@ enum init_chain
     infinite_loop
 };
 enum init_chain initialization_chain = init_assertions;
+void control(void);
 
-int main(void)
-{
-    int status = UZ_SUCCESS;
-    while (1)
+
+    int main(void)
     {
-        switch (initialization_chain)
+        int status = UZ_SUCCESS;
+        while (1)
         {
-        case init_assertions:
-            uz_assert_configuration(); // This has to be the first line of code in main.c
-            initialization_chain = init_gpios;
-            break;
-        case init_gpios:
-            Initialize_AXI_GPIO();               // This has to be the second line of code in main.c since the assertion callback uses the AXI_GPIO to disable the system
-            uz_frontplane_button_and_led_init(); // This has to be the third line of code since the assertion callback uses the LEDs to indicate an error
-            initialization_chain = init_software;
-            break;
-        case init_software:
-            uz_SystemTime_init();
-            JavaScope_initialize(&Global_Data);
-            initialization_chain = init_ip_cores;
-            break;
-        case init_ip_cores:
-            uz_adcLtc2311_ip_core_init();
-            Global_Data.objects.deadtime_interlock_d1_pin_0_to_5 = uz_interlockDeadtime2L_staticAllocator_slotD1_pin_0_to_5();
-            Global_Data.objects.deadtime_interlock_d1_pin_6_to_11 = uz_interlockDeadtime2L_staticAllocator_slotD1_pin_6_to_11();
-            Global_Data.objects.deadtime_interlock_d1_pin_12_to_17 = uz_interlockDeadtime2L_staticAllocator_slotD1_pin_12_to_17();
-            Global_Data.objects.deadtime_interlock_d1_pin_18_to_23 = uz_interlockDeadtime2L_staticAllocator_slotD1_pin_18_to_23();
-            uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1_pin_0_to_5, true);
-            uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1_pin_6_to_11, true);
-            uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1_pin_12_to_17, true);
-            uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1_pin_18_to_23, true);
-            Global_Data.objects.pwm_d1_pin_0_to_5 = initialize_pwm_2l_on_D1_pin_0_to_5();
-            Global_Data.objects.pwm_d1_pin_6_to_11 = initialize_pwm_2l_on_D1_pin_6_to_11();
-            Global_Data.objects.pwm_d1_pin_12_to_17 = initialize_pwm_2l_on_D1_pin_12_to_17();
-            Global_Data.objects.pwm_d1_pin_18_to_23 = initialize_pwm_2l_on_D1_pin_18_to_23();
-            Global_Data.objects.mux_axi = initialize_uz_mux_axi();
-            PWM_3L_Initialize(&Global_Data); // three-level modulator
-            Global_Data.objects.encoder_D5 = initialize_incremental_encoder_ipcore_on_D5(UZ_D5_INCREMENTAL_ENCODER_RESOLUTION, UZ_D5_MOTOR_POLE_PAIR_NUMBER);
-            initialization_chain = print_msg;
-            break;
-	    case print_msg:
-            uz_printf("\r\n\r\n");
-            uz_printf("Welcome to the UltraZohm\r\n");
-            uz_printf("----------------------------------------\r\n");
-            uz_printf("RPU Build Date: %s at %s,\r\n",__DATE__, __TIME__);
+            switch (initialization_chain)
+            {
+            case init_assertions:
+                uz_assert_configuration(); // This has to be the first line of code in main.c
+                initialization_chain = init_gpios;
+                break;
+            case init_gpios:
+                Initialize_AXI_GPIO();               // This has to be the second line of code in main.c since the assertion callback uses the AXI_GPIO to disable the system
+                uz_frontplane_button_and_led_init(); // This has to be the third line of code since the assertion callback uses the LEDs to indicate an error
+                initialization_chain = init_software;
+                break;
+            case init_software:
+                uz_SystemTime_init();
+                JavaScope_initialize(&Global_Data);
+                initialization_chain = init_ip_cores;
+                break;
+            case init_ip_cores:
+                uz_adcLtc2311_ip_core_init();
+                Global_Data.objects.deadtime_interlock_d1_pin_0_to_5 = uz_interlockDeadtime2L_staticAllocator_slotD1_pin_0_to_5();
+                Global_Data.objects.deadtime_interlock_d1_pin_6_to_11 = uz_interlockDeadtime2L_staticAllocator_slotD1_pin_6_to_11();
+                Global_Data.objects.deadtime_interlock_d1_pin_12_to_17 = uz_interlockDeadtime2L_staticAllocator_slotD1_pin_12_to_17();
+                Global_Data.objects.deadtime_interlock_d1_pin_18_to_23 = uz_interlockDeadtime2L_staticAllocator_slotD1_pin_18_to_23();
+                uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1_pin_0_to_5, true);
+                uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1_pin_6_to_11, true);
+                uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1_pin_12_to_17, true);
+                uz_interlockDeadtime2L_set_enable_output(Global_Data.objects.deadtime_interlock_d1_pin_18_to_23, true);
+                Global_Data.objects.pwm_d1_pin_0_to_5 = initialize_pwm_2l_on_D1_pin_0_to_5();
+                Global_Data.objects.pwm_d1_pin_6_to_11 = initialize_pwm_2l_on_D1_pin_6_to_11();
+                Global_Data.objects.pwm_d1_pin_12_to_17 = initialize_pwm_2l_on_D1_pin_12_to_17();
+                Global_Data.objects.pwm_d1_pin_18_to_23 = initialize_pwm_2l_on_D1_pin_18_to_23();
+                Global_Data.objects.mux_axi = initialize_uz_mux_axi();
+                PWM_3L_Initialize(&Global_Data); // three-level modulator
+                Global_Data.objects.encoder_D5 = initialize_incremental_encoder_ipcore_on_D5(UZ_D5_INCREMENTAL_ENCODER_RESOLUTION, UZ_D5_MOTOR_POLE_PAIR_NUMBER);
+                initialization_chain = print_msg;
+                break;
+            case print_msg:
+                uz_printf("\r\n\r\n");
+                uz_printf("Welcome to the UltraZohm\r\n");
+                uz_printf("----------------------------------------\r\n");
+                uz_printf("RPU Build Date: %s at %s,\r\n", __DATE__, __TIME__);
 
-            initialization_chain = init_interrupts;
-            break;
-        case init_interrupts:
-            uz_axigpio_enable_datamover();
-            Initialize_ISR(); // Initialize the Interrupts and enable them - last line of code before infinite loop
-            initialization_chain = infinite_loop;
-            break;
-        case infinite_loop:
-            ultrazohm_state_machine_step();
-            break;
-        default:
-            break;
+                initialization_chain = init_interrupts;
+                break;
+            case init_interrupts:
+                uz_axigpio_enable_datamover();
+                Initialize_ISR(); // Initialize the Interrupts and enable them - last line of code before infinite loop
+                initialization_chain = infinite_loop;
+                break;
+            case infinite_loop:
+                ultrazohm_state_machine_step();
+                if (do_control)
+                {
+                    control();
+                    do_control = false;
+                }
+                break;
+            default:
+                break;
+            }
         }
+        return (status);
     }
-    return (status);
-}
+
+    void control(void)
+    {
+        uz_SystemTime_ISR_Tic(); // Reads out the global timer, has to be the first function in the isr
+        ReadAllADC();
+        update_speed_and_position_of_encoder_on_D5(&Global_Data);
+
+        platform_state_t current_state = ultrazohm_state_machine_get_state();
+        if (current_state == control_state)
+        {
+            // Start: Control algorithm - only if ultrazohm is in control state
+        }
+        uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_0_to_5, Global_Data.rasv.halfBridge1DutyCycle, Global_Data.rasv.halfBridge2DutyCycle, Global_Data.rasv.halfBridge3DutyCycle);
+        uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_6_to_11, Global_Data.rasv.halfBridge4DutyCycle, Global_Data.rasv.halfBridge5DutyCycle, Global_Data.rasv.halfBridge6DutyCycle);
+        uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_12_to_17, Global_Data.rasv.halfBridge7DutyCycle, Global_Data.rasv.halfBridge8DutyCycle, Global_Data.rasv.halfBridge9DutyCycle);
+        uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_18_to_23, Global_Data.rasv.halfBridge10DutyCycle, Global_Data.rasv.halfBridge11DutyCycle, Global_Data.rasv.halfBridge12DutyCycle);
+
+        // Set duty cycles for three-level modulator
+        PWM_3L_SetDutyCycle(Global_Data.rasv.halfBridge1DutyCycle,
+                            Global_Data.rasv.halfBridge2DutyCycle,
+                            Global_Data.rasv.halfBridge3DutyCycle);
+        JavaScope_update(&Global_Data);
+        // Read the timer value at the very end of the ISR to minimize measurement error
+        // This has to be the last function executed in the ISR!
+        uz_SystemTime_ISR_Toc();
+    }
+
+    static void ReadAllADC()
+    {
+        ADC_readCardALL(&Global_Data);
+    };
--- vitis/software/Baremetal/src/sw/isr.c
+++ vitis/software/Baremetal/src/sw/isr.c
@@ -44,32 +44,15 @@ extern DS_Data Global_Data;
 // - triggered from PL
 // - start of the control period
 //----------------------------------------------------
-static void ReadAllADC();
+extern bool do_control;
 
 void ISR_Control(void *data)
 {
-    uz_SystemTime_ISR_Tic(); // Reads out the global timer, has to be the first function in the isr
-    ReadAllADC();
-    update_speed_and_position_of_encoder_on_D5(&Global_Data);
-
-    platform_state_t current_state=ultrazohm_state_machine_get_state();
-    if (current_state==control_state)
-    {
-        // Start: Control algorithm - only if ultrazohm is in control state
-    }
-    uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_0_to_5, Global_Data.rasv.halfBridge1DutyCycle, Global_Data.rasv.halfBridge2DutyCycle, Global_Data.rasv.halfBridge3DutyCycle);
-    uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_6_to_11, Global_Data.rasv.halfBridge4DutyCycle, Global_Data.rasv.halfBridge5DutyCycle, Global_Data.rasv.halfBridge6DutyCycle);
-    uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_12_to_17, Global_Data.rasv.halfBridge7DutyCycle, Global_Data.rasv.halfBridge8DutyCycle, Global_Data.rasv.halfBridge9DutyCycle);
-    uz_PWM_SS_2L_set_duty_cycle(Global_Data.objects.pwm_d1_pin_18_to_23, Global_Data.rasv.halfBridge10DutyCycle, Global_Data.rasv.halfBridge11DutyCycle, Global_Data.rasv.halfBridge12DutyCycle);
-
-    // Set duty cycles for three-level modulator
-    PWM_3L_SetDutyCycle(Global_Data.rasv.halfBridge1DutyCycle,
-                        Global_Data.rasv.halfBridge2DutyCycle,
-                        Global_Data.rasv.halfBridge3DutyCycle);
-    JavaScope_update(&Global_Data);
-    // Read the timer value at the very end of the ISR to minimize measurement error
-    // This has to be the last function executed in the ISR!
-    uz_SystemTime_ISR_Toc();
+	if (do_control){
+		uz_assert(0);
+	}else{
+		do_control=true;
+	}
 }
 
 //==============================================================================================================================================================
@@ -191,8 +174,3 @@ u32 Rpu_IpiInit(u16 DeviceId)
     xil_printf("RPU: RPU_IpiInit: Done\r\n");
     return XST_SUCCESS;
 }
-
-static void ReadAllADC()
-{
-    ADC_readCardALL(&Global_Data);
-};

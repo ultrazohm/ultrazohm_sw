9097041428e3f6b7e3ca201b255537361d90ffa4
Philipp Doelger <doelgerph90359@th-nuernberg.de>
2024-05-16 13:52:25 +0200

    first sketch of fitting script and init file with proper naming

--- ip_cores/uz_pmsm_model/uz_pmsm_model_init_parameter_clean.m
+++ ip_cores/uz_pmsm_model/uz_pmsm_model_init_parameter_clean.m
@@ -8,8 +8,6 @@ L_q=0.002;
 psi_pm=0.0194;
 polepair=4;
 ts=1/5e5; 
-rpm1 = 100;
-rpm2 = 100;
 
 % Controller parameter
 ts_regler=1/25e3;
@@ -31,49 +29,26 @@ static_friction_torque=0.01;
 % Simulation settings
 simulate_mechanical=false;
 simulate_open_loop=false;
-simulate_nonlinear_modell=true;
+simulate_nonlinear_modell=true; %Only set this true if the fitting parameter script ran successfully
 
 parameter_nachfuerung=false;
 
-%Only one should be set true
-praediktion_entkopplung_nonlinear=true;
-entkopplung_linear=false;
-entkopplung_static=false;
-entkopplung_dynamic=false;
-
 setpoint_step=10.0;
 
-
-
-%% extract Lookup table data
-%For nonlinear Model to work LUTs have to be implemented
-% Import the data from Excel for lookup table
-FluxMapData = readtable('FluxMapData_Prototyp_1000rpm');
-
-% Currents
-id = FluxMapData{1,1:20};
-iq = FluxMapData{22:41,1};
-%Psi_d
-psi_d_forLUT = FluxMapData{43:62,1:20}'*(1e-3);
-psi_d = FluxMapData{43:62,1:20}*(1e-3);
-%Psi_q
-psi_q_forLUT = FluxMapData{108:127,1:20}'*(1e-3);
-psi_q = FluxMapData{108:127,1:20}*(1e-3);
-
-%Inductances
-[Ldq,Ldd]=gradient(psi_d,2.5263,1.6842);
-[Lqq,Lqd]=gradient(psi_q,2.5263,1.6842);
-
-[Ldd_real,Ldq_real]=gradient(psi_d_forLUT,1.6842,2.5263);
-[Lqd_real,Lqq_real]=gradient(psi_q_forLUT,1.6842,2.5263);
-
-figure;
-surf(id,iq,psi_d)
-figure;
-surf(id,iq,psi_q)
-
-
-
-
-
+% Fitting Parameter 
+% Initilize the Parameters one time so the model is not 
+ad1 = 0;
+ad2 = 0;
+ad3 = 0;
+ad4 = 0;
+ad5 = 0;
+ad6 = 0;
+aq1 = 0;
+aq2 = 0;
+aq3 = 0;
+aq4 = 0;
+aq5 =0;
+aq6 =0;
+Fid1_Giq1 = 0;
+Fid2_Giq2 = 0;
 

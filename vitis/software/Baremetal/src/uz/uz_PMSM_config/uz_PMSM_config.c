e93d067a6ab7e452f611de3dd88ecdbac88cb4cd
Philipp Dölger <doelgerph90359@th-nuernberg.de>
2024-06-20 16:16:23 +0000

    added flux approximation sw module, added fitting parameter to pmsm_config aswell

--- vitis/software/Baremetal/src/uz/uz_PMSM_config/uz_PMSM_config.c
+++ vitis/software/Baremetal/src/uz/uz_PMSM_config/uz_PMSM_config.c
@@ -11,3 +11,20 @@ void uz_PMSM_config_assert(uz_PMSM_t config){
     uz_assert(config.J_kg_m_squared > 0.0f);
     uz_assert(config.I_max_Ampere > 0.0f);
 }
+
+void uz_PMSM_flux_fitting_parameter_config_assert(uz_PMSM_flux_fitting_parameter_config_t fitting_config){
+    uz_assert(fitting_config.ad1_parameter != 0.0f);
+    uz_assert(fitting_config.ad2_parameter != 0.0f);
+	uz_assert(fitting_config.ad3_parameter != 0.0f);
+	uz_assert(fitting_config.ad4_parameter != 0.0f);
+    uz_assert(fitting_config.ad5_parameter != 0.0f);
+    uz_assert(fitting_config.ad6_parameter != 0.0f);
+    uz_assert(fitting_config.aq1_parameter != 0.0f);
+    uz_assert(fitting_config.aq2_parameter != 0.0f);
+    uz_assert(fitting_config.aq3_parameter != 0.0f);
+    uz_assert(fitting_config.aq4_parameter != 0.0f);
+    uz_assert(fitting_config.aq5_parameter != 0.0f);
+    uz_assert(fitting_config.aq6_parameter != 0.0f);
+    uz_assert(fitting_config.F1G1_parameter != 0.0f);
+    uz_assert(fitting_config.F2G2_parameter != 0.0f);
+}
--- vitis/software/Baremetal/src/uz/uz_global_configuration.h
+++ vitis/software/Baremetal/src/uz/uz_global_configuration.h
@@ -70,6 +70,7 @@
 #define UZ_DQ_SETPOINT_FILTER_MAX_INSTANCES             0U
 #define UZ_SUBSYSTEM_RESONANT_CONTROL_MAX_INSTANCES     0U
 #define UZ_TRAJECTORY_MAX_INSTANCES                     0U
+#define UZ_APPROXIMATE_FLUX_MAX_INSTANCES               0U
 #endif
 
 // Configuration defines for the number of used instances for testing with ceedling
@@ -118,4 +119,5 @@
     #define UZ_DQ_SETPOINT_FILTER_MAX_INSTANCES             10U
     #define UZ_SUBSYSTEM_RESONANT_CONTROL_MAX_INSTANCES     10U
     #define UZ_TRAJECTORY_MAX_INSTANCES                     50U
+    #define UZ_APPROXIMATE_FLUX_MAX_INSTANCES               5U
 #endif

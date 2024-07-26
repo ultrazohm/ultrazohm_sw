863f7bfd1dda912ca86f55e90abe56d7cbde1368
Nikita Votintsev <votintsevni75814@th-nuernberg.de>
2024-07-26 09:15:51 +0200

    removed aux clk

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Sat Jul 20 20:05:15 2024
+--Date        : Fri Jul 26 08:27:13 2024
 --Host        : LAPTOP-QM3EKBPJ running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

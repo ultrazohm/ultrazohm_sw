823be844a0e9c0fb1bb958c70936190db949be6f
Nikita Votintsev <votintsevni75814@th-nuernberg.de>
2024-07-28 17:43:04 +0200

    updated

--- ip_cores/uz_EnDat_IPCore/hdl_prj/ipcore/uz_EnDat_v10_4/include/uz_EnDat_addr.h
+++ ip_cores/uz_EnDat_IPCore/hdl_prj/ipcore/uz_EnDat_v10_4/include/uz_EnDat_addr.h
@@ -1,7 +1,7 @@
 /*
  * File Name:         C:\Users\ELSYS_PC\Documents\repos\ultrazohm_sw\ip_cores\uz_EnDat_IPCore\hdl_prj\ipcore\uz_EnDat_v10_4\include\uz_EnDat_addr.h
  * Description:       C Header File
- * Created:           2024-07-26 08:20:03
+ * Created:           2024-07-27 16:38:08
 */
 
 #ifndef UZ_ENDAT_H_
@@ -9,7 +9,7 @@
 
 #define  IPCore_Reset_uz_EnDat              0x0  //write 0x1 to bit 0 to reset IP core
 #define  IPCore_Enable_uz_EnDat             0x4  //enabled (by default) when bit 0 is 0x1
-#define  IPCore_Timestamp_uz_EnDat          0x8  //contains unique IP timestamp (yymmddHHMM): 2407260819
+#define  IPCore_Timestamp_uz_EnDat          0x8  //contains unique IP timestamp (yymmddHHMM): 2407271638
 #define  DIVIDER_Data_uz_EnDat              0x800  //data register for Inport DIVIDER
 #define  FKT1DATAFLOW_Data_uz_EnDat         0x804  //data register for Inport FKT1DATAFLOW
 #define  CONTROLWORD_Data_uz_EnDat          0x808  //data register for Inport CONTROLWORD
--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Fri Jul 26 08:27:13 2024
+--Date        : Sun Jul 28 16:48:09 2024
 --Host        : LAPTOP-QM3EKBPJ running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

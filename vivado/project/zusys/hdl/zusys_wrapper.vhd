23075c9eff382cfc09fe705673fcaee6f88d2828
Florian Rosner <florian.rosner@zohm-control.com>
2024-08-07 21:03:24 +0200

    corrected slicer_5. Was slicing like slicer_0 (0), corrected it to (5).

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Tue Aug  6 19:09:10 2024
---Host        : LAPTOP-1OLGUFKM running 64-bit major release  (build 9200)
+--Date        : Wed Aug  7 18:53:54 2024
+--Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

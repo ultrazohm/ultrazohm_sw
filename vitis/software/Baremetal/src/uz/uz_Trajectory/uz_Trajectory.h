7c176a018fcd2842da0544f76f7dd190e08ee159
R_Zipprich <r.zipprich@uni-kassel.de>
2024-08-19 11:05:05 +0200

    couple header-defines for the tests use random generated trajectory for the tests (hopefully this doesn't blow up the CI) added is_running_latch to ensure start-funktion is once called and imprive output behavior

--- vitis/software/Baremetal/src/uz/uz_Trajectory/uz_Trajectory.h
+++ vitis/software/Baremetal/src/uz/uz_Trajectory/uz_Trajectory.h
@@ -7,7 +7,7 @@
  * @brief Maximum number of samples that can be stored. Should be as small as possible to save resources.
  * 
  */
-#define MAX_TRAJECTORY_SAMPLES 	20U
+#define MAX_TRAJECTORY_SAMPLES 	50U
 
 /*! enum for readable configuration of the interpolation type */
 enum uz_Trajectory_interpolation_selection {

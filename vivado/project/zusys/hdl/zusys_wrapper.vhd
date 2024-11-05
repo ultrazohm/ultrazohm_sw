d5361045f918787b5c72d0a7a40f358b519e863c
Nina Diringer <nina.diringer@th-nuernberg.de>
2024-11-05 18:34:43 +0100

    Changes pins to match Encoder rev02

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Thu Jul 25 14:26:34 2024
---Host        : LAPTOP-1OLGUFKM running 64-bit major release  (build 9200)
+--Date        : Tue Nov  5 18:27:40 2024
+--Host        : DESKTOP-URCAD6E running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

718729fd53a759dec312be5839dcd1b14be5eea6
Nina Diringer <nina.diringer@th-nuernberg.de>
2024-11-06 17:26:38 +0100

    fixes vivado project

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Tue Nov  5 18:27:40 2024
+--Date        : Wed Nov  6 13:58:43 2024
 --Host        : DESKTOP-URCAD6E running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

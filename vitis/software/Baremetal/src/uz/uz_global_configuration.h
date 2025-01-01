529d1a2d88d4614d3781dea88c3c2b43555d8d37
Sebastian Wendel <sebastian.wendel@th-nuernberg.de>
2025-01-01 13:11:17 +0100

    Fixed merge pipeline

--- vitis/software/Baremetal/src/uz/uz_global_configuration.h
+++ vitis/software/Baremetal/src/uz/uz_global_configuration.h
@@ -34,7 +34,6 @@
 #define UZ_WAVEGEN_CHIRP_MAX_INSTANCES                  2U
 #define UZ_MYIP2_MAX_INSTANCES                          0U
 #define UZ_ADCLTC2311_MAX_INSTANCES                     3U
-#define UZ_ADCMAX11331_MAX_INSTANCES                    3U
 #define UZ_PI_CONTROLLER_MAX_INSTANCES                  3U
 #define UZ_CURRENTCONTROL_MAX_INSTANCES                 2U
 #define UZ_INCREMENTALENCODER_MAX_INSTANCES             1U
@@ -80,7 +79,6 @@
     #define UZ_MYIP2_MAX_INSTANCES                          5U
     #define UZ_MYIP_MAX_INSTANCES                           5U
     #define UZ_ADCLTC2311_MAX_INSTANCES                     50U
-    #define UZ_ADCMAX11331_MAX_INSTANCES                    3U
     #define UZ_PI_CONTROLLER_MAX_INSTANCES                  100U
     #define UZ_CURRENTCONTROL_MAX_INSTANCES                 100U
     #define UZ_INCREMENTALENCODER_MAX_INSTANCES             20U

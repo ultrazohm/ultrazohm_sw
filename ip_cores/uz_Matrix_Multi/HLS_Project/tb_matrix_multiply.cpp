40dcfada9f066bbfbf00662922b6891347e262d9
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2025-01-17 15:20:13 +0100

    Revert "Changes size of ip-core in hls"
    This reverts commit 9251a62d379ef2e1b93c076bdd219b365b271cb8.

--- ip_cores/uz_Matrix_Multi/HLS_Project/tb_matrix_multiply.cpp
+++ ip_cores/uz_Matrix_Multi/HLS_Project/tb_matrix_multiply.cpp
@@ -4,18 +4,18 @@
 void MatrixMultiplication(float *A_input, float *B_input, float *C_output, uint_fast32_t A_rows, uint_fast32_t B_rows, uint_fast32_t B_columns);
 
 #define A_rows 1
-#define B_rows 2
-#define B_columns 16
+#define B_rows 4
+#define B_columns 4
 
 int main() {
-   float A[A_rows * B_rows] = {1,2};
-   float B[B_rows * B_columns] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32};
+   float A[A_rows * B_rows] = {1,2,3,4};
+   float B[B_rows * B_columns] = {5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
    float C_out[A_rows * B_columns] = {0};
-   float expected[A_rows * B_columns]={35  ,  38  ,  41  ,  44  ,  47  ,  50  ,  53  ,  56,    59   , 62  ,  65   , 68   , 71  ,  74 ,   77 ,   80};
+   float expected[A_rows * B_columns]={130,140,150,160};
    MatrixMultiplication(&A[0], &B[0], &C_out[0], A_rows, B_rows, B_columns);
    bool result=false;
 
-   for (uint_fast32_t i=0;i<B_columns; i++) {
+   for (uint_fast32_t i=0;i<4; i++) {
 	   if ((C_out[i] == expected[i])) {
 		   result = true;
 	   } else {

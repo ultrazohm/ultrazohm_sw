bd7e7be6d6d0d3474bf2d22ba351d34121956b0e
Eyke <eyke.aufderheide@tum.de>
2025-03-12 09:28:58 +0100

    upgrade ikmt 9phase example to vivado and vitis 2022.2 version

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
---Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+--Copyright 1986-2023 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
---Date        : Wed Aug 31 15:16:03 2022
---Host        : LAPTOP-ME665JGB running 64-bit major release  (build 9200)
+--Tool Version: Vivado v.2022.2.2 (win64) Build 3788238 Tue Feb 21 20:00:34 MST 2023
+--Date        : Thu Feb 20 12:37:24 2025
+--Host        : TUEIEAL-TM22 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

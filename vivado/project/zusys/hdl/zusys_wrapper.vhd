0a22f3360980f0c768fd8bc9ad3d113266dee77c
Florian Rosner <florian.rosner@zohm-control.com>
2025-04-01 09:57:41 +0200

    updated bitstream with ip core with improved timing for sampling serial data

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Mon Mar 31 10:05:29 2025
+--Date        : Mon Mar 31 19:24:13 2025
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

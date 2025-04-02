49d8be96587cb8ddda3d9bc60cb54095040c89b4
hoernermi <michael.hoerner@th-nuernberg.de>
2025-04-02 17:32:01 +0200

    ip core with configurable delays for delay_first_clk, clk, and data

---
+++ ip_cores/uz_ssi_interface/simulation/hdl_prj/hdlsrc/uz_ssi_interface_2023a/clearHighlightingRemovedDeadBlocks.m
@@ -0,0 +1,1 @@
+SLStudio.Utils.RemoveHighlighting(get_param('uz_ssi_interface_2023a','Handle'));
---
+++ ip_cores/uz_ssi_interface/simulation/hdl_prj/hdlsrc/uz_ssi_interface_2023a/highlightRemovedDeadBlocks.m
@@ -0,0 +1,5 @@
+cs.HiliteType = 'user1';
+cs.ForegroundColor = 'black';
+cs.BackgroundColor = 'red';
+set_param(0, 'HiliteAncestorsData', cs);
+hilite_system('uz_ssi_interface_2023a/uz_ssi_interface/Goto4','user1');

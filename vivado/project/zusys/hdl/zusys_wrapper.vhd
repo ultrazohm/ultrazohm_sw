e2afb8157aa54c6c7091cca6622b34f7a5456cc8
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2025-04-09 09:39:12 +0200

    adds ip-core to bitstream

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Wed Mar 19 10:14:06 2025
+--Date        : Tue Apr  8 16:23:31 2025
 --Host        : LAPTOP-68C4B02L running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

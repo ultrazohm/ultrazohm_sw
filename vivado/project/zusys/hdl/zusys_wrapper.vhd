7a1d3352d61a3533a33ad2dcde0640b7d4751c10
Florian Rosner <florian.rosner@zohm-control.com>
2025-05-26 15:11:57 +0200

    exported bitstream for testing 64bit max serial data buffer

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Mon May 26 09:45:51 2025
+--Date        : Mon May 26 14:25:29 2025
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

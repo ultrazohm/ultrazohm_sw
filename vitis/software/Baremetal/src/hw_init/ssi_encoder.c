e4d048a7682d6a2bee73e95e463dc823b64fa244
hoernermi <michael.hoerner@th-nuernberg.de>
2025-06-02 10:54:31 +0200

    ip v2.5 with variable delay for sample and hold of data line

--- vitis/software/Baremetal/src/hw_init/ssi_encoder.c
+++ vitis/software/Baremetal/src/hw_init/ssi_encoder.c
@@ -18,9 +18,9 @@ struct uz_ssi_interface_config_t ssi_0_config = {
 struct uz_ssi_interface_config_t ssi_1_config = {
 	.base_address = XPAR_UZ_USER_UZ_SSI_INTERFACE_1_BASEADDR,
 	.ip_clk_frequency_Hz = 100000000U,
-	.position_encoding = gray_code,
-	.machine_polepairs = 2U,
-	.ssi_clk_frequency_Hz = 2000000U,
+	.position_encoding = binary,
+	.machine_polepairs = 1U,
+	.ssi_clk_frequency_Hz = 300000U,
 	.position_mech_offset_si_single_turn = 0.0f,
 	.ssi_encoder_bit_width_single_turn = 12U,
 	.ssi_encoder_bit_width_multi_turn = 12U,
@@ -42,4 +42,3 @@ uz_ssi_interface_t* ssi_encoder_init_ssi1() {
 
 	return(uz_ssi_interface_init(ssi_1_config, ssi_1_outputs));
 }
-

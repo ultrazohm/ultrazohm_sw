a2d5e1d8fcc7a2a5cd01c8580a2fda2b62721fd5
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2025-10-16 10:59:12 +0200

    Adds IP-Core with dutycycle calculation removed ->done on PS now

---
+++ ip_cores/uz_pwmdutyfreqdetection/hdl_prj/hdlsrc/uz_pwmdutyfreqdetection/clearhighlighting.m
@@ -0,0 +1,8 @@
+SLStudio.Utils.RemoveHighlighting(get_param('uz_pwmdutyfreqdetection', 'handle'));
+SLStudio.Utils.RemoveHighlighting(get_param('gm_uz_pwmdutyfreqdetection', 'handle'));
+annotate_port('gm_uz_pwmdutyfreqdetection/uz_pwmdutyfreqdetection/Detect Rise Positive', 0, 1, '');
+annotate_port('gm_uz_pwmdutyfreqdetection/uz_pwmdutyfreqdetection/Delay7', 0, 1, '');
+annotate_port('gm_uz_pwmdutyfreqdetection/uz_pwmdutyfreqdetection/HDL Counter', 1, 1, '');
+annotate_port('gm_uz_pwmdutyfreqdetection/uz_pwmdutyfreqdetection/Logical Operator', 0, 1, '');
+annotate_port('gm_uz_pwmdutyfreqdetection/uz_pwmdutyfreqdetection/Detect Rise Positive/FixPt Relational Operator', 0, 1, '');
+annotate_port('gm_uz_pwmdutyfreqdetection/uz_pwmdutyfreqdetection/Detect Rise Positive/Positive', 0, 1, '');

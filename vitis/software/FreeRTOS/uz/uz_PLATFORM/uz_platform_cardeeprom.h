691c11fc2fe0fd0511f99cc4af50e3817c9c9e10
Martin Geier <martin.geier@zohm-control.com>
2025-11-25 12:56:15 +0100

    fix whitespace

--- vitis/software/FreeRTOS/uz/uz_PLATFORM/uz_platform_cardeeprom.h
+++ vitis/software/FreeRTOS/uz/uz_PLATFORM/uz_platform_cardeeprom.h
@@ -41,21 +41,21 @@
 #endif
 #define UZ_NUMENUM(ENUMNAME, ENUMITEM)		/* NB: Update /docs/source/guide/apu_software/uzp_a53.rst, too */ \
 	ENUMNAME(uz_platform_eeprom_group000model004variants)														/* Variants in Group 0 (Adapter cards), Model 4 (Digital Optical) */ \
-	ENUMITEM( 0,	UZP_HWGROUP_ADCARD_DIGOPT_18TX,				"18 TX")			/*  */ \
-	ENUMITEM( 1,	UZP_HWGROUP_ADCARD_DIGOPT_18RX,				"18 RX")			/*  */ \
-	ENUMITEM( 2,	UZP_HWGROUP_ADCARD_DIGOPT_14TX4RX,			"14 TX / 4 RX")		/*  */ \
-	ENUMITEM( 3,	UZP_HWGROUP_ADCARD_DIGOPT_18TX12TX,			"18 TX + 12 TX")	/*  */ \
-	ENUMITEM( 4,	UZP_HWGROUP_ADCARD_DIGOPT_18RX12RX,			"18 RX + 12 RX")	/* ... and *no* \ after the end of the last line! */
+	ENUMITEM( 0,	UZP_HWGROUP_ADCARD_DIGOPT_18TX,								"18 TX")						/*  */ \
+	ENUMITEM( 1,	UZP_HWGROUP_ADCARD_DIGOPT_18RX,								"18 RX")						/*  */ \
+	ENUMITEM( 2,	UZP_HWGROUP_ADCARD_DIGOPT_14TX4RX,							"14 TX / 4 RX")					/*  */ \
+	ENUMITEM( 3,	UZP_HWGROUP_ADCARD_DIGOPT_18TX12TX,							"18 TX + 12 TX")				/*  */ \
+	ENUMITEM( 4,	UZP_HWGROUP_ADCARD_DIGOPT_18RX12RX,							"18 RX + 12 RX")				/* ... and *no* \ after the end of the last line! */
 #include "../uz_enum/uz_numberedenum.h"
+
 // Variants for Digital Voltage 3V3/5V
 #ifndef UZ_PLATFORM_C
  #define UZ_NUMENUM_NOHELPER
 #endif
 #define UZ_NUMENUM(ENUMNAME, ENUMITEM)		/* NB: Update /docs/source/guide/apu_software/uzp_a53.rst, too */ \
 	ENUMNAME(uz_platform_eeprom_group000model014variants) 														/* Variants in Group 0 (Adapter cards), Model 14 (Digital Voltage) */ \
-	ENUMITEM(0, UZP_HWGROUP_ADCARD_DIGVOLT335_10KPD,  "10k Pulldown IO") 	/*  */ \
-	ENUMITEM(1, UZP_HWGROUP_ADCARD_DIGVOLT335_100KPD, "100k Pulldown IO")	/* ... and *no* \ after the end of the last line! */
-
+	ENUMITEM( 0,	UZP_HWGROUP_ADCARD_DIGVOLT335_10KPD,						"10k Pulldown IO")						/*  */ \
+	ENUMITEM( 1,	UZP_HWGROUP_ADCARD_DIGVOLT335_100KPD,						"100k Pulldown IO")				/* ... and *no* \ after the end of the last line! */
 #include "../uz_enum/uz_numberedenum.h"
 
 // Bit masks for I²C GPIO of Digital Voltage 3V3/5V

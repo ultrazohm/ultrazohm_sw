d9e9c1c4d545ca3d8e8210d7d6290538cddc726c
hoernermi <michael.hoerner@th-nuernberg.de>
2025-11-27 13:22:56 +0100

    generated v0.1 of ip core

---
+++ ip_cores/uz_endat_interface/hdl_prj/hdlsrc/uz_endat_interface_2023a/highlightRemovedDeadBlocks.m
@@ -0,0 +1,9 @@
+cs.HiliteType = 'user1';
+cs.ForegroundColor = 'black';
+cs.BackgroundColor = 'red';
+set_param(0, 'HiliteAncestorsData', cs);
+hilite_system('uz_endat_interface_2023a/uz_endat_interface/serial_communication/sample_serial_ssi_data/Mux','user1');
+hilite_system('uz_endat_interface_2023a/uz_endat_interface/serial_communication/endat_clock_enable_controller/Relational Operator3','user1');
+hilite_system('uz_endat_interface_2023a/uz_endat_interface/serial_communication/endat_clock_enable_controller/Constant2','user1');
+hilite_system('uz_endat_interface_2023a/uz_endat_interface/serial_communication/Goto6','user1');
+hilite_system('uz_endat_interface_2023a/uz_endat_interface/serial_communication/From12','user1');

7f36cb46ee3899e3cf266c392fa8656271e36280
Florian Rosner <florian.rosner@zohm-control.com>
2025-11-28 14:51:24 +0100

    exported bitstream for testing ip v0.3

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Fri Nov 28 10:07:40 2025
+--Date        : Fri Nov 28 14:10:27 2025
 --Host        : enc177112 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

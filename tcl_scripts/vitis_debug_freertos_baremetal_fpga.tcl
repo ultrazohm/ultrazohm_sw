8e4645ffad6abd1f61d6174794d9fca674fe490f
Eyke <eyke.aufderheide@tum.de>
2025-12-03 14:01:48 +0100

    add UZ programming method with reseting bitstream

--- tcl_scripts/vitis_debug_freertos_baremetal_fpga.tcl
+++ tcl_scripts/vitis_debug_freertos_baremetal_fpga.tcl
@@ -4,5 +4,7 @@
 
 set UZ_PROGRAMMING_MODE "DEBUG"  
 
+set UZ_FPGA_FAST_RESTART_ACTIVATED 1 ; 
+
 cd [getws]
 source {../../tcl_scripts/vitis_debug_run_UltraZohm.tcl}
--- tcl_scripts/vitis_debug_run_UltraZohm.tcl
+++ tcl_scripts/vitis_debug_run_UltraZohm.tcl
@@ -26,6 +26,7 @@ proc _find_one {dir pattern} {
 proc bitstream {dir} { _find_one $dir "*.bit" }
 proc xsa       {dir} { _find_one $dir "*.xsa" }
 
+
 puts "\n\n====================================================="
 puts "INFO: UZ_PROGRAMMING_MODE = $UZ_PROGRAMMING_MODE"
 puts "=====================================================\n\n"
@@ -34,55 +35,80 @@ cd [getws]
 connect -url tcp:127.0.0.1:3121
 puts "INFO: Connected to target on host '127.0.0.1' and port '3121'."
 #####################################################
-source $PATH_zynqmp_utils 
-puts "INFO: sourcing of '$PATH_zynqmp_utils ' is done."
+# default if not set elsewhere
+if {![info exists UZ_FPGA_FAST_RESTART_ACTIVATED]} {
+    set UZ_FPGA_FAST_RESTART_ACTIVATED 0
+}
 #####################################################
-targets -set -nocase -filter {name =~"APU*"}
-puts "INFO: Context for 'APU' is selected."
+set UZ_FPGA_STATUS [fpga -state]
+if { [string match "*FPGA is configured*" $UZ_FPGA_STATUS] && $UZ_FPGA_FAST_RESTART_ACTIVATED == 1 } {
+    puts "INFO: $UZ_FPGA_STATUS and fast restart activated, skipping FPGA programming."
+    set UZ_FPGA_FAST_RESTART 1   ;# skip rst/bitstream/psu_init 
+} else {
+    puts "INFO: $UZ_FPGA_STATUS, run full reset of UZ."
+    set UZ_FPGA_FAST_RESTART 0   ;# full PS/PL init + FPGA reprogram
+}
 #####################################################
-rst -system
-puts "INFO: System reset is completed."
+if {$UZ_FPGA_FAST_RESTART == 0} {
+    puts "INFO: Full FPGA reprogramming and PS init selected."
+    source $PATH_zynqmp_utils 
+    puts "INFO: sourcing of '$PATH_zynqmp_utils ' is done."
+    #####################################################
+    targets -set -nocase -filter {name =~"APU*"}
+    puts "INFO: Context for 'APU' is selected."
+    #####################################################
+    rst -system
+    puts "INFO: System reset is completed."
+    #####################################################
+    after 3000
+    targets -set -nocase -filter {name =~"RPU*"}
+    puts "INFO: Context for 'RPU' is selected."
+    #####################################################
+    enable_split_mode
+    puts "INFO: Split mode is enabled for R5#1."
+    #####################################################
+    target 1 
+    set BIT_FILE [lindex [bitstream $BIT_DIR] 0]
+    fpga -file $BIT_FILE
+    puts "INFO: Device configured successfully with $BIT_FILE."
+    #####################################################
+    targets -set -nocase -filter {name =~"APU*"}
+    puts "INFO: Context for 'APU' is selected."
+    #####################################################
+    set XSA_FILE [lindex [xsa $XSA_DIR] 0]
+    catch {loadhw -hw $XSA_FILE}
+    puts "INFO: Hardware design and registers information is loaded from $XSA_FILE."
+    #####################################################
+    configparams force-mem-access 1
+    puts "INFO: 'configparams force-mem-access 1' command is executed."
+    #####################################################
+    targets -set -nocase -filter {name =~"APU*"}
+    puts "INFO: Context for 'APU' is selected."
+    #####################################################
+    source UltraZohm/hw/psu_init.tcl
+    puts "INFO: sourcing of 'workspace/UltraZohm/hw/psu_init.tcl' is done."
+    #####################################################
+    psu_init
+    puts "INFO:'psu_init' command is executed."
+    #####################################################
+    source $PATH_fsbl 
+    puts "INFO: sourcing of '$PATH_fsbl' is done."
+    #####################################################
+    after 1000
+    psu_ps_pl_isolation_removal
+    puts "INFO: 'psu_ps_pl_isolation_removal' command is executed."
+    #####################################################
+    after 1000 
+} else {
+    #####################################################
+    puts "INFO: Fast restart selected: skip FPGA reprogramming and PS init."
+    targets -set -nocase -filter {name =~ "*R5*#0"}
+    stop
+    targets -set -nocase -filter {name =~ "*A53*#0"}
+    stop
+    puts "INFO: Stopped processors R5:0 and A53:0."
+}
 #####################################################
-after 3000
-targets -set -nocase -filter {name =~"RPU*"}
-puts "INFO: Context for 'RPU' is selected."
-#####################################################
-enable_split_mode
-puts "INFO: Split mode is enabled for R5#1."
-#####################################################
-target 1 
-set BIT_FILE [lindex [bitstream $BIT_DIR] 0]
-fpga -file $BIT_FILE
-puts "INFO: Device configured successfully with $BIT_FILE."
-#####################################################
-targets -set -nocase -filter {name =~"APU*"}
-puts "INFO: Context for 'APU' is selected."
-#####################################################
-set XSA_FILE [lindex [xsa $XSA_DIR] 0]
-# catch {loadhw -hw $XSA_FILE -mem-ranges [list {0x80000000 0xbfffffff} {0x400000000 0x5ffffffff} {0x1000000000 0x7fffffffff}] -regs}
-catch {loadhw -hw $XSA_FILE}
-puts "INFO: Hardware design and registers information is loaded from $XSA_FILE."
-#####################################################
-configparams force-mem-access 1
-puts "INFO: 'configparams force-mem-access 1' command is executed."
-#####################################################
-targets -set -nocase -filter {name =~"APU*"}
-puts "INFO: Context for 'APU' is selected."
-#####################################################
-source UltraZohm/hw/psu_init.tcl
-puts "INFO: sourcing of 'workspace/UltraZohm/hw/psu_init.tcl' is done."
-#####################################################
-psu_init
-puts "INFO:'psu_init' command is executed."
-#####################################################
-source $PATH_fsbl 
-puts "INFO: sourcing of '$PATH_fsbl' is done."
-#####################################################
-after 1000
-psu_ps_pl_isolation_removal
-puts "INFO: 'psu_ps_pl_isolation_removal' command is executed."
-#####################################################
-after 1000
 psu_ps_pl_reset_config
 puts "INFO: 'psu_ps_pl_reset_config' command is executed."
 #####################################################

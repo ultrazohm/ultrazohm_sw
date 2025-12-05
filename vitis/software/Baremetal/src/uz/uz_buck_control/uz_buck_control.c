f63023f1bd3481dd726aa01b705dc1e118f13276
ninad <nina.diringer@zohm-control.com>
2025-12-05 11:35:30 +0100

    fixes paths

--- vitis/software/Baremetal/src/uz/uz_buck_control/uz_buck_control.c
+++ vitis/software/Baremetal/src/uz/uz_buck_control/uz_buck_control.c
@@ -1,8 +1,8 @@
 #include "../uz_global_configuration.h"
 #if UZ_BUCK_CONTROL_MAX_INSTANCES > 0U
 #include <stdbool.h> 
-#include "/uz_HAL.h"
-#include "uz_buck_control_h" 
+#include "../uz_HAL.h"
+#include "uz_buck_control.h" 
 
 struct uz_buck_control_t {
     bool is_ready;
@@ -14,10 +14,8 @@ static uz_buck_control_t instances[UZ_BUCK_CONTROL_MAX_INSTANCES] = { 0 };
 static uz_buck_control_t* uz_buck_control_allocation(void);
 
 static uz_buck_control_t* uz_buck_control_allocation(void){
-    uz_assert(instance_counter < UZ_BUCK_CONTROL_
-    MAX_INSTANCES);
-    uz_buck_control_
-t* self = &instances[instance_counter];
+    uz_assert(instance_counter < UZ_BUCK_CONTROL_MAX_INSTANCES);
+    uz_buck_control_t* self = &instances[instance_counter];
     uz_assert_false(self->is_ready);
     instance_counter++;
     self->is_ready = true;
@@ -25,9 +23,7 @@ t* self = &instances[instance_counter];
 }
 
 uz_buck_control_t* uz_buck_control_init() {
-    uz_buck_control_
-t* self = uz_buck_control_
-allocation();
+    uz_buck_control_t* self = uz_buck_control_allocation();
     return (self);
 }
 #endif
--- vitis/software/Baremetal/src/uz/uz_buck_control/uz_buck_control.h
+++ vitis/software/Baremetal/src/uz/uz_buck_control/uz_buck_control.h
@@ -1,6 +1,6 @@
 #ifndef UZ_BUCK_CONTROL_H
 #define UZ_BUCK_CONTROL_H
 
-
+typedef struct uz_buck_control_t uz_buck_control_t;
 
 #endif // UZ_BUCK_CONTROL_H

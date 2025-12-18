56246ee3ed34862ae074d1a72ca966fce9959f81
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2025-12-18 13:02:48 +0100

    adds nn ip-core to bitstream

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
---Date        : Wed Jul 12 17:21:32 2023
---Host        : hufnagelde-TP-T14s running 64-bit Ubuntu 22.04.2 LTS
+--Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+--Date        : Thu Dec 18 10:05:26 2025
+--Host        : LAPTOP-68C4B02L running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

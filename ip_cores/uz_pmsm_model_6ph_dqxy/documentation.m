e21b01885cab532427c638b032664ca696ecc07f
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2026-01-22 09:51:04 +0100

    adjusts documentation file for ip-core

--- ip_cores/uz_pmsm_model_6ph_dqxy/documentation.m
+++ ip_cores/uz_pmsm_model_6ph_dqxy/documentation.m
@@ -3,19 +3,19 @@ clear
 pa=which('hdlworkflow.m'); %gets the current (absolute) Path of this script
 pa_to_proj=regexprep(pa,'hdlworkflow.m','');
 
-handle=get_param('uz_pmsm_model','handle');
-print(handle,'-dsvg','pmsm_model');
+handle=get_param('uz_pmsm_model_6ph_dqxy','handle');
+print(handle,'-dsvg','uz_pmsm_model_6ph_dqxy');
 
-handle=get_param('uz_pmsm_model/uz_pmsm_model','handle');
-print(handle,'-dsvg','pmsm_model_inside');
+handle=get_param('uz_pmsm_model_6ph_dqxy/uz_pmsm_model','handle');
+print(handle,'-dsvg','uz_pmsm_model_6ph_dqxy_inside');
 
-handle=get_param('uz_pmsm_model/uz_pmsm_model/pmsm','handle');
-print(handle,'-dsvg','pmsm_model_inside_pmsm');
-
-
-handle=get_param('uz_pmsm_model/uz_pmsm_model/torque_calculation','handle');
-print(handle,'-dsvg','pmsm_model_inside_torque');
+handle=get_param('uz_pmsm_model_6ph_dqxy/uz_pmsm_model/pmsm','handle');
+print(handle,'-dsvg','uz_pmsm_model_6ph_dqxy_inside_pmsm');
 
 
-handle=get_param('uz_pmsm_model/uz_pmsm_model/mechanical_system','handle');
-print(handle,'-dsvg','pmsm_model_inside_mechanical');
+handle=get_param('uz_pmsm_model_6ph_dqxy/uz_pmsm_model/torque_calculation','handle');
+print(handle,'-dsvg','uz_pmsm_model_6ph_dqxy_inside_torque');
+
+
+handle=get_param('uz_pmsm_model_6ph_dqxy/uz_pmsm_model/mechanical_system','handle');
+print(handle,'-dsvg','uz_pmsm_model_6ph_dqxy_inside_mechanical');

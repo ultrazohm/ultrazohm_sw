937c83613c72a6a6d57622e43d9cd1134789084f
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2026-01-22 17:00:51 +0100

    adds 6ph CIL and NN IP-core to bitstream

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Wed Jan 21 11:43:36 2026
+--Date        : Thu Jan 22 13:49:06 2026
 --Host        : LAPTOP-68C4B02L running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper

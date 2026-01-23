b61df04a00a35feac573c8aefaf80f764a612a40
hoernermi <michael.hoerner@th-nuernberg.de>
2026-01-23 17:46:50 +0100

    adjusted ip core v0.7 with additional debug port for received data word

--- ip_cores/uz_endat_interface/hdl_prj/ipcore/uz_endat_interface_v0_7/prj_ip/vivado_ip_package.tcl
+++ ip_cores/uz_endat_interface/hdl_prj/ipcore/uz_endat_interface_v0_7/prj_ip/vivado_ip_package.tcl
@@ -65,7 +65,7 @@ foreach family $Families {append IPSupportedFamily "{$family} {Production} "}
 set_property supported_families $IPSupportedFamily [ipx::current_core]
 set_property taxonomy {{/HDL Coder Generated IP}} [ipx::current_core]
 set_property description {HDL Coder generated IP} [ipx::current_core]
-set_property core_revision 2114447453 [ipx::current_core]
+set_property core_revision 2114448943 [ipx::current_core]
 
 # Add HDL source files to IP
 ipx::add_file {hdl/uz_endat_interface_src_uz_endat_interface_pkg.vhd} [ipx::get_file_groups xilinx_anylanguagesynthesis -of_objects [ipx::current_core]]

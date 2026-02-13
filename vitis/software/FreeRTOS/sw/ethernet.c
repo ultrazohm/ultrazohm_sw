099ae8447dc7841dcdacb87c6019667678139683
EykeAufderheide <eyke.aufderheide@tum.de>
2026-02-13 10:40:26 +0100

    ethernet diagnostics

--- vitis/software/FreeRTOS/sw/ethernet.c
+++ vitis/software/FreeRTOS/sw/ethernet.c
@@ -14,11 +14,15 @@
 ******************************************************************************/
 
 #include <string.h>
+#include <stdint.h>
+#include <errno.h>
 #include "lwip/sockets.h"
+#include "lwip/def.h"
 #include "netif/xadapter.h"
 #include "lwipopts.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
 #include "APU_RPU_shared.h"
 
 #include "../main.h"
@@ -28,6 +32,24 @@ extern struct APU_to_RPU_t ControlData;
 
 int js_connection_established = 0;
 int i_LifeCheck_process_Ethernet = 0;
+
+static uint32_t debug_now_ms(void)
+{
+	return (uint32_t)(xTaskGetTickCount() * portTICK_PERIOD_MS);
+}
+
+static void print_peer_info(int clientfd, const struct sockaddr_in *remote)
+{
+	uint32_t ip_host = lwip_ntohl(remote->sin_addr.s_addr);
+	unsigned int b1 = (ip_host >> 24U) & 0xFFU;
+	unsigned int b2 = (ip_host >> 16U) & 0xFFU;
+	unsigned int b3 = (ip_host >> 8U) & 0xFFU;
+	unsigned int b4 = ip_host & 0xFFU;
+	unsigned int port = lwip_ntohs(remote->sin_port);
+
+	uz_printf("APU: Javascope peer %u.%u.%u.%u:%u fd=%d at %lu ms\r\n",
+			b1, b2, b3, b4, port, clientfd, (unsigned long)debug_now_ms());
+}
 
 
 //==============================================================================================================================================================
@@ -57,8 +79,17 @@ void process_request_thread(void *p)
 	int clientfd = (int)p;
 	int nread = 0;
 	int nwrote = 0;
+	uint32_t session_start_ms = debug_now_ms();
+	uint32_t last_heartbeat_ms = session_start_ms;
+	uint32_t tx_packet_count = 0;
+	uint32_t tx_byte_count = 0;
+	uint32_t rx_byte_count = 0;
+	uint32_t ctrl_packet_count = 0;
+	uint32_t ctrl_bad_size_count = 0;
+	uint32_t queue_empty_count = 0;
+	uint32_t last_status = 0;
 
-	uz_printf("APU: Javascope connected 0x%x\n", clientfd);
+	uz_printf("APU: Javascope connected fd=%d at %lu ms\r\n", clientfd, (unsigned long)session_start_ms);
 	js_connection_established = clientfd;
 
 	xQueueReset(js_queue); //purge queue once new connection is established
@@ -69,7 +100,9 @@ void process_request_thread(void *p)
 
 			// Take one element from queue
 			// The maximum amount of time the task should block waiting for an item to receive should the queue be empty at the time of the call.
-			xQueueReceive(js_queue, &javascope_data_sending, JS_QUEUE_RECEIVE_TICKS2WAIT);
+			if (xQueueReceive(js_queue, &javascope_data_sending, JS_QUEUE_RECEIVE_TICKS2WAIT) != pdTRUE) {
+				queue_empty_count++;
+			}
 
 			// copy data into nwsend struct
 			nwsend.val_01[i] 	= javascope_data_sending.scope_ch[0];
@@ -96,6 +129,7 @@ void process_request_thread(void *p)
 			nwsend.slowDataID[i] 		= javascope_data_sending.slowDataID;
 		}
 		nwsend.status = javascope_data_sending.status;
+		last_status = nwsend.status;
 
 		// At this point, Ethernet Package is full and ready to be sent
 		i_LifeCheck_process_Ethernet++;
@@ -106,12 +140,23 @@ void process_request_thread(void *p)
 		// write the data -> handle request /
 		// The data is sent here
 		if ((nwrote = write(clientfd, &nwsend, sizeof(nwsend))) < 0) {
-			uz_printf("APU: %s: ERROR responding to client echo request. received = %d, written = %d\r\n",
-			__FUNCTION__, nread, nwrote);
+			int saved_errno = errno;
+			uz_printf("APU: %s: WRITE drop fd=%d err=%d recv=%d write=%d t=%lu ms up=%lu ms txPkts=%lu txB=%lu rxB=%lu qAvail=%lu qEmpty=%lu status=0x%08lX\r\n",
+					__FUNCTION__, clientfd, saved_errno, nread, nwrote,
+					(unsigned long)debug_now_ms(),
+					(unsigned long)(debug_now_ms() - session_start_ms),
+					(unsigned long)tx_packet_count,
+					(unsigned long)tx_byte_count,
+					(unsigned long)rx_byte_count,
+					(unsigned long)uxQueueMessagesWaiting(js_queue),
+					(unsigned long)queue_empty_count,
+					(unsigned long)last_status);
 			uz_printf("APU: Closing socket %d\r\n", clientfd);
 			js_connection_established = 0;
 			break;
 		}
+		tx_packet_count++;
+		tx_byte_count += (uint32_t)nwrote;
 		asm("nop");
 
 		// read a max of RECV_BUF_SIZE bytes from socket /
@@ -119,30 +164,80 @@ void process_request_thread(void *p)
 			// read a max of RECV_BUF_SIZE bytes from socket /
 			nread = read(clientfd, (char *)recv_buf, TCPPACKETSIZE);
 			if (nread < 0) {
-				uz_printf("APU: %s: error reading from socket %d, closing Javascope socket\r\n", __FUNCTION__, clientfd);
+				int saved_errno = errno;
+				uz_printf("APU: %s: READ drop fd=%d err=%d t=%lu ms up=%lu ms txPkts=%lu txB=%lu rxB=%lu ctrlOk=%lu ctrlBad=%lu qAvail=%lu qEmpty=%lu status=0x%08lX\r\n",
+						__FUNCTION__, clientfd, saved_errno,
+						(unsigned long)debug_now_ms(),
+						(unsigned long)(debug_now_ms() - session_start_ms),
+						(unsigned long)tx_packet_count,
+						(unsigned long)tx_byte_count,
+						(unsigned long)rx_byte_count,
+						(unsigned long)ctrl_packet_count,
+						(unsigned long)ctrl_bad_size_count,
+						(unsigned long)uxQueueMessagesWaiting(js_queue),
+						(unsigned long)queue_empty_count,
+						(unsigned long)last_status);
 				js_connection_established = 0;
 				break;
 			}
+			rx_byte_count += (uint32_t)nread;
 			//asm(" nop");
 			if ( nread == sizeof(ControlData) ){
 				Received_Data = ((struct APU_to_RPU_t*)recv_buf); // cast received bytes
 				ControlData.id 		= Received_Data->id;
 				ControlData.value 	= Received_Data->value;
+				ctrl_packet_count++;
+			} else if (nread > 0) {
+				ctrl_bad_size_count++;
 			}
 
 			// break if client closed connection /
 			if (nread <= 0){
+				uz_printf("APU: %s: peer closed fd=%d (nread=%d) at %lu ms up=%lu ms txPkts=%lu txB=%lu rxB=%lu ctrlOk=%lu ctrlBad=%lu qAvail=%lu qEmpty=%lu\r\n",
+						__FUNCTION__, clientfd, nread,
+						(unsigned long)debug_now_ms(),
+						(unsigned long)(debug_now_ms() - session_start_ms),
+						(unsigned long)tx_packet_count,
+						(unsigned long)tx_byte_count,
+						(unsigned long)rx_byte_count,
+						(unsigned long)ctrl_packet_count,
+						(unsigned long)ctrl_bad_size_count,
+						(unsigned long)uxQueueMessagesWaiting(js_queue),
+						(unsigned long)queue_empty_count);
 				close(clientfd);
 				js_connection_established = 0;
 				break;
 			}
 		}else{
+			uz_printf("APU: %s: write returned %d, closing fd=%d at %lu ms\r\n",
+					__FUNCTION__, nwrote, clientfd, (unsigned long)debug_now_ms());
 			close(clientfd);
 			js_connection_established = 0;
+		}
+
+		{
+			uint32_t now_ms = debug_now_ms();
+			if ((uint32_t)(now_ms - last_heartbeat_ms) >= 30000U) {
+				uz_printf("APU: JS heartbeat t=%lu ms up=%lu ms fd=%d txPkts=%lu txB=%lu rxB=%lu ctrlOk=%lu ctrlBad=%lu qAvail=%lu qEmpty=%lu life=%d status=0x%08lX\r\n",
+						(unsigned long)now_ms,
+						(unsigned long)(now_ms - session_start_ms),
+						clientfd,
+						(unsigned long)tx_packet_count,
+						(unsigned long)tx_byte_count,
+						(unsigned long)rx_byte_count,
+						(unsigned long)ctrl_packet_count,
+						(unsigned long)ctrl_bad_size_count,
+						(unsigned long)uxQueueMessagesWaiting(js_queue),
+						(unsigned long)queue_empty_count,
+						i_LifeCheck_process_Ethernet,
+						(unsigned long)last_status);
+				last_heartbeat_ms = now_ms;
+			}
 		}
 	}
 
 	// close connection
+	uz_printf("APU: Javascope disconnected fd=%d at %lu ms\r\n", clientfd, (unsigned long)debug_now_ms());
 	close(clientfd);
 	js_connection_established = 0;
 	vTaskDelete(NULL);
@@ -179,6 +274,7 @@ void application_thread()
 
 	while (1) {
 		if ((new_clientfd = lwip_accept(sock, (struct sockaddr *)&remote, (socklen_t *)&size)) > 0) {
+			print_peer_info(new_clientfd, &remote);
 			sys_thread_new("echos", process_request_thread,
 				(void*)new_clientfd,
 				THREAD_STACKSIZE,

656d070d1402592a81e0ed809e6ac909962cf1d6
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2026-03-20 13:39:06 +0100

    fix bug with startmarker, increase stepsize to 20ms

--- vitis/software/Baremetal/src/sw/isr.c
+++ vitis/software/Baremetal/src/sw/isr.c
@@ -235,12 +235,12 @@ void ISR_Control(void *data)
         	}
         	if (start_angle_found) {
         		Global_Data.av.Torque_ref = M_ref_setpoints[setpoint_index] * Global_Data.av.SynRM_config.M_rated_Nm;
+    			Global_Data.rasv.StartMarker=1.0f;
 
         		// step throught the array
-        		if((current_uptime > (old_uptime +100)) && (!change_speed) ){
+        		if((current_uptime > (old_uptime +200)) && (!change_speed) ){
         			old_uptime=current_uptime;
         			Global_Data.av.Torque_ref = M_ref_setpoints[setpoint_index] * Global_Data.av.SynRM_config.M_rated_Nm;
-        			Global_Data.rasv.StartMarker=1.0f;
         			if(setpoint_index < 50){
         				setpoint_index++;
         			}else{

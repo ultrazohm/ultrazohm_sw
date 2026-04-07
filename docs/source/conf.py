1f0c95cc42b469da1ac5eea6c9ee83eb8e21dba5
Tobias Schindler <tobias.schindler@th-nuernberg.de>
2026-04-07 15:19:37 +0000

    Updates sphinx related toolchain

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ author = 'UltraZohm Community'
 
 extensions = [
     "sphinxcontrib.mermaid",
-    "sphinxcontrib.yt",
+    "sphinxcontrib.youtube",
     "sphinx_issues",
     "sphinx_copybutton",
     "sphinxcontrib.tikz",
@@ -90,4 +90,4 @@ nitpick_ignore = [
     ("c:identifier", "bool"),
 ]
 
-tikz_latex_preamble = "\\newcommand\Foo[1]{Z}" # https://github.com/sphinx-contrib/tikz/issues/19
+tikz_latex_preamble = r"\newcommand\Foo[1]{Z}" # https://github.com/sphinx-contrib/tikz/issues/19

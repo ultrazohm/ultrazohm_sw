49adc6e3f1c7f0ab1e404713867ad9b75f434b7a
hufnagelde <dennis.hufnagel@th-nuernberg.de>
2026-04-08 15:25:59 +0200

    restore accidentally commited changes to commit da6f5b

--- vivado/project/zusys/hdl/zusys_wrapper.vhd
+++ vivado/project/zusys/hdl/zusys_wrapper.vhd
@@ -1,8 +1,8 @@
---Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+--Copyright 1986-2023 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
---Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
---Date        : Wed Apr  8 13:53:05 2026
---Host        : LAPTOP-68C4B02L running 64-bit major release  (build 9200)
+--Tool Version: Vivado v.2022.2.2 (win64) Build 3788238 Tue Feb 21 20:00:34 MST 2023
+--Date        : Wed Apr  1 12:35:29 2026
+--Host        : TUEIEAL-TM22 running 64-bit major release  (build 9200)
 --Command     : generate_target zusys_wrapper.bd
 --Design      : zusys_wrapper
 --Purpose     : IP block netlist

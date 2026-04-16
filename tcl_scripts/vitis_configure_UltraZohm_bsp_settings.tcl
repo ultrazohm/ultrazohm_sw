d61a60e801fac5c6a208eb6d416f30e4d5fa6f9f
EykeAufderheide <eyke.aufderheide@tum.de>
2026-04-16 09:16:25 +0200

    clean up A53 bsp

--- tcl_scripts/vitis_configure_UltraZohm_bsp_settings.tcl
+++ tcl_scripts/vitis_configure_UltraZohm_bsp_settings.tcl
@@ -1,5 +1,5 @@
 ###########################################################################
-# Copyright 2021-2026 Eyke Liegmann, Tobias Schindler, Martin Geier
+# Copyright 2021-2026 Eyke Aufderheide, Tobias Schindler, Martin Geier
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,9 +17,49 @@
 proc uz_vitis_apply_freertos_bsp_settings {enable_dhcp} {
   puts "Info (UltraZohm): change FreeRTOS BSP settings"
 
+  set disabled_ips {
+    psu_ams
+    psu_apm_0
+    psu_apm_1
+    psu_apm_2
+    psu_apm_5
+    psu_coresight_0
+    psu_csudma
+    psu_adma_0
+    psu_adma_1
+    psu_adma_2
+    psu_adma_3
+    psu_adma_4
+    psu_adma_5
+    psu_adma_6
+    psu_adma_7
+    psu_gdma_0
+    psu_gdma_1
+    psu_gdma_2
+    psu_gdma_3
+    psu_gdma_4
+    psu_gdma_5
+    psu_gdma_6
+    psu_gdma_7
+    psu_qspi_0
+    psu_crf_apb
+    psu_rtc
+    psu_sd_1
+    psu_spi_0
+    psu_spi_1
+    psu_rcpu_gic
+    uz_system_axi_timebase_wdt_0
+    uz_system_timer_uptime_64bit
+    uz_system_uz_enable_axi_gpio_2
+  }
+
   # Keep the FreeRTOS BSP limited to the libraries supported on A53.
   bsp setlib -name lwip211
   bsp setlib -name libmetal
+
+  foreach ip $disabled_ips {
+    bsp setdriver -ip $ip -driver none
+  }
 
   # get list of configurable parameters for lwip lib
   #bsp listparams -lib lwip211
@@ -73,7 +113,6 @@ proc uz_vitis_apply_baremetal_bsp_settings {} {
   }
 
   bsp setlib -name libmetal
-  bsp setlib -name openamp
 
   # Hide peripherals used by FreeRTOS/Linux from Baremetal(_domain).
   foreach ip $disabled_ips {

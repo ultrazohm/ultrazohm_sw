9c669ad5b86e2a9d4b5990bcad89142940c0d2f0
hoernermi <michael.hoerner@th-nuernberg.de>
2026-04-17 09:33:04 +0200

    updated endat hwAddresses.h

--- vitis/software/Baremetal/src/IP_Cores/uz_endat_interface/uz_endat_interface_hwAddresses.h
+++ vitis/software/Baremetal/src/IP_Cores/uz_endat_interface/uz_endat_interface_hwAddresses.h
@@ -5,13 +5,12 @@
 
 #define  IPCore_Reset_uz_endat_interface                                   0x0  //write 0x1 to bit 0 to reset IP core
 #define  IPCore_Enable_uz_endat_interface                                  0x4  //enabled (by default) when bit 0 is 0x1
-#define  IPCore_Timestamp_uz_endat_interface                               0x8  //contains unique IP timestamp (yymmddHHMM): 2601291518
+#define  IPCore_Timestamp_uz_endat_interface                               0x8  //contains unique IP timestamp (yymmddHHMM): 2603101404
 #define  endat_clk_divider_AXI_Data_uz_endat_interface                     0x100  //data register for Inport endat_clk_divider_AXI
 #define  endat_encoder_bit_width_single_turn_AXI_Data_uz_endat_interface   0x104  //data register for Inport endat_encoder_bit_width_single_turn_AXI
 #define  endat_encoder_bit_width_multi_turn_AXI_Data_uz_endat_interface    0x108  //data register for Inport endat_encoder_bit_width_multi_turn_AXI
 #define  endat_encoder_number_of_CRC_bits_AXI_Data_uz_endat_interface      0x10C  //data register for Inport endat_encoder_number_of_CRC_bits_AXI
 #define  delay_first_clk_on_off_AXI_Data_uz_endat_interface                0x110  //data register for Inport delay_first_clk_on_off_AXI
-#define  pos_is_binary_or_gray_AXI_Data_uz_endat_interface                 0x114  //data register for Inport pos_is_binary_or_gray_AXI
 #define  endat_mode_command_in_AXI_Data_uz_endat_interface                 0x118  //data register for Inport endat_mode_command_in_AXI
 #define  endat_com_enable_AXI_Data_uz_endat_interface                      0x11C  //data register for Inport endat_com_enable_AXI
 #define  reciprocal_bit_width_single_turn_AXI_Data_uz_endat_interface      0x120  //data register for Inport reciprocal_bit_width_single_turn_AXI
@@ -26,7 +25,6 @@
 #define  position_raw_single_turn_AXI_Data_uz_endat_interface              0x144  //data register for Outport position_raw_single_turn_AXI
 #define  position_raw_multi_turn_AXI_Data_uz_endat_interface               0x148  //data register for Outport position_raw_multi_turn_AXI
 #define  CRC_raw_AXI_Data_uz_endat_interface                               0x14C  //data register for Outport CRC_raw_AXI
-#define  CRC_calc_AXI_Data_uz_endat_interface                              0x150  //data register for Outport CRC_calc_AXI
 #define  position_mech_SI_single_turn_AXI_Data_uz_endat_interface          0x154  //data register for Outport position_mech_SI_single_turn_AXI
 #define  position_el_SI_AXI_Data_uz_endat_interface                        0x158  //data register for Outport position_el_SI_AXI
 #define  position_multi_turn_AXI_Data_uz_endat_interface                   0x15C  //data register for Outport position_multi_turn_AXI

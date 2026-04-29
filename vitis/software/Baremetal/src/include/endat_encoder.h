a892769f11e6c58731965365ba16a8574230d505
hoernermi <michael.hoerner@th-nuernberg.de>
2026-04-29 17:23:23 +0200

    removed endat test init files

--- vitis/software/Baremetal/src/include/endat_encoder.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../IP_Cores/uz_endat_interface/uz_endat_interface.h"
-#include "xparameters.h"
-
-uz_endat_interface_t* endat_encoder_init_endat_d5_1(void);
-uz_endat_interface_t* endat_encoder_init_endat_d5_2(void);
-uz_endat_interface_t* endat_encoder_init_endat_d5_3(void);

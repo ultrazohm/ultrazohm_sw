bcc6043f2ddb07414e8cca50ed1e5d073ef5eff2
hoernermi <michael.hoerner@th-nuernberg.de>
2026-05-08 22:19:15 +0200

    WIP need to fix tcl for absoulte encoder and test it

--- xzohm_project_wizard/templates/cards/absolute_encoder_channel.tcl
+++ xzohm_project_wizard/templates/cards/absolute_encoder_channel.tcl
@@ -1,8 +1,40 @@
 # {{ option_label }}: {{ choice_label }}
 {% if enabled %}
+set digital_adapter_hier uz_digital_Adapter
+set adapter_hier_name {{ slot }}_adapter
+set adapter_hier_path ${digital_adapter_hier}/${adapter_hier_name}
+
+proc uz_pw_create_hier_if_missing {hier_path} {
+  if {[llength [get_bd_cells -quiet $hier_path]] == 0} {
+    create_bd_cell -type hier $hier_path
+  } else {
+    puts "Reusing existing hierarchy $hier_path"
+  }
+}
+
+proc uz_pw_create_bd_pin_if_missing {direction pin_path} {
+  if {[llength [get_bd_pins -quiet $pin_path]] == 0} {
+    create_bd_pin -dir $direction $pin_path
+  } else {
+    puts "Reusing existing pin $pin_path"
+  }
+}
+
+uz_pw_create_hier_if_missing ${digital_adapter_hier}
+uz_pw_create_hier_if_missing ${adapter_hier_path}
+uz_pw_create_bd_pin_if_missing I ${adapter_hier_path}/aclk
+uz_pw_create_bd_pin_if_missing I ${adapter_hier_path}/aresetn
+
 {% for ip in ip_cores %}
-set {{ ip.instance_name }} [create_bd_cell -type ip -vlnv {{ ip.vlnv }} {{ ip.instance_name }}]
+set ip_path ${adapter_hier_path}/{{ ip.instance_name }}
+if {[llength [get_bd_cells -quiet $ip_path]] == 0} {
+  set {{ ip.instance_name }} [create_bd_cell -type ip -vlnv {{ ip.vlnv }} $ip_path]
+} else {
+  puts "Reusing existing IP $ip_path"
+}
 # Module: {{ ip.module }}
+connect_bd_net [get_bd_pins ${adapter_hier_path}/aclk] [get_bd_pins ${ip_path}/AXI4_Lite_ACLK]
+connect_bd_net [get_bd_pins ${adapter_hier_path}/aresetn] [get_bd_pins ${ip_path}/AXI4_Lite_ARESETN]
 {% endfor %}
 {% for port in ports %}
 # {{ port.signal }} ({{ port.direction }}) -> {{ port.pin }}

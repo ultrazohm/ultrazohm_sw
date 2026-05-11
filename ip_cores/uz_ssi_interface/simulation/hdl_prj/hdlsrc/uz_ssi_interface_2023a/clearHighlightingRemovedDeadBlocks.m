5d20d0cf7e1876af98c6fd9ec9273340becb9fea
hoernermi <michael.hoerner@th-nuernberg.de>
2026-05-11 11:34:08 +0200

    removed unnecessary files in ssi ip core folder

--- ip_cores/uz_ssi_interface/simulation/hdl_prj/hdlsrc/uz_ssi_interface_2023a/clearHighlightingRemovedDeadBlocks.m
+++
@@ -1,1 +0,0 @@
-SLStudio.Utils.RemoveHighlighting(get_param('uz_ssi_interface_2023a','Handle'));
--- ip_cores/uz_ssi_interface/simulation/hdl_prj/hdlsrc/uz_ssi_interface_2023a/highlightRemovedDeadBlocks.m
+++
@@ -1,6 +0,0 @@
-cs.HiliteType = 'user1';
-cs.ForegroundColor = 'black';
-cs.BackgroundColor = 'red';
-set_param(0, 'HiliteAncestorsData', cs);
-hilite_system('uz_ssi_interface_2023a/uz_ssi_interface/Goto17','user1');
-hilite_system('uz_ssi_interface_2023a/uz_ssi_interface/From26','user1');

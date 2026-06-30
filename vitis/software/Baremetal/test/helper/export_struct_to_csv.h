cf781cb066890e1cb28583594b08d7304898789f
schindlerto <tobias.schindler@th-nuernberg.de>
2026-06-30 14:50:36 +0000

    Revert "testing binary export for tests"
    This reverts commit fe9bcb5397850cb5a35a23d703f8c08da9b44e99.

--- vitis/software/Baremetal/test/helper/export_struct_to_csv.h
+++ vitis/software/Baremetal/test/helper/export_struct_to_csv.h
@@ -42,24 +42,4 @@ void export_array_of_struct_to_csv(const char *filename,
                                   size_t length,
                                   float add_time);
 
-void export_input_output_arrays_to_binary(const char *filename,
-                                          const void *input_array,
-                                          size_t input_element_size,
-                                          const struct csv_field_descriptor_t *input_fields,
-                                          size_t input_field_count,
-                                          const void *output_array,
-                                          size_t output_element_size,
-                                          const struct csv_field_descriptor_t *output_fields,
-                                          size_t output_field_count,
-                                          size_t length,
-                                          float add_time);
-
-void export_array_of_struct_to_binary(const char *filename,
-                                      const void *array,
-                                      size_t element_size,
-                                      const struct csv_field_descriptor_t *fields,
-                                      size_t field_count,
-                                      size_t length,
-                                      float add_time);
-
 void write_csv_field(FILE *file, const void *field_ptr, enum csv_field_type_t type);

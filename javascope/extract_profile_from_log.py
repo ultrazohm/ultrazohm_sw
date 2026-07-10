8e7e536c01d92fd9ec7c7e74582a0678f6abdd0d
Tobias Schindler <tobias.schindler@th-nuernberg.de>
2026-07-10 13:32:50 +0000

    extract profile script matches ceedling timing

--- javascope/extract_profile_from_log.py
+++ javascope/extract_profile_from_log.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-input_file = "Log_2026-07-10_15-11-49.csv"
+input_file = "Log_2026-07-10_15-31-13.csv"
 output_file = "output_data.csv"
 
 data = pd.read_csv(input_file, sep=";")
 output_data = data.loc[data["CH1=50)DQN_PT1_EVAL_PROFILE_EPISODE"] != 0]
+output_data['time'] = output_data['time']*10000 # Make time to step k to match ceedling and python
 
 output_data.to_csv(
     output_file,
